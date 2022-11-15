eb797d33e58499fcc55b0767adff3deeefe488ba
Ludwig Chieng <ludwig.chieng@octo.com>
2022-11-15 17:50:33 +0100

    :wrench:(21) : Ajouter les polices d'écriture

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
+
+// Design system external dependencies
 import 'remixicon/fonts/remixicon.css';
+import '/public/fonts/marianne.css';
+import '/public/fonts/spectral.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<Component {...pageProps} />);
