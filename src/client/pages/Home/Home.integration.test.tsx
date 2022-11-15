e0ec9d1200270be44efc56473aada10801c84f46
Ludwig Chieng <ludwig.chieng@octo.com>
2022-11-15 14:20:46 +0100

    🔧(15) : Configurer Jest

---
+++ src/client/pages/Home/Home.integration.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "./Home";
+
+test("loads and displays", () => {
+  const user = {
+    firstname: "John",
+    lastname: "Doe",
+  };
+  render(<Home user={user} />);
+  expect(screen.getByText("Ceci est le footer")).toBeDefined();
+});
