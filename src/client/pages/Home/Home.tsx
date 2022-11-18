2baa3ccaf7dcb05192308777e5718d75e9004fce
Maxime <maxime.vigier@octo.com>
2022-11-18 11:03:20 +0100

    refactor: Remplace l'intégration react du design system de l'Etat par une intégration custom (21)

--- src/client/pages/Home/Home.tsx
+++ src/client/pages/Home/Home.tsx
@@ -1,22 +1,16 @@
 import HomeProps from './Home.interface';
 import styles from './Home.module.scss';
-import { Alert } from '@dataesr/react-dsfr';
 
 export default function Home({ user }: HomeProps) {
   return (
     <div className={styles.container}>
-      <main>
+      <main className='fr-container'>
         <h1>
           Squelette NextJS
         </h1>
         <p className={styles.description}>
           {JSON.stringify(user)}
         </p>
-        <Alert
-          closable
-          description="Ceci est une super alerte"
-          title="Information"
-        />
       </main>
       <footer className={styles.footer}>
         Ceci est le footer
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import type { AppProps } from 'next/app';
+import '@gouvfr/dsfr/dist/core/core.min.css';
+import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css';
 
-// Design system external dependencies
-import 'remixicon/fonts/remixicon.css';
-import '/public/fonts/marianne.css';
-import '/public/fonts/spectral.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<Component {...pageProps} />);
+  return (
+    <>
+      <Script
+        src="js/dsfr/dsfr.module.min.js"
+        type='module'
+      />
+      <Script
+        noModule
+        src="js/dsfr/dsfr.nomodule.min.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
