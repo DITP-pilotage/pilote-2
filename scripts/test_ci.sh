1e79ff6c17fc1e36054cf73d5ca929a58a62a04d
sroccaserra <sroccaserra@yahoo.com>
2022-11-28 18:26:55 +0100

    fix: essaye de setup la base de donnée en CI (8)

--- scripts/test_ci.sh
+++ scripts/test_ci.sh
@@ -4,7 +4,7 @@ status=0
 
 npm run test:unit || status=1
 npm run test:integration || status=1
-# npm run test:database:init || status=1
-# npm run test:database || status=1
+npm run test:database:init:ci || status=1
+npm run test:database:ci || status=1
 
 exit $status
