674533bb1e91f27f1aeb4bb34e7c2e9e2fb011e4
Manon Coulombe <manon.coulombe@octo.com>
2022-11-28 12:03:08 +0100

    feat: mise en place du footer de page (26)

--- src/client/components/_commons/Layout/Layout.tsx
+++ src/client/components/_commons/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import LayoutProps from './Layout.interface';
 import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Header />
       {children}
+      <Footer />
     </>
   );
 }
