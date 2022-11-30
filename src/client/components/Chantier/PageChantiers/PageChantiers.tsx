7dd2443968648398fb8d2ce8b099b36fe691bef2
Manon Coulombe <manon.coulombe@octo.com>
2022-11-30 14:14:18 +0100

    feat: mise en place des grilles sur la page chantiers (28)

--- src/client/components/Chantier/PageChantiers/PageChantiers.tsx
+++ src/client/components/Chantier/PageChantiers/PageChantiers.tsx
@@ -5,11 +5,53 @@ import PageChantiersProps from './PageChantiers.interface';
 export default function PageChantiers({ chantiers }: PageChantiersProps) {
   return (
     <div className="fr-container">
+      <Title
+        as='h1'
+        look="fr-hidden"
+      >
+        Page des chantiers
+      </Title>
+      <div className="fr-grid-row">
+        <Title as='h2'> 
+          62 chantiers dans les résultats
+        </Title>
+      </div>
       <div className="fr-grid-row">
         <div className="fr-col">
-          <Title as='h1'>
-            Page des chantiers
-          </Title>
+          Carte
+        </div>
+        <div className="fr-col">
+          <div className="fr-grid-row">
+            <Title as='h3'>
+              Taux d’avancement moyen de la sélection
+            </Title>
+            <div className="fr-col">
+              barre annuel
+            </div>      
+            <div className="fr-col">
+              barre global
+            </div>
+          </div>
+          <div className="fr-grid-row">
+            <Title as='h3'>
+              Répartition des taux d’avancement de la sélection
+            </Title>
+            <div>
+              bloques
+            </div>
+          </div>
+          <div className="fr-grid-row">
+            <Title as='h3'>
+              Répartition des météos de la sélection
+            </Title>
+            <div>
+              Bloques
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="fr-grid-row">
+        <div className="fr-col">
           <ListeChantiers chantiers={chantiers} />
         </div>
       </div>
