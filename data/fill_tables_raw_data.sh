6472202e3687922eca52c95abdc31538f760ffa9
Fabien Roussel <fabien.roussel@accenture.com>
2022-12-08 01:12:19 +0100

    refactor: séparation des data publiques et privées en deux dossiers pour les jobs de chargement (55)

--- data/fill_tables_raw_data.sh
+++ data/fill_tables_raw_data.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied : running job on open_data"
+    FOLDER="open_data"
+else
+    FOLDER=$1
+fi
+
 export $(grep -v '^#' .env | xargs)
 
 psql $DATABASE_URL -c "truncate table raw_data.metadata_chantier"
 psql $DATABASE_URL -c "truncate table raw_data.metadata_perimetre"
 
-psql $DATABASE_URL -c "copy raw_data.metadata_chantier from STDIN with csv delimiter ',' header;" < data/input_data/PPG_metadata/views/chantier/view_meta_chantier.csv
-psql $DATABASE_URL -c "copy raw_data.metadata_perimetre from STDIN with csv delimiter ',' header;" < data/input_data/PPG_metadata/views/perimetre/view_meta_perimetre.csv
+psql $DATABASE_URL -c "copy raw_data.metadata_chantier from STDIN with csv delimiter ',' header;" < data/input_data/$FOLDER/PPG_metadata/views/chantier/view_meta_chantier.csv
+psql $DATABASE_URL -c "copy raw_data.metadata_perimetre from STDIN with csv delimiter ',' header;" < data/input_data/$FOLDER/PPG_metadata/views/perimetre/view_meta_perimetre.csv
