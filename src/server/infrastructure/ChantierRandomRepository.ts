5ce36f6082552d03ab5383997e1e724af1f3f4bc
sroccaserra <sroccaserra@yahoo.com>
2022-12-16 10:08:51 +0100

    fix: corrige le linter (00)

--- src/server/infrastructure/ChantierRandomRepository.ts
+++ src/server/infrastructure/ChantierRandomRepository.ts
@@ -20,7 +20,7 @@ export default class ChantierRandomRepository implements ChantierRepository {
     const valeursFixes = [];
     const nbPérimètres = this.idPérimètres.length;
     for (let i = 0; i < this.nombreDeChantiers; i = i + 1) {
-      valeursFixes.push({ id_périmètre : this.idPérimètres[i % (nbPérimètres-1)].id });
+      valeursFixes.push({ id_périmètre : this.idPérimètres[i % (nbPérimètres - 1)].id });
     }
     return ChantiersFixture.générerPlusieurs(this.nombreDeChantiers, valeursFixes);
   }
