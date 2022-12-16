5783cf256200762e35ecb780a4e6b385b3760eff
sroccaserra <sroccaserra@yahoo.com>
2022-12-16 15:11:52 +0100

    fix: corrige la structure des chantiers sur les transformations (72)

--- data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/chantier.sql
+++ data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/chantier.sql
@@ -1,8 +1,5 @@
 SELECT
     chantier_id as id,
     ch_nom as nom,
-    string_to_array(ch_per, ' | ') as perimetre_ids
+    (string_to_array(ch_per, ' | '))[1] as id_perimetre
 FROM raw_data.metadata_chantier
-
-
-
