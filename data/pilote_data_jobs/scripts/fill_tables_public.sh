51cf43b4aaa11a12f5912651d0f4eee37acc5db7
Fabien Roussel <fabien.roussel@accenture.com>
2022-12-17 14:40:47 +0100

    chore: add error messag when .env not given (92)

--- data/pilote_data_jobs/scripts/fill_tables_public.sh
+++ data/pilote_data_jobs/scripts/fill_tables_public.sh
@@ -3,7 +3,13 @@
 # Uniquement sur du local
 if [ -z $PG_HOST ] || [ -z $PG_PORT ] || [ -z $PG_USER ] || [ -z $PG_PASSWORD ] || [ -z $PG_DATABASE ];
 then
-  export $(grep -v '^#' .env | xargs)
+  if [ -f .env ];
+  then
+    export $(grep -v '^#' .env | xargs)
+  else
+    echo "ERROR : .env does not exist. Cannot load variable DATABASE_URL. Exiting"
+    exit 1
+  fi
 fi
 
 # TODO : attention au chemin des fichier qui casse la commande npm
