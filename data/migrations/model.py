3ee25980137bef68f99f4e72dd80f1b2c3135fa4
Fabien Roussel <fabien.roussel@accenture.com>
2022-12-17 20:54:50 +0100

    feat: création de la table raw_data.metadata_indicateur (93)

--- data/migrations/model.py
+++ data/migrations/model.py
@@ -31,3 +31,18 @@ class MetadataPerimetre(Base):
     per_nom = Column(VARCHAR(255))
     per_short = Column(VARCHAR(255))
     per_picto = Column(VARCHAR(255))
+
+
+class MetadataIndicateur(Base):
+    __tablename__ = 'metadata_indicateur'
+    __table_args__ = {"schema": "raw_data"}
+
+    indic_id = Column(VARCHAR(7), primary_key=True)
+    indic_parent_indic = Column(VARCHAR(7))
+    indic_parent_ch = Column(VARCHAR(7), nullable=False)
+    indic_nom = Column(VARCHAR(255), nullable=False)
+    indic_descr = Column(TEXT)
+    indic_is_perseverant = Column(BOOLEAN)
+    indic_is_phare = Column(BOOLEAN)
+    indic_is_baro = Column(BOOLEAN)
+    indic_type = Column(VARCHAR(31), nullable=True)
