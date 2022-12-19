2e69d8cbadbd6506d8a6f148d7936d409c65cc40
Jordan Dullier <jordan.dullier29@gmail.com>
2022-12-19 16:49:32 +0100

    test: ajoute le test du composant Tag (39)

---
+++ src/client/components/_commons/Tag/Tag.integration.test.tsx
@@ -0,0 +1,40 @@
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import Tag from '@/components/_commons/Tag/Tag';
+
+test('le tag comporte le texte précisé', () =>{
+  render(
+    <Tag
+      libellé='Texte du tag'
+      suppressionCallback={()=>{}}
+    />,
+  );
+  expect(screen.getByText('Texte du tag')).toBeInTheDocument();
+});
+
+test('le tag comporte un bouton cliquable', () =>{
+  render(
+    <Tag
+      libellé='Texte du tag'
+      suppressionCallback={()=>{}}
+    />,
+  );
+  expect(screen.getByRole('button')).toBeInTheDocument();
+});
+
+test('le tag lance un event au clic', async () =>{
+  const auClicCallback = jest.fn();
+
+  render(
+    <Tag
+      libellé='Texte du tag'
+      suppressionCallback={auClicCallback}
+    />,
+  );
+  await userEvent.click(screen.getByRole('button'));
+  expect(auClicCallback).toBeCalledTimes(1);
+});
+
+
+
