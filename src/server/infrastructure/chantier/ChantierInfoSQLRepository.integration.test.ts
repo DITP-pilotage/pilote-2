92abe7d8a03ff2f55620ea1497c2920e2a55a071
Fabien Roussel <fabien.roussel@accenture.com>
2022-12-23 14:42:58 +0100

    fix: réparation de la construction des chantiers dans le back (91)

--- src/server/infrastructure/chantier/ChantierInfoSQLRepository.integration.test.ts
+++ src/server/infrastructure/chantier/ChantierInfoSQLRepository.integration.test.ts
@@ -3,7 +3,7 @@ import ChantierInfoRepository from '@/server/domain/chantier/ChantierInfoReposit
 import ChantierRepository from '@/server/domain/chantier/ChantierRepository.interface';
 import ChantierInfoSQLRepository from './ChantierInfoSQLRepository';
 import ChantierSQLRepository from './ChantierSQLRepository';
-import { generateChantier } from './ChantierRandomRepository';
+import { générerChantier } from './ChantierRandomRepository';
 
 describe('ChantierInfoSQLRepository', () => {
   test('Accède à une liste de chantier', async () => {
@@ -11,8 +11,8 @@ describe('ChantierInfoSQLRepository', () => {
     const prisma = new PrismaClient();
     const repository: ChantierInfoRepository = new ChantierInfoSQLRepository(prisma);
     const chantierRepository: ChantierRepository = new ChantierSQLRepository(prisma);
-    const chantier1 = generateChantier('CH-001');
-    const chantier2 = generateChantier('CH-002');
+    const chantier1 = générerChantier('CH-001');
+    const chantier2 = générerChantier('CH-002');
     await chantierRepository.add(chantier1);
     await chantierRepository.add(chantier2);
 
@@ -22,52 +22,5 @@ describe('ChantierInfoSQLRepository', () => {
     // THEN
     const ids = chantiers.map((c) => c.id);
     expect(ids).toEqual(['CH-001', 'CH-002']);
-  });
-});
-
-import Chantier from '@/server/domain/chantier/Chantier.interface';
-import ChantierSQLRepository from '@/server/infrastructure/chantier/ChantierSQLRepository';
-describe('A JETER', () => {
-  test('Accède à une liste de chantier', async () => {
-    // GIVEN
-    const prisma = new PrismaClient();
-    const repository: ChantierInfoRepository = new ChantierInfoSQLRepository(prisma);
-    const chantierSQLRepository: ChantierSQLRepository = new ChantierSQLRepository(prisma);
-    const chantierInitial: Chantier = {
-      id: 'THD',
-      nom: 'Chantier 1',
-      axe: null,
-      nomPPG: null,
-      id_périmètre: 'PER-001',
-      perimètreIds: ['PER-001'],
-      zoneNom: 'National',
-      codeInsee: 'FR',
-      tauxAvancement: 88.5,
-      météo: null,
-      avancement: { annuel: null, global: null },
-      indicateurs: [],
-    };
-    const chantierInitial2: Chantier = {
-      id: 'TUP',
-      nom: 'Chantier 2',
-      axe: null,
-      nomPPG: null,
-      id_périmètre: 'PER-001',
-      perimètreIds: ['PER-001'],
-      zoneNom: 'National',
-      codeInsee: 'FR',
-      tauxAvancement: null,
-      météo: null,
-      avancement: { annuel: null, global: null },
-      indicateurs: [],
-    };
-    await chantierSQLRepository.add(chantierInitial);
-    await chantierSQLRepository.add(chantierInitial2);
-
-    // WHEN
-    const chantiersInfos = await repository.getListe();
-
-    // THEN
-    expect(chantiersInfos).toEqual([chantierInitial, chantierInitial2]);
   });
 });
--- src/server/infrastructure/chantier/ChantierRandomRepository.ts
+++ src/server/infrastructure/chantier/ChantierRandomRepository.ts
@@ -23,15 +23,20 @@ function générerIndicateurs(nombreIndicateurs: number) : Indicateur[] {
   return indicateurs;
 }
 
-export function generateChantier(id: string): Chantier {
+export function générerChantier(id: string): Chantier {
   const valeurs = Array.from({ length:8 }, () => faker.datatype.number({ min: 0, max: 100 }) / 100);
   valeurs.sort();
+  const perimetreId = 'PER-' + faker.random.numeric(3);
   return {
     id,
     nom: faker.lorem.words(3),
     axe: { id: 'AXE-' + faker.random.alphaNumeric(3), nom: faker.lorem.words(3) },
     nomPPG: faker.lorem.words(3),
-    id_périmètre: 'PER-' + faker.random.numeric(3),
+    id_périmètre: perimetreId,
+    perimètreIds: [perimetreId],
+    zoneNom: 'National',
+    codeInsee: 'FR',
+    tauxAvancement: 88.5,
     météo: valeursMeteo[faker.datatype.number({ min: 0, max: 4 })] as Météo,
     avancement: {
       annuel: { minimum: valeurs[0], médiane: valeurs[2], moyenne: valeurs[4], maximum: valeurs[6] },
@@ -48,7 +53,7 @@ export default class ChantierRandomRepository implements ChantierRepository {
   }
 
   async getById(id: string) {
-    return generateChantier(id);
+    return générerChantier(id);
   }
 
 }
