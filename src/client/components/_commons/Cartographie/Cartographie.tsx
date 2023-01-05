5d7d8db3925f89187c1ef23ae800c125f15349b4
Jordan Dullier <jordan.dullier29@gmail.com>
2023-01-05 15:09:16 +0100

    refactor: déporte le traitement des données géographiques côté serveur (113)

--- src/client/components/_commons/Cartographie/Cartographie.tsx
+++ src/client/components/_commons/Cartographie/Cartographie.tsx
@@ -1,41 +1,15 @@
-import { useEffect, useState } from 'react';
-import CartographieProps, { TracéDépartement, TracéRégion } from '@/components/_commons/Cartographie/Cartographie.interface';
+import CartographieProps from '@/components/_commons/Cartographie/Cartographie.interface';
 import CartographieAffichage from '@/components/_commons/Cartographie/CartographieAffichage/CartographieAffichage';
+import tracésDépartements from './départements.json';
 
 export default function Cartographie({ zone }: CartographieProps) {
-
-  const [départements, setDépartements] = useState<TracéDépartement[]>();
-  const [régions, setRégions] = useState<TracéRégion[]>();
-
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/comma-dangle
-    const télécharger = <T,>(url: string, callback: (données: T) => void) => {
-      fetch(url).then((response) => {
-        if (response.status > 400)
-          throw new Error('Erreur au téléchargement de ' + url);
-        return response.json();
-      }).then((données: T) => {
-        callback(données);
-      });
-    };
-
-    télécharger('/geo/départements.json', setDépartements);
-    télécharger('/geo/régions.json', setRégions);
-  }, []);
-
   return (
-    départements && régions) ? (
-      <CartographieAffichage
-        tracésTerritoires={
-          zone.divisionAdministrative === 'région'
-            ? départements.filter(département => département.codeInseeRégion === zone.codeInsee)
-            : départements
+    <CartographieAffichage
+      tracésTerritoires={
+            zone.divisionAdministrative === 'région'
+              ? tracésDépartements.filter(tracéDépartement => tracéDépartement.codeInseeRégion === zone.codeInsee)
+              : tracésDépartements
         }
-      />
-    )
-    : (
-      <p>
-        Aucune donnée
-      </p>
-    );
+    />
+  );
 }
