cbac4e63f220d515de85c21f2583dd9c32af41bc
Ludwig Chieng <ludwig.chieng@octo.com>
2023-01-12 12:24:57 +0100

    refactor: redécoupe le traitement de données dans Cartographie (139)

--- src/client/components/_commons/Cartographie/Cartographie.interface.tsx
+++ src/client/components/_commons/Cartographie/Cartographie.interface.tsx
@@ -1,11 +1,18 @@
 import { Maille } from '@/server/domain/chantier/Chantier.interface';
 import { CartographieTerritoireCodeInsee, CartographieValeur } from './CartographieAffichage/CartographieAffichage.interface';
 
-export type CartographieTracéRégionJSON = {
+export type CartographieRégionJSON = {
   tracéSVG: string,
   codeInsee: CartographieTerritoireCodeInsee,
   nom: string
-}[];
+};
+
+export type CartographieDépartementJSON = {
+  tracéSVG: string,
+  codeInsee: CartographieTerritoireCodeInsee,
+  codeInseeRégion: CartographieTerritoireCodeInsee,
+  nom: string
+};
 
 export type CartographieTerritoireAffiché = {
   codeInsee: Exclude<CartographieTerritoireCodeInsee, 'FR'>,
