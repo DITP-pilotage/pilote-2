d74eb35515515bc662000e3f4c38e6667f472aa3
fabien.roussel <fabien.roussel921@gmail.com>
2023-01-12 16:20:42 +0100

    fix: lint les migrations d'alembic (131)

--- data/pilote_data_jobs/transformations/chantier/brute_vers_public.sql
+++ data/pilote_data_jobs/transformations/chantier/brute_vers_public.sql
@@ -11,7 +11,7 @@ WITH dfakto_chantier AS (
     FROM raw_data.fact_progress_chantier
         JOIN raw_data.dim_tree_nodes ON fact_progress_chantier.tree_node_id = dim_tree_nodes.tree_node_id
         JOIN raw_data.dim_structures ON dim_tree_nodes.structure_id = dim_structures.structure_id
-        left join raw_data.view_data_properties on view_data_properties.reforme_code = dim_tree_nodes.tree_node_code
+        LEFT JOIN raw_data.view_data_properties on view_data_properties.reforme_code = dim_tree_nodes.tree_node_code
     )
 INSERT INTO public.chantier
     (SELECT m_chantier.chantier_id AS id,
@@ -30,14 +30,14 @@ INSERT INTO public.chantier
      		FROM   unnest(string_to_array(m_chantier."porteur_ids_noDAC", ' | ')) WITH ORDINALITY a(id, i)
      		JOIN   raw_data.metadata_porteur m_porteur ON m_porteur.porteur_id = id
      		ORDER  BY a.i
-     	)  AS ministeres,
+     	) AS ministeres,
         array(SELECT m_porteur.porteur_name_short
      		FROM   unnest(string_to_array(m_chantier."porteur_ids_DAC", ' | ')) WITH ORDINALITY a(id, i)
      		JOIN   raw_data.metadata_porteur m_porteur ON m_porteur.porteur_id = id
      		ORDER  BY a.i
-     	)  AS directions_administration_centrale,
+     	) AS directions_administration_centrale,
         string_to_array(m_chantier.ch_dp, ' | ') AS directeurs_projet,
-     	case WHEN chantier_meteo.ch_meteo_id NOTNULL THEN chantier_meteo.ch_meteo_id ELSE 'NON_RENSEIGNEE' END as meteo,
+     	CASE WHEN chantier_meteo.ch_meteo_id NOTNULL THEN chantier_meteo.ch_meteo_id ELSE 'NON_RENSEIGNEE' END as meteo,
      	d_chantier.synthese_des_resultats as synthese_des_resultats
 FROM raw_data.metadata_chantier m_chantier
         LEFT JOIN dfakto_chantier d_chantier ON m_chantier.ch_perseverant = d_chantier.code_region AND d_chantier.structure_name='Réforme'
@@ -61,12 +61,12 @@ UNION
      		FROM   unnest(string_to_array(m_chantier."porteur_ids_noDAC", ' | ')) WITH ORDINALITY a(id, i)
      		JOIN   raw_data.metadata_porteur m_porteur ON m_porteur.porteur_id = id
      		ORDER  BY a.i
-     	)  AS ministeres,
+     	) AS ministeres,
         array(SELECT m_porteur.porteur_name_short
      		FROM   unnest(string_to_array(m_chantier."porteur_ids_DAC", ' | ')) WITH ORDINALITY a(id, i)
      		JOIN   raw_data.metadata_porteur m_porteur ON m_porteur.porteur_id = id
      		ORDER  BY a.i
-     	)  AS directions_administration_centrale,
+     	) AS directions_administration_centrale,
         string_to_array(m_chantier.ch_dp, ' | ') AS directeurs_projet,
      	'NON_NECESSAIRE' as meteo,
      	'' as synthese_des_resultats
