55d4d251774b0cd15a2f3eac02d02289f6135839
Ludwig Chieng <ludwig.chieng@octo.com>
2023-01-13 12:06:31 +0100

    refactor: déplace le traitement dans des fichiers différents (139)

---
+++ src/client/utils/cartographie/préparerDonnéesCartographie.ts
@@ -0,0 +1,26 @@
+import { Territoire } from '@/server/domain/chantier/Chantier.interface';
+import { Agrégation } from '@/client/utils/types';
+import {
+  agrégerDonnéesTerritoires, DonnéesTerritoires, réduireDonnéesTerritoires,
+  TerritoireSansCodeInsee,
+} from '@/client/utils/chantier/donnéesTerritoires/donnéesTerritoires';
+import {
+  CartographieValeur,
+} from '@/components/_commons/Cartographie/CartographieAffichage/CartographieAffichage.interface';
+import { CartographieDonnées } from '@/components/_commons/Cartographie/Cartographie.interface';
+
+export default function préparerDonnéesCartographie(
+  listeDonnéesTerritoires: DonnéesTerritoires<Territoire>[],
+  fonctionDeRéduction: (territoiresAgrégés: Agrégation<TerritoireSansCodeInsee>) => CartographieValeur,
+): CartographieDonnées {
+  const donnéesTerritoiresAgrégés = agrégerDonnéesTerritoires(listeDonnéesTerritoires);
+
+  return réduireDonnéesTerritoires<CartographieValeur>(
+    donnéesTerritoiresAgrégés,
+    fonctionDeRéduction,
+    {
+      brute: null,
+      affichée: null,
+    },
+  );
+}
