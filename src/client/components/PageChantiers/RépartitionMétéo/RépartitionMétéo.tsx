6150801a2c9c1f1c7d9c44ea582a363456cc472f
Ludwig Chieng <ludwig.chieng@octo.com>
2023-01-18 10:36:48 +0100

    feat: affiche les météos nationales de la sélection (140)

--- src/client/components/PageChantiers/RépartitionMétéo/RépartitionMétéo.tsx
+++ src/client/components/PageChantiers/RépartitionMétéo/RépartitionMétéo.tsx
@@ -1,9 +1,43 @@
+import { useMemo } from 'react';
 import Titre from '@/components/_commons/Titre/Titre';
 import { pictosMétéos } from '@/components/_commons/PictoMétéo/PictoMétéo';
+import RépartitonMétéoProps, { CompteurMétéos } from '@/components/PageChantiers/RépartitionMétéo/RépartitionMétéoProps.interface';
+import {
+  agrégerDonnéesTerritoires,
+  réduireDonnéesTerritoires,
+} from '@/client/utils/chantier/donnéesTerritoires/donnéesTerritoires';
 import RépartitionMétéoÉlément from './RépartitionMétéoÉlément/RépartitionMétéoÉlément';
 import RépartitionMétéoStyled from './RépartitionMétéo.styled';
 
-export default function RépartitionMétéo() {
+const météosÀAfficher = ['ORAGE', 'NUAGE', 'COUVERT', 'SOLEIL'] as const;
+
+export default function RépartitionMétéo({ chantiers }: RépartitonMétéoProps) {
+
+  const compteursMétéos = useMemo(() => (
+    réduireDonnéesTerritoires<CompteurMétéos>(
+      agrégerDonnéesTerritoires(chantiers.map(chantier => chantier.mailles)),
+      (territoiresAgrégés) => {
+        const météos = territoiresAgrégés.météo;
+        return {
+          'NON_RENSEIGNEE': météos.filter(météo => météo === 'NON_RENSEIGNEE').length,
+          'ORAGE': météos.filter(météo => météo === 'ORAGE').length,
+          'COUVERT': météos.filter(météo => météo === 'COUVERT').length,
+          'NUAGE': météos.filter(météo => météo === 'NUAGE').length,
+          'SOLEIL': météos.filter(météo => météo === 'SOLEIL').length,
+          'NON_NECESSAIRE': météos.filter(météo => météo === 'NON_NECESSAIRE').length,
+        };
+      },
+      {
+        'NON_RENSEIGNEE': 0,
+        'ORAGE': 0,
+        'COUVERT': 0,
+        'NUAGE': 0,
+        'SOLEIL': 0,
+        'NON_NECESSAIRE': 0,
+      },
+    )
+  ), [chantiers]);
+
   return (
     <RépartitionMétéoStyled>
       <Titre
@@ -14,17 +48,17 @@ export default function RépartitionMétéo() {
       </Titre>
       <ul className='fr-grid-row fr-grid-row--gutters'>
         {
-           Object.values(pictosMétéos).map(météo => (
-             <li
-               className='fr-col-3'
-               key={météo.nom}
-             >
-               <RépartitionMétéoÉlément
-                 météo={météo}
-                 nombreDeChantiers="-"
-               />
-             </li>
-           ))
+          météosÀAfficher.map(météo => (
+            <li
+              className='fr-col-3'
+              key={pictosMétéos[météo].nom}
+            >
+              <RépartitionMétéoÉlément
+                météo={pictosMétéos[météo]}
+                nombreDeChantiers={`${compteursMétéos.nationale.FR[météo]}`}
+              />
+            </li>
+          ))
         }
       </ul>
     </RépartitionMétéoStyled>
--- src/client/utils/cartographie/préparerDonnéesCartographie.ts
+++ src/client/utils/cartographie/préparerDonnéesCartographie.ts
@@ -1,12 +1,12 @@
 import { Maille, Territoire } from '@/server/domain/chantier/Chantier.interface';
 import { Agrégation } from '@/client/utils/types';
-import { agrégerDonnéesTerritoires, DonnéesTerritoires, réduireDonnéesTerritoires, TerritoireSansCodeInseeNiMétéo } from '@/client/utils/chantier/donnéesTerritoires/donnéesTerritoires';
+import { agrégerDonnéesTerritoires, DonnéesTerritoires, réduireDonnéesTerritoires, TerritoireSansCodeInsee } from '@/client/utils/chantier/donnéesTerritoires/donnéesTerritoires';
 import { CartographieValeur } from '@/components/_commons/Cartographie/CartographieAffichage/CartographieAffichage.interface';
 import { CartographieDonnées } from '@/components/_commons/Cartographie/Cartographie.interface';
 
 export function préparerDonnéesCartographieÀPartirDUneListe(
   listeDonnéesTerritoires: DonnéesTerritoires<Territoire>[],
-  fonctionDeRéduction: (territoiresAgrégés: Agrégation<TerritoireSansCodeInseeNiMétéo>) => CartographieValeur,
+  fonctionDeRéduction: (territoiresAgrégés: Agrégation<TerritoireSansCodeInsee>) => CartographieValeur,
 ): CartographieDonnées {
   const donnéesTerritoiresAgrégés = agrégerDonnéesTerritoires(listeDonnéesTerritoires);
 
