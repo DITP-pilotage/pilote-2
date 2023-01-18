43e209c1c39f5bd7363c0d7fe207660a3b13c9f6
Ludwig Chieng <ludwig.chieng@octo.com>
2023-01-18 10:36:48 +0100

    refactor: déplace l'implémentation dans différents fichiers (140)

---
+++ src/client/utils/chantier/avancement/avancement.ts
@@ -0,0 +1,13 @@
+export function comparerAvancementChantier(a: number | null, b: number | null) {
+  if (a === null && b === null)
+    return 0;
+  if (a === null)
+    return -1;
+  if (b === null)
+    return 1;
+  if (a < b)
+    return 1;
+  if (a > b)
+    return -1;
+  return 0;
+}
