2bc1d6eba9c46e4298ecb113794f61d66468ba7a
Ludwig Chieng <ludwig.chieng@octo.com>
2023-01-18 11:46:58 +0100

    test: rend testable et teste le compteur de météos (140)

--- src/client/components/PageChantiers/RépartitionMétéo/RépartitionMétéo.tsx
+++ src/client/components/PageChantiers/RépartitionMétéo/RépartitionMétéo.tsx
@@ -1,11 +1,8 @@
 import { useMemo } from 'react';
 import Titre from '@/components/_commons/Titre/Titre';
 import { pictosMétéos } from '@/components/_commons/PictoMétéo/PictoMétéo';
-import RépartitonMétéoProps, { CompteurMétéos } from '@/components/PageChantiers/RépartitionMétéo/RépartitionMétéoProps.interface';
-import {
-  agrégerDonnéesTerritoires,
-  réduireDonnéesTerritoires,
-} from '@/client/utils/chantier/donnéesTerritoires/donnéesTerritoires';
+import RépartitonMétéoProps from '@/components/PageChantiers/RépartitionMétéo/RépartitionMétéoProps.interface';
+import compterLesMétéosÀPartirDeChantiers from '@/client/utils/chantier/météo/compterLesMétéosÀPartirDeChantiers';
 import RépartitionMétéoÉlément from './RépartitionMétéoÉlément/RépartitionMétéoÉlément';
 import RépartitionMétéoStyled from './RépartitionMétéo.styled';
 
@@ -13,30 +10,7 @@ const météosÀAfficher = ['ORAGE', 'NUAGE', 'COUVERT', 'SOLEIL'] as const;
 
 export default function RépartitionMétéo({ chantiers }: RépartitonMétéoProps) {
 
-  const compteursMétéos = useMemo(() => (
-    réduireDonnéesTerritoires<CompteurMétéos>(
-      agrégerDonnéesTerritoires(chantiers.map(chantier => chantier.mailles)),
-      (territoiresAgrégés) => {
-        const météos = territoiresAgrégés.météo;
-        return {
-          'NON_RENSEIGNEE': météos.filter(météo => météo === 'NON_RENSEIGNEE').length,
-          'ORAGE': météos.filter(météo => météo === 'ORAGE').length,
-          'COUVERT': météos.filter(météo => météo === 'COUVERT').length,
-          'NUAGE': météos.filter(météo => météo === 'NUAGE').length,
-          'SOLEIL': météos.filter(météo => météo === 'SOLEIL').length,
-          'NON_NECESSAIRE': météos.filter(météo => météo === 'NON_NECESSAIRE').length,
-        };
-      },
-      {
-        'NON_RENSEIGNEE': 0,
-        'ORAGE': 0,
-        'COUVERT': 0,
-        'NUAGE': 0,
-        'SOLEIL': 0,
-        'NON_NECESSAIRE': 0,
-      },
-    )
-  ), [chantiers]);
+  const compteursMétéos = useMemo(() => compterLesMétéosÀPartirDeChantiers(chantiers), [chantiers]);
 
   return (
     <RépartitionMétéoStyled>
