8f3c386cd877593e8d3f8164d4e61fefe5e3bd92
Ludwig Chieng <ludwig.chieng@octo.com>
2023-01-19 17:46:31 +0100

    feat: branche store avec liste des chantiers (143)

--- src/client/components/PageChantiers/ListeChantiers/ListeChantiers.tsx
+++ src/client/components/PageChantiers/ListeChantiers/ListeChantiers.tsx
@@ -4,9 +4,15 @@ import Tableau from '@/components/_commons/Tableau/Tableau';
 import BarreDeProgression from '@/components/_commons/BarreDeProgression/BarreDeProgression';
 import BarreDeProgressionProps from '@/components/_commons/BarreDeProgression/BarreDeProgression.interface';
 import { PictoMétéo } from '@/components/_commons/PictoMétéo/PictoMétéo';
-import { Avancement, Territoires } from '@/server/domain/chantier/Chantier.interface';
+import { Avancement } from '@/server/domain/chantier/Chantier.interface';
 import { comparerAvancementChantier } from '@/client/utils/chantier/avancement/avancement';
 import { comparerMétéo } from '@/client/utils/chantier/météo/météo';
+import {
+  PérimètreGéographiqueIdentifiant,
+} from '@/components/PageChantiers/BarreLatérale/SélecteursGéographiques/SélecteurDePérimètreGéographique/SélecteurDePérimètreGéographique.interface';
+import {
+  périmètreGéographique as périmètreGéographiqueStore,
+} from '@/stores/useSélecteursPageChantiersStore/useSélecteursPageChantiersStore';
 import ListeChantiersProps from './ListeChantiers.interface';
 
 function afficherLesBarresDeProgression(avancement: Avancement) {
@@ -30,41 +36,48 @@ function afficherLesBarresDeProgression(avancement: Avancement) {
 
 const reactTableColonnesHelper = createColumnHelper<ListeChantiersProps['chantiers'][number]>();
 
-const colonnes = [
-  reactTableColonnesHelper.accessor('nom', {
-    header: 'Chantiers',
-    cell: nom => {
-      const id = nom.row.original.id;
-      return (
-        <Link href={`/chantier/${id}`}>
-          {nom.getValue()}
-        </Link>
-      );
-    },
-    enableSorting: false,
-  }),
-  reactTableColonnesHelper.accessor('mailles.nationale.FR.météo', {
-    header: 'Météo',
-    cell: météo => <PictoMétéo valeur={météo.getValue()} />,
-    enableGlobalFilter: false,
-    sortingFn: (a, b, columnId) => {
-      return comparerMétéo(a.getValue(columnId), b.getValue(columnId));
-    },
-  }),
-  reactTableColonnesHelper.accessor('mailles.nationale', {
-    header: 'Avancement',
-    cell: avancement => afficherLesBarresDeProgression(avancement.getValue().FR.avancement),
-    enableGlobalFilter: false,
-    sortingFn: (a, b, columnId) => {
-      return comparerAvancementChantier(a.getValue<Territoires>(columnId).FR.avancement.global, b.getValue<Territoires>(columnId).FR.avancement.global);
-    },
-  }),
-];
+function colonnesChantiers(périmètreGéographique: PérimètreGéographiqueIdentifiant) {
+  const { codeInsee, maille } = périmètreGéographique;
+  const cheminChantierMétéoDuTerritoire = `mailles.${maille}.${codeInsee}.météo` as const;
+  const cheminChantierAvancementDuTerritoire = `mailles.${maille}.${codeInsee}.avancement` as const;
+  return [
+    reactTableColonnesHelper.accessor('nom', {
+      header: 'Chantiers',
+      cell: nom => {
+        const id = nom.row.original.id;
+        return (
+          <Link href={`/chantier/${id}`}>
+            {nom.getValue()}
+          </Link>
+        );
+      },
+      enableSorting: false,
+    }),
+    reactTableColonnesHelper.accessor(cheminChantierMétéoDuTerritoire, {
+      header: 'Météo',
+      cell: météo => <PictoMétéo valeur={météo.getValue()} />,
+      enableGlobalFilter: false,
+      sortingFn: (a, b, columnId) => {
+        return comparerMétéo(a.getValue(columnId), b.getValue(columnId));
+      },
+    }),
+    reactTableColonnesHelper.accessor(cheminChantierAvancementDuTerritoire, {
+      header: 'Avancement',
+      cell: avancement => afficherLesBarresDeProgression(avancement.getValue()),
+      enableGlobalFilter: false,
+      sortingFn: (a, b, columnId) => {
+        return comparerAvancementChantier(a.getValue<Avancement>(columnId).global, b.getValue<Avancement>(columnId).global);
+      },
+    }),
+  ];
+}
 
 export default function ListeChantiers({ chantiers }: ListeChantiersProps) {
+  const périmètreGéographique = périmètreGéographiqueStore();
+
   return (
     <Tableau<typeof chantiers[number]>
-      colonnes={colonnes}
+      colonnes={colonnesChantiers(périmètreGéographique)}
       données={chantiers}
       entité="chantiers"
       titre="Liste des chantiers"
