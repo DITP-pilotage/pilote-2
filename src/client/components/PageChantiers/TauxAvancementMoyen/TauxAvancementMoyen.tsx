f5eba2cd1cf58017fd5a33b53fe1e30855cf7ad0
Manon Coulombe <manon.coulombe@octo.com>
2023-01-19 16:15:00 +0100

    feat: ajoute les valeurs médiane, min et max (154)

--- src/client/components/PageChantiers/TauxAvancementMoyen/TauxAvancementMoyen.tsx
+++ src/client/components/PageChantiers/TauxAvancementMoyen/TauxAvancementMoyen.tsx
@@ -1,15 +1,35 @@
-import { useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import Titre from '@/components/_commons/Titre/Titre';
 import BarreDeProgression from '@/components/_commons/BarreDeProgression/BarreDeProgression';
-import { calculerMoyenne } from '@/client/utils/statistiques';
-import TauxAvancementMoyenProps from './TauxAvancementMoyen.interface';
+import { calculerMoyenne, calculerMédiane, valeurMaximum, valeurMinimum } from '@/client/utils/statistiques';
+import TauxAvancementMoyenProps, { AvancementsBarreDeProgression } from './TauxAvancementMoyen.interface';
 
 export default function TauxAvancementMoyen({ chantiers }: TauxAvancementMoyenProps) {
-  const moyenneAvancementNational = useMemo(() => ({
-    annuel: calculerMoyenne(chantiers.map(chantier => (chantier.mailles.nationale.FR.avancement.annuel))),
-    global: calculerMoyenne(chantiers.map(chantier => (chantier.mailles.nationale.FR.avancement.global))),
-  }), [chantiers]);
+  const [valeursAvancementNational, setValeursAvancementNational] = useState<AvancementsBarreDeProgression>({ 
+    annuel: { moyenne: null, médiane: null, minimum: null, maximum: null }, 
+    global: { moyenne: null, médiane: null, minimum: null, maximum: null }, 
+  });
 
+  useEffect(() => {
+    const listeAvancementsNationauxAnnuel = chantiers.map(chantier => (chantier.mailles.nationale.FR.avancement.annuel));
+    const listeAvancementsNationauxGlobaux = chantiers.map(chantier => (chantier.mailles.nationale.FR.avancement.global));
+
+    setValeursAvancementNational({ 
+      annuel: { 
+        moyenne: calculerMoyenne(listeAvancementsNationauxAnnuel), 
+        médiane: calculerMédiane(listeAvancementsNationauxAnnuel), 
+        minimum: valeurMinimum(listeAvancementsNationauxAnnuel), 
+        maximum: valeurMaximum(listeAvancementsNationauxAnnuel), 
+      }, 
+      global: { 
+        moyenne: calculerMoyenne(listeAvancementsNationauxGlobaux), 
+        médiane: calculerMédiane(listeAvancementsNationauxAnnuel), 
+        minimum: valeurMinimum(listeAvancementsNationauxGlobaux), 
+        maximum: valeurMaximum(listeAvancementsNationauxGlobaux), 
+      },
+    });
+  }, [chantiers]);
+  
   return (
     <>
       <Titre
@@ -23,16 +43,22 @@ export default function TauxAvancementMoyen({ chantiers }: TauxAvancementMoyenPr
           annuel
         </p>
         <BarreDeProgression
+          maximum={valeursAvancementNational.annuel.maximum}
+          minimum={valeursAvancementNational.annuel.minimum}
+          médiane={valeursAvancementNational.annuel.médiane}
           taille="grande"
-          valeur={moyenneAvancementNational.annuel}
+          valeur={valeursAvancementNational.annuel.moyenne}
           variante="secondaire"
         />
         <p className="fr-mb-1v fr-mt-3w">
           global
         </p>
         <BarreDeProgression
+          maximum={valeursAvancementNational.global.maximum}
+          minimum={valeursAvancementNational.global.minimum}
+          médiane={valeursAvancementNational.global.médiane}
           taille="grande"
-          valeur={moyenneAvancementNational.global}
+          valeur={valeursAvancementNational.global.moyenne}
           variante="primaire"
         />
       </div>
--- src/client/utils/statistiques.ts
+++ src/client/utils/statistiques.ts
@@ -1,9 +1,32 @@
+function filtrerValeurs(valeurs: (number | null)[]) {
+  return valeurs.filter((valeur): valeur is number => valeur !== null);
+}
+
 export function calculerMoyenne(valeurs: (number | null)[]) {
-  const valeursFiltrées = valeurs.filter((valeur): valeur is number => valeur !== null);
+  const valeursFiltrées = filtrerValeurs(valeurs);
   const somme = valeursFiltrées.reduce(
     (accumulateur, valeur) => accumulateur + valeur,
     0,
   );
 
   return valeursFiltrées.length === 0 ? null : somme / valeursFiltrées.length;
+}
+
+export function calculerMédiane(valeurs: (number | null)[]) {
+  const valeursFiltrées = filtrerValeurs(valeurs);
+  valeursFiltrées.sort((a, b) => a - b);
+  const longueur = valeursFiltrées.length;
+  
+  return longueur % 2 === 0 ? (valeursFiltrées[longueur / 2 - 1] + valeursFiltrées[longueur / 2]) / 2 : valeursFiltrées[(longueur - 1) / 2];
+}
+
+export function valeurMinimum(valeurs: (number | null)[]) {
+  const valeursFiltrées = filtrerValeurs(valeurs);
+  return Math.min(...valeursFiltrées);
+}
+
+
+export function valeurMaximum(valeurs: (number | null)[]) {
+  const valeursFiltrées = filtrerValeurs(valeurs);
+  return Math.max(...valeursFiltrées);
 }
