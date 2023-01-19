4668877b8a88281330de20ba150404c870dd83d3
Manon Coulombe <manon.coulombe@octo.com>
2023-01-19 16:58:12 +0100

    test: ajoute des tests aux calculs de statistiques (154)

--- src/client/utils/statistiques.ts
+++ src/client/utils/statistiques.ts
@@ -1,12 +1,11 @@
-function filtrerValeurs(valeurs: (number | null)[]) {
+export function filtrerValeurs(valeurs: (number | null)[]) {
   return valeurs.filter((valeur): valeur is number => valeur !== null);
 }
 
 export function calculerMoyenne(valeurs: (number | null)[]) {
   const valeursFiltrées = filtrerValeurs(valeurs);
   const somme = valeursFiltrées.reduce(
-    (accumulateur, valeur) => accumulateur + valeur,
-    0,
+    (accumulateur, valeur) => accumulateur + valeur, 0,
   );
 
   return valeursFiltrées.length === 0 ? null : somme / valeursFiltrées.length;
@@ -16,8 +15,11 @@ export function calculerMédiane(valeurs: (number | null)[]) {
   const valeursFiltrées = filtrerValeurs(valeurs);
   valeursFiltrées.sort((a, b) => a - b);
   const longueur = valeursFiltrées.length;
-  
-  return longueur % 2 === 0 ? (valeursFiltrées[longueur / 2 - 1] + valeursFiltrées[longueur / 2]) / 2 : valeursFiltrées[(longueur - 1) / 2];
+
+  if (longueur === 0) return null;
+  return longueur % 2 === 0 
+    ? (valeursFiltrées[longueur / 2 - 1] + valeursFiltrées[longueur / 2]) / 2 
+    : valeursFiltrées[(longueur - 1) / 2];
 }
 
 export function valeurMinimum(valeurs: (number | null)[]) {
