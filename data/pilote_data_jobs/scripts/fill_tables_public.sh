28c1fb0064e703e2e5812917e47f1f46bacbaff1
fabien.roussel <fabien.roussel921@gmail.com>
2023-01-20 17:38:17 +0100

    refactor: suppression de la synthese des resultats dans le back du chantier (155)

--- data/pilote_data_jobs/scripts/fill_tables_public.sh
+++ data/pilote_data_jobs/scripts/fill_tables_public.sh
@@ -16,3 +16,4 @@ dbt run --project-dir pilote_data_jobs/transformations/ditp_ppg_dbt/ --profiles-
 
 psql -f pilote_data_jobs/transformations/indicateur/brute_vers_public.sql
 psql -f pilote_data_jobs/transformations/chantier/brute_vers_public.sql
+psql -f pilote_data_jobs/transformations/synthese_des_resultats/brute_vers_public.sql
