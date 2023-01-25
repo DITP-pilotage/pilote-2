a36fa3677e3c3617719d3c8f01d0da2152142c1a
Jordan Dullier <jordan.dullier29@gmail.com>
2023-01-25 12:10:43 +0100

    fix: corrige la taille du tableau de la liste des chantiers (144)

---
+++ src/client/components/PageChantiers/ListeChantiers/ListeChantiers.styled.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+const ListeChantiersStyled = styled.div`
+
+  display: grid;
+  
+  thead th {
+    &:nth-child(1){
+      min-width: 23.75rem;
+    },
+    &:nth-child(2){
+      min-width: 15rem;
+    },
+    &:nth-child(3){
+      min-width: 20rem;
+    },
+  }
+`;
+
+export default ListeChantiersStyled;
