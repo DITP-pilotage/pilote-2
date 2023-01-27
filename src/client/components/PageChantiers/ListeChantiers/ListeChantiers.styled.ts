9c12cac0ec6f9969b1d70364bdd57da97a3ef575
Ludwig Chieng <ludwig.chieng@octo.com>
2023-01-27 10:45:33 +0100

    fix: corrige les largeurs de colonne du tableau de chantiers (144)

--- src/client/components/PageChantiers/ListeChantiers/ListeChantiers.styled.ts
+++ src/client/components/PageChantiers/ListeChantiers/ListeChantiers.styled.ts
@@ -5,17 +5,13 @@ const ListeChantiersStyled = styled.div`
   display: grid;
   
   thead th {
-    &:nth-of-type(1){
-      min-width: 23.75rem;
-      width: 23.75rem;
-    },
     &:nth-of-type(2){
-      min-width: 15rem;
-      width: 15rem;
+      min-width: 9rem;
+      width: 9rem;
     },
     &:nth-of-type(3){
-      min-width: 20rem;
-      width: 20rem;
+      min-width: 14rem;
+      width: 14rem;
     },
   }
 `;
