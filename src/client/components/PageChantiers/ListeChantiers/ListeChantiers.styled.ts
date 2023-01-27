ef873a947394a155654846226a4d21e99e306e18
Jordan Dullier <jordan.dullier29@gmail.com>
2023-01-27 09:43:18 +0100

    fix: corrige un warning sur l'utilisation de nth-child (144)

--- src/client/components/PageChantiers/ListeChantiers/ListeChantiers.styled.ts
+++ src/client/components/PageChantiers/ListeChantiers/ListeChantiers.styled.ts
@@ -5,15 +5,15 @@ const ListeChantiersStyled = styled.div`
   display: grid;
   
   thead th {
-    &:nth-child(1){
+    &:nth-of-type(1){
       min-width: 23.75rem;
       width: 23.75rem;
     },
-    &:nth-child(2){
+    &:nth-of-type(2){
       min-width: 15rem;
       width: 15rem;
     },
-    &:nth-child(3){
+    &:nth-of-type(3){
       min-width: 20rem;
       width: 20rem;
     },
