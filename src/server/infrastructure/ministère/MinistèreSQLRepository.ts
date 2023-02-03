2dedfdab7bc026a7d26281da1a0c872540891aa2
sroccaserra <sroccaserra@yahoo.com>
2023-02-03 07:56:48 +0100

    refactor: met en valeur le parsing d'un ministère (175)

--- src/server/infrastructure/ministère/MinistèreSQLRepository.ts
+++ src/server/infrastructure/ministère/MinistèreSQLRepository.ts
@@ -4,7 +4,7 @@ import PérimètreMinistériel from '@/server/domain/ministère/PérimètreMinis
 import MinistèreRepository from '@/server/domain/ministère/MinistèreRepository.interface';
 import Ministère from '@/server/domain/ministère/Ministère.interface';
 
-type IdEtNomParMinistere = { ministere: string, ids: string[], noms: string[] };
+type MinistèreQueryResult = { ministere: string, ids: string[], noms: string[] };
 
 export default class MinistèreSQLRepository implements MinistèreRepository {
   private prisma: PrismaClient;
@@ -14,21 +14,21 @@ export default class MinistèreSQLRepository implements MinistèreRepository {
   }
 
   async getListe(): Promise<Ministère[]> {
-    const périmètreIdEtNomParMinistère: IdEtNomParMinistere[] = await this.prisma.$queryRaw`
+    const queryResults: MinistèreQueryResult[] = await this.prisma.$queryRaw`
      select ministere, array_agg(id order by nom) as ids, array_agg(nom order by nom) as noms from perimetre group by ministere order by ministere;
     `;
-    return this.parseMinistères(périmètreIdEtNomParMinistère);
+    return queryResults.map(queryResult => this.parseMinistère(queryResult));
   }
 
-  private parseMinistères(périmètreIdEtNomParMinistère: IdEtNomParMinistere[]): Ministère[] {
-    const result = [];
-    for (const idEtNomParMinistère of périmètreIdEtNomParMinistère) {
-      const périmètres: PérimètreMinistériel[] = [];
-      for (let i = 0; i < idEtNomParMinistère.ids.length; ++i) {
-        périmètres.push({ id: idEtNomParMinistère.ids[i], nom: idEtNomParMinistère.noms[i] });
-      }
-      result.push({ nom: idEtNomParMinistère.ministere, périmètresMinistériels: périmètres });
+  private parseMinistère(ministèreQueryResult: MinistèreQueryResult): Ministère {
+    const périmètres: PérimètreMinistériel[] = [];
+    for (let i = 0; i < ministèreQueryResult.ids.length; ++i) {
+      périmètres.push({ id: ministèreQueryResult.ids[i], nom: ministèreQueryResult.noms[i] });
     }
-    return result;
+
+    return {
+      nom: ministèreQueryResult.ministere,
+      périmètresMinistériels: périmètres,
+    };
   }
 }
