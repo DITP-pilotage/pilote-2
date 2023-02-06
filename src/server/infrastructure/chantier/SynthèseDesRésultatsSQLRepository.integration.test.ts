370c4f0371b5a3ea0af2e45bd8a3036063813027
Jordan Dullier <jordan.dullier29@gmail.com>
2023-02-06 15:19:00 +0100

    fix: corrige la récupération de la synthèse des résultats en cas de données nulles (176)

--- src/server/infrastructure/chantier/SynthèseDesRésultatsSQLRepository.integration.test.ts
+++ src/server/infrastructure/chantier/SynthèseDesRésultatsSQLRepository.integration.test.ts
@@ -3,10 +3,12 @@ import { prisma } from '@/server/infrastructure/test/integrationTestSetup';
 
 describe('SynthèseDesRésultatsSQLRepository ', function () {
   test('Premier test', async () => {
+    const createdDate = '2022-07-05T00:00:00.000Z';
+
     const résultatAttendu = {
       commentaireSynthèse: {
         contenu: 'Le suivi de l’indicateur relatif à la protection Animale...',
-        date: new Date('2022-09-05T00:00:00.000Z'),
+        date: new Date(createdDate).toLocaleDateString('fr-FR'),
       },
     };
 
@@ -14,7 +16,41 @@ describe('SynthèseDesRésultatsSQLRepository ', function () {
       data: {
         chantier_id: 'CH-001',
         commentaire: résultatAttendu.commentaireSynthèse.contenu,
-        date_commentaire: résultatAttendu.commentaireSynthèse.date,
+        date_commentaire: createdDate,
+        maille: 'NAT',
+        code_insee: 'FR',
+      },
+    });
+
+    const repository = new SynthèseDesRésultatsSQLRepository(prisma);
+    const result = await repository.findNewestByChantierId('CH-001');
+    expect(result).toStrictEqual(résultatAttendu);
+  });
+
+  test('Une ligne avec date de commentaire mais sans commentaire est sautée', async () => {
+    const createdDate = '2022-07-05T00:00:00.000Z';
+
+    const résultatAttendu = {
+      commentaireSynthèse: {
+        contenu: 'Le suivi de l’indicateur relatif à la protection Animale...',
+        date: new Date(createdDate).toLocaleDateString('fr-FR'),
+      },
+    };
+
+    await prisma.synthese_des_resultats.create({
+      data: {
+        chantier_id: 'CH-001',
+        date_commentaire: '2022-08-05T00:00:00.000Z',
+        maille: 'NAT',
+        code_insee: 'FR',
+      },
+    });
+
+    await prisma.synthese_des_resultats.create({
+      data: {
+        chantier_id: 'CH-001',
+        date_commentaire: createdDate,
+        commentaire: résultatAttendu.commentaireSynthèse.contenu,
         maille: 'NAT',
         code_insee: 'FR',
       },
--- src/server/infrastructure/chantier/SynthèseDesRésultatsSQLRepository.ts
+++ src/server/infrastructure/chantier/SynthèseDesRésultatsSQLRepository.ts
@@ -1,4 +1,5 @@
 import { synthese_des_resultats, PrismaClient } from '@prisma/client';
+import { AssertionError } from 'node:assert';
 import SynthèseDesRésultatsRepository from '@/server/domain/chantier/SynthèseDesRésultatsRepository.interface';
 import SynthèseDesRésultats from '@/server/domain/chantier/SynthèseDesRésultats.interface';
 
@@ -15,20 +16,30 @@ export class SynthèseDesRésultatsSQLRepository implements SynthèseDesRésulta
         chantier_id : chantierId,
         maille: 'NAT',
         code_insee: 'FR',
-        NOT : {
-          commentaire: null,
-          date_commentaire: null,
-        },
+        NOT : [
+          {
+            commentaire: null,
+          },
+          {
+            date_commentaire: null,
+          },
+        ],
       },
       orderBy: { date_commentaire : 'desc' },
     });
 
     //TODO:
-    // - gestion des données nulles au niveau de l'interface ? Typage des données ?
     // - voir la véracité des commentaire vides avec la DITP ? Comportement front ?
     // - formater la date côté front ?
 
     if (synthèseDesRésultats) {
+
+      if (synthèseDesRésultats.commentaire === null || synthèseDesRésultats.date_commentaire === null) {
+        throw new AssertionError({
+          message: `La requête doit sélectionner un commentaire et une date non null. Synthèse id : ${synthèseDesRésultats.id}`,
+        });
+      }
+
       return {
         commentaireSynthèse : {
           contenu : synthèseDesRésultats.commentaire,
