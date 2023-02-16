8e9ccbd7045fe045a8c8254019df9479c8af0142
Jordan Dullier <jordan.dullier29@gmail.com>
2023-02-16 11:13:48 +0100

    refactor: mutualise les codesInsee des départemetns et régions dans les fixtures (210)

--- src/fixtures/ChantierFixture.ts
+++ src/fixtures/ChantierFixture.ts
@@ -2,25 +2,10 @@ import { faker } from '@faker-js/faker';
 import Chantier, { Territoires } from '@/server/domain/chantier/Chantier.interface';
 import MétéoFixture from '@/fixtures/MétéoFixture';
 import FixtureInterface from '@/fixtures/Fixture.interface';
+import { codeInseeDépartements, codeInseeRégions } from '@/fixtures/codesInsee';
 import { générerCaractèresSpéciaux, générerUnIdentifiantUnique } from './utils';
 
 class ChantierFixture implements FixtureInterface<Chantier> {
-  private readonly code_insee_départements = [
-    '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13',
-    '14', '15', '16', '17', '18', '19', '21', '22', '23', '24', '25', '26', '27',
-    '28', '29', '2A', '2B', '30', '31', '32', '33', '34', '35', '36', '37', '38',
-    '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51',
-    '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64',
-    '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77',
-    '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90',
-    '91', '92', '93', '94', '95', '971', '972', '973', '974', '976',
-  ];
-
-  private readonly code_insee_régions = [
-    '01', '02', '03', '04', '06', '11', '24', '27', '28', '32', '44', '52', '53', '75',
-    '76', '84', '93', '94',
-  ];
-
   générer(valeursFixes: Partial<Chantier> = {}): Chantier {
     return {
       id: générerUnIdentifiantUnique('CH'),
@@ -30,8 +15,8 @@ class ChantierFixture implements FixtureInterface<Chantier> {
       périmètreIds: [générerUnIdentifiantUnique('PER')],
       mailles: {
         nationale: this.générérFakeTerritoires(['FR']),
-        régionale: this.générérFakeTerritoires(this.code_insee_régions),
-        départementale: this.générérFakeTerritoires(this.code_insee_départements),
+        régionale: this.générérFakeTerritoires(codeInseeRégions),
+        départementale: this.générérFakeTerritoires(codeInseeDépartements),
       },
       responsables: {
         porteur: 'Min1',
--- src/fixtures/IndicateurFixture.ts
+++ src/fixtures/IndicateurFixture.ts
@@ -3,27 +3,11 @@ import Indicateur, {
   IndicateurDonnéesTerritoires,
   typesIndicateur,
 } from '@/server/domain/indicateur/Indicateur.interface';
+import { codeInseeDépartements, codeInseeRégions } from '@/fixtures/codesInsee';
 import FixtureInterface from './Fixture.interface';
 import { générerUnIdentifiantUnique } from './utils';
 
 class IndicateurFixture implements FixtureInterface<Indicateur> {
-  // TODO refactor avec ChantierFixture
-  private readonly code_insee_départements = [
-    '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13',
-    '14', '15', '16', '17', '18', '19', '21', '22', '23', '24', '25', '26', '27',
-    '28', '29', '2A', '2B', '30', '31', '32', '33', '34', '35', '36', '37', '38',
-    '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51',
-    '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64',
-    '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77',
-    '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90',
-    '91', '92', '93', '94', '95', '971', '972', '973', '974', '976',
-  ];
-
-  // TODO refactor avec ChantierFixture
-  private readonly code_insee_régions = [
-    '01', '02', '03', '04', '06', '11', '24', '27', '28', '32', '44', '52', '53', '75',
-    '76', '84', '93', '94',
-  ];
 
   générer(valeursFixes: Partial<Indicateur> = {}): Indicateur {
     return {
@@ -33,8 +17,8 @@ class IndicateurFixture implements FixtureInterface<Indicateur> {
       estIndicateurDuBaromètre: faker.datatype.boolean(),
       mailles: {
         nationale: this.générérFakeIndicateurTerritorialisé(['FR']),
-        régionale: this.générérFakeIndicateurTerritorialisé(this.code_insee_régions),
-        départementale: this.générérFakeIndicateurTerritorialisé(this.code_insee_départements),
+        régionale: this.générérFakeIndicateurTerritorialisé(codeInseeRégions),
+        départementale: this.générérFakeIndicateurTerritorialisé(codeInseeDépartements),
       },
       ...valeursFixes,
     };
