febee77e78f7e774254eed448e163fdef43b389d
Jordan Dullier <jordan.dullier29@gmail.com>
2023-02-24 12:37:48 +0100

    refactor: utilise les fixtures dans les dependencies pour avoir un nombre de données cohérents (224)

--- src/server/infrastructure/Dependencies.ts
+++ src/server/infrastructure/Dependencies.ts
@@ -20,6 +20,8 @@ import AxeSQLRepository from '@/server/infrastructure/axe/AxeSQLRepository';
 import PpgRepository from '@/server/domain/ppg/PpgRepository.interface';
 import PpgSQLRepository from '@/server/infrastructure/ppg/PpgSQLRepository';
 import PpgRandomRepository from '@/server/infrastructure/ppg/PpgRandomRepository';
+import AxeFixture from '@/fixtures/AxeFixture';
+import PpgFixture from '@/fixtures/PpgFixture';
 
 class Dependencies {
   private readonly _chantierRepository: ChantierRepository;
@@ -48,8 +50,8 @@ class Dependencies {
       logger.info('Not using database.');
       const nombreDeChantiers = 500;
       const idPérimètres = [ { id: 'PER-001' }, { id: 'PER-002' }, { id: 'PER-003' }, { id: 'PER-004' } ];
-      const axes =  [ { id: 'AXE-001', nom: 'Axe numéro 1' }, { id: 'AXE-002', nom: 'Axe numéro 2' }, { id: 'AXE-003', nom: 'Axe numéro 3' }, { id: 'AXE-004', nom: 'Axe numéro 4' } ];
-      const ppgs =  [ { id: 'PPG-001', nom: 'PPG court' }, { id: 'PPG-002', nom: 'PPG numéro 2 dont le libellé est de taille moyenne' }, { id: 'PPG-003', nom: 'PPG numéro 3 dont le libellé occupe vraiment plus de place que le précédent' }, { id: 'PPG-004', nom: 'PPG numéro 4 dont le libellé est de loin le plus grand de l\'ensemble du jeu de test, il prend beaucoup de place' } ];
+      const axes =  AxeFixture.générerPlusieurs(5);
+      const ppgs =  PpgFixture.générerPlusieurs(60, [{ id: 'PPG-004', nom: 'PPG numéro 4 dont le libellé est de loin le plus grand de l\'ensemble du jeu de test, il prend beaucoup de place' }] );
       const valeursFixes = [];
       const nbPérimètres = idPérimètres.length;
       for (let i = 0; i < nombreDeChantiers; i = i + 1) {
