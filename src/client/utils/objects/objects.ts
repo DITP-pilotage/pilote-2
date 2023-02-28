cbdfbf92bcd65a74c22cc478d97d291a7aaa4d89
Ludwig Chieng <ludwig.chieng@octo.com>
2023-02-28 15:04:36 +0100

    chore: ajoute objectKeys (233)

--- src/client/utils/objects/objects.ts
+++ src/client/utils/objects/objects.ts
@@ -1,3 +1,7 @@
-export const objectEntries = <Obj extends Object>(object: Obj): [keyof Obj, Obj[keyof Obj]][] => {
-  return Object.entries(object) as [keyof Obj, Obj[keyof Obj]][];
+export const objectEntries = <Obj extends Object>(object: Obj): Array<[keyof Obj, Obj[keyof Obj]]> => {
+  return Object.entries(object) as Array<[keyof Obj, Obj[keyof Obj]]>;
+};
+
+export const objectKeys = <Obj extends Object>(object: Obj): Array<keyof Obj> => {
+  return Object.keys(object) as Array<keyof Obj>;
 };
