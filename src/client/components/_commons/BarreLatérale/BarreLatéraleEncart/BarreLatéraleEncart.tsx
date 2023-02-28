aab6eb59bc1272e43cf3caa3429b5444f2ec73c8
Ludwig Chieng <ludwig.chieng@octo.com>
2023-02-28 17:00:04 +0100

    chore: renomme et déplace des interfaces (233)

--- src/client/components/_commons/BarreLatérale/BarreLatéraleEncart/BarreLatéraleEncart.tsx
+++ src/client/components/_commons/BarreLatérale/BarreLatéraleEncart/BarreLatéraleEncart.tsx
@@ -1,7 +1,8 @@
-import { ReactNode } from 'react';
+import BarreLatéraleEncartProps
+  from '@/components/_commons/BarreLatérale/BarreLatéraleEncart/BarreLatéraleEncart.interface';
 import BarreLatéraleEncartStyled from './BarreLatéraleEncart.styled';
 
-export default function BarreLatéraleEncart({ children }: { children: ReactNode }) {
+export default function BarreLatéraleEncart({ children }: BarreLatéraleEncartProps) {
   return (
     <BarreLatéraleEncartStyled>
       <div className='fr-p-3w'>
