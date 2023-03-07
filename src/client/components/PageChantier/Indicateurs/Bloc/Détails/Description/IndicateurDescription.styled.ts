4e479b06de59174116a16310674131e2a8dd6247
Jordan Dullier <jordan.dullier29@gmail.com>
2023-03-07 18:47:46 +0100

    feat: ajoute le bloc de description de l'indicateur (260)

---
+++ src/client/components/PageChantier/Indicateurs/Bloc/Détails/Description/IndicateurDescription.styled.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const IndicateurDescriptionStyled = styled.div`
+  background-color: var(--grey-975-125);
+  
+  .sous-titre{
+    color: var(--text-title-blue-france);
+    margin-bottom: 0.25rem;
+  }
+  
+  p{
+    margin-bottom: 0;
+  }
+`;
+
+export default IndicateurDescriptionStyled;
--- src/client/components/PageChantier/Indicateurs/Bloc/Détails/IndicateurDétails.tsx
+++ src/client/components/PageChantier/Indicateurs/Bloc/Détails/IndicateurDétails.tsx
@@ -4,6 +4,8 @@ import Titre from '@/components/_commons/Titre/Titre';
 import CartographieAvancement from '@/components/_commons/Cartographie/CartographieAvancement/CartographieAvancement';
 import CartographieValeurActuelle from '@/components/_commons/Cartographie/CartographieValeurActuelle/CartographieValeurActuelle';
 import useCartographie from '@/components/_commons/Cartographie/useCartographie';
+import IndicateurDescription
+  from '@/components/PageChantier/Indicateurs/Bloc/Détails/Description/IndicateurDescription';
 import IndicateurDétailsProps from './IndicateurDétails.interface';
 import useIndicateurDétails from './useIndicateurDétails';
 
@@ -26,35 +28,40 @@ export default function IndicateurDétails({ indicateurId, indicateurDétailsPar
         </button>
       </div>
       <div
-        className="fr-collapse fr-ml-3w"
+        className="fr-collapse"
         id={`détails-${indicateurId}`}
       >
-        <div className="fr-grid-row fr-grid-row--gutters fr-mb-1w">
-          <div className="fr-col-12 fr-col-xl-6">
-            <Titre
-              baliseHtml='h4'
-              className='fr-text--lg'
-            >
-              Répartition géographique de l&apos;avancement
-            </Titre>
-            <CartographieAvancement
-              auClicTerritoireCallback={auClicTerritoireMultiSélectionCallback}
-              données={donnéesCartographieAvancement}
-              options={{ territoireSélectionnable: true, multiséléction: true }}
-            />
-          </div>
-          <div className="fr-col-12 fr-col-xl-6">
-            <Titre
-              baliseHtml='h4'
-              className='fr-text--lg'
-            >
-              Répartition géographique de la valeur actuelle de l&apos;indicateur
-            </Titre>
-            <CartographieValeurActuelle
-              auClicTerritoireCallback={auClicTerritoireMultiSélectionCallback}
-              données={donnéesCartographieValeurActuelle}
-              options={{ territoireSélectionnable: true, multiséléction: true }}
-            />
+        <div className="fr-container">
+          <div className="fr-grid-row fr-grid-row--gutters fr-mb-1w">
+            <div className="fr-col-12">
+              <IndicateurDescription />
+            </div>
+            <div className="fr-col-12 fr-col-xl-6">
+              <Titre
+                baliseHtml='h4'
+                className='fr-text--lg'
+              >
+                Répartition géographique de l&apos;avancement
+              </Titre>
+              <CartographieAvancement
+                auClicTerritoireCallback={auClicTerritoireMultiSélectionCallback}
+                données={donnéesCartographieAvancement}
+                options={{ territoireSélectionnable: true, multiséléction: true }}
+              />
+            </div>
+            <div className="fr-col-12 fr-col-xl-6">
+              <Titre
+                baliseHtml='h4'
+                className='fr-text--lg'
+              >
+                Répartition géographique de la valeur actuelle de l&apos;indicateur
+              </Titre>
+              <CartographieValeurActuelle
+                auClicTerritoireCallback={auClicTerritoireMultiSélectionCallback}
+                données={donnéesCartographieValeurActuelle}
+                options={{ territoireSélectionnable: true, multiséléction: true }}
+              />
+            </div>
           </div>
         </div>
         <hr className='fr-hr' />
