d046f6b90da7ce38f11574affdeac8b75c0fe970
fabien.roussel <fabien.roussel921@gmail.com>
2023-03-13 11:21:59 +0100

    chore: import du nouveau fichier dim_structures (268)

--- data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/raw/dfakto/dim_structures.py
+++ data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/raw/dfakto/dim_structures.py
@@ -3,9 +3,10 @@ import os
 
 
 def model(dbt, session):
-    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_RP_RAW')
+    #dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_RP_RAW')
+    dump_dfakto_rp_raw = '/Users/fabien.roussel/Documents/Missions/DITP/ditp-pilote-draft/data/input_data/private_data/dump_dfakto_octo_2'
     dim_structures = pd.read_csv(
-        f'{dump_dfakto_rp_raw}/dim_structures.csv',
+        f'{dump_dfakto_rp_raw}/dim_structures_202303091524.csv',
         sep=';'
     )
     columns_type = {
