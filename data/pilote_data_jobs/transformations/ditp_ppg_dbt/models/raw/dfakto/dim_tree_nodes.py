54df7a98d2856a5bd8730808a16f98a94d237127
fabien.roussel <fabien.roussel921@gmail.com>
2023-03-13 11:09:58 +0100

    chore: import du nouveau fichier dim_tree_nodes (268)

--- data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/raw/dfakto/dim_tree_nodes.py
+++ data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/raw/dfakto/dim_tree_nodes.py
@@ -3,7 +3,8 @@ import os
 
 
 def model(dbt, session):
-    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_RP_RAW')
+    #dump_dfakto_rp_raw = os.getenv()
+    dump_dfakto_rp_raw = '/Users/fabien.roussel/Documents/Missions/DITP/ditp-pilote-draft/data/input_data/private_data/dump_dfakto_octo_2'
     dim_tree_nodes = pd.read_csv(
         f'{dump_dfakto_rp_raw}/dim_tree_nodes.csv',
         sep=';'
@@ -11,11 +12,10 @@ def model(dbt, session):
 
     columns_type = {
         'tree_node_last_synchronization_date': 'datetime64[ns]',
-        'tree_node_last_update_properties_date': 'datetime64',
-        'tree_node_last_update_scorecard_date': 'datetime64',
-        'tree_node_last_scorecard_update_by_anybody_date': 'datetime64',
-        'tree_node_last_update_children_date': 'datetime64',
-        'snapshot_date': 'datetime64',
+        'tree_node_last_update_properties_date': 'datetime64[ns]',
+        'tree_node_last_update_scorecard_date': 'datetime64[ns]',
+        'tree_node_last_scorecard_update_by_anybody_date': 'datetime64[ns]',
+        'snapshot_date': 'datetime64[ns]',
     }
 
     dim_tree_nodes = dim_tree_nodes.astype(columns_type)
--- data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/staging/dfakto/stg_dfakto__dim_tree_nodes.sql
+++ data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/staging/dfakto/stg_dfakto__dim_tree_nodes.sql
@@ -15,24 +15,20 @@ renamed as (
         maturity_id,
         tree_node_name as nom,
         tree_node_code as code,
-        case
-            when {{ dbt.split_part('tree_node_code', "'-'", 1) }} = 'OVQ' then 'FRANCE'
-            else {{ dbt.split_part('tree_node_code', "'-'", 2) }}
-        end as zone_code,
-        case
-            when {{ dbt.split_part('tree_node_code', "'-'", 1) }} = 'OVQ' then {{ dbt.split_part('tree_node_code', "'-'", 2) }}
-            else {{ dbt.split_part('tree_node_code', "'-'", 1) }}
-        end as code_chantier,
+        SUBSTRING(tree_node_code, 1, 6) as code_chantier,
+        SUBSTRING(tree_node_code, 8) as zone_code,
         tree_node_status as status,
         tree_node_last_synchronization_date as last_synchronization_date,
         tree_node_last_update_properties_date as last_update_properties_date,
         tree_node_last_update_scorecard_date as last_update_scorecard_date,
         tree_node_last_scorecard_update_by_anybody_date as last_scorecard_update_by_anybody_date,
-        tree_node_last_update_children_date as last_update_children_date,
-        tree_node_currency as currency,
         snapshot_date
     from source
 
 )
 
-select * from renamed
+select *
+    from renamed
+    where LOWER(code) not like '%temporaire%' and
+          LOWER(code) not like '%test%' and
+          CHAR_LENGTH(code) > 6
