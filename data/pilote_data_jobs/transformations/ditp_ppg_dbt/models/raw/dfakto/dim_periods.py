27452e8b19bdfea2a426d1850839a8854d219463
fabien.roussel <fabien.roussel921@gmail.com>
2023-03-13 11:17:26 +0100

    chore: import du nouveau fichier dim_periods (268)

--- data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/raw/dfakto/dim_periods.py
+++ data/pilote_data_jobs/transformations/ditp_ppg_dbt/models/raw/dfakto/dim_periods.py
@@ -3,9 +3,10 @@ import os
 
 
 def model(dbt, session):
-    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_RP_RAW')
+    #dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_RP_RAW')
+    dump_dfakto_rp_raw = '/Users/fabien.roussel/Documents/Missions/DITP/ditp-pilote-draft/data/input_data/private_data/dump_dfakto_octo_2'
     dim_periods = pd.read_csv(
-        f'{dump_dfakto_rp_raw}/dim_periods.csv',
+        f'{dump_dfakto_rp_raw}/dim_periods_202303091524.csv',
         sep=';'
     )
 
