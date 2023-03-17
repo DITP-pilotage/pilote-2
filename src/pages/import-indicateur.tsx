32ec2cf97bf7eac96e0e4944d376263a484031d9
GUIC <guillaume.chauvet@octo.com>
2023-03-17 11:57:16 +0100

    feat: création de la page import indicateur (367)

---
+++ src/pages/import-indicateur.tsx
@@ -0,0 +1,5 @@
+import PageImportIndicateur from '@/components/PageImportIndicateur/PageImportIndicateur';
+
+export default function NextPageImportIndicateur() {
+  return <PageImportIndicateur />;
+}
