70e0906f6ca03cf5cbecd5f429344189f9ee8d5d
Maxime <maxime.vigier@octo.com>
2023-03-29 15:20:30 +0200

    feat: ajoute des headers de sécurité (276)

--- next.config.js
+++ next.config.js
@@ -4,7 +4,36 @@ const nextConfig = {
   swcMinify: true,
   compiler: {
     emotion: true,
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: `default-src 'self' data: 'unsafe-inline'; script-src 'self' ${process.env.NODE_ENV !== 'production' && "'unsafe-eval'"};`,
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
