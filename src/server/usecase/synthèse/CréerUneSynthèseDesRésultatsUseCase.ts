2d8684b98dbaf539ac5535511a6af963be55b285
Jordan Dullier <jordan.dullier29@gmail.com>
2023-04-04 13:45:50 +0200

    feat: ajoute le usecase CréerUneSynthèseDesRésultats (274)

---
+++ src/server/usecase/synthèse/CréerUneSynthèseDesRésultatsUseCase.ts
@@ -0,0 +1,17 @@
+import { dependencies } from '@/server/infrastructure/Dependencies';
+import SynthèseDesRésultatsRepository from '@/server/domain/synthèseDesRésultats/SynthèseDesRésultatsRepository.interface';
+import { Maille } from '@/server/domain/maille/Maille.interface';
+import { CodeInsee } from '@/server/domain/territoire/Territoire.interface';
+import { Météo } from '@/server/domain/météo/Météo.interface';
+import SynthèseDesRésultats from '@/server/domain/synthèseDesRésultats/SynthèseDesRésultats.interface';
+
+export default class CréerUneSynthèseDesRésultatsUseCase {
+  constructor(
+    private readonly synthèsesDesRésultatsRepository: SynthèseDesRésultatsRepository = dependencies.getSynthèseDesRésultatsRepository(),
+  ) {}
+
+  async run(chantierId: string, maille: Maille, codeInsee: CodeInsee, contenu: string, auteur: string, météo: Météo): Promise<SynthèseDesRésultats> {
+    const date = new Date();
+    return this.synthèsesDesRésultatsRepository.créer(chantierId, maille, codeInsee, contenu, auteur, météo, date);
+  }
+}
