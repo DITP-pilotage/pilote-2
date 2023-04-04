3414f21dbb7c5ac0cbdfb1fc84bb64c33000b8ed
Ludwig Chieng <ludwig.chieng@octo.com>
2023-04-04 10:29:03 +0200

    feat: ajoute un hook pour basculer entre vue mobile et desktop (278)

---
+++ src/hooks/useEstVueMobile.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const VUE_MOBILE_SEUIL_LARGEUR = 1500;
+
+export default function useEstVueMobile() {
+  const [largeurDÉcran, setLargeurDÉcran] = useState<number | null>(null);
+  const [estVueMobile, setEstVueMobile] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    if (largeurDÉcran === null) {
+      setEstVueMobile(null);
+    } else {
+      setEstVueMobile(largeurDÉcran < VUE_MOBILE_SEUIL_LARGEUR);
+    }
+  }, [largeurDÉcran]);
+
+  useEffect(() => {
+    setLargeurDÉcran(window.innerWidth);
+
+    const auRedimensionnementDeLaFenêtre = () => {
+      setLargeurDÉcran(window.innerWidth);
+    };
+
+    window.addEventListener('resize', auRedimensionnementDeLaFenêtre);
+
+    return () => window.removeEventListener('resize', auRedimensionnementDeLaFenêtre);
+  }, []);
+
+  return estVueMobile;
+}
