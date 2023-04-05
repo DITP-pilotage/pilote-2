b762fb53d7ded78549aa01362ded623dc229ff5a
louisehbrt <louise.hubert@octo.com>
2023-04-05 16:00:25 +0200

    chore: mise à jour de l'import des tables raw (000)

--- data_management/data_factory/models/raw/dfakto/dim_periods.py
+++ data_management/data_factory/models/raw/dfakto/dim_periods.py
@@ -3,9 +3,9 @@ import os
 
 
 def model(dbt, session):
-    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_2')
+    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_3')
     dim_periods = pd.read_csv(
-        f'{dump_dfakto_rp_raw}/dim_periods_202303091524.csv',
+        f'{dump_dfakto_rp_raw}/dim_periods_202304051159.csv',
         sep=';'
     )
 
--- data_management/data_factory/models/staging/dfakto/stg_dfakto__fact_progress_indicateurs.sql
+++ data_management/data_factory/models/staging/dfakto/stg_dfakto__fact_progress_indicateurs.sql
@@ -11,14 +11,14 @@ renamed as (
     select
         tree_node_id,
         effect_id,
-        valeur_initiale,
-        valeur_actuelle,
-        valeur_cible_intermediaire as valeur_cible_annuel,
-        valeur_cible_globale,
-        progress_intermediaire as avancement_annuel,
-        bounded_progress_intermediaire as avancement_borne_annuel,
-        progress_globale as avancement, -- TODO A RENOMMER pour global
-        bounded_progress_globale as avancement_borne, -- TODO A RENOMMER pour global
+        CAST(valeur_initiale as DOUBLE PRECISION) as valeur_initiale,
+        CAST(valeur_actuelle as DOUBLE PRECISION) as valeur_actuelle,
+        CAST(valeur_cible_intermediaire as DOUBLE PRECISION) as valeur_cible_annuel,
+        CAST(valeur_cible_globale as DOUBLE PRECISION) as valeur_cible_globale,
+        CAST(progress_intermediaire as DOUBLE PRECISION) as avancement_annuel,
+        CAST(bounded_progress_intermediaire as DOUBLE PRECISION) as avancement_borne_annuel,
+        CAST(progress_globale as DOUBLE PRECISION) as avancement, -- TODO A RENOMMER pour global
+        CAST(bounded_progress_globale as DOUBLE PRECISION) as avancement_borne, -- TODO A RENOMMER pour global
         date_valeur_initiale::DATE,
         date_valeur_actuelle::DATE,
         date_valeur_cible_intermediaire::DATE as date_valeur_cible_annuel,
