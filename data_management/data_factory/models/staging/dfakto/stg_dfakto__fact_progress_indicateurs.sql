140bfc9507c80a62240825660d03ad318f9ebf39
fabien.roussel <fabien.roussel921@gmail.com>
2023-04-05 21:03:10 +0200

    fix: import des données de dfakto avec les bons formats de données (000)

--- data_management/data_factory/models/staging/dfakto/stg_dfakto__fact_progress_indicateurs.sql
+++ data_management/data_factory/models/staging/dfakto/stg_dfakto__fact_progress_indicateurs.sql
@@ -11,10 +11,10 @@ renamed as (
     select
         tree_node_id,
         effect_id,
-        CAST(REPLACE(valeur_initiale, ',', '.') as DOUBLE PRECISION) as valeur_initiale,
-        CAST(REPLACE(valeur_actuelle, ',', '.') as DOUBLE PRECISION) as valeur_actuelle,
-        CAST(REPLACE(valeur_cible_intermediaire, ',', '.') as DOUBLE PRECISION) as valeur_cible_annuel,
-        CAST(REPLACE(valeur_cible_globale, ',', '.') as DOUBLE PRECISION) as valeur_cible_globale,
+        CAST(REPLACE(valeur_initiale, ',', '') as DOUBLE PRECISION) as valeur_initiale,
+        CAST(REPLACE(valeur_actuelle, ',', '') as DOUBLE PRECISION) as valeur_actuelle,
+        CAST(REPLACE(valeur_cible_intermediaire, ',', '') as DOUBLE PRECISION) as valeur_cible_annuel,
+        CAST(REPLACE(valeur_cible_globale, ',', '') as DOUBLE PRECISION) as valeur_cible_globale,
         progress_intermediaire as avancement_annuel,
         bounded_progress_intermediaire as avancement_borne_annuel,
         progress_globale as avancement, -- TODO A RENOMMER pour global
