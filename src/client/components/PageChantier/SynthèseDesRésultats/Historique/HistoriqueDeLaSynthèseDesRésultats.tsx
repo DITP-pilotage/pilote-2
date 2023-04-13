c2f540e4839f6eaab7cc017469180b7eee33e929
Jordan Dullier <jordan.dullier29@gmail.com>
2023-04-13 18:11:55 +0200

    feat: ajoute la météo dans l'historique de la synthèse des résultats (291)

--- src/client/components/PageChantier/SynthèseDesRésultats/Historique/HistoriqueDeLaSynthèseDesRésultats.tsx
+++ src/client/components/PageChantier/SynthèseDesRésultats/Historique/HistoriqueDeLaSynthèseDesRésultats.tsx
@@ -1,6 +1,10 @@
 import { Fragment } from 'react';
 import Modale from '@/components/_commons/Modale/Modale';
 import Publication from '@/components/PageChantier/Publication/Publication';
+import MétéoBadge from '@/components/_commons/Météo/Badge/MétéoBadge';
+import MétéoPicto from '@/components/_commons/Météo/Picto/MétéoPicto';
+import HistoriqueDeLaSynthèseDesRésultatsStyled
+  from '@/components/PageChantier/SynthèseDesRésultats/Historique/HistoriqueDeLaSynthèseDesRésultats.styled';
 import useHistoriqueDeLaSynthèseDesRésultats from './useHistoriqueDeLaSynthèseDesRésultats';
 
 export default function HistoriqueDeLaSynthèseDesRésultats() {
@@ -14,7 +18,7 @@ export default function HistoriqueDeLaSynthèseDesRésultats() {
       sousTitre={territoireSélectionné.nom}
       titre="Historique - Synthèse des résultats"
     >
-      <div>
+      <HistoriqueDeLaSynthèseDesRésultatsStyled>
         {
           historiqueDeLaSynthèseDesRésultats === null ? (
             <p>
@@ -28,19 +32,38 @@ export default function HistoriqueDeLaSynthèseDesRésultats() {
                     <hr className="fr-mt-4w" />
                   )
                 }
-                <div className="fr-mx-2w">
-                  <Publication
-                    auteur={synthèse?.auteur ?? null}
-                    contenu={synthèse?.contenu ?? null}
-                    date={synthèse?.date ?? null}
-                    messageSiAucunContenu="La synthèse des résutats est vide."
-                  />
+                <div className="conteneur">
+                  <div className="conteneur-météo fr-mb-3w fr-mb-md-0">
+                    <MétéoBadge météo={synthèse?.météo ?? 'NON_RENSEIGNEE'} />
+                    {
+                      !!synthèse &&
+                        <div>
+                          <MétéoPicto météo={synthèse.météo} />
+                        </div>
+                    }
+                  </div>
+                  <div className="fr-pl-md-3w">
+                    {
+                      synthèse
+                        ?
+                          <Publication
+                            auteur={synthèse.auteur}
+                            contenu={synthèse.contenu}
+                            date={synthèse.date}
+                            messageSiAucunContenu="Aucune synthèse des résultats."
+                          />
+                        :
+                          <p className='fr-text--sm texte-gris'>
+                            Aucune synthèse des résultats.
+                          </p>
+                    }
+                  </div>
                 </div>
               </Fragment>
             ))
           )
         }
-      </div>
+      </HistoriqueDeLaSynthèseDesRésultatsStyled>
     </Modale>
   );
 }
