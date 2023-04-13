40a4e6998b1e786b6094df48c963dcf91fb451a1
fabien.roussel <fabien.roussel921@gmail.com>
2023-04-13 16:23:23 +0200

    chore: linter des fichiers sql et renommage explicite des noms des mesures dans le pivot (258)

--- data_management/data_factory/models/marts/pivot_faits_indicateur_par_date.sql
+++ data_management/data_factory/models/marts/pivot_faits_indicateur_par_date.sql
@@ -6,83 +6,81 @@
 -- Ajout : gestion du cas de plusieurs valeur pour le même jour avec les dates d'import
 -- FIXME: on ignore les académies avec la condition "AND zone_type_parent IN ('REG', 'NAT')" => a améliorer, Louise tu as peut-être des idées ?
 WITH
-pivot_vi as (
+pivot_valeur_initale as (
     SELECT
         ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, type_mesure, date_releve ORDER by date_import DESC) AS row_id,
         indicateur_id,
         zone_id,
         date_releve,
-        valeur as indic_vi
+        valeur as indicateur_valeur_initiale
     FROM {{ ref("faits_indicateur") }}
     WHERE type_mesure = 'vi'
       AND zone_type_parent IN ('REG', 'NAT') -- condition temporaire, c'est juste pour avoir les departements et région
 ),
 
-pivot_vi_unique as (
-    select * from pivot_vi WHERE row_id = 1
+pivot_valeur_initale_unique as (
+    select * from pivot_valeur_initale WHERE row_id = 1
 ),
 
-pivot_va as (
+pivot_valeur_actuelle as (
     SELECT
         ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, type_mesure, date_releve ORDER by date_import DESC) AS row_id,
         indicateur_id,
         zone_id,
         date_releve,
-        valeur as indic_va
+        valeur as indicateur_valeur_actuelle
     FROM {{ ref("faits_indicateur") }}
     WHERE type_mesure = 'va'
       AND zone_type_parent IN ('REG', 'NAT') -- condition temporaire, c'est juste pour avoir les departements et région
 ),
 
-pivot_va_unique as (
-    select * from pivot_va WHERE row_id = 1
+pivot_valeur_actuelle_unique as (
+    select * from pivot_valeur_actuelle WHERE row_id = 1
 ),
 
-pivot_vc as (
+pivot_valeur_cible as (
     SELECT
         ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, type_mesure, date_releve ORDER by date_import DESC) AS row_id,
         indicateur_id,
         zone_id,
         date_releve,
-        valeur as indic_vc
+        valeur as indicateur_valeur_cible
     FROM {{ ref("faits_indicateur") }}
     WHERE type_mesure = 'vc'
       AND zone_type_parent IN ('REG', 'NAT') -- condition temporaire, c'est juste pour avoir les departements et région
 ),
 
-pivot_vc_unique as (
-    select * from pivot_vc WHERE row_id = 1
+pivot_valeur_cible_unique as (
+    select * from pivot_valeur_cible WHERE row_id = 1
 ),
 
-join_vi_va as (
+join_valeur_initiale_valeur_actuelle as (
     SELECT
-        COALESCE(pivot_vi_unique.indicateur_id, pivot_va_unique.indicateur_id) as indicateur_id,
-        COALESCE(pivot_vi_unique.zone_id, pivot_va_unique.zone_id) as zone_id,
-        COALESCE(pivot_vi_unique.date_releve, pivot_va_unique.date_releve) as date_releve,
-        pivot_vi_unique.indic_vi,
-        pivot_va_unique.indic_va
-    FROM pivot_vi_unique full join pivot_va_unique
-        ON pivot_vi_unique.indicateur_id = pivot_va_unique.indicateur_id
-               AND pivot_vi_unique.zone_id = pivot_va_unique.zone_id
-               AND pivot_vi_unique.date_releve = pivot_va_unique.date_releve
-    WHERE pivot_vi_unique.row_id = 1
-        OR pivot_va_unique.row_id = 1
+        COALESCE(pivot_valeur_initale_unique.indicateur_id, pivot_valeur_actuelle_unique.indicateur_id) as indicateur_id,
+        COALESCE(pivot_valeur_initale_unique.zone_id, pivot_valeur_actuelle_unique.zone_id) as zone_id,
+        COALESCE(pivot_valeur_initale_unique.date_releve, pivot_valeur_actuelle_unique.date_releve) as date_releve,
+        pivot_valeur_initale_unique.indicateur_valeur_initiale,
+        pivot_valeur_actuelle_unique.indicateur_valeur_actuelle
+    FROM pivot_valeur_initale_unique full join pivot_valeur_actuelle_unique
+        ON pivot_valeur_initale_unique.indicateur_id = pivot_valeur_actuelle_unique.indicateur_id
+               AND pivot_valeur_initale_unique.zone_id = pivot_valeur_actuelle_unique.zone_id
+               AND pivot_valeur_initale_unique.date_releve = pivot_valeur_actuelle_unique.date_releve
 ),
 
-join_vi_va_vc as (
+join_valeur_initiale_valeur_actuelle_valeur_cible as (
     SELECT
-        COALESCE(pivot_vc_unique.indicateur_id, join_vi_va.indicateur_id) as indicateur_id,
-        COALESCE(pivot_vc_unique.zone_id, join_vi_va.zone_id) as zone_id,
-        COALESCE(pivot_vc_unique.date_releve, join_vi_va.date_releve) as date_releve,
-        join_vi_va.indic_vi,
-        join_vi_va.indic_va,
-        pivot_vc_unique.indic_vc
-    FROM pivot_vc_unique full join join_vi_va
-        ON pivot_vc_unique.indicateur_id = join_vi_va.indicateur_id
-               AND pivot_vc_unique.zone_id = join_vi_va.zone_id
-               AND pivot_vc_unique.date_releve = join_vi_va.date_releve
+        COALESCE(pivot_valeur_cible_unique.indicateur_id, join_valeur_initiale_valeur_actuelle.indicateur_id) as indicateur_id,
+        COALESCE(pivot_valeur_cible_unique.zone_id, join_valeur_initiale_valeur_actuelle.zone_id) as zone_id,
+        COALESCE(pivot_valeur_cible_unique.date_releve, join_valeur_initiale_valeur_actuelle.date_releve) as date_releve,
+        join_valeur_initiale_valeur_actuelle.indicateur_valeur_initiale,
+        join_valeur_initiale_valeur_actuelle.indicateur_valeur_actuelle,
+        pivot_valeur_cible_unique.indicateur_valeur_cible
+    FROM pivot_valeur_cible_unique full join join_valeur_initiale_valeur_actuelle
+        ON pivot_valeur_cible_unique.indicateur_id = join_valeur_initiale_valeur_actuelle.indicateur_id
+               AND pivot_valeur_cible_unique.zone_id = join_valeur_initiale_valeur_actuelle.zone_id
+               AND pivot_valeur_cible_unique.date_releve = join_valeur_initiale_valeur_actuelle.date_releve
 )
 
 SELECT *
-FROM join_vi_va_vc
+FROM join_valeur_initiale_valeur_actuelle_valeur_cible
 ORDER BY indicateur_id, zone_id, date_releve
