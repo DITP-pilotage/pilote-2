ecf1ad68b9006bb9c20545de9c17528141c205cf
GUIC <guillaume.chauvet@octo.com>
2023-04-17 16:29:39 +0200

    refactor: création du composant EnTeteChantier (18)

--- src/client/components/_commons/EnTêteChantier/EnTêteChantier.tsx
+++ src/client/components/_commons/EnTêteChantier/EnTêteChantier.tsx
@@ -8,20 +8,18 @@ export default function EnTêteChantier({ nom, axe, ppg }: EnTêteChantierProps)
       <Titre baliseHtml='h1'>
         { nom }
       </Titre>
-      <div className='fr-text--xs fr-mb-0'>
-        <p className='fr-mb-0 fr-text--xs chantier-données-propriété'>
-          Axe
-        </p>
-        <p className='fr-mb-1w fr-text--xs chantier-données-valeur'>
-          { axe }
-        </p>
-        <p className='fr-mb-0 fr-text--xs chantier-données-propriété'>
-          Politique Prioritaire du Gouvernement
-        </p>
-        <p className='fr-mb-0 fr-text--xs chantier-données-valeur'>
-          { ppg }
-        </p>
-      </div>
+      <p className='fr-mb-0 fr-text--xs chantier-données-propriété'>
+        Axe
+      </p>
+      <p className='fr-mb-1w fr-text--xs chantier-données-valeur'>
+        { axe }
+      </p>
+      <p className='fr-mb-0 fr-text--xs chantier-données-propriété'>
+        Politique Prioritaire du Gouvernement
+      </p>
+      <p className='fr-mb-0 fr-text--xs chantier-données-valeur'>
+        { ppg }
+      </p>
     </EnTêteChantierStyled>
   );
 }
