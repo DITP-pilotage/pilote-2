de29cf6a189968d09d54cdf62ff7d01217d0f61d
fabien.roussel <fabien.roussel921@gmail.com>
2023-04-18 10:55:48 +0200

    fix: indicateur a 100% alors qu il devrait etre null fixé + nettoyage sql (378)

--- data_management/data_factory/models/marts/pivot_faits_indicateur.sql
+++ data_management/data_factory/models/marts/pivot_faits_indicateur.sql
@@ -1,78 +1,74 @@
-with valeurs_initales_ordonne_par_indicateur_zone_et_date_releve as (
+with valeurs_initales_ordonne_par_indicateur_zone_et_date_releve AS (
 	SELECT
-		ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, zone_id_parent ORDER BY date_releve DESC) AS row_id,
+		ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, zone_id_parent ORDER BY date_releve DESC, date_import DESC) AS row_id,
 		indicateur_id,
 		zone_id,
 		zone_id_parent,
-		date_releve
+		date_releve AS date_releve_derniere_valeur_initiale
     FROM {{ ref("faits_indicateur")}}
-	WHERE zone_id_parent not like 'A%' and type_mesure = 'vi'
+	WHERE type_mesure = 'vi'
 ),
     
-valeur_initale_par_indicateur_zone_et_date_releve as (
-	SELECT
-		indicateur_id,
-		zone_id,
-		zone_id_parent,
-		date_releve as date_releve_derniere_valeur_initiale
+derniere_valeur_initale_par_indicateur_zone_et_date_releve AS (
+	SELECT *
 	FROM valeurs_initales_ordonne_par_indicateur_zone_et_date_releve
 	WHERE row_id = 1
 ),
 
-faits_indicateur_reduit as (
+faits_indicateur_depuis_la_derniere_vi AS (
 	SELECT
 		faits_indicateur.*,
 		date_releve_derniere_valeur_initiale
 	FROM {{ ref("faits_indicateur")}}
-	left join valeur_initale_par_indicateur_zone_et_date_releve
-		ON faits_indicateur.indicateur_id = valeur_initale_par_indicateur_zone_et_date_releve.indicateur_id
-	       AND faits_indicateur.zone_id = valeur_initale_par_indicateur_zone_et_date_releve.zone_id
-	       AND faits_indicateur.zone_id_parent = valeur_initale_par_indicateur_zone_et_date_releve.zone_id_parent
-	WHERE date_releve_derniere_valeur_initiale <= date_releve or date_releve_derniere_valeur_initiale is null
+	LEFT JOIN derniere_valeur_initale_par_indicateur_zone_et_date_releve
+		ON faits_indicateur.indicateur_id = derniere_valeur_initale_par_indicateur_zone_et_date_releve.indicateur_id
+	       AND faits_indicateur.zone_id = derniere_valeur_initale_par_indicateur_zone_et_date_releve.zone_id
+	       AND faits_indicateur.zone_id_parent = derniere_valeur_initale_par_indicateur_zone_et_date_releve.zone_id_parent
+	WHERE date_releve_derniere_valeur_initiale <= date_releve OR date_releve_derniere_valeur_initiale IS null
 ),
 
-faits_indicateur_order_by_date as (
+faits_indicateur_order_by_date AS (
     SELECT
-        ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, zone_id_parent, type_mesure ORDER BY date_releve DESC) AS row_id_by_date_releve_desc,
-        ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, zone_id_parent, type_mesure ORDER BY date_releve ASC) AS row_id_by_date_releve_asc,
+        ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, zone_id_parent, type_mesure ORDER BY date_releve DESC, date_import DESC) AS row_id_by_date_releve_desc,
+        ROW_NUMBER() OVER (PARTITION BY indicateur_id, zone_id, zone_id_parent, type_mesure ORDER BY date_releve ASC, date_import DESC) AS row_id_by_date_releve_asc,
         *,
         /* dimensions zones */
-        case when(zone_type = 'DEPT') then zone_code else NULL end as departement_code,
+        case when(zone_type = 'DEPT') then zone_code else NULL end AS departement_code,
         case
             when(zone_type_parent = 'REG') then zone_code_parent
             when(zone_type = 'REG') then zone_code
             else NULL
-            end as region_code,
-        'FRANCE' as national_code
-    FROM faits_indicateur_reduit
+            end AS region_code,
+        'FRANCE' AS national_code
+    FROM faits_indicateur_depuis_la_derniere_vi
     ORDER BY indicateur_id, zone_id, zone_id_parent, row_id_by_date_releve_desc
 ),
 
-premiere_mesure_valeur_actuelle_par_indicateur_et_zone as (
+premiere_mesure_valeur_actuelle_par_indicateur_et_zone AS (
 	SELECT indicateur_id,
 		zone_id,
 	    zone_id_parent,
-		MAX(date_releve) as date_releve_premiere_mesure_valeur_actuelle,
-		MAX(valeur) as premiere_mesure_valeur_actuelle
+		MAX(date_releve) AS date_releve_premiere_mesure_valeur_actuelle,
+		MAX(valeur) AS premiere_mesure_valeur_actuelle
 	FROM faits_indicateur_order_by_date
-	WHERE row_id_by_date_releve_asc = 1 and type_mesure = 'va'
-	group BY indicateur_id, zone_id, zone_id_parent, type_mesure
+	WHERE row_id_by_date_releve_asc = 1 AND type_mesure = 'va'
+	GROUP BY indicateur_id, zone_id, zone_id_parent, type_mesure
 )
 
 SELECT
     faits_indicateur_order_by_date.indicateur_id,
     faits_indicateur_order_by_date.zone_id,
     faits_indicateur_order_by_date.zone_id_parent,
-    COALESCE(MAX(valeur) FILTER (WHERE type_mesure = 'vi'), MAX(premiere_mesure_valeur_actuelle)) as valeur_initiale,
-    MAX(valeur) FILTER (WHERE type_mesure = 'va') as valeur_actuelle,
-    MAX(valeur) FILTER (WHERE type_mesure = 'vc') as valeur_cible, -- dernière valeur cible dispo
-    COALESCE(MAX(date_releve) FILTER (WHERE type_mesure = 'vi'), MAX(date_releve_premiere_mesure_valeur_actuelle)) as date_valeur_initiale,
-    MAX(date_releve) FILTER (WHERE type_mesure = 'va') as date_valeur_actuelle,
-    MAX(date_releve) FILTER (WHERE type_mesure = 'vc') as date_valeur_cible, -- dernière valeur cible dispo
-    MAX(departement_code) as departement_code,
-    MAX(region_code) as region_code,
-    MAX(national_code) as national_code
-FROM faits_indicateur_order_by_date left join premiere_mesure_valeur_actuelle_par_indicateur_et_zone
+    COALESCE(MAX(valeur) FILTER (WHERE type_mesure = 'vi'), MAX(premiere_mesure_valeur_actuelle)) AS valeur_initiale,
+    MAX(valeur) FILTER (WHERE type_mesure = 'va') AS valeur_actuelle,
+    MAX(valeur) FILTER (WHERE type_mesure = 'vc') AS valeur_cible, -- dernière valeur cible dispo
+    COALESCE(MAX(date_releve) FILTER (WHERE type_mesure = 'vi'), MAX(date_releve_premiere_mesure_valeur_actuelle)) AS date_valeur_initiale,
+    MAX(date_releve) FILTER (WHERE type_mesure = 'va') AS date_valeur_actuelle,
+    MAX(date_releve) FILTER (WHERE type_mesure = 'vc') AS date_valeur_cible, -- dernière valeur cible dispo
+    MAX(departement_code) AS departement_code,
+    MAX(region_code) AS region_code,
+    MAX(national_code) AS national_code
+FROM faits_indicateur_order_by_date LEFT JOIN premiere_mesure_valeur_actuelle_par_indicateur_et_zone
 	ON faits_indicateur_order_by_date.indicateur_id = premiere_mesure_valeur_actuelle_par_indicateur_et_zone.indicateur_id
                AND faits_indicateur_order_by_date.zone_id = premiere_mesure_valeur_actuelle_par_indicateur_et_zone.zone_id
                AND faits_indicateur_order_by_date.zone_id_parent = premiere_mesure_valeur_actuelle_par_indicateur_et_zone.zone_id_parent
--- data_management/data_factory/models/marts/pivot_faits_indicateur_avec_taux_avancement.sql
+++ data_management/data_factory/models/marts/pivot_faits_indicateur_avec_taux_avancement.sql
@@ -1,14 +1,22 @@
-WITH pivot_fait_indicateur_renommage as (
-
-    Select
+WITH pivot_fait_indicateur_renommage AS (
+    SELECT
         *,
-        valeur_actuelle as valeur_actuelle_comparable
+        valeur_actuelle AS valeur_actuelle_comparable
     FROM {{ ref('pivot_faits_indicateur')}}
+),
 
+pivot_fait_indicateur_avec_avancement AS (
+    SELECT
+        *,
+        (valeur_actuelle_comparable - valeur_initiale) / (valeur_cible - valeur_initiale) * 100 AS avancement_global
+    FROM pivot_fait_indicateur_renommage
 )
 
 SELECT
     *,
-    (valeur_actuelle_comparable - valeur_initiale) / (valeur_cible - valeur_initiale) * 100 as avancement_global,
-    GREATEST(LEAST((valeur_actuelle - valeur_initiale) / (valeur_cible - valeur_initiale) * 100, 100), 0) as avancement_global_borne
-from pivot_fait_indicateur_renommage
+    CASE
+        WHEN avancement_global IS NOT null
+            THEN GREATEST(LEAST(avancement_global, 100), 0)
+        ELSE null
+        END AS avancement_global_borne
+from pivot_fait_indicateur_avec_avancement
