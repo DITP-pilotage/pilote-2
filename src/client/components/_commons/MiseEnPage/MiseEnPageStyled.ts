b017cee96a0abd0dd7328e83441a554fdd2b06e3
Jordan Dullier <jordan.dullier29@gmail.com>
2023-04-19 17:04:47 +0200

    feat: impression de la page chantier (297)

--- src/client/components/_commons/MiseEnPage/MiseEnPageStyled.ts
+++ src/client/components/_commons/MiseEnPage/MiseEnPageStyled.ts
@@ -2,6 +2,12 @@ import styled from '@emotion/styled';
 
 const MiseEnPageStyled = styled.div`
   word-break: break-word;
+
+  @media print {
+     .fr-header, .fr-footer{
+      display: none;
+    }
+  }
 `;
 
 export default MiseEnPageStyled;
