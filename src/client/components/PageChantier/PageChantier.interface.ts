c37ed2870b6815888875044e61a9c1c243099153
Ludwig Chieng <ludwig.chieng@octo.com>
2023-04-19 14:24:53 +0200

    refactor: retire les props inutiles (289)

--- src/client/components/PageChantier/PageChantier.interface.ts
+++ src/client/components/PageChantier/PageChantier.interface.ts
@@ -1,9 +1,7 @@
-import Chantier from '@/server/domain/chantier/Chantier.interface';
 import { Habilitation } from '@/server/domain/identité/Habilitation';
 import Indicateur from '@/server/domain/indicateur/Indicateur.interface';
 
 export default interface PageChantierProps {
-  chantier: Chantier
   indicateurs: Indicateur[]
   habilitation: Habilitation
 }
--- src/pages/chantier/[id].tsx
+++ src/pages/chantier/[id].tsx
@@ -2,8 +2,7 @@ import { getServerSession } from 'next-auth/next';
 import { GetServerSidePropsContext } from 'next';
 import PageChantier from '@/components/PageChantier/PageChantier';
 
-import Chantier from '@/server/domain/chantier/Chantier.interface';
-import { ChantierId, Habilitation, SCOPE_LECTURE } from '@/server/domain/identité/Habilitation';
+import { ChantierId, Habilitation } from '@/server/domain/identité/Habilitation';
 import Indicateur from '@/server/domain/indicateur/Indicateur.interface';
 
 import { dependencies } from '@/server/infrastructure/Dependencies';
@@ -11,15 +10,13 @@ import logger from '@/server/infrastructure/logger';
 import { authOptions } from '@/server/infrastructure/api/auth/[...nextauth]';
 
 interface NextPageChantierProps {
-  chantier: Chantier,
   habilitation: Habilitation,
   indicateurs: Indicateur[],
 }
 
-export default function NextPageChantier({ chantier, indicateurs, habilitation }: NextPageChantierProps) {
+export default function NextPageChantier({ indicateurs, habilitation }: NextPageChantierProps) {
   return (
     <PageChantier
-      chantier={chantier}
       habilitation={habilitation}
       indicateurs={indicateurs}
     />
@@ -41,15 +38,12 @@ export async function getServerSideProps(context: GetServerSidePropsContext<Para
   }
 
   const habilitation = session.habilitation;
-  const chantierRepository = dependencies.getChantierRepository();
-  const chantier: Chantier = await chantierRepository.getById(params.id, session.habilitation, SCOPE_LECTURE);
 
   const indicateurRepository = dependencies.getIndicateurRepository();
   const indicateurs: Indicateur[] = await indicateurRepository.récupérerParChantierId(params.id);
 
   return {
     props: {
-      chantier,
       indicateurs,
       habilitation,
     },
