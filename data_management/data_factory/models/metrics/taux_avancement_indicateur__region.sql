702104278d15cb4928d22861d18b3195c254b39b
fabien.roussel <fabien.roussel921@gmail.com>
2023-04-21 17:01:19 +0200

    fix: ajout des dates dans la vue sur les taux avancement indicateur region (384)

--- data_management/data_factory/models/metrics/taux_avancement_indicateur__region.sql
+++ data_management/data_factory/models/metrics/taux_avancement_indicateur__region.sql
@@ -1,50 +1,55 @@
-WITH pivot_faits_indicateur_reg_dept as (
+WITH pivot_faits_indicateur_reg_dept AS (
     SELECT * from {{ ref("pivot_faits_indicateur") }} WHERE zone_type = 'REG'
 ),
 
-taux_avancement_region_avec_mesures as (
+taux_avancement_region_avec_mesures AS (
     SELECT
         indicateur_id,
         zone_id,
-        max(zone_id_parent),
-        sum(valeur_initiale) total_vi,
-        sum(valeur_actuelle) total_va,
-        sum(valeur_cible) total_vc,
+        zone_id_parent,
+        valeur_initiale,
+        valeur_actuelle,
+        valeur_cible,
+        date_valeur_initiale,
+        date_valeur_actuelle,
+        date_valeur_cible,
         CASE
-            WHEN sum(valeur_cible) <> sum(valeur_initiale)
-                THEN sum(valeur_actuelle - valeur_initiale) / sum(valeur_cible - valeur_initiale) * 100
+            WHEN valeur_cible <> valeur_initiale
+                THEN (valeur_actuelle - valeur_initiale) / (valeur_cible - valeur_initiale) * 100
             ELSE NULL
             END AS avancement_global
     FROM pivot_faits_indicateur_reg_dept
-    WHERE departement_code is null
-    GROUP BY indicateur_id, zone_id
+    WHERE departement_code is null -- TODO condition vraiment nécessaire ? car avant zone_type = 'REG' a vérifier si on peut enlever quand on aura plus de données
 ),
 
-pivot_faits_indicateur_departements_sans_mesure_regionale as (
+pivot_faits_indicateur_departements_sans_mesure_regionale AS (
     SELECT
         taux_avancement_indicateur__departement.indicateur_id,
         taux_avancement_indicateur__departement.zone_id,
         taux_avancement_indicateur__departement.zone_id_parent,
-        'FRANCE' as national_code,
-        taux_avancement_indicateur__departement.valeur_initiale as valeur_initiale,
-        taux_avancement_indicateur__departement.valeur_actuelle as valeur_actuelle,
-        taux_avancement_indicateur__departement.valeur_cible as valeur_cible
+        'FRANCE' AS national_code,
+        taux_avancement_indicateur__departement.valeur_initiale AS valeur_initiale,
+        taux_avancement_indicateur__departement.valeur_actuelle AS valeur_actuelle,
+        taux_avancement_indicateur__departement.valeur_cible AS valeur_cible
     FROM {{ ref("taux_avancement_indicateur__departement") }}
-    LEFT JOIN taux_avancement_region_avec_mesures as ta_reg_avec_mesures
+    LEFT JOIN taux_avancement_region_avec_mesures AS ta_reg_avec_mesures
         ON taux_avancement_indicateur__departement.indicateur_id = ta_reg_avec_mesures.indicateur_id
             AND taux_avancement_indicateur__departement.zone_id_parent = ta_reg_avec_mesures.zone_id
     WHERE ta_reg_avec_mesures.indicateur_id IS NULL
         AND taux_avancement_indicateur__departement.zone_type_parent = 'REG'
 ),
 
-taux_avancement_region as (
+taux_avancement_region AS (
     SELECT
         indicateur_id,
-        zone_id_parent as zone_id,
-        max(zone_id_parent),
-        sum(valeur_initiale) total_vi,
-        sum(valeur_actuelle) total_va,
-        sum(valeur_cible) total_vc,
+        zone_id_parent AS zone_id,
+        max(national_code) AS zone_id_parent,
+        sum(valeur_initiale) AS valeur_initiale,
+        sum(valeur_actuelle) AS valeur_actuelle,
+        sum(valeur_cible) AS valeur_cible,
+        NULL AS date_valeur_initiale,
+        NULL AS date_valeur_actuelle,
+        NULL AS date_valeur_cible,
         CASE
             WHEN sum(valeur_cible) <> sum(valeur_initiale)
                 THEN sum(valeur_actuelle - valeur_initiale) / sum(valeur_cible - valeur_initiale) * 100
@@ -59,8 +64,8 @@ taux_avancement_region as (
 SELECT
     *,
     CASE
-        WHEN avancement_global IS NOT null
+        WHEN avancement_global IS NOT NULL
             THEN GREATEST(LEAST(avancement_global, 100), 0)
-        ELSE null
+        ELSE NULL
         END AS avancement_global_borne
 from taux_avancement_region
