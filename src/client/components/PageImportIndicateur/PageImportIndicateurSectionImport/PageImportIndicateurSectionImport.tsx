f048ea7f1893a91f7e29d6df030a1b43f7c017f4
GUIC <guillaume.chauvet@octo.com>
2023-05-02 10:40:44 +0200

    feat: ajout du stepper et du select de l'indicateur id dans la page import indicateur (29)

--- src/client/components/PageImportIndicateur/PageImportIndicateurSectionImport/PageImportIndicateurSectionImport.tsx
+++ src/client/components/PageImportIndicateur/PageImportIndicateurSectionImport/PageImportIndicateurSectionImport.tsx
@@ -1,37 +1,55 @@
+import { useState } from 'react';
 import Titre from '@/components/_commons/Titre/Titre';
-import Indicateurs from '@/components/PageChantier/Indicateurs/Indicateurs';
+import IndicateurDEtapes from '@/components/_commons/IndicateurDEtapes/IndicateurDEtapes';
+import Bloc from '@/components/_commons/Bloc/Bloc';
+import Sélecteur from '@/components/_commons/Sélecteur/Sélecteur';
+import Bouton from '@/components/_commons/Bouton/Bouton';
 import PageImportIndicateurSectionImportStyled from './PageImportIndicateurSectionImport.styled';
 import { PageImportIndicateurSectionImportProps } from './PageImportIndicateurSectionImport.interface';
 
+const étapes = ['Sélectionnez l’indicateur', 'Chargez votre fichier et vérifiez sa conformité ', 'Prévisualiser les données et les publier ']; 
+
 export default function PageImportIndicateurSectionImport({
-  détailsIndicateurs,
   indicateurs,
 }: PageImportIndicateurSectionImportProps) {
+  const [etapeCourante, setEtapeCourante] = useState(1);
+  const optionsSélecteur = indicateurs.map(elem => ({
+    libellé: elem.nom,
+    valeur: elem.id,
+  }));
+
+  enum Etapes {
+    SELECTION_INDICATEUR = 1,
+  }
+  
   return (
     <PageImportIndicateurSectionImportStyled>
       <div className='fr-container fr-py-3w'>
         <Titre baliseHtml='h2'>
-          Indicateurs
+          Importez vos données
         </Titre>
-        <Titre baliseHtml='h3'>
-          Indicateurs d&apos;impact
-        </Titre>
-        {
-          détailsIndicateurs !== null ? (
-            <div className="fr-grid-row fr-grid-row--gutters fr-my-0 fr-pb-1w">
-              <div className="fr-col-12">
-                <Indicateurs
-                  détailsIndicateurs={détailsIndicateurs}
-                  estDisponibleALImport
-                  indicateurs={indicateurs}
-                />
-              </div>
+        <Bloc>
+          <IndicateurDEtapes
+            étapeCourante={etapeCourante}
+            étapes={étapes}
+          />
+          <form action="">
+            <div className={`${etapeCourante !== Etapes.SELECTION_INDICATEUR && 'fr-hidden'}`}>
+              <Titre baliseHtml='h4'>
+                Pour quel indicateur souhaitez vous mettre à jour les données ?
+              </Titre>
+              <Sélecteur
+                htmlName="indicateurId"
+                libellé='Choix de l’indicateur'
+                options={optionsSélecteur}
+              />
+              <Bouton
+                label="Suivant"
+                onClick={() => setEtapeCourante(etapeCourante + 1)}
+              />
             </div>
-          ) :
-            <p>
-              Chargement des indicateurs...
-            </p>
-        }
+          </form>
+        </Bloc>
       </div>
     </PageImportIndicateurSectionImportStyled>
   );
