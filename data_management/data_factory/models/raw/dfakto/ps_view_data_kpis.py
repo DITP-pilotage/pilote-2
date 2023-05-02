2f937072c9ba52407ae460379b882a819fd5def9
louisehbrt <louise.hubert@octo.com>
2023-05-02 17:35:19 +0200

    chore: import ps_view_data_kpis du dump dfakto (133)

---
+++ data_management/data_factory/models/raw/dfakto/ps_view_data_kpis.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+
+def model(dbt, session):
+    dump_dfakto_octo_ps = os.getenv('DUMP_DFAKTO_PS')
+    ps_view_data_kpis = pd.read_csv(
+        f'{dump_dfakto_octo_ps}/ps_view_data_kpis_202304191200.csv',
+        sep=';',
+    )
+
+    columns_type = {
+        'unite': 'object',
+        'vi': 'float64',
+        'va': 'float64',
+        'vc': 'float64',
+        'vi_maj': 'datetime64',
+        'va_maj': 'datetime64',
+        'vc_maj': 'datetime64'
+    }
+
+    ps_view_data_kpis = ps_view_data_kpis.astype(columns_type)
+
+    return ps_view_data_kpis
