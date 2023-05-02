24b4a9639b949686c02244c61fbf0d60bbcca8e0
louisehbrt <louise.hubert@octo.com>
2023-05-02 12:00:35 +0200

    chore: ajout colonne ministere_id dans la table périmètre (311)

--- data_management/data_factory/models/exposition/perimetre.sql
+++ data_management/data_factory/models/exposition/perimetre.sql
@@ -1,5 +1,7 @@
 SELECT
     id,
     nom,
-    ministere_nom as ministere
+    ministere_nom as ministere,
+    porteurs.id as ministere_id
 FROM {{ ref('stg_ppg_metadata__perimetres') }}
+LEFT JOIN {{ ref('stg_ppg_metadata__porteurs') }} porteurs ON ministere_polygramme = polygramme
