b5962040c28bd4e80f2a7d2fe78caf86340722ce
fabien.roussel <fabien.roussel921@gmail.com>
2023-05-04 19:00:02 +0200

    chore: suppression des COM pour les territoires et donc chantier et indicateur (000)

--- data_management/data_factory/models/raw/dfakto/fact_financials.py
+++ data_management/data_factory/models/raw/dfakto/fact_financials.py
@@ -5,7 +5,7 @@ import os
 def model(dbt, session):
     dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_5')
     fact_financials = pd.read_csv(
-        f'{dump_dfakto_rp_raw}/fact_financials_202305041143_short.csv',
+        f'{dump_dfakto_rp_raw}/fact_financials_202305041143.csv',
         sep=';',
     )
 
