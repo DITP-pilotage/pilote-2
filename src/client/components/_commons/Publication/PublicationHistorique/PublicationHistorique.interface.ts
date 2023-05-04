616b2b51729bcc71db1cc2fe445d8813bd48a8d6
Ludwig Chieng <ludwig.chieng@octo.com>
2023-05-04 12:48:18 +0200

    fix: corrige les tests et un type (312)

--- src/client/components/_commons/Publication/PublicationHistorique/PublicationHistorique.interface.ts
+++ src/client/components/_commons/Publication/PublicationHistorique/PublicationHistorique.interface.ts
@@ -1,8 +1,8 @@
 import PublicationProps from '@/client/components/_commons/Publication/Publication.interface';
 
 export default interface PublicationHistoriqueProps {
-  type: PublicationProps['type']['id']
-  entité: PublicationProps['entité']
+  type: PublicationProps['caractéristiques']['type']
+  entité: PublicationProps['caractéristiques']['entité']
   chantierId: PublicationProps['chantierId']
   maille: PublicationProps['maille']
   codeInsee: PublicationProps['codeInsee']
