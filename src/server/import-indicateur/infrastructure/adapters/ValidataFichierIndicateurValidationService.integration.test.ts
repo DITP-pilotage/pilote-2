b3328643d93d7f7d332987b7975b8fbd74a00189
GUIC <guillaume.chauvet@octo.com>
2023-05-04 10:46:12 +0200

    fix: correction du bug quand les en tetes ne sont pas défini (31)

--- src/server/import-indicateur/infrastructure/adapters/ValidataFichierIndicateurValidationService.integration.test.ts
+++ src/server/import-indicateur/infrastructure/adapters/ValidataFichierIndicateurValidationService.integration.test.ts
@@ -1,5 +1,5 @@
 import { mock, MockProxy } from 'jest-mock-extended';
-import {  ValidataFichierIndicateurValidationService } from '@/server/import-indicateur/infrastructure/adapters/ValidataFichierIndicateurValidationService';
+import { ValidataFichierIndicateurValidationService } from '@/server/import-indicateur/infrastructure/adapters/ValidataFichierIndicateurValidationService';
 import { ReportErrorTaskBuilder } from '@/server/import-indicateur/app/builder/ReportErrorTask.builder';
 import { ReportTaskBuilder, ReportResourceTaskBuilder } from '@/server/import-indicateur/app/builder/ReportTask.builder';
 import { ReportValidataBuilder } from '@/server/import-indicateur/app/builder/ReportValidata.builder';
@@ -212,12 +212,12 @@ describe('ValidataFichierIndicateurValidationService', () => {
         ['identifiant_indic', 'constraint-error', 'constraint \"pattern\" is \"^IND-[0-9]{3}$\"', 2, "L'identifiant de l'indicateur doit être renseigné dans le format IND-XXX. Vous pouvez vous référer au guide des indicateurs pour trouver l'identifiant de votre indicateur."],
         ['identifiant_indic', 'constraint-error', 'another message', 2, "message d'origine"],
         ['type_valeur', 'constraint-error', "constraint \"enum\" is \"['vi', 'va', 'vc']\"", 2, 'Le type de valeur doit être vi (valeur initiale), va (valeur actuelle) ou vc (valeur cible).'],
-        ['type_valeur', 'constraint-error', "constraint \"enum\" is \"['va']\"",  2, 'Le type de valeur doit être va (valeur actuelle). Vous ne pouvez saisir que des valeurs actuelles.'],
+        ['type_valeur', 'constraint-error', "constraint \"enum\" is \"['va']\"", 2, 'Le type de valeur doit être va (valeur actuelle). Vous ne pouvez saisir que des valeurs actuelles.'],
         ['type_valeur', 'constraint-error', 'another message', 2, "message d'origine"],
-        ['zone_id', 'constraint-error', 'constraint "pattern" is "^(R[0-9]{2,3})$"',  2, "Veuillez entrer uniquement une zone régionale dans la colonne zone_id. 'F001' n'est pas une zone régionale."],
+        ['zone_id', 'constraint-error', 'constraint "pattern" is "^(R[0-9]{2,3})$"', 2, "Veuillez entrer uniquement une zone régionale dans la colonne zone_id. 'F001' n'est pas une zone régionale."],
         ['zone_id', 'constraint-error', 'another message', 2, "message d'origine"],
-        [null, 'primary-key-error', 'the same as in the row at position 2',  2, "La ligne 2 comporte la même zone, date, identifiant d'indicateur et type de valeur qu'une autre ligne. Veuillez en supprimer une des deux."],
-        [null, 'primary-key-error', 'the same as in the row at position 3',  3, "La ligne 3 comporte la même zone, date, identifiant d'indicateur et type de valeur qu'une autre ligne. Veuillez en supprimer une des deux."],
+        [null, 'primary-key-error', 'the same as in the row at position 2', 2, "La ligne 2 comporte la même zone, date, identifiant d'indicateur et type de valeur qu'une autre ligne. Veuillez en supprimer une des deux."],
+        [null, 'primary-key-error', 'the same as in the row at position 3', 3, "La ligne 3 comporte la même zone, date, identifiant d'indicateur et type de valeur qu'une autre ligne. Veuillez en supprimer une des deux."],
         [null, 'primary-key-error', 'another message', 2, "message d'origine"],
       ])('pour le champ %s, le code %s et la note %s à la ligne %i', async (
         fieldName,
@@ -261,6 +261,57 @@ describe('ValidataFichierIndicateurValidationService', () => {
         const result = await validataFichierIndicateurValidationService.validerFichier(body);
 
         expect(result.listeErreursValidation.at(0)?.message).toEqual(expected);
+      });
+    });
+
+    describe("quand identifiant_indic n'est pas défini dans les en-têtes", () => {
+      it("doit identifier que l'en-tête identifiant_indic n'est pas présente", async () => {
+        // GIVEN
+        const body: ValiderFichierPayload = { cheminCompletDuFichier, nomDuFichier, schema };
+        const report = new ReportValidataBuilder()
+          .avecValid(false)
+          .avecTasks(
+            new ReportTaskBuilder()
+              .avecErrors(
+                new ReportErrorTaskBuilder()
+                  .avecCell('cellule 1')
+                  .avecName('nom 1')
+                  .avecFieldName('nom du champ 1')
+                  .avecFieldPosition(1)
+                  .avecMessage('message 1')
+                  .avecRowNumber(1)
+                  .avecRowPosition(1)
+                  .build(),
+              )
+              .avecResource(
+                new ReportResourceTaskBuilder()
+                  .avecData([
+                    ['id_indic', 'zone_id', 'date_valeur', 'type_valeur', 'valeur'],
+                    ['IND-001', 'D001', metricDateValue1, 'vi', '9'],
+                    ['IND-001', 'D004', metricDateValue2, 'vc', '3'],
+                  ])
+                  .build(),
+              )
+              .build(),
+          )
+          .build();
+
+        httpClient.post.mockResolvedValue(report);
+
+        // WHEN
+        const result = await validataFichierIndicateurValidationService.validerFichier(body);
+
+        // THEN
+        expect(result.estValide).toEqual(false);
+        expect(result.listeErreursValidation).toHaveLength(2);
+        expect(result.listeIndicateursData).toHaveLength(0);
+
+        expect(result.listeErreursValidation.at(0)?.nom).toEqual('identifiant_indic');
+        expect(result.listeErreursValidation.at(0)?.message).toEqual("L'en-tête identifiant_indic n'est pas présente");
+
+        expect(result.listeErreursValidation.at(1)?.cellule).toEqual('cellule 1');
+        expect(result.listeErreursValidation.at(1)?.nom).toEqual('nom 1');
+        expect(result.listeErreursValidation.at(1)?.nomDuChamp).toEqual('nom du champ 1');
       });
     });
 
