2ae72389aed025e217948cd9c31451b9be2c11d2
Ludwig Chieng <ludwig.chieng@octo.com>
2023-05-09 14:56:34 +0200

    feat: ajoute le feature flipping (247)

--- src/pages/api/export/chantiers-sans-filtre.ts
+++ src/pages/api/export/chantiers-sans-filtre.ts
@@ -3,5 +3,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import handleExportDesChantiersSansFiltre from '@/server/infrastructure/api/export/chantiers-sans-filtre';
 
 export default function handle(request: NextApiRequest, response: NextApiResponse) {
+  if (process.env.NEXT_PUBLIC_FF_EXPORT_CSV !== 'true') {
+    response.status(404);
+    response.end('Non disponible');
+    return;
+  }
+
   return handleExportDesChantiersSansFiltre(request, response);
 }
--- src/pages/api/export/indicateurs-sans-filtre.ts
+++ src/pages/api/export/indicateurs-sans-filtre.ts
@@ -3,5 +3,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import handleExportDesIndicateursSansFiltre from '@/server/infrastructure/api/export/indicateurs-sans-filtre';
 
 export default function handle(request: NextApiRequest, response: NextApiResponse) {
+  if (process.env.NEXT_PUBLIC_FF_EXPORT_CSV !== 'true') {
+    response.status(404);
+    response.end('Non disponible');
+    return;
+  }
+
   return handleExportDesIndicateursSansFiltre(request, response);
 }
