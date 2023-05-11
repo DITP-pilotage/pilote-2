89bf9b6345e7f0144328a074ee2cff45e447b16d
louisehbrt <louise.hubert@octo.com>
2023-05-11 16:58:09 +0200

    chore: remplit tables objectif et decision strategique de l'import (000)

--- data_management/data_factory/models/exposition/commentaire.sql
+++ data_management/data_factory/models/exposition/commentaire.sql
@@ -3,7 +3,8 @@ SELECT
                  ['chantier_id',
                  'type',
                  'maille',
-                 'code_insee']
+                 'code_insee',
+                 'date']
              ) }} as id,
     chantier_id,
     type,
@@ -13,3 +14,9 @@ SELECT
     COALESCE(maille, 'NAT') as maille, --TODO supprimer le coalesce car la maille est sensé etre renseignée
     COALESCE(code_insee, 'FR') as code_insee --TODO supprimer le coalesce car le code_insee est sensé etre renseigné
 FROM {{ ref('stg_import_massif__commentaires') }}
+WHERE type='commentaires_sur_les_donnees'
+    OR type='autres_resultats_obtenus'
+    OR type='autres_resultats_obtenus_non_correles_aux_indicateurs'
+    OR type='freins_a_lever'
+    OR type='actions_a_venir'
+    OR type='actions_a_valoriser'
---
+++ data_management/data_factory/models/exposition/objectif.sql
@@ -0,0 +1,15 @@
+SELECT
+    {{ dbt_utils.surrogate_key(
+                 ['chantier_id',
+                 'type',
+                 'date']
+             ) }} as id,
+    auteur,
+    type::type_objectif,
+    contenu,
+    date,
+    chantier_id
+FROM {{ ref('stg_import_massif__commentaires') }}
+WHERE type='notre_ambition'
+    OR type='deja_fait'
+    OR type='a_faire'
