782438ccecdafa2abb3c5754c82574c1a6cf627f
fabien.roussel <fabien.roussel921@gmail.com>
2023-05-12 18:13:57 +0200

    refactor: suppression des anciennes meteo des reformes pour les nvx chantier (3)

--- data_management/data_factory/models/exposition/chantier.sql
+++ data_management/data_factory/models/exposition/chantier.sql
@@ -6,13 +6,10 @@ dfakto_chantier as (
         fact_progress_chantier.avancement_borne,
         dim_tree_nodes.code_chantier,
         dim_tree_nodes.zone_code as zone_id,
-        dim_structures.nom as structure_nom,
-        view_data_properties.meteo_nom
+        dim_structures.nom as structure_nom
     FROM {{ ref('stg_dfakto__fact_progress_chantiers') }} fact_progress_chantier
         JOIN {{ ref('stg_dfakto__dim_tree_nodes') }} dim_tree_nodes ON fact_progress_chantier.tree_node_id = dim_tree_nodes.id
         JOIN {{ ref('stg_dfakto__dim_structures') }} dim_structures ON dim_tree_nodes.structure_id = dim_structures.id
-        -- TODO: suppression de la prochaine jointure car météo rentré dans l'application
-        LEFT JOIN {{ ref('stg_dfakto__view_data_properties') }} view_data_properties ON view_data_properties.reforme_code = dim_tree_nodes.code
 
 ),
 
@@ -34,7 +31,7 @@ chantier_est_barometre as (
         m_chantiers.ministeres,
         m_chantiers.directions_administration_centrale,
         m_chantiers.directeurs_projet,
-        COALESCE(chantier_meteos.id, 'NON_RENSEIGNEE') AS meteo,
+        NULL AS meteo, -- todo a supprimer de la table prisma
         m_axes.nom AS axe,
         m_ppgs.nom AS ppg,
         m_chantiers.directeurs_projet_mails,
@@ -47,7 +44,6 @@ chantier_est_barometre as (
                 AND m_chantiers.zone_id = d_chantiers.zone_id
                 AND d_chantiers.structure_nom='Chantier'
         LEFT JOIN {{ ref('stg_ppg_metadata__porteurs') }} m_porteurs ON m_porteurs.id = ANY(m_chantiers.directeurs_administration_centrale_ids)
-        LEFT JOIN {{ ref('stg_ppg_metadata__chantier_meteos') }} chantier_meteos ON chantier_meteos.nom_dfakto = d_chantiers.meteo_nom
         LEFT JOIN {{ ref('stg_ppg_metadata__ppgs') }} m_ppgs ON m_ppgs.id = m_chantiers.ppg_id
         LEFT JOIN {{ ref('stg_ppg_metadata__axes') }} m_axes ON m_axes.id = m_ppgs.axe_id
         LEFT JOIN chantier_est_barometre on m_chantiers.id = chantier_est_barometre.chantier_id
@@ -64,7 +60,7 @@ UNION
         m_chantiers.ministeres,
         m_chantiers.directions_administration_centrale,
         m_chantiers.directeurs_projet,
-        'NON_NECESSAIRE' AS meteo,
+        NULL AS meteo, -- todo a supprimer de la table prisma
         m_axes.nom AS axe,
         m_ppgs.nom AS ppg,
         m_chantiers.directeurs_projet_mails,
