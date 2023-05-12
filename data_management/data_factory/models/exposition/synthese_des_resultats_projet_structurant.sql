02cdb855ea9b8f1370c47cb804a1760a3fd8a22f
OCTO-GUIC <guillaume.chauvet@octo.com>
2023-05-12 17:56:57 +0200

    feat: création de la table synthese des resultats projet structurant (103)

---
+++ data_management/data_factory/models/exposition/synthese_des_resultats_projet_structurant.sql
@@ -0,0 +1,22 @@
+SELECT
+DISTINCT ON(ps.id, dfakto_view.zone_code, dfakto_view.synthese_des_resultats_date, dfakto_view.meteo_date)
+    {{ dbt_utils.surrogate_key(
+        ['ps.id',
+        'dfakto_view.zone_code',
+        'dfakto_view.synthese_des_resultats_date',
+        'dfakto_view.meteo_date',
+        ]
+    ) }} as id,
+    ps.id as projet_structurant_id,
+    dfakto_view.zone_code as territoire_id,
+    dfakto_view.meteo as meteo,
+    dfakto_view.meteo_date as date_meteo,
+    dfakto_view.synthese_des_resultats as commentaire,
+    dfakto_view.synthese_des_resultats_date as date_commentaire,
+    NULL as auteur
+FROM {{ ref('stg_dfakto__ps_view_data_financials') }} dfakto_view
+    JOIN {{ ref('projet_structurant') }} ps 
+    ON dfakto_view.projet_structurant_code = ps.code
+WHERE (dfakto_view.synthese_des_resultats IS NOT NULL AND dfakto_view.synthese_des_resultats_date IS NOT NULL)
+    OR (dfakto_view.meteo IS NOT NULL AND dfakto_view.meteo_date IS NOT NULL)
+
---
+++ src/database/prisma/migrations/20230516093904_creation_de_la_table_synthese_des_resultats_projet_structurant/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "public"."synthese_des_resultats_projet_structurant" (
+    "id" TEXT NOT NULL,
+    "projet_structurant_id" TEXT NOT NULL,
+    "territoire_id" TEXT NOT NULL,
+    "meteo" TEXT,
+    "date_meteo" TIMESTAMP,
+    "commentaire" TEXT,
+    "date_commentaire" TIMESTAMP,
+    "auteur" TEXT,
+
+    CONSTRAINT "synthese_des_resultats_projet_structurant_pkey" PRIMARY KEY ("id")
+);
