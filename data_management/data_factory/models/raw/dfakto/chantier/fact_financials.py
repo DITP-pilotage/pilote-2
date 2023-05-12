90c06a2cabdc9c8856c0c058821b3ef45977543f
fabien.roussel <fabien.roussel921@gmail.com>
2023-05-12 18:13:11 +0200

    refactor: deplacement des fichiers dfakto dans des dossiers par perimetre (3)

--- data_management/data_factory/models/raw/dfakto/chantier/fact_financials.py
+++ data_management/data_factory/models/raw/dfakto/chantier/fact_financials.py
@@ -3,9 +3,9 @@ import os
 
 
 def model(dbt, session):
-    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_5')
+    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_TEMP')
     fact_financials = pd.read_csv(
-        f'{dump_dfakto_rp_raw}/fact_financials_202305041143_short.csv',
+        f'{dump_dfakto_rp_raw}/fact_financials.csv',
         sep=';',
     )
 
--- data_management/data_factory/models/raw/dfakto/chantier/fact_progress_indicateurs.py
+++ data_management/data_factory/models/raw/dfakto/chantier/fact_progress_indicateurs.py
@@ -3,9 +3,9 @@ import os
 
 
 def model(dbt, session):
-    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_5')
+    dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_TEMP')
     fact_progress_indicateurs = pd.read_csv(
-        f'{dump_dfakto_rp_raw}/fact_progress_202305041027.csv',
+        f'{dump_dfakto_rp_raw}/fact_progress.csv',
         sep=';'
     )
 
