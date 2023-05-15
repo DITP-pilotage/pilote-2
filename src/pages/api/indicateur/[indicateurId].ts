1276cb05258dd4ce9d87712cf5e27872fb761ac1
Jordan Dullier <jordan.dullier29@gmail.com>
2023-05-15 17:48:54 +0200

    refactor: supprime des fichiers devenus inutiles (300)

--- src/pages/api/indicateur/[indicateurId].ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import handleIndicateurId from '@/server/infrastructure/api/indicateur/[indicateurId]';
-
-export default function handle(req: NextApiRequest, res: NextApiResponse) {
-  return handleIndicateurId(req, res);
-}
