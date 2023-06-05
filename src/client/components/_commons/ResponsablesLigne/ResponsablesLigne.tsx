b2f9cbc62f828ce47769ae4dc591287efe255281
Ludwig Chieng <ludwig.chieng@octo.com>
2023-06-05 15:01:34 +0200

    fix: corrige le css de Responsables (000)

--- src/client/components/_commons/ResponsablesLigne/ResponsablesLigne.tsx
+++ src/client/components/_commons/ResponsablesLigne/ResponsablesLigne.tsx
@@ -4,10 +4,10 @@ import ResponsablesLigneProps from './ResponsablesLigne.interface';
 export default function ResponsablesLigne({ libellé, contenu }: ResponsablesLigneProps) {
   return (
     <div className='ligne fr-grid-row fr-grid-row--gutters fr-pb-2w'>
-      <div className='fr-text--sm fr-text--bold fr-col-12 fr-col-md-6 fr-m-0 fr-pb-1v'>
+      <div className='fr-text--sm fr-text--bold fr-col-12 fr-col-md-6 fr-m-0 fr-pb-1v fr-py-md-1w'>
         {libellé}
       </div>
-      <div className='fr-text--sm fr-col-12 fr-col-md-6 fr-m-0 fr-pt-0'>
+      <div className='fr-text--sm fr-col-12 fr-col-md-6 fr-m-0 fr-pt-0 fr-p-md-1w'>
         { 
           contenu.length > 0 
             ? contenu.map((élément, i) => {
