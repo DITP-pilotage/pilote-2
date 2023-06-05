ab91e5a0f9fb6f02ec0ffc97ea4946039e7d1da5
Ludwig Chieng <ludwig.chieng@octo.com>
2023-06-05 18:31:27 +0200

    fix: corrige le footer (000)

--- src/client/components/_commons/MiseEnPage/PiedDePage/PiedDePage.tsx
+++ src/client/components/_commons/MiseEnPage/PiedDePage/PiedDePage.tsx
@@ -24,48 +24,6 @@ export default function PiedDePage() {
             <p className="fr-footer__content-desc">
               PILOTE est le dispositif de suivi des politiques prioritaires du gouvernement. Il permet d’évaluer les résultats des politiques publiques dans les territoires. 
             </p>
-            <ul className="fr-footer__content-list">
-              <li className="fr-footer__content-item">
-                <a
-                  className="fr-footer__content-link"
-                  href="https://legifrance.gouv.fr"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  legifrance.gouv.fr
-                </a>
-              </li>
-              <li className="fr-footer__content-item">
-                <a
-                  className="fr-footer__content-link"
-                  href="https://gouvernement.fr"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  gouvernement.fr
-                </a>
-              </li>
-              <li className="fr-footer__content-item">
-                <a
-                  className="fr-footer__content-link"
-                  href="https://service-public.fr"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  service-public.fr
-                </a>
-              </li>
-              <li className="fr-footer__content-item">
-                <a
-                  className="fr-footer__content-link"
-                  href="https://data.gouv.fr"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  data.gouv.fr
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
         <div className="fr-footer__bottom">
