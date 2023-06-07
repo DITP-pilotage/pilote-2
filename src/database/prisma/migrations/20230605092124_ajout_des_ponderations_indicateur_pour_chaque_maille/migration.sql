c54f3197312f32a2b4e7b7d22cd78d860c803d30
Ludwig Chieng <ludwig.chieng@octo.com>
2023-06-07 14:01:24 +0200

    feat: ajoute les colonnes ponderation dans la table indicateur (302)

---
+++ src/database/prisma/migrations/20230605092124_ajout_des_ponderations_indicateur_pour_chaque_maille/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "public"."indicateur" ADD COLUMN     "ponderation_dept" DOUBLE PRECISION,
+ADD COLUMN     "ponderation_nat" DOUBLE PRECISION,
+ADD COLUMN     "ponderation_reg" DOUBLE PRECISION;
