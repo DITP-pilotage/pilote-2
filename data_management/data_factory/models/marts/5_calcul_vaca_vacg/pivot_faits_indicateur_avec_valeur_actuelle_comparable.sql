83a5f7145e85f37fa1a2d2a2d9f34a491e618052
louisehbrt <louise.hubert@octo.com>
2023-06-13 10:53:34 +0200

    fix: corrige le from previous month et le paramétrage (419)

--- data_management/data_factory/models/marts/5_calcul_vaca_vacg/pivot_faits_indicateur_avec_valeur_actuelle_comparable.sql
+++ data_management/data_factory/models/marts/5_calcul_vaca_vacg/pivot_faits_indicateur_avec_valeur_actuelle_comparable.sql
@@ -1,3 +1,29 @@
+WITH vac_from_previous_month_3 as (
+SELECT indicateur_id,
+zone_id,
+date_releve,
+AVG(valeur_actuelle_decumulee)
+    OVER (
+        PARTITION BY indicateur_id, zone_id
+        ORDER BY date_releve
+        RANGE BETWEEN INTERVAL '70 days' PRECEDING AND CURRENT ROW
+    )
+    FROM {{ ref('pivot_faits_indicateur_avec_valeur_actuelle_decumulee')}}
+),
+
+vac_from_previous_month_6 as (
+SELECT indicateur_id,
+zone_id,
+date_releve,
+AVG(valeur_actuelle_decumulee)
+    OVER (
+        PARTITION BY indicateur_id, zone_id
+        ORDER BY date_releve
+        RANGE BETWEEN INTERVAL '180 days' PRECEDING AND CURRENT ROW
+    )
+    FROM {{ ref('pivot_faits_indicateur_avec_valeur_actuelle_decumulee')}}
+)
+
 SELECT
     pivot.indicateur_id,
     pivot.zone_id,
@@ -27,19 +53,14 @@ SELECT
                     AND date_trunc('year', pivot_pour_cumuler_valeur_actuelle.date_releve) = date_trunc('year', pivot.date_releve)
                     AND pivot_pour_cumuler_valeur_actuelle.date_releve <= pivot.date_releve
             )
-        WHEN parametrage.partitionne_vaca_par = 'from_previous_month' AND parametrage.vaca_operation = 'avg' THEN
+        WHEN parametrage.partitionne_vaca_par = 'from_previous_month' AND parametrage.partitionne_vaca_depuis = '1000-01-03' AND parametrage.vaca_operation = 'avg' THEN
         (
-            SELECT AVG(pivot_pour_cumuler_valeur_actuelle.valeur_actuelle_decumulee)
-                OVER (
-                    PARTITION BY pivot_pour_cumuler_valeur_actuelle.indicateur_id, pivot_pour_cumuler_valeur_actuelle.zone_id
-                    ORDER BY pivot_pour_cumuler_valeur_actuelle.date_releve
-                    RANGE BETWEEN INTERVAL '70 days' PRECEDING AND CURRENT ROW
-                )
-            FROM {{ ref('pivot_faits_indicateur_avec_valeur_actuelle_decumulee')}} pivot_pour_cumuler_valeur_actuelle
-            WHERE pivot_pour_cumuler_valeur_actuelle.indicateur_id = pivot.indicateur_id
-                AND pivot_pour_cumuler_valeur_actuelle.zone_id = pivot.zone_id
-                AND pivot_pour_cumuler_valeur_actuelle.date_releve = pivot.date_releve
-                AND pivot_pour_cumuler_valeur_actuelle.valeur_actuelle_decumulee IS NOT NULL
+            SELECT vac_from_previous_month_3.avg
+            FROM vac_from_previous_month_3
+            WHERE vac_from_previous_month_3.indicateur_id = pivot.indicateur_id
+                AND vac_from_previous_month_3.zone_id = pivot.zone_id
+                AND vac_from_previous_month_3.date_releve = pivot.date_releve
+                AND vac_from_previous_month_3.avg IS NOT NULL
         )
         ELSE valeur_actuelle_decumulee -- todo: mettre à nulle cette valeur afin de ne pas calculer de taux d'avancement annuel (et donc utiliser celui de dfakto pour le moment)
     END AS valeur_actuelle_comparable_annuelle,
@@ -64,6 +85,15 @@ SELECT
                     AND pivot_pour_cumuler_valeur_actuelle.valeur_actuelle_decumulee IS NOT NULL
                     AND pivot_pour_cumuler_valeur_actuelle.date_releve <= pivot.date_releve
                     AND pivot_pour_cumuler_valeur_actuelle.date_releve >= parametrage.partitionne_vacg_depuis
+            )
+        WHEN parametrage.partitionne_vacg_par = 'from_previous_month' AND parametrage.partitionne_vacg_depuis = '1000-01-06' AND parametrage.vacg_operation = 'avg' THEN
+            (
+                SELECT vac_from_previous_month_6.avg
+                FROM vac_from_previous_month_6
+                WHERE vac_from_previous_month_6.indicateur_id = pivot.indicateur_id
+                    AND vac_from_previous_month_6.zone_id = pivot.zone_id
+                    AND vac_from_previous_month_6.date_releve = pivot.date_releve
+                    AND vac_from_previous_month_6.avg IS NOT NULL
             )
         ELSE valeur_actuelle_decumulee -- todo: mettre à nulle cette valeur afin de ne pas calculer de taux d'avancement global (et donc utiliser celui de dfakto pour le moment)
     END AS valeur_actuelle_comparable_globale,
