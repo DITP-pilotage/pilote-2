8a4a6d76cbfefb8840ce9f7b6e0eb7938eb16bbe
louisehbrt <louise.hubert@octo.com>
2023-06-16 14:43:48 +0200

    fix: ajoute tous les cas pour previous month pour vaca et vacg (419)

--- data_management/data_factory/models/marts/5_calcul_vaca_vacg/pivot_faits_indicateur_avec_valeur_actuelle_comparable.sql
+++ data_management/data_factory/models/marts/5_calcul_vaca_vacg/pivot_faits_indicateur_avec_valeur_actuelle_comparable.sql
@@ -1,25 +1,43 @@
-vac_from_previous_month as (
+WITH vac_from_previous_month as (
 SELECT pivot.indicateur_id,
     pivot.zone_id,
     pivot.date_releve,
-    CASE when partitionne_vacg_par like 'from_previous_month' and vacg_operation = 'avg' then
+    CASE when partitionne_vaca_par like 'from_previous_month' and vaca_operation = 'avg' then
+            CASE
+               WHEN partitionne_vaca_nombre_de_mois = 3 THEN AVG(pivot.valeur_actuelle_decumulee) over w3
+               WHEN partitionne_vaca_nombre_de_mois = 6 THEN AVG(pivot.valeur_actuelle_decumulee) over w6
+               WHEN partitionne_vaca_nombre_de_mois = 12 THEN AVG(pivot.valeur_actuelle_decumulee) over w12
+               WHEN partitionne_vaca_nombre_de_mois = 48 THEN AVG(pivot.valeur_actuelle_decumulee) over w48
+             ELSE null
+            END
+        END as vaca_moyenne,
+    CASE when partitionne_vaca_par like 'from_previous_month' and vaca_operation = 'sum' then
         CASE
-           WHEN partitionne_vacg_nombre_de_mois = '3' THEN AVG(pivot.valeur_actuelle_decumulee) over w3
-           WHEN partitionne_vacg_nombre_de_mois = '6' THEN AVG(pivot.valeur_actuelle_decumulee) over w6
-           WHEN partitionne_vacg_nombre_de_mois = '12' THEN AVG(pivot.valeur_actuelle_decumulee) over w12
-           WHEN partitionne_vacg_nombre_de_mois = '48' THEN AVG(pivot.valeur_actuelle_decumulee) over w48
-         ELSE null
-        END
-    END as moyenne,
-    CASE when partitionne_vacg_par like 'from_previous_month' and vacg_operation = 'sum' then
-        CASE
-           WHEN partitionne_vacg_nombre_de_mois = '3' THEN SUM(pivot.valeur_actuelle_decumulee) over w3
-           WHEN partitionne_vacg_nombre_de_mois = '6' THEN SUM(pivot.valeur_actuelle_decumulee) over w6
-           WHEN partitionne_vacg_nombre_de_mois = '12' THEN SUM(pivot.valeur_actuelle_decumulee) over w12
-           WHEN partitionne_vacg_nombre_de_mois = '48' THEN SUM(pivot.valeur_actuelle_decumulee) over w48
+           WHEN partitionne_vaca_nombre_de_mois = 3 THEN SUM(pivot.valeur_actuelle_decumulee) over w3
+           WHEN partitionne_vaca_nombre_de_mois = 6 THEN SUM(pivot.valeur_actuelle_decumulee) over w6
+           WHEN partitionne_vaca_nombre_de_mois = 12 THEN SUM(pivot.valeur_actuelle_decumulee) over w12
+           WHEN partitionne_vaca_nombre_de_mois = 48 THEN SUM(pivot.valeur_actuelle_decumulee) over w48
         ELSE null
         END
-    END as somme
+    END as vaca_somme,
+    CASE when partitionne_vacg_par like 'from_previous_month' and vacg_operation = 'avg' then
+        CASE
+           WHEN partitionne_vacg_nombre_de_mois = 3 THEN AVG(pivot.valeur_actuelle_decumulee) over w3
+           WHEN partitionne_vacg_nombre_de_mois = 6 THEN AVG(pivot.valeur_actuelle_decumulee) over w6
+           WHEN partitionne_vacg_nombre_de_mois = 12 THEN AVG(pivot.valeur_actuelle_decumulee) over w12
+           WHEN partitionne_vacg_nombre_de_mois = 48 THEN AVG(pivot.valeur_actuelle_decumulee) over w48
+         ELSE null
+        END
+    END as vacg_moyenne,
+    CASE when partitionne_vacg_par like 'from_previous_month' and vacg_operation = 'sum' then
+        CASE
+           WHEN partitionne_vacg_nombre_de_mois = 3 THEN SUM(pivot.valeur_actuelle_decumulee) over w3
+           WHEN partitionne_vacg_nombre_de_mois = 6 THEN SUM(pivot.valeur_actuelle_decumulee) over w6
+           WHEN partitionne_vacg_nombre_de_mois = 12 THEN SUM(pivot.valeur_actuelle_decumulee) over w12
+           WHEN partitionne_vacg_nombre_de_mois = 48 THEN SUM(pivot.valeur_actuelle_decumulee) over w48
+        ELSE null
+        END
+    END as vacg_somme
     FROM {{ ref('pivot_faits_indicateur_avec_valeur_actuelle_decumulee')}} pivot
     LEFT JOIN {{ ref('stg_ppg_metadata__parametrage_indicateurs')}} parametrage ON pivot.indicateur_id = parametrage.indicateur_id
     WINDOW
@@ -58,23 +76,23 @@ SELECT
                     AND date_trunc('year', pivot_pour_cumuler_valeur_actuelle.date_releve) = date_trunc('year', pivot.date_releve)
                     AND pivot_pour_cumuler_valeur_actuelle.date_releve <= pivot.date_releve
             )
-        WHEN parametrage.partitionne_vaca_par = 'from_previous_month' and parametrage.vaca_operation = 'avg'THEN
+        WHEN parametrage.partitionne_vaca_par = 'from_previous_month' and parametrage.vaca_operation = 'avg' THEN
             (
-                SELECT vac_from_previous_month.moyenne
+                SELECT vac_from_previous_month.vaca_moyenne
                 FROM vac_from_previous_month
                 WHERE vac_from_previous_month.indicateur_id = pivot.indicateur_id
                     AND vac_from_previous_month.zone_id = pivot.zone_id
                     AND vac_from_previous_month.date_releve = pivot.date_releve
-                    AND vac_from_previous_month.moyenne IS NOT NULL
+                    AND vac_from_previous_month.vaca_moyenne IS NOT NULL
             )
-        WHEN parametrage.partitionne_vaca_par = 'from_previous_month' and parametrage.vac_operation = 'sum' THEN
+        WHEN parametrage.partitionne_vaca_par = 'from_previous_month' and parametrage.vaca_operation = 'sum' THEN
             (
-                SELECT vac_from_previous_month_3.somme
-                FROM vac_from_previous_month_3
-                WHERE vac_from_previous_month_3.indicateur_id = pivot.indicateur_id
-                    AND vac_from_previous_month_3.zone_id = pivot.zone_id
-                    AND vac_from_previous_month_3.date_releve = pivot.date_releve
-                    AND vac_from_previous_month_3.somme IS NOT NULL
+                SELECT vac_from_previous_month.vaca_somme
+                FROM vac_from_previous_month
+                WHERE vac_from_previous_month.indicateur_id = pivot.indicateur_id
+                    AND vac_from_previous_month.zone_id = pivot.zone_id
+                    AND vac_from_previous_month.date_releve = pivot.date_releve
+                    AND vac_from_previous_month.vaca_somme IS NOT NULL
             )
         ELSE valeur_actuelle_decumulee -- todo: mettre à nulle cette valeur afin de ne pas calculer de taux d'avancement annuel (et donc utiliser celui de dfakto pour le moment)
     END AS valeur_actuelle_comparable_annuelle,
@@ -99,42 +117,24 @@ SELECT
                     AND pivot_pour_cumuler_valeur_actuelle.valeur_actuelle_decumulee IS NOT NULL
                     AND pivot_pour_cumuler_valeur_actuelle.date_releve <= pivot.date_releve
                     AND pivot_pour_cumuler_valeur_actuelle.date_releve >= parametrage.partitionne_vacg_depuis
-            )-- todo la suite
-        WHEN parametrage.partitionne_vacg_par = 'from_previous_month' AND parametrage.partitionne_vacg_nombre_de_mois = 3 AND parametrage.vacg_operation = 'avg' THEN
-            (
-                SELECT vac_from_previous_month_3.moyenne
-                FROM vac_from_previous_month_3
-                WHERE vac_from_previous_month_3.indicateur_id = pivot.indicateur_id
-                    AND vac_from_previous_month_3.zone_id = pivot.zone_id
-                    AND vac_from_previous_month_3.date_releve = pivot.date_releve
-                    AND vac_from_previous_month_3.moyenne IS NOT NULL
             )
-        WHEN parametrage.partitionne_vacg_par = 'from_previous_month' AND parametrage.partitionne_vacg_nombre_de_mois = 3 AND parametrage.vacg_operation = 'sum' THEN
+        WHEN parametrage.partitionne_vacg_par = 'from_previous_month' and parametrage.vacg_operation = 'avg' THEN
             (
-                SELECT vac_from_previous_month_3.somme
-                FROM vac_from_previous_month_3
-                WHERE vac_from_previous_month_3.indicateur_id = pivot.indicateur_id
-                    AND vac_from_previous_month_3.zone_id = pivot.zone_id
-                    AND vac_from_previous_month_3.date_releve = pivot.date_releve
-                    AND vac_from_previous_month_3.somme IS NOT NULL
+                SELECT vac_from_previous_month.vacg_moyenne
+                FROM vac_from_previous_month
+                WHERE vac_from_previous_month.indicateur_id = pivot.indicateur_id
+                    AND vac_from_previous_month.zone_id = pivot.zone_id
+                    AND vac_from_previous_month.date_releve = pivot.date_releve
+                    AND vac_from_previous_month.vacg_moyenne IS NOT NULL
             )
-        WHEN parametrage.partitionne_vacg_par = 'from_previous_month' AND parametrage.partitionne_vacg_nombre_de_mois = 6 AND parametrage.vacg_operation = 'avg' THEN
+        WHEN parametrage.partitionne_vacg_par = 'from_previous_month' and parametrage.vacg_operation = 'sum' THEN
             (
-                SELECT vac_from_previous_month_6.moyenne
-                FROM vac_from_previous_month_6
-                WHERE vac_from_previous_month_6.indicateur_id = pivot.indicateur_id
-                    AND vac_from_previous_month_6.zone_id = pivot.zone_id
-                    AND vac_from_previous_month_6.date_releve = pivot.date_releve
-                    AND vac_from_previous_month_6.moyenne IS NOT NULL
-            )
-        WHEN parametrage.partitionne_vacg_par = 'from_previous_month' AND parametrage.partitionne_vacg_nombre_de_mois = 6 AND parametrage.vacg_operation = 'sum' THEN
-            (
-                SELECT vac_from_previous_month_6.somme
-                FROM vac_from_previous_month_6
-                WHERE vac_from_previous_month_6.indicateur_id = pivot.indicateur_id
-                    AND vac_from_previous_month_6.zone_id = pivot.zone_id
-                    AND vac_from_previous_month_6.date_releve = pivot.date_releve
-                    AND vac_from_previous_month_6.somme IS NOT NULL
+                SELECT vac_from_previous_month.vacg_somme
+                FROM vac_from_previous_month
+                WHERE vac_from_previous_month.indicateur_id = pivot.indicateur_id
+                    AND vac_from_previous_month.zone_id = pivot.zone_id
+                    AND vac_from_previous_month.date_releve = pivot.date_releve
+                    AND vac_from_previous_month.vacg_somme IS NOT NULL
             )
         ELSE valeur_actuelle_decumulee -- todo: mettre à nulle cette valeur afin de ne pas calculer de taux d'avancement global (et donc utiliser celui de dfakto pour le moment)
     END AS valeur_actuelle_comparable_globale,
