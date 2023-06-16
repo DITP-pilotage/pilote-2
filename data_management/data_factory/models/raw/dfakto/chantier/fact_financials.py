4caf03b702b989a8bd63546294fef31dde87d653
fabien.roussel <fabien.roussel921@gmail.com>
2023-06-16 10:25:44 +0200

    chore: pour ameliorer les perfs, on utilise le fact_financials_lite.csv (000)

--- data_management/data_factory/models/raw/dfakto/chantier/fact_financials.py
+++ data_management/data_factory/models/raw/dfakto/chantier/fact_financials.py
@@ -5,7 +5,7 @@ import os
 def model(dbt, session):
     dump_dfakto_rp_raw = os.getenv('DUMP_DFAKTO_TEMP')
     fact_financials = pd.read_csv(
-        f'{dump_dfakto_rp_raw}/fact_financials.csv',
+        f'{dump_dfakto_rp_raw}/fact_financials_lite.csv',
         sep=';',
     )
 
