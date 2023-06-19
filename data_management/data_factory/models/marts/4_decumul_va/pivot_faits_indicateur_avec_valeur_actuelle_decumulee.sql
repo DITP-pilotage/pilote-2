7b8e849c041e457cee4e0d412856d2e05860d9f2
louisehbrt <louise.hubert@octo.com>
2023-06-19 11:48:07 +0200

    feat: implémente le param decumul from custom date (439)

--- data_management/data_factory/models/marts/4_decumul_va/pivot_faits_indicateur_avec_valeur_actuelle_decumulee.sql
+++ data_management/data_factory/models/marts/4_decumul_va/pivot_faits_indicateur_avec_valeur_actuelle_decumulee.sql
@@ -13,11 +13,24 @@ SELECT
                 ),
                 0
             )
+        WHEN parametrage.decumule_vaa_par = 'from_custom_date' THEN
+            pivot.valeur_actuelle - COALESCE(
+                LAG(pivot.valeur_actuelle) OVER (
+                    PARTITION BY pivot.indicateur_id, pivot.zone_id
+                    ORDER BY pivot.date_releve
+                ),
+                0
+            )
         ELSE pivot.valeur_actuelle
     END AS valeur_actuelle_decumulee,
     pivot.valeur_cible_annuelle,
     pivot.valeur_cible_globale
 FROM {{ ref('pivot_faits_indicateur_avec_vi_vca_et_vcg_completees')}} pivot
     LEFT JOIN {{ ref('stg_ppg_metadata__parametrage_indicateurs')}} parametrage ON pivot.indicateur_id = parametrage.indicateur_id
+    WHERE
+    (
+        parametrage.decumule_vaa_par = 'from_year_start' OR
+        (parametrage.decumule_vaa_par = 'from_custom_date' AND pivot.date_releve >= parametrage.decumule_vaa_depuis)
+    )
 ORDER BY
     pivot.indicateur_id, pivot.zone_id, pivot.date_releve
