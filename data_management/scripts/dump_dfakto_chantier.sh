cd1e3d9d2a11d5c5a8bca3b03039bce910810e5f
louisehbrt <louise.hubert@octo.com>
2023-06-20 17:21:10 +0200

    fix: corrige le script dump dfakto en local (000)

--- data_management/scripts/dump_dfakto_chantier.sh
+++ data_management/scripts/dump_dfakto_chantier.sh
@@ -1,7 +1,6 @@
 #!/bin/env bash
 
 set -o errexit   # abort on nonzero exitstatus
-set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 # Uniquement sur du local
