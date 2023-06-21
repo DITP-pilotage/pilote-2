a8228802c8bffe1ba786ab34467559d5d7fe1a40
fabien.roussel <fabien.roussel921@gmail.com>
2023-06-21 15:52:52 +0200

    fix: ajout du fichier mapping perimetre projet struct dans ppg_metatadata (448)

--- data_management/data_factory/models/exposition/perimetre_projet_structurant.sql
+++ data_management/data_factory/models/exposition/perimetre_projet_structurant.sql
@@ -3,8 +3,9 @@ SELECT
         ['perimetres_ppg_id',
         'perimetre_ps_nom']
     ) }} as id,
-    perimetres_ppg_nom,
+    perimetres.nom as perimetres_ppg_nom,
     perimetres_ppg_id,
     perimetre_ps_nom 
 FROM {{ ref('stg_ppg_metadata__perimetres_ps') }}
+    LEFT JOIN {{ ref('stg_ppg_metadata__perimetres') }} perimetres
 ORDER BY perimetres_ppg_id
