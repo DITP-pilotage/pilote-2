f5b32832538bad9a2f1c75ae1fc2257d8e738107
fabien.roussel <fabien.roussel921@gmail.com>
2023-06-21 15:47:02 +0200

    fix: ajout du fichier mapping perimetre projet struct dans ppg_metatadata (448)

--- data_management/data_factory/models/raw/ppg_metadata/metadata_perimetres_projet_structurant.py
+++ data_management/data_factory/models/raw/ppg_metadata/metadata_perimetres_projet_structurant.py
@@ -3,9 +3,9 @@ import os
 
 
 def model(dbt, session):
-    ppg_metadata_views = os.getenv('PRIVATE_DATA')
+    ppg_metadata_views = os.getenv('PPG_METADATA_VIEWS')
     perimetres_ps = pd.read_csv(
-        f'{ppg_metadata_views}/mapping_perimetres_projets_structurants_ppg.csv',
+        f'{ppg_metadata_views}/perimetre/mapping_per_pst.csv',
         sep=';'
     )
 
