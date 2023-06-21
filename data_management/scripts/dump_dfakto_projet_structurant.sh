b4ef9e978b7ad8521742d2bd67cf8641cb02b648
louisehbrt <louise.hubert@octo.com>
2023-06-21 09:58:09 +0200

    feat: automatise le chargement du dump projet structurant (442)

--- data_management/scripts/dump_dfakto_projet_structurant.sh
+++ data_management/scripts/dump_dfakto_projet_structurant.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
+set -o errexit   # abort on nonzero exitstatus
+set -o pipefail  # don't hide errors within pipes
+
 # Uniquement sur du local
 if [[ -z $SSH_KEY_INGEST_DATA_DFAKTO ]] || [[ -z $URL_INGEST_DFAKTO ]] || [[ -z $PGHOST ]] || [[ -z $PGPORT ]] || [[ -z $PGUSER ]] || [[ -z $PGPASSWORD ]] || [[ -z $PGDATABASE ]];
 then
@@ -12,23 +15,27 @@ then
   fi
 fi
 
-mkdir -p input_data/temp
+TEMP_DIR=input_data/temp
+
+mkdir -p $TEMP_DIR
 mkdir -p ~/.ssh
 
 echo "$SSH_KEY_INGEST_DATA_DFAKTO" > input_data/temp/id_ed25519
 chmod 600 input_data/temp/id_ed25519
 
 cmd_retrieve_files=$(cat <<EOF
-get fact_progress_kpis.csv input_data/temp/
-get fact_progress_project.csv input_data/temp/
-get dim_tree_nodes.csv input_data/temp/dim_tree_nodes_ps.csv
-get ps_view_data_kpis.csv input_data/temp/
-get ps_view_data_financials.csv input_data/temp/
+get fact_progress_kpis.csv $TEMP_DIR
+get fact_progress_project.csv $TEMP_DIR
+get dim_tree_nodes.csv $TEMP_DIR/dim_tree_nodes_ps.csv
+get ps_view_data_kpis.csv $TEMP_DIR
+get ps_view_data_financials.csv $TEMP_DIR
 EOF
 )
 
 ssh-keyscan -p 2022 -H $URL_INGEST_DFAKTO >> ~/.ssh/known_hosts
 
-echo -e "$cmd_retrieve_files" | sftp -oPort=2022 -i input_data/temp/id_ed25519 "$USER_INGEST_PROJET_DFAKTO@$URL_INGEST_DFAKTO"
+echo -e "$cmd_retrieve_files" | sftp -oPort=2022 -i $TEMP_DIR/id_ed25519 "$USER_INGEST_PROJET_DFAKTO@$URL_INGEST_DFAKTO"
 
-dbt run --project-dir data_factory/ --select raw.dfakto.projet_structurant
+PROJECT_DIR=data_factory
+dbt deps --project-dir $PROJECT_DIR
+dbt run --project-dir $PROJECT_DIR --select raw.dfakto.projet_structurant
