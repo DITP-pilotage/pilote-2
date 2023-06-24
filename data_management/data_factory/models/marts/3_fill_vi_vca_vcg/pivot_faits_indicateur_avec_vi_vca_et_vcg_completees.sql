d5917919b09bedd8b65eb19bbca1fded9c92555d
fabien.roussel <fabien.roussel921@gmail.com>
2023-06-24 18:19:29 +0200

    refactor: suppression de la jointure non utilisée dans le pivot (444)

--- data_management/data_factory/models/marts/3_fill_vi_vca_vcg/pivot_faits_indicateur_avec_vi_vca_et_vcg_completees.sql
+++ data_management/data_factory/models/marts/3_fill_vi_vca_vcg/pivot_faits_indicateur_avec_vi_vca_et_vcg_completees.sql
@@ -25,15 +25,17 @@ SELECT
     )) AS valeur_initiale,
     pivot.valeur_actuelle,
     COALESCE(valeur_cible,
-                (SELECT valeur_cible
-                 FROM marts.pivot_faits_indicateur_toutes_mailles AS pivot_premiere_prochaine_val_cible
-                 WHERE pivot_premiere_prochaine_val_cible.indicateur_id = pivot.indicateur_id
-                   AND pivot_premiere_prochaine_val_cible.zone_id = pivot.zone_id
-                   AND pivot_premiere_prochaine_val_cible.date_releve > pivot.date_releve
-                   AND pivot_premiere_prochaine_val_cible.valeur_cible IS NOT NULL
-                 ORDER BY pivot_premiere_prochaine_val_cible.date_releve
-                 LIMIT 1)
-               ) as valeur_cible_annuelle,
+        (
+            SELECT valeur_cible
+            FROM {{ ref('pivot_faits_indicateur_toutes_mailles')}}  pivot_pour_premiere_valeur_cible_non_nulle
+            WHERE pivot_pour_premiere_valeur_cible_non_nulle.indicateur_id = pivot.indicateur_id
+                AND pivot_pour_premiere_valeur_cible_non_nulle.zone_id = pivot.zone_id
+                AND pivot_pour_premiere_valeur_cible_non_nulle.date_releve > pivot.date_releve
+                AND pivot_pour_premiere_valeur_cible_non_nulle.valeur_cible IS NOT NULL
+            ORDER BY pivot_pour_premiere_valeur_cible_non_nulle.date_releve
+            LIMIT 1
+        )
+    ) as valeur_cible_annuelle,
     (
         SELECT pivot_pour_derniere_valeur_cible_non_nulle.valeur_cible
         FROM {{ ref('pivot_faits_indicateur_toutes_mailles')}} pivot_pour_derniere_valeur_cible_non_nulle
@@ -44,9 +46,4 @@ SELECT
         LIMIT 1
     ) AS valeur_cible_globale
 FROM {{ ref('pivot_faits_indicateur_toutes_mailles')}} pivot
-LEFT JOIN valeurs_cibles_annuelles
-    ON pivot.indicateur_id = valeurs_cibles_annuelles.indicateur_id
-        AND pivot.zone_id = valeurs_cibles_annuelles.zone_id
-        AND date_trunc('year', pivot.date_releve) = valeurs_cibles_annuelles.annee_releve
-        AND valeurs_cibles_annuelles.row_id = 1
 ORDER BY pivot.indicateur_id, pivot.zone_id, pivot.date_releve
