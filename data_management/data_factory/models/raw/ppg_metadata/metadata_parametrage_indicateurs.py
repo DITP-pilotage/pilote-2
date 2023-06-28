e56dcaee9008e1b06a0d64095e2c79a82c6ad746
fabien.roussel <fabien.roussel921@gmail.com>
2023-06-28 10:47:34 +0200

    refactor: suppression de tables metrics et de leur configuration dbt (457)

--- data_management/data_factory/models/raw/ppg_metadata/metadata_parametrage_indicateurs.py
+++ data_management/data_factory/models/raw/ppg_metadata/metadata_parametrage_indicateurs.py
@@ -3,7 +3,7 @@ import os
 
 
 def model(dbt, session):
-    ppg_metadata_views = os.getenv('PPG_METADATA_CONFIG_CALCUL')
-    parametrage_indicateurs = pd.read_csv(f'{ppg_metadata_views}/calculs_params-sample.csv')
+    config_calcul = os.getenv('PPG_METADATA_CONFIG_CALCUL')
+    parametrage_indicateurs = pd.read_csv(f'{config_calcul}')
 
     return parametrage_indicateurs
