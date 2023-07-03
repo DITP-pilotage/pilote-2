4d66bdda0287c6ea4bbc1c20ffcd75ebaf480928
fabien.roussel <fabien.roussel921@gmail.com>
2023-07-03 09:51:44 +0200

    recactor: suppression jointure paramétrage pour les indicateurs (000)

--- data_management/data_factory/models/exposition/indicateur.sql
+++ data_management/data_factory/models/exposition/indicateur.sql
@@ -21,9 +21,9 @@ SELECT m_indicateurs.id,
     m_indicateurs.source,
     m_indicateurs.mode_de_calcul,
     CONCAT(chantiers_ayant_des_indicateurs.maille, '-', chantiers_ayant_des_indicateurs.code_insee) as territoire_code,
-    parametrage_indicateurs.poids_pourcent_dept as ponderation_dept,
-    parametrage_indicateurs.poids_pourcent_nat as ponderation_nat,
-    parametrage_indicateurs.poids_pourcent_reg as ponderation_reg
+    null as ponderation_dept,
+    null as ponderation_nat,
+    null as ponderation_reg
 FROM {{ ref('stg_ppg_metadata__indicateurs') }} m_indicateurs
 	JOIN {{ ref('int_chantiers_with_mailles_and_territoires') }} chantiers_ayant_des_indicateurs ON m_indicateurs.chantier_id = chantiers_ayant_des_indicateurs.id
 	LEFT JOIN {{ ref('taux_avancement_indicateur') }} ON m_indicateurs.id = taux_avancement_indicateur.indicateur_id
@@ -37,5 +37,4 @@ FROM {{ ref('stg_ppg_metadata__indicateurs') }} m_indicateurs
 	    AND chantiers_ayant_des_indicateurs.id = d_indicateurs.code_chantier  -- TODO: a supprimer car temporaire pour bug dfakto avec ligne 43 du fichier int_dfakto_indicateurs_metrics.sql
 	    AND chantiers_ayant_des_indicateurs.zone_id = d_indicateurs.zone_code
 	    AND d_indicateurs.nom_structure IN ('Département', 'Région', 'Chantier')
-	LEFT JOIN {{ ref('stg_ppg_metadata__parametrage_indicateurs') }} parametrage_indicateurs ON parametrage_indicateurs.indicateur_id = m_indicateurs.id
 ORDER BY m_indicateurs.nom, chantiers_ayant_des_indicateurs.maille, chantiers_ayant_des_indicateurs.code_insee, d_indicateurs.date_valeur_actuelle DESC
