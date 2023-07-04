b095d774b056fc258d1684834973b3a15eb30ca6
Ludwig Chieng <ludwig.chieng@octo.com>
2023-07-04 14:39:55 +0200

    refactor: retire une prop inutile (388)

--- src/pages/admin/utilisateurs.tsx
+++ src/pages/admin/utilisateurs.tsx
@@ -1,17 +1,11 @@
 import { GetServerSidePropsContext } from 'next';
 import { getServerAuthSession } from '@/server/infrastructure/api/auth/[...nextauth]';
-import Utilisateur from '@/server/domain/utilisateur/Utilisateur.interface';
 import Habilitation from '@/server/domain/utilisateur/habilitation/Habilitation';
-import RécupérerListeUtilisateursUseCase from '@/server/usecase/utilisateur/RécupérerListeUtilisateursUseCase';
 import PageAdminUtilisateurs from '@/components/PageAdminUtilisateurs/PageAdminUtilisateurs';
 
-interface NextPageUtilisateursProps {
-  utilisateurs: Utilisateur[]
-}
-
-export default function NextPageUtilisateurs({ utilisateurs } : NextPageUtilisateursProps) {
+export default function NextPageUtilisateurs() {
   return (
-    <PageAdminUtilisateurs utilisateurs={utilisateurs} />
+    <PageAdminUtilisateurs />
   );
 }
 
@@ -33,11 +27,7 @@ export async function getServerSideProps({ req, res }: GetServerSidePropsContext
     return redirigerVersPageAccueil;
   }
 
-  const utilisateurs = await new RécupérerListeUtilisateursUseCase().run(session.habilitations);
-
   return {
-    props: {
-      utilisateurs,
-    },
+    props: {},
   };
 }
