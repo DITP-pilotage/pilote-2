e88a97cc2374b5d435f1c3479a389aa42529b297
Colin Dugeai <colin.dugeai@gmail.com>
2023-07-06 15:51:59 +0200

    move and rename script

--- data_management/serve_doc.sh
+++
@@ -1,6 +0,0 @@
-echo "-- [log] Chargement des variables d'env depuis le .env"
-source .env
-echo "-- [dbt.docs] Génération de la doc"
-pipenv run dbt docs generate --profiles-dir . --project-dir data_factory/ 
-echo "-- [dbt.docs] Lancement du serveur de doc"
-pipenv run dbt docs serve --profiles-dir . --project-dir data_factory/
