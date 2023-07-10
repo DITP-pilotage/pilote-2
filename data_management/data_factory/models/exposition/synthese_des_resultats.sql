f42c2707c2d7a2bbf62fbf27b42cda7e273d31b9
fabien.roussel <fabien.roussel921@gmail.com>
2023-07-10 09:56:09 +0200

    feat: ajout d'import massif pour la synthese des resultats (000)

---
+++ data_management/data_factory/models/exposition/synthese_des_resultats.sql
@@ -0,0 +1,17 @@
+SELECT
+    {{ dbt_utils.surrogate_key(
+                 ['chantier_id',
+                 'maille',
+                 'code_insee',
+                 'date'] --TODO date_meteo a ajouter lorsqu'elle sera présente dans l'import massif
+             ) }} as id,
+    chantier_id,
+    COALESCE(maille, 'NAT') as maille, --TODO supprimer le coalesce car la maille est sensé etre renseignée
+    COALESCE(code_insee, 'FR') as code_insee, --TODO supprimer le coalesce car le code_insee est sensé etre renseigné
+    auteur,
+    null as meteo, --TODO meteo a ajouter dans le fichier d'import action @ditp
+    null::date as date_meteo, --TODO date_meteo a ajouter dans le fichier d'import action @ditp
+    contenu as commentaire,
+    date as date_commentaire
+FROM {{ ref('stg_import_massif__commentaires') }}
+WHERE type='synthese_des_resultats'
