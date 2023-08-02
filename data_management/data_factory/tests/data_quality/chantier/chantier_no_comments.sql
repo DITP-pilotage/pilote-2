442cb84fa5099c57d15e293a7fc209f403dc34c2
Colin Dugeai <colin.dugeai@gmail.com>
2023-08-02 17:07:24 +0200

    refactor: config of tests are now in sql files
    not anymore in dbt_project.yml

--- data_management/data_factory/tests/data_quality/chantier/chantier_no_comments.sql
+++ data_management/data_factory/tests/data_quality/chantier/chantier_no_comments.sql
@@ -1,7 +1,14 @@
+{{ config(
+    enabled=true,
+    tags="data_quality",
+    severity = "warn",
+    store_failures = true,
+    limit = 100,
+) }}
 
 -- Comppte le nb de chantiers qui n'ont pas de commentaires du tout
 with
-types_comm as (select chantier_id , "type", count(*) from {{ref('commentaire')}}c group by chantier_id , "type" order by chantier_id),
+types_comm as (select chantier_id , "type", count(*) from {{ref('commentaire')}} c group by chantier_id , "type" order by chantier_id),
 all_ch as (select id as chantier_id from {{ref('chantier')}} group by id),
 cnt_distinct_comm_types as (
 	select chantier_id, count(*) as nb_distinct_coms_types from types_comm group by chantier_id)
