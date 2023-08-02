639cc945d4db66671999b153c607bc73007cb292
Colin Dugeai <colin.dugeai@gmail.com>
2023-08-02 16:39:12 +0200

    fix: fix tests config dbt_project.yml

--- data_management/data_factory/tests/exposition/chantier_no_comments.sql
+++ data_management/data_factory/tests/exposition/chantier_no_comments.sql
@@ -1,10 +1,8 @@
 
---select tree_node_id from {{ref('dim_tree_nodes')}} where tree_node_id = '2210412fb0db40a19f8b2cf8d39ea4e2'
-
 -- Comppte le nb de chantiers qui n'ont pas de commentaires du tout
 with
-types_comm as (select chantier_id , "type", count(*) from public.commentaire c group by chantier_id , "type" order by chantier_id),
-all_ch as (select id as chantier_id from chantier group by id),
+types_comm as (select chantier_id , "type", count(*) from {{ref('commentaire')}}c group by chantier_id , "type" order by chantier_id),
+all_ch as (select id as chantier_id from {{ref('chantier')}} group by id),
 cnt_distinct_comm_types as (
 	select chantier_id, count(*) as nb_distinct_coms_types from types_comm group by chantier_id)
 
