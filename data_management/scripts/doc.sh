567cf6d6b570a14c1fc77f798e1e12507a811a60
Colin Dugeai <colin.dugeai@gmail.com>
2023-09-06 14:15:04 +0200

    doc: create script doc
    Le script "doc.sh" permet de servir la doc dbt et de lancer prisma studio.

--- data_management/scripts/doc.sh
+++ data_management/scripts/doc.sh
@@ -1,6 +1,11 @@
+## dbt docs
 echo "-- [log] Chargement des variables d'env depuis le .env"
 source .env
 echo "-- [dbt.docs] Génération de la doc"
 pipenv run dbt docs generate --profiles-dir . --project-dir data_factory/ 
 echo "-- [dbt.docs] Lancement du serveur de doc"
-pipenv run dbt docs serve --profiles-dir . --project-dir data_factory/
+pipenv run dbt docs serve --profiles-dir . --project-dir data_factory/ &
+
+## Prisma studio
+echo "-- [prisma.studio] Lancement de prisma studio"
+npx prisma studio --schema ../src/database/prisma/schema.prisma
