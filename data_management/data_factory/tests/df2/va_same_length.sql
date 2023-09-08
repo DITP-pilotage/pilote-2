5698d6e067bf17228eb7481d8002fa09e64a7859
Colin Dugeai <colin.dugeai@gmail.com>
2023-09-08 11:52:42 +0200

    feat: test.create va_same_length + doc tests
    Ce test vérifie que l'on a autant de VA dans la table originale et la table df2

---
+++ data_management/data_factory/tests/df2/va_same_length.sql
@@ -0,0 +1,22 @@
+{{ config(
+    enabled=true,
+    tags="df2",
+    severity = "error",
+    store_failures = false,
+    limit = 100,
+) }}
+
+-- Vérifie que l'on a bien le même nombre de VA
+WITH original AS (
+    SELECT id, territoire_code, COALESCE(array_length(evolution_valeur_actuelle, 1),-1) as cnt_original FROM {{ ref('indicateur') }}
+),
+df2 as (
+    SELECT id, territoire_code, COALESCE(array_length(evolution_valeur_actuelle, 1), -1) as cnt_df2 FROM {{ ref('df2_indicateur') }}
+),
+same_length_t AS (
+    SELECT a.*, b.cnt_df2 , cnt_original=cnt_df2 AS va_same_length FROM original a FULL JOIN df2 b ON a.id=b.id AND a.territoire_code=b.territoire_code
+)
+
+SELECT * from same_length_t where va_same_length=false
+
+
