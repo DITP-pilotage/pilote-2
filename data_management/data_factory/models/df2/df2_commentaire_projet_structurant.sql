824482545fd705f9dd33dd8e4e62c01f968aaedb
Colin Dugeai <colin.dugeai@gmail.com>
2023-09-11 09:52:44 +0200

    delete pst df2
    Le fait de prendre ppg_metadata comme source pour les pst n'est pas dans le scope de cette branche

--- data_management/data_factory/models/df2/df2_commentaire_projet_structurant.sql
+++
@@ -1,4 +0,0 @@
-{{ config(tags="scope_pst") }}
-
--- TODO: implement
-SELECT * FROM {{ ref('commentaire_projet_structurant') }}
--- data_management/data_factory/models/df2/df2_projet_structurant_temporaire.sql
+++
@@ -1,4 +0,0 @@
-{{ config(tags="scope_pst") }}
-
--- TODO: implement
-SELECT * FROM {{ ref('projet_structurant_temporaire') }}
