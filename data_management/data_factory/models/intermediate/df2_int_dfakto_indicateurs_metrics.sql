7d00d907eca4e9705d1cf156c57962cd994420b2
Colin Dugeai <colin.dugeai@gmail.com>
2023-09-11 17:26:34 +0200

    df2_int_metrics: final fix
    build taux_avancement_indicateur_rank to get right row count at the end.
    
    public.df2_indicateur now has same count of rows as public.indicateur

--- data_management/data_factory/models/intermediate/df2_int_dfakto_indicateurs_metrics.sql
+++ data_management/data_factory/models/intermediate/df2_int_dfakto_indicateurs_metrics.sql
@@ -1,12 +1,29 @@
-SELECT 
+
+-- taux_avancement_indicateur_last_va: Keep 1 row per {indicateur_id, zone_id}: the one with latest date_releve
+WITH taux_avancement_indicateur_rank AS (
+    select 
     'ignored' AS tree_node_id, 
-    taux_avancement_globale_borne AS objectif_taux_avancement,
-    valeur_cible_globale AS objectif_valeur_cible,
+    taux_avancement_globale_borne,
+    valeur_cible_globale,
+    taux_avancement_annuel_borne,
+    valeur_cible_annuelle,
+    valeur_actuelle,
+    date_releve, valeur_initiale, zone_id, indicateur_id,
+    rank() over (partition by indicateur_id, zone_id order by date_releve) as r
+    FROM {{ ref('taux_avancement_indicateur') }}  a
+), 
+taux_avancement_indicateur_last_va as (select * from taux_avancement_indicateur_rank where r=1)
+
+
+SELECT 
+    tree_node_id, 
+    a.taux_avancement_globale_borne AS objectif_taux_avancement,
+    a.valeur_cible_globale AS objectif_valeur_cible,
+    a.taux_avancement_annuel_borne  AS objectif_taux_avancement_intermediaire,
+    a.valeur_cible_annuelle  AS objectif_valeur_cible_intermediaire,
     -- Dans cette colonne, on sélectionne la dernière VC saisie, peu importe l'année visée.
     d.valeur_cible AS objectif_valeur_cible_colin,
     d.date_releve AS objectif_date_valeur_cible,
-    taux_avancement_annuel_borne  AS objectif_taux_avancement_intermediaire,
-    valeur_cible_annuelle  AS objectif_valeur_cible_intermediaire,
     -- Dans cette colonne, on sélectionne la VC de l'année la plus tardive, et non la dernière VC saisie par un utilisateur.
     c.valeur_cible as objectif_valeur_cible_intermediaire_colin,
     c.date_releve  AS objectif_date_valeur_cible_intermediaire,
@@ -26,10 +43,10 @@ SELECT
     b.evolution_valeur_actuelle  AS evolution_valeur_actuelle,
     b.evolution_date_valeur_actuelle  AS evolution_date_valeur_actuelle,
     f.indic_parent_ch AS code_chantier 
-    FROM {{ ref('taux_avancement_indicateur') }}  a
+    FROM taux_avancement_indicateur_last_va a
 LEFT JOIN {{ ref('evolution_indicateur') }} b ON a.indicateur_id = b.indicateur_id AND a.zone_id = b.zone_id 
-left join {{ ref('last_vc_annuel') }} c on a.indicateur_id =c.indicateur_id and a.zone_id =c.zone_id and extract(year from a.date_releve)=c.yyear
-left join {{ ref('last_vc_global') }} d on a.indicateur_id =d.indicateur_id and a.zone_id =d.zone_id
-left join {{ ref('metadata_zones') }} e on a.zone_id =e.zone_id
-left join {{ ref('metadata_indicateurs') }} f on a.indicateur_id =f.indic_id 
-left join {{ ref('first_vi_global') }} g on a.indicateur_id =g.indicateur_id and a.zone_id =g.zone_id 
+LEFT JOIN {{ ref('last_vc_annuel') }} c ON a.indicateur_id =c.indicateur_id AND a.zone_id =c.zone_id AND extract(year from a.date_releve)=c.yyear
+LEFT JOIN {{ ref('last_vc_global') }} d ON a.indicateur_id =d.indicateur_id AND a.zone_id =d.zone_id
+LEFT JOIN {{ ref('metadata_zones') }} e ON a.zone_id =e.zone_id
+LEFT JOIN {{ ref('metadata_indicateurs') }} f ON a.indicateur_id =f.indic_id 
+LEFT JOIN {{ ref('first_vi_global') }} g ON a.indicateur_id =g.indicateur_id AND a.zone_id =g.zone_id 
