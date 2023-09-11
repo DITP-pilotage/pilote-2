22d29422f4e6c737e226bf2fd8bbc14f3c487ec1
Colin Dugeai <colin.dugeai@gmail.com>
2023-09-11 14:55:18 +0200

    df2_int_metrics: ajout des VC globales + test

--- data_management/data_factory/models/intermediate/df2_int_dfakto_indicateurs_metrics.sql
+++ data_management/data_factory/models/intermediate/df2_int_dfakto_indicateurs_metrics.sql
@@ -2,7 +2,9 @@ SELECT
     'ignored' AS tree_node_id, 
     taux_avancement_globale_borne AS objectif_taux_avancement,
     valeur_cible_globale AS objectif_valeur_cible,
-    'todo' AS objectif_date_valeur_cible,
+    -- Dans cette colonne, on sélectionne la dernière VC saisie, peu importe l'année visée.
+    d.valeur_cible AS objectif_valeur_cible_colin,
+    d.date_releve AS objectif_date_valeur_cible,
     taux_avancement_annuel_borne  AS objectif_taux_avancement_intermediaire,
     valeur_cible_annuelle  AS objectif_valeur_cible_intermediaire,
     -- Dans cette colonne, on sélectionne la VC de l'année la plus tardive, et non la dernière VC saisie par un utilisateur.
@@ -21,3 +23,4 @@ SELECT
     FROM {{ ref('taux_avancement_indicateur') }}  a
 LEFT JOIN {{ ref('evolution_indicateur') }} b ON a.indicateur_id = b.indicateur_id AND a.zone_id = b.zone_id 
 left join {{ ref('last_vc_annuel') }} c on a.indicateur_id =c.indicateur_id and a.zone_id =c.zone_id and extract(year from a.date_releve)=c.yyear
+left join {{ ref('last_vc_global') }} d on a.indicateur_id =d.indicateur_id and a.zone_id =d.zone_id
