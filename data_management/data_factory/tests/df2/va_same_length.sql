86bfde48faee85cd1f237496c77e4ff84b633651
Colin Dugeai <colin.dugeai@gmail.com>
2023-09-11 18:04:40 +0200

    test: va_same_length fix add results

--- data_management/data_factory/tests/df2/va_same_length.sql
+++ data_management/data_factory/tests/df2/va_same_length.sql
@@ -1,22 +1,21 @@
 {{ config(
     enabled=true,
-    tags=["df2", "scope_chantier"],
+    tags=["df2", "scope_chantier", "df2_validation"],
     severity = "error",
-    store_failures = false,
-    limit = 100,
+    store_failures = false
 ) }}
 
 -- Vérifie que l'on a bien le même nombre de VA
 WITH original AS (
-    SELECT id, territoire_code, COALESCE(array_length(evolution_valeur_actuelle, 1),-1) as cnt_original FROM {{ ref('indicateur') }}
+    SELECT evolution_valeur_actuelle as e1, id, territoire_code, COALESCE(array_length(evolution_valeur_actuelle, 1),-1) as cnt_original FROM public.indicateur i 
 ),
 df2 as (
-    SELECT id, territoire_code, COALESCE(array_length(evolution_valeur_actuelle, 1), -1) as cnt_df2 FROM {{ ref('df2_indicateur') }}
+    SELECT evolution_valeur_actuelle as e2, id, territoire_code, COALESCE(array_length(evolution_valeur_actuelle, 1), -1) as cnt_df2 from public.df2_indicateur
 ),
 same_length_t AS (
-    SELECT a.*, b.cnt_df2 , cnt_original=cnt_df2 AS va_same_length FROM original a FULL JOIN df2 b ON a.id=b.id AND a.territoire_code=b.territoire_code
+    SELECT e2, a.*, b.cnt_df2 , cnt_original=cnt_df2 AS va_same_length FROM original a FULL JOIN df2 b ON a.id=b.id AND a.territoire_code=b.territoire_code
 )
 
-SELECT * from same_length_t where va_same_length=false
+SELECT id from same_length_t where not va_same_length
 
 
