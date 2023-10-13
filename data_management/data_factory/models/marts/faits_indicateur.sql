35e6a057856a040dba879a06e4251507028476aa
Colin Dugeai <colin.dugeai@gmail.com>
2023-10-13 10:35:42 +0200

    fix: jointure avec mauvaise table
    cette jaointure duppliquait certaines lignes. On passe de 346K lignes à 254K

--- data_management/data_factory/models/marts/faits_indicateur.sql
+++ data_management/data_factory/models/marts/faits_indicateur.sql
@@ -13,5 +13,5 @@ SELECT
     meta_zone_parent.zone_type as zone_type_parent
 FROM {{ ref("stg_import_fichier__mesures_indicateurs") }}
 NATURAL JOIN {{ ref("stg_ppg_metadata__zones_unnest")}} as meta_zone
-LEFT JOIN {{ ref("stg_ppg_metadata__zones_unnest")}} as meta_zone_parent on meta_zone_parent.zone_id = meta_zone.zone_id_parent
+LEFT JOIN {{ ref("metadata_zones")}} as meta_zone_parent on meta_zone_parent.zone_id = meta_zone.zone_id_parent
 ORDER BY date_releve
