29c74e58aa559f3d221d6d11c7588590f08f9ca6
Colin Dugeai <colin.dugeai@gmail.com>
2023-10-13 15:24:46 +0200

    dbt: model.agg_reg ok
    cf doc

--- data_management/data_factory/models/df2/agg_reg.sql
+++ data_management/data_factory/models/df2/agg_reg.sql
@@ -15,14 +15,84 @@ mesure_last_params_reg as (
 ), 
 -- Valeurs REG saisies directement par l'utilisateur
 mesure_last_params_reg_user as (
-    select * from mesure_last_params_reg
+    select a.*, b.zone_type from mesure_last_params_reg a
+    left join raw_data.metadata_zones b on a.zone_id=b.zone_id
     where 
-        (metric_type='vi' and vi_reg_from='user_input') OR
+        ((metric_type='vi' and vi_reg_from='user_input') OR
         (metric_type='va' and va_reg_from='user_input') OR
-        (metric_type='vc' and va_reg_from='user_input')
+        (metric_type='vc' and va_reg_from='user_input')) and
+		zone_type='REG'
+),
+
+
+-- Liste des indicateurs qui ont un paramétrage d'aggrégation pour les valeurs REG
+indic_agg_from_dept as (
+	select b.indic_id, b.vi_reg_from , b.vi_reg_op, b.va_reg_from , b.va_reg_op, b.vc_reg_from , b.vc_reg_op 
+	from "postgres"."raw_data"."metadata_parametrage_indicateurs" b
+	where 
+		b.vi_reg_from not in ('_', 'user_input') OR
+		b.va_reg_from not in ('_', 'user_input') OR
+		b.vc_reg_from not in ('_', 'user_input')
+),
+
+-- On prend chaque mesure
+--  on lui associe sa zone parente
+--  on lui associe ses paramètres d'aggrégation
+--  et on sélectionne que les valeurs qui sont DEPT avec un parent REG
+mesure_last_params_reg_from_dept as (
+	select a.indic_id as indic_id1 , metric_date , metric_type , metric_value, a.zone_id,
+	b.zone_type, b.zone_parent , b.zone_parent_type ,
+	c.*
+	from "postgres"."df2"."mesure_select_last" a
+	inner join df2.zone_parent b on a.zone_id = b.zone_id 
+	right join indic_agg_from_dept c on a.indic_id =c.indic_id
+	where zone_type='DEPT' and zone_parent_type='REG'
+),
+
+-- Ici on calcule une aggrégation en somme ET en moyenne des DEPT vers REG
+--	La sélection de la valeur correcte se fera ensuite en fonction des paramètres
+compute_op_sum_avg as (
+	select 
+		zone_parent, indic_id as indic_id1, metric_date, metric_type, 
+		sum(metric_value::float) as op_sum,
+		avg(metric_value::float) as op_avg
+	from mesure_last_params_reg_from_dept
+	group by zone_parent, indic_id, metric_date, metric_type
+),
+
+-- On sélectionne le bon résultat de calcul de l'aggrégation 
+compute_op_selected as (
+	select b.*, a.*,
+	case 
+        WHEN metric_type='vi' AND vi_reg_op='sum' THEN op_sum
+        WHEN metric_type='vi' AND vi_reg_op='avg' THEN op_avg
+        WHEN metric_type='va' AND va_reg_op='sum' THEN op_sum
+        WHEN metric_type='va' AND va_reg_op='avg' THEN op_avg
+        WHEN metric_type='vc' AND vc_reg_op='sum' THEN op_sum
+        WHEN metric_type='vc' AND vc_reg_op='avg' THEN op_avg
+        -- Si opération non supportée, ie hors de [sum, avg]
+		else -1.212121
+	end as op_selected
+	from indic_agg_from_dept a
+	right join compute_op_sum_avg b on a.indic_id=b.indic_id1
+),
+
+-- On sélectionne qq colonne puis tri
+mesure_last_params_reg_aggregated as (
+    select 
+        indic_id,
+        zone_parent as zone_id,
+        metric_date, metric_type,
+        op_selected as metric_value
+    from compute_op_selected
+    order by indic_id, zone_id, metric_date, metric_type
 )
 
--- Valeurs REG aggrégées: TODO
 
--- On retourne donc simlement les valeurs DEPT saisies, et attendues comme tel + les valeurs agg
-select * from mesure_last_params_reg_user
+
+
+-- On retourne donc les valeurs REG saisies, et attendues comme tel
+select indic_id, zone_id, metric_date, metric_type, metric_value::float from mesure_last_params_reg_user
+union
+-- ET les valeurs agg
+select * from mesure_last_params_reg_aggregated
