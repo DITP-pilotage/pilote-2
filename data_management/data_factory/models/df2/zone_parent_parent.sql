82bd23b869f17a75f85a199532e5a2274270761a
Colin Dugeai <colin.dugeai@gmail.com>
2023-10-13 13:09:39 +0200

    dbt.model.add: zone_parent
    cf doc

--- data_management/data_factory/models/df2/zone_parent_parent.sql
+++ data_management/data_factory/models/df2/zone_parent_parent.sql
@@ -1,21 +1,11 @@
 -- Pour chaque zone, on récupère tous ses parents, et grand parents ainsi que leur type de zone
 --	Utile pour les aggrégations géographiques.
 
--- Unnest des parent
-with zones_unnest_parent as (
-    select a.zone_id , a.zone_type , unnest(string_to_array(a.zone_parent, ' | ')) as zone_parent 
-    from {{ ref('metadata_zones') }} a
-),
--- Ajout du type de zone des parents
-zones_parent_type as (
-    select a.*, b.zone_type  as zone_parent_type, b.zone_parent as zone_parent_parent 
-    from zones_unnest_parent a 
-    left join {{ ref('metadata_zones') }} b 
-    ON a.zone_parent = b.zone_id ),
+WITH
 -- Unnest des parents des parents
 zones_unnest_parent_parent as (
     select  zone_id, zone_type, zone_parent, zone_parent_type, unnest(string_to_array(zone_parent_parent, ' | ')) as zone_parent_parent  
-    from zones_parent_type 
+    from {{ ref('zone_parent') }} 
 ),
 -- Ajout du type de zone des parents parents
 zones_parent_parent_type as (
