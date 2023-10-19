692eb52ec8ce24c49a8a8e5f5231817ad4718eab
Colin Dugeai <colin.dugeai@gmail.com>
2023-10-19 19:10:16 +0200

    model.df3_indicateur replace ref

--- data_management/data_factory/models/df2/df3_indicateur.sql
+++ data_management/data_factory/models/df2/df3_indicateur.sql
@@ -14,7 +14,7 @@ list_indic_terr as (
 	t.code as territoire_code, 
 	t.zone_id
 	from public.territoire t 
-	cross join raw_data.metadata_indicateurs mi
+	cross join {{ ref('metadata_indicateurs') }} mi
 -- Pour prendre en compte le bool indic_hidden_pilote et ne pas retourner ces indicateurs:
 --	where not coalesce (mi.indic_hidden_pilote, false)
 ),
@@ -26,7 +26,7 @@ get_evol_va as (
 	indic_id, zone_id,
 	array_agg(metric_date)as evolution_date_valeur_actuelle,
 	array_agg(va) as evolution_valeur_actuelle
-	from df2.compute_ta_indic
+	from {{ ref('compute_ta_indic') }}
 	where va is not null
 	group by indic_id, zone_id
 ),
@@ -34,7 +34,7 @@ get_evol_va as (
 sort_mesures_va as (
 	select *,
 	rank() over (partition by indic_id, zone_id order by metric_date desc) as r
-	from df2.compute_ta_indic
+	from {{ ref('compute_ta_indic') }}
 	where va is not null),
 sort_mesures_va_last as (
 	select * from sort_mesures_va where r=1
@@ -75,16 +75,16 @@ sort_mesures_va_last as (
     -- todo
     null::bool as a_supprimer
 	from public.territoire t 
-	cross join raw_data.metadata_indicateurs mi
+	cross join {{ ref('metadata_indicateurs') }} mi
 	left join sort_mesures_va_last a on a.indic_id=mi.indic_id and a.zone_id=t.zone_id
 	-- donc la liste des terr X liste des indic vont ressortir ici.
 	-- list_indic_terr list_indic left join sort_mesures_va a on t.indic_id = list_indic.indic_id and t.zone_id = list_indic.zone_id
 	left join get_evol_va b on mi.indic_id=b.indic_id and t.zone_id=b.zone_id
 	--left join raw_data.metadata_indicateurs mi on mi1.indic_id = mi.indic_id 
-	left join raw_data.metadata_indicateur_types mit on mit.indic_type_id = mi.indic_type 
-	left join raw_data.metadata_parametrage_indicateurs mpi on mi.indic_id = mpi.indic_id 
+	left join {{ ref('metadata_indicateur_types') }} mit on mit.indic_type_id = mi.indic_type 
+	left join {{ ref('metadata_parametrage_indicateurs') }} mpi on mi.indic_id = mpi.indic_id 
 	left join public.territoire terr on t.zone_id = terr.zone_id 
-	LEFT JOIN marts.int_indicateurs_zones_applicables z_appl ON z_appl.indic_id = mi.indic_id AND z_appl.zone_id = t.zone_id
+	LEFT JOIN {{ ref('int_indicateurs_zones_applicables') }} z_appl ON z_appl.indic_id = mi.indic_id AND z_appl.zone_id = t.zone_id
 	right join list_indic_terr lit on mi.indic_id=lit.indic_id and terr.code=lit.territoire_code
 	--where a.r=1
 	order by mi.indic_id, terr.code
