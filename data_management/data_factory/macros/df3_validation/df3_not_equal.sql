fe23bb5694d01985646d5ddad6bb5886093d83fe
Colin Dugeai <colin.dugeai@gmail.com>
2023-10-19 17:37:53 +0200

    macros for tests

---
+++ data_management/data_factory/macros/df3_validation/df3_not_equal.sql
@@ -0,0 +1,33 @@
+{% macro df3_not_equal(colname, tolerance=-1)%}
+
+{{ config(
+    tags=["df3","df3_validation"],
+    severity = "error",
+    store_failures = false
+) }}
+
+
+with jjoin as (select 
+    coalesce(a.id, b.id) as id, 
+    coalesce(a.territoire_code, b.territoire_code) as territoire_code,
+    a.{{colname}} as computed_value,
+    b.{{colname}} as target_value,
+    case
+        when {{tolerance}}<0 then null
+		when a.{{colname}}=0 then null
+		when b.{{colname}}=0 then null
+		else abs(a.{{colname}}-b.{{colname}})/b.{{colname}}
+	end as ratio
+
+    from {{ ref('df3_indicateur') }} a
+    full join  {{ ref('indicateur') }} b
+    on a.territoire_code=b.territoire_code and a.id=b.id
+)
+
+select * from jjoin
+where 
+	(computed_value is not null and target_value is not null)
+    AND (computed_value <> target_value)
+    AND (ratio is null or ratio > 0.01)
+
+{% endmacro %}
---
+++ data_management/data_factory/macros/df3_validation/df3_should_notnull.sql
@@ -0,0 +1,24 @@
+{% macro df3_should_notnull(colname)%}
+
+{{ config(
+    tags=["df3","df3_validation"],
+    severity = "error",
+    store_failures = false
+) }}
+
+
+with jjoin as (select 
+    coalesce(a.id, b.id) as id, 
+    coalesce(a.territoire_code, b.territoire_code) as territoire_code,
+    a.{{colname}} as computed_value,
+    b.{{colname}} as target_value
+    from {{ ref('df3_indicateur') }} a
+    full join  {{ ref('indicateur') }} b
+    on a.territoire_code=b.territoire_code and a.id=b.id
+)
+
+select * from jjoin
+where 
+	(computed_value is null and target_value is not null) 
+
+{% endmacro %}
