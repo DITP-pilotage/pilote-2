1f5cc02470202a6f3d0f0f3c10d0b4bb35fb34b3
Colin Dugeai <colin.dugeai@gmail.com>
2023-10-19 17:38:32 +0200

    df3_validation: define tests using macros

---
+++ data_management/data_factory/tests/df3/va/df3_va_different.sql
@@ -0,0 +1,3 @@
+{{ config(enabled=true, tags=["scope_indicateur", "metric_va"]) }}
+
+{{ df3_not_equal('valeur_actuelle') }}
---
+++ data_management/data_factory/tests/df3/z_appl/df3_zappl_different.sql
@@ -0,0 +1,3 @@
+{{ config(enabled=true, tags=["scope_indicateur", "metric_zappl"]) }}
+
+{{ df3_not_equal_bool('est_applicable') }}
