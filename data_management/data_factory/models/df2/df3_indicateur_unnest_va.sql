dddce4e1df1a1721f3515aa43b1903dac4c809fc
Colin Dugeai <colin.dugeai@gmail.com>
2023-10-24 10:59:10 +0200

    refacto dir

--- data_management/data_factory/models/df2/df3_indicateur_unnest_va.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table') }}
-
-select 
-    id, territoire_code, 
-    unnest(evolution_valeur_actuelle) as va_unnest_computed, 
-    unnest(evolution_date_valeur_actuelle)::date as va_date_unnest_computed 
-from {{ ref('df3_indicateur') }}
