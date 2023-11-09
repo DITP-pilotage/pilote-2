511713bd4600a21753368dadb66cd76702058147
Colin Dugeai <colin.dugeai@gmail.com>
2023-11-09 23:05:18 +0100

    fix: jointure en 2 temps
    La jointure en 2 temps permet de ne pas créer de doublons de lignes

--- data_management/data_factory/models/df3/3_pivot/pivot_mesures.sql
+++ data_management/data_factory/models/df3/3_pivot/pivot_mesures.sql
@@ -16,15 +16,31 @@ pivot_vc as (
     select indic_id, zone_id, metric_date,
     metric_value as vc
     from {{ ref('agg_all') }} where metric_type ='vc'
-)
+),
 
--- Union des 3 tables pivotées
-select 
-    coalesce(a.indic_id, b.indic_id, c.indic_id) as indic_id,
-    coalesce(a.zone_id, b.zone_id, c.zone_id) as zone_id,
-    coalesce(a.metric_date, b.metric_date, c.metric_date) as metric_date,
-    a.vi, b.va, c.vc
+-- Jointure des 2 tables pivotées: vi X va => u_vi_va
+--  pour construire les colonnes vi et va
+u_vi_va as (select 
+    coalesce(a.indic_id, b.indic_id) as indic_id,
+    coalesce(a.zone_id, b.zone_id) as zone_id,
+    coalesce(a.metric_date, b.metric_date) as metric_date,
+    -- on garde la vi de table a, la va de table b
+    a.vi, b.va
 from pivot_vi a
     full join pivot_va b on a.indic_id=b.indic_id and a.zone_id=b.zone_id and a.metric_date=b.metric_date
+ORDER BY indic_id, zone_id, metric_date asc
+), 
+-- Jointure des 2 tables pivotées: u_vi_va X vc => u_vi_va_vc
+--  pour ajouter la colonne vc
+u_vi_va_vc as (select 
+    coalesce(a.indic_id, c.indic_id) as indic_id,
+    coalesce(a.zone_id, c.zone_id) as zone_id,
+    coalesce(a.metric_date, c.metric_date) as metric_date,
+    -- on garde la vi et va de table a, la vc de table c
+    a.vi, a.va, c.vc
+from u_vi_va a
     full join pivot_vc c on a.indic_id=c.indic_id and a.zone_id=c.zone_id and a.metric_date=c.metric_date
-ORDER BY indic_id, zone_id, metric_date ASC
+ORDER BY indic_id, zone_id, metric_date asc
+)
+
+select * from u_vi_va_vc
