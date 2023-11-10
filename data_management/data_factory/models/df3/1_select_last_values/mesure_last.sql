d47b4ad942ad33bdc9c3984861a0bf7c555fd524
Colin Dugeai <colin.dugeai@gmail.com>
2023-11-10 01:51:23 +0100

    redmine::545
    Si un `NULL` est *valable*, on supprime toutes les autres valeurs *valables* de ce mois.

--- data_management/data_factory/models/df3/1_select_last_values/mesure_last.sql
+++ data_management/data_factory/models/df3/1_select_last_values/mesure_last.sql
@@ -37,6 +37,32 @@ rank_mesures_1_cleaned as (
         end as metric_value,
         zone_id, id, rapport_id
     from rank_mesures_1
+),
+-- Pour chaque {indic_id, mois, metric_type, zone_id}, indique s'il y a un NULL comme valeur de rank 1
+compute_month_contains_null as (
+	select 
+	indic_id, date_trunc('month', metric_date::date) as mmonth, metric_type, zone_id,
+	-- Liste des valeurs pour ce mois
+	-- array_agg(metric_value) as all_metric_value,
+	-- Retourne TRUE is un NULL est contenu dans la liste des valeurs pour ce mois
+	true=ANY (SELECT unnest((array_agg(metric_value))) IS NULL) as month_contains_null
+	from rank_mesures_1_cleaned
+	group by indic_id, date_trunc('month', metric_date::date), metric_type, zone_id
+),
+-- Table précédente à laquelle on a enlevé les valeurs
+--  pour lesquelles on a un NULL comme valeur de rank 1 (valeur valable) pour n'importe quel jour du mois de cette valeur
+rank_mesures_1_cleaned_no_value_month_if_null as (
+
+    select a.*
+        --, b.month_contains_null
+        --, b.month_contains_null and (metric_value is not null) as to_delete
+    from rank_mesures_1_cleaned a
+    left join compute_month_contains_null b 
+    on a.indic_id=b.indic_id and a.metric_type=b.metric_type and a.zone_id=b.zone_id and date_trunc('month', a.metric_date::date)=b.mmonth
+    -- On supprime les lignes si:
+    --		- le mois contient un NULL (month_contains_null=TRUE)
+    --		- ET que la valeur n'est pas ce NULL en question. Ainsi, on continue de propager les NULL saisis
+    where not (b.month_contains_null and (metric_value is not null))
 )
 
-select * from rank_mesures_1_cleaned
+select * from rank_mesures_1_cleaned_no_value_month_if_null
