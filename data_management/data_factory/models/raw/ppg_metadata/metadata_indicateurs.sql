fe138741ffa5d6a7a8bbf0fd449c9ea2f7f5efd0
Colin Dugeai <colin.dugeai@gmail.com>
2023-11-13 17:07:04 +0100

    hotfix-fix: cast indic_hidden_pilote to bool
    fix a861bf98

--- data_management/data_factory/models/raw/ppg_metadata/metadata_indicateurs.sql
+++ data_management/data_factory/models/raw/ppg_metadata/metadata_indicateurs.sql
@@ -1,1 +1,1 @@
-select * from {{ ref('metadata_indicateurs_hidden') }} where not coalesce(indic_hidden_pilote::boolean, false)
+select * from {{ ref('metadata_indicateurs_hidden') }} where not coalesce(indic_hidden_pilote::text::boolean, false)
