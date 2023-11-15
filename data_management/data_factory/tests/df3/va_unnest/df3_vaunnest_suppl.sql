51cc493a14ef425da65b1cd2d799061dab647f23
Colin Dugeai <colin.dugeai@gmail.com>
2023-11-15 11:31:18 +0100

    fix: test va_unnest dates
    on compare les mois et non plus les jours exacts

--- data_management/data_factory/tests/df3/va_unnest/df3_vaunnest_suppl.sql
+++ data_management/data_factory/tests/df3/va_unnest/df3_vaunnest_suppl.sql
@@ -12,7 +12,7 @@ unnest_va_computed as (
 unnest_va_target as (
 	select id, territoire_code, 
     unnest(evolution_valeur_actuelle) as va_unnest_target, 
-    unnest(evolution_date_valeur_actuelle) as va_date_unnest_target
+    unnest(evolution_date_valeur_actuelle)::date as va_date_unnest_target
     from {{ ref('indicateur') }} di 
 )
 
@@ -21,9 +21,9 @@ select a.*, b.va_unnest_target, b.va_date_unnest_target
 from unnest_va_computed a
 full join unnest_va_target b 
 on
-    -- Jointure sur l'indicateur, zone, et date de la VA
+    -- Jointure sur l'indicateur, zone, et le MOIS de la date de la VA
     a.id=b.id and 
     a.territoire_code=b.territoire_code and 
-    a.va_date_unnest_computed=b.va_date_unnest_target
+    date_trunc('month', a.va_date_unnest_computed)=date_trunc('month', b.va_date_unnest_target)
 -- Condition du test
 where b.va_unnest_target is null and a.va_unnest_computed is not null
