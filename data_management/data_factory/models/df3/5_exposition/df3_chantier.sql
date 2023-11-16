ae9ce665b547060fe117148dd70e065859b1840d
Colin Dugeai <colin.dugeai@gmail.com>
2023-11-16 10:21:33 +0100

    df3_chantier first version

--- data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
+++ data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
@@ -24,4 +24,4 @@ select
     'todo' as a_taux_avancement_regional,
     false as a_supprimer,
     'todo' as est_applicable
-from {{ ref('metadata_chantiers') }} mc 
+from raw_data.metadata_chantiers mc 
