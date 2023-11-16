ab3717e7ce5ba836fef9336342010fe7ec4945ab
Colin Dugeai <colin.dugeai@gmail.com>
2023-11-16 17:07:13 +0100

    ajout meteo + porteur_directeur

--- data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
+++ data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
@@ -1,16 +1,28 @@
+with 
+-- Table des synthèses triée par date
+synthese_triee_par_date as (
+    SELECT ROW_NUMBER() OVER (PARTITION BY chantier_id, code_insee, maille ORDER BY date_meteo DESC) AS row_id_by_date_meteo_desc,
+        chantier_id,
+        code_insee,
+        maille,
+        meteo,
+        date_meteo
+    FROM {{ ref('synthese_des_resultats') }}
+)
+
 select 
-    chantier_id as id,
+    mc.chantier_id as id,
     ch_nom as nom,
     t.code_insee as code_insee,
     -1 as taux_avancement,
     t.nom as territoire_nom,
     string_to_array(ch_per, ' | ') as perimetre_ids, 
     z.zone_type as "maille",
-    'todo' as directeurs_administration_centrale,
+    string_to_array("porteur_directeur" , ' | ') as directeurs_administration_centrale, 
     string_to_array("porteur_ids_noDAC" , ' | ') as ministeres, 
     string_to_array("porteur_shorts_DAC" , ' | ') as directions_administration_centrale, 
     string_to_array("ch_dp" , ' | ') as directeurs_projet,
-    'todo' as meteo,
+    sr.meteo as meteo,
     'todo' as axe,
     'todo'as ppg,
     string_to_array("ch_dp_mail" , ' | ') as directeurs_projet_mails,
@@ -24,10 +36,15 @@ select
     'todo' as a_taux_avancement_regional,
     false as a_supprimer,
     'todo' as est_applicable
-from {{ ref('metadata_chantiers') }} mc 
+from {{ ref('metadata_chantiers') }} mc
 -- On dupplique les lignes chantier pour chaque territoire
 cross join territoire t
 left join raw_data.metadata_zones z on z.zone_id=t.zone_id
-order by chantier_id, t.zone_id
+left join raw_data.metadata_porteurs po on mc."porteur_ids_DAC"=po.porteur_id
+left join synthese_triee_par_date sr ON 
+	sr.chantier_id =mc.chantier_id AND
+	sr.maille = z.zone_type AND
+	sr.code_insee = t.code_insee
+order by mc.chantier_id, t.zone_id
 
 
