ea55b3a35c39dade295291b9510bbbb22a12903c
Colin Dugeai <colin.dugeai@gmail.com>
2023-11-16 17:15:30 +0100

    ajout axe+ppg

--- data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
+++ data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
@@ -7,7 +7,7 @@ synthese_triee_par_date as (
         maille,
         meteo,
         date_meteo
-    FROM {{ ref('synthese_des_resultats') }}
+    FROM public.synthese_des_resultats
 )
 
 select 
@@ -23,8 +23,8 @@ select
     string_to_array("porteur_shorts_DAC" , ' | ') as directions_administration_centrale, 
     string_to_array("ch_dp" , ' | ') as directeurs_projet,
     sr.meteo as meteo,
-    'todo' as axe,
-    'todo'as ppg,
+    ax.axe_name as axe,
+    ppg.ppg_nom as ppg,
     string_to_array("ch_dp_mail" , ' | ') as directeurs_projet_mails,
     'todo' as est_barometre,
     'todo' as est_territorialise,
@@ -36,7 +36,7 @@ select
     'todo' as a_taux_avancement_regional,
     false as a_supprimer,
     'todo' as est_applicable
-from {{ ref('metadata_chantiers') }} mc
+from raw_data.metadata_chantiers mc
 -- On dupplique les lignes chantier pour chaque territoire
 cross join territoire t
 left join raw_data.metadata_zones z on z.zone_id=t.zone_id
@@ -45,6 +45,8 @@ left join synthese_triee_par_date sr ON
 	sr.chantier_id =mc.chantier_id AND
 	sr.maille = z.zone_type AND
 	sr.code_insee = t.code_insee
+left join raw_data.metadata_ppgs ppg on mc.ch_ppg =ppg.ppg_id
+left join raw_data.metadata_axes ax on ppg.ppg_axe =ax.axe_id
 order by mc.chantier_id, t.zone_id
 
 
