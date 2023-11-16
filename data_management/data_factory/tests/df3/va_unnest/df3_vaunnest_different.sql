9e1282ea930ebe4c51a56d85e7821b972cc6ff85
Colin Dugeai <46027833+cdugeai@users.noreply.github.com>
2023-11-16 14:41:42 +0100

    df3 fixes (#643)
    * redmine::568 fix-tests-vaunnest (#640)
    
    on compare les mois et non plus les jours exacts
    
    * redmine::574 select indics not hidden (#641)
    
    On ne garde que les indics qui ne sont pas "hidden" dans metadata_indicateurs

--- data_management/data_factory/tests/df3/va_unnest/df3_vaunnest_different.sql
+++ data_management/data_factory/tests/df3/va_unnest/df3_vaunnest_different.sql
@@ -1,5 +1,7 @@
 {{ config(enabled=true, tags=["scope_indicateur", "metric_vaunnest"]) }}
 
+    -- TOLERANCE de 1 pourcent (cf fin du test)
+
 -- Ce test va vérifier pour chaque VA disposant d'une VA dans la
 --  table computed et la table de reference si la VA est IDENTIQUE
 
@@ -12,19 +14,33 @@ unnest_va_computed as (
 unnest_va_target as (
 	select id, territoire_code, 
     unnest(evolution_valeur_actuelle) as va_unnest_target, 
-    unnest(evolution_date_valeur_actuelle) as va_date_unnest_target
+    unnest(evolution_date_valeur_actuelle)::date as va_date_unnest_target
     from {{ ref('indicateur') }} di 
-)
+),
 
+compare_values as (
 -- Jointure avec la table des résultats attendus
-select a.*, b.va_unnest_target, b.va_date_unnest_target 
+select a.*, b.va_unnest_target, b.va_date_unnest_target,
+-- Différence entre le calculé et attendu (en pourcent)
+case
+	when b.va_unnest_target=0 and a.va_unnest_computed=0 then 0
+	when b.va_unnest_target=0 and a.va_unnest_computed<>0 then null
+	else 100*(a.va_unnest_computed-b.va_unnest_target)/b.va_unnest_target
+end as percent_change
 from unnest_va_computed a
 full join unnest_va_target b 
 on
-    -- Jointure sur l'indicateur, zone, et date de la VA
+    -- Jointure sur l'indicateur, zone, et le MOIS de la date de la VA
     a.id=b.id and 
     a.territoire_code=b.territoire_code and 
-    a.va_date_unnest_computed=b.va_date_unnest_target
+    date_trunc('month', a.va_date_unnest_computed)=date_trunc('month', b.va_date_unnest_target)
+)
+
+select * from compare_values
 -- Condition du test
-where (b.va_unnest_target is not null and a.va_unnest_computed is not null) AND
-    (b.va_unnest_target <> a.va_unnest_computed)
+where (va_unnest_target is not null and va_unnest_computed is not null) AND
+    (va_unnest_target <> va_unnest_computed) AND
+    -- TOLERANCE de 1 pourcent
+    (percent_change<-1 or percent_change>1)
+    
+    
