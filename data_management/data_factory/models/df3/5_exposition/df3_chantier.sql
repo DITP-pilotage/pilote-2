99d2d030589d36143fa55406da9cbb004f3a9a01
Colin Dugeai <colin.dugeai@gmail.com>
2023-11-17 14:25:32 +0100

    df3_chantier: replace by dbt ref

--- data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
+++ data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
@@ -14,7 +14,7 @@ synthese_triee_par_date as (
 chantier_est_barometre as (
     SELECT m_indicateurs.chantier_id,
         bool_or(m_indicateurs.est_barometre) as est_barometre
-    FROM raw_data.stg_ppg_metadata__indicateurs m_indicateurs
+    FROM {{ ref('stg_ppg_metadata__indicateurs') }} m_indicateurs
     GROUP BY m_indicateurs.chantier_id
 ),
 indicateurs_zones AS (
@@ -22,11 +22,11 @@ indicateurs_zones AS (
         spmi.id as indic_id,
         chantier_id, 
         zones.id as zone_id 
-    FROM raw_data.stg_ppg_metadata__indicateurs spmi
+    FROM {{ ref('stg_ppg_metadata__indicateurs') }} spmi
 	CROSS JOIN (
         SELECT DISTINCT
             id
-        FROM raw_data.stg_ppg_metadata__zones
+        FROM {{ ref('stg_ppg_metadata__zones') }}
         ) as zones 
 ),
 indicateurs_zones_applicables AS (
@@ -36,7 +36,7 @@ indicateurs_zones_applicables AS (
         iz.zone_id,
         COALESCE(iiza.est_applicable, TRUE) AS est_applicable 
     FROM indicateurs_zones iz
-	LEFT JOIN marts.int_indicateurs_zones_applicables iiza 
+	LEFT JOIN {{ ref('int_indicateurs_zones_applicables') }} iiza 
     ON iz.indic_id = iiza.indic_id AND iz.zone_id = iiza.zone_id
 ),
 chantiers_zones_applicables AS (
@@ -52,9 +52,9 @@ select a.chantier_id,
 	bool_or(tag_ch is not null) filter (where z.zone_type='DEPT') as has_ta_dept,
 	bool_or(tag_ch is not null) filter (where z.zone_type='REG') as has_ta_reg,
 	bool_or(tag_ch is not null) filter (where z.zone_type='NAT') as has_ta_nat
-from df3.compute_ta_ch a
+from {{ ref('compute_ta_ch') }} a
 left join territoire t on a.territoire_code =t.code
-left join raw_data.metadata_zones z on t.zone_id=z.zone_id
+left join {{ ref('metadata_zones') }} z on t.zone_id=z.zone_id
 group by chantier_id 
 ),
 
@@ -95,26 +95,26 @@ select
     has_ta.has_ta_reg as a_taux_avancement_regional,
     false as a_supprimer,
     chantier_za.est_applicable as est_applicable
-from raw_data.metadata_chantiers mc
+from {{ ref('metadata_chantiers') }} mc
 -- On dupplique les lignes chantier pour chaque territoire
 cross join territoire t
-left join raw_data.metadata_zones z on z.zone_id=t.zone_id
-left join raw_data.metadata_porteurs po on mc."porteur_ids_DAC"=po.porteur_id
+left join {{ ref('metadata_zones') }} z on z.zone_id=t.zone_id
+left join {{ ref('metadata_porteurs') }} po on mc."porteur_ids_DAC"=po.porteur_id
 left join 
     (select * from synthese_triee_par_date where row_id_by_date_meteo_desc=1) sr
 	    ON sr.chantier_id =mc.chantier_id AND
 	    sr.maille = z.zone_type AND
 	    sr.code_insee = t.code_insee
-left join raw_data.metadata_ppgs ppg on mc.ch_ppg =ppg.ppg_id
-left join raw_data.metadata_axes ax on ppg.ppg_axe =ax.axe_id
+left join {{ ref('metadata_ppgs') }} ppg on mc.ch_ppg =ppg.ppg_id
+left join {{ ref('metadata_axes') }} ax on ppg.ppg_axe =ax.axe_id
 LEFT JOIN chantier_est_barometre on mc.chantier_id = chantier_est_barometre.chantier_id
 LEFT JOIN chantiers_zones_applicables chantier_za ON chantier_za.chantier_id = mc.chantier_id AND chantier_za.zone_id = z.zone_id
 left join ch_maille_has_ta_pivot_clean as has_ta on has_ta.chantier_id=mc.chantier_id
 left join ch_has_meteo on ch_has_meteo.chantier_id=mc.chantier_id 
 left join 
-	(select * from df3.compute_ta_ch where valid_on='today') as ta_ch_today on ta_ch_today.chantier_id=mc.chantier_id and ta_ch_today.zone_id=z.zone_id 
+	(select * from {{ ref('compute_ta_ch') }} where valid_on='today') as ta_ch_today on ta_ch_today.chantier_id=mc.chantier_id and ta_ch_today.zone_id=z.zone_id 
 left join 
-	(select * from df3.compute_ta_ch where valid_on='prev_month') as ta_ch_prev_month on ta_ch_prev_month.chantier_id=mc.chantier_id and ta_ch_prev_month.zone_id=z.zone_id
+	(select * from {{ ref('compute_ta_ch') }} where valid_on='prev_month') as ta_ch_prev_month on ta_ch_prev_month.chantier_id=mc.chantier_id and ta_ch_prev_month.zone_id=z.zone_id
 order by mc.chantier_id, t.zone_id
 
 
