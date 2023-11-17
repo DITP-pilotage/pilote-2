7769324d6aa46feabfb1999bd529c51dd81803ab
Colin Dugeai <46027833+cdugeai@users.noreply.github.com>
2023-11-17 15:25:16 +0100

    df3: table chantier (#645)
    * start from base chantier table
    
    * first implem ok
    
    * df3_chantier first version
    
    * disable model
    
    * replace by ref dbt model
    
    * model.new: compute_ta_ch
    
    pour calculer le ta_chantier, colonne tag_ch
    
    * model.new: compute_ta_ch_2
    
    compute_ta_ch for all dates
    
    * df3_chantier first version
    
    * replace by ref dbt model
    
    * ajout infos zones
    
    * ajout meteo + porteur_directeur
    
    * ajout axe+ppg
    
    * ch est baro + z_applicable
    
    * chantier has_ta pour chaque maille
    
    * refacto code has_ta
    
    * add has_meteo_dept + has_meteo_reg
    
    * compute ta_ch last month
    
    draft version
    
    * clean compute ta_ch  previous month
    
    * compute_ta_ch_2 materialized to table
    
    * dbt doc df3_chantier
    
    * Update compute_ta_ch_2.sql
    
    rename col
    
    * df3_chantier as table
    
    * add ta et ta_precedent
    
    * fix: dupplicate when using synthese_triee_par_date
    
    * clean: compte ta ch
    
    * clean: df3_chantier
    
    * Delete compute_ta_ch.sql
    
    * rename compute_ta_ch_2
    
    * replace by dbt ref
    
    * compute_ta_ch: add territoire_code
    
    * df3_chantier: replace by dbt ref
    
    * dbt: use sources to refer to public tables
    
    * Delete df2_chantier.sql
    
    * clean + comment
    
    * dbt.doc: compute_ta_ch
    
    * dbt.doc: df3_chantier
    
    + comments in sql code

---
+++ data_management/data_factory/models/df3/5_exposition/df3_chantier.sql
@@ -0,0 +1,120 @@
+{{ config(materialized='table') }}
+
+with 
+-- Table des synthèses triée par date
+synthese_triee_par_date as (
+    SELECT ROW_NUMBER() OVER (PARTITION BY chantier_id, code_insee, maille ORDER BY date_meteo DESC) AS row_id_by_date_meteo_desc,
+        chantier_id,
+        code_insee,
+        maille,
+        meteo,
+        date_meteo
+    FROM {{ source('db_schema_public', 'synthese_des_resultats') }}
+),
+chantier_est_barometre as (
+    SELECT m_indicateurs.chantier_id,
+        bool_or(m_indicateurs.est_barometre) as est_barometre
+    FROM {{ ref('stg_ppg_metadata__indicateurs') }} m_indicateurs
+    GROUP BY m_indicateurs.chantier_id
+),
+indicateurs_zones AS (
+	SELECT DISTINCT
+        spmi.id as indic_id,
+        chantier_id, 
+        zones.id as zone_id 
+    FROM {{ ref('stg_ppg_metadata__indicateurs') }} spmi
+	CROSS JOIN (
+        SELECT DISTINCT
+            id
+        FROM {{ ref('stg_ppg_metadata__zones') }}
+        ) as zones 
+),
+indicateurs_zones_applicables AS (
+	SELECT DISTINCT
+        iz.indic_id,
+        iz.chantier_id,
+        iz.zone_id,
+        COALESCE(iiza.est_applicable, TRUE) AS est_applicable 
+    FROM indicateurs_zones iz
+	LEFT JOIN {{ ref('int_indicateurs_zones_applicables') }} iiza 
+    ON iz.indic_id = iiza.indic_id AND iz.zone_id = iiza.zone_id
+),
+chantiers_zones_applicables AS (
+    SELECT 
+        chantier_id,
+        zone_id,
+        bool_or(est_applicable) as est_applicable
+    FROM indicateurs_zones_applicables 
+    GROUP BY chantier_id, zone_id
+),
+-- Indique pour chaque chantier s'il existe un TA à chaque maille
+ch_maille_has_ta_pivot_clean as (
+select a.chantier_id,
+	bool_or(tag_ch is not null) filter (where z.zone_type='DEPT') as has_ta_dept,
+	bool_or(tag_ch is not null) filter (where z.zone_type='REG') as has_ta_reg,
+	bool_or(tag_ch is not null) filter (where z.zone_type='NAT') as has_ta_nat
+from {{ ref('compute_ta_ch') }} a
+left join {{ source('db_schema_public', 'territoire') }} t on a.territoire_code =t.code
+left join {{ ref('metadata_zones') }} z on t.zone_id=z.zone_id
+group by chantier_id 
+),
+-- Indique pour chaque chantier s'il existe une météo à chaque maille
+ch_has_meteo as (
+select a.chantier_id,
+	bool_or(meteo is not null) filter (where a.maille='DEPT') as has_meteo_dept,
+	bool_or(meteo is not null) filter (where a.maille='REG') as has_meteo_reg,
+	bool_or(meteo is not null) filter (where a.maille='NAT') as has_meteo_nat
+from synthese_triee_par_date a
+group by chantier_id 
+)
+
+
+select 
+    mc.chantier_id as id,
+    ch_nom as nom,
+    t.code_insee as code_insee,
+    ta_ch_today.tag_ch as taux_avancement,
+    t.nom as territoire_nom,
+    string_to_array(ch_per, ' | ') as perimetre_ids, 
+    z.zone_type as "maille",
+    string_to_array("porteur_directeur" , ' | ') as directeurs_administration_centrale, 
+    string_to_array("porteur_ids_noDAC" , ' | ') as ministeres, 
+    string_to_array("porteur_shorts_DAC" , ' | ') as directions_administration_centrale, 
+    string_to_array("ch_dp" , ' | ') as directeurs_projet,
+    sr.meteo as meteo,
+    ax.axe_name as axe,
+    ppg.ppg_nom as ppg,
+    string_to_array("ch_dp_mail" , ' | ') as directeurs_projet_mails,
+    chantier_est_barometre.est_barometre,
+    mc.ch_territo as est_territorialise,
+    ta_ch_prev_month.tag_ch as taux_avancement_precedent,
+	LOWER(mc.ch_saisie_ate)::type_ate as ate,
+    has_ta.has_ta_dept as a_taux_avancement_departemental,
+    ch_has_meteo.has_meteo_dept as a_meteo_departemental,
+    ch_has_meteo.has_meteo_reg as a_meteo_regional,
+    has_ta.has_ta_reg as a_taux_avancement_regional,
+    false as a_supprimer,
+    chantier_za.est_applicable as est_applicable
+from {{ ref('metadata_chantiers') }} mc
+-- On dupplique les lignes chantier pour chaque territoire
+cross join {{ source('db_schema_public', 'territoire') }} t
+left join {{ ref('metadata_zones') }} z on z.zone_id=t.zone_id
+left join {{ ref('metadata_porteurs') }} po on mc."porteur_ids_DAC"=po.porteur_id
+left join 
+    (select * from synthese_triee_par_date where row_id_by_date_meteo_desc=1) sr
+	    ON sr.chantier_id =mc.chantier_id AND
+	    sr.maille = z.zone_type AND
+	    sr.code_insee = t.code_insee
+left join {{ ref('metadata_ppgs') }} ppg on mc.ch_ppg =ppg.ppg_id
+left join {{ ref('metadata_axes') }} ax on ppg.ppg_axe =ax.axe_id
+LEFT JOIN chantier_est_barometre on mc.chantier_id = chantier_est_barometre.chantier_id
+LEFT JOIN chantiers_zones_applicables chantier_za ON chantier_za.chantier_id = mc.chantier_id AND chantier_za.zone_id = z.zone_id
+left join ch_maille_has_ta_pivot_clean as has_ta on has_ta.chantier_id=mc.chantier_id
+left join ch_has_meteo on ch_has_meteo.chantier_id=mc.chantier_id 
+left join 
+	(select * from {{ ref('compute_ta_ch') }} where valid_on='today') as ta_ch_today on ta_ch_today.chantier_id=mc.chantier_id and ta_ch_today.zone_id=z.zone_id 
+left join 
+	(select * from {{ ref('compute_ta_ch') }} where valid_on='prev_month') as ta_ch_prev_month on ta_ch_prev_month.chantier_id=mc.chantier_id and ta_ch_prev_month.zone_id=z.zone_id
+order by mc.chantier_id, t.zone_id
+
+
