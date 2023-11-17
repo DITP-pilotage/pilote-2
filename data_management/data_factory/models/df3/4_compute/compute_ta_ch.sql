344bf20bf507b871429ac3b700fea81fcd4ccc5a
Colin Dugeai <46027833+cdugeai@users.noreply.github.com>
2023-11-17 15:25:16 +0100

    df3: table chantier (#645)
    * start from base chantier table
    
    * first implem ok
    
    * df3_chantier first version
    
    * disable model
    
    * replace by ref dbt model
    
    * model.new: compute_ta_ch
    
    pour calculer le ta_chantier, colonne tag_ch
    
    * model.new: compute_ta_ch_2
    
    compute_ta_ch for all dates
    
    * df3_chantier first version
    
    * replace by ref dbt model
    
    * ajout infos zones
    
    * ajout meteo + porteur_directeur
    
    * ajout axe+ppg
    
    * ch est baro + z_applicable
    
    * chantier has_ta pour chaque maille
    
    * refacto code has_ta
    
    * add has_meteo_dept + has_meteo_reg
    
    * compute ta_ch last month
    
    draft version
    
    * clean compute ta_ch  previous month
    
    * compute_ta_ch_2 materialized to table
    
    * dbt doc df3_chantier
    
    * Update compute_ta_ch_2.sql
    
    rename col
    
    * df3_chantier as table
    
    * add ta et ta_precedent
    
    * fix: dupplicate when using synthese_triee_par_date
    
    * clean: compte ta ch
    
    * clean: df3_chantier
    
    * Delete compute_ta_ch.sql
    
    * rename compute_ta_ch_2
    
    * replace by dbt ref
    
    * compute_ta_ch: add territoire_code
    
    * df3_chantier: replace by dbt ref
    
    * dbt: use sources to refer to public tables
    
    * Delete df2_chantier.sql
    
    * clean + comment
    
    * dbt.doc: compute_ta_ch
    
    * dbt.doc: df3_chantier
    
    + comments in sql code

---
+++ data_management/data_factory/models/df3/4_compute/compute_ta_ch.sql
@@ -0,0 +1,80 @@
+{{ config(materialized='table') }}
+
+with 
+-- TA de chaque {indic-zone} à chaque date
+ta_zone_indic as (
+	select 
+	b.indic_parent_ch, a.zone_id, z.zone_type as "maille", metric_date,a.indic_id,
+	vaca, vig, vca, vcg,
+	taa, tag
+	from {{ ref('compute_ta_indic') }} a
+	left join {{ ref('metadata_indicateurs') }} b on a.indic_id =b.indic_id
+	left join {{ ref('metadata_zones') }} z on a.zone_id=z.zone_id 
+	order by indic_parent_ch, zone_id, metric_date, indic_id
+),
+-- Calcul du TA pondéré
+--	On va pondérer chaque TA par sa pondération à cette maille
+ta_zone_indic_pond as (
+select a.*, b.poids_pourcent_dept, b.poids_pourcent_reg, b.poids_pourcent_nat,
+	case 
+		when maille='DEPT' then taa*0.01*poids_pourcent_dept
+		when maille='REG' then taa*0.01*poids_pourcent_reg
+		when maille='NAT' then taa*0.01*poids_pourcent_nat
+	end as taa_pond,
+	case 
+		when maille='DEPT' then tag*0.01*poids_pourcent_dept
+		when maille='REG' then tag*0.01*poids_pourcent_reg
+		when maille='NAT' then tag*0.01*poids_pourcent_nat
+	end as tag_pond
+from ta_zone_indic a
+left join {{ ref('metadata_parametrage_indicateurs') }} b on a.indic_id=b.indic_id 
+order by indic_parent_ch, zone_id, metric_date, indic_id
+),
+-- Pour chaque indic-zone, on garde la ligne avec une vaca la plus récente
+ta_zone_indic_pond_today as (
+select * from (
+	select *, rank() over (partition by zone_id, indic_id order by metric_date desc) as r,
+	'today' as valid_on
+	from ta_zone_indic_pond 
+	where vaca is not null) a
+where a.r=1
+),
+-- Pour chaque indic-zone, on garde la ligne avec une vaca la plus récente
+--	MAIS en excluant les valeurs du mois courant. Ainsi, on aura le TA en vigeur au mois précédent.
+ta_zone_indic_pond_prev_month as (
+select * from (
+	select *, rank() over (partition by zone_id, indic_id order by metric_date desc) as r,
+	'prev_month' as valid_on
+	from ta_zone_indic_pond 
+	where vaca is not null
+	and date_trunc('month', metric_date::date)<date_trunc('month', now()) 
+	) a
+where a.r=1
+),
+-- Calcul du TA chantier
+ta_ch as (
+	select indic_parent_ch as chantier_id, zone_id, valid_on,
+	array_agg(indic_id) as indic_ids,
+	array_agg(poids_pourcent_dept) as p_dept,
+	array_agg(poids_pourcent_reg) as p_reg,
+	array_agg(poids_pourcent_nat) as p_nat,
+	array_agg(vaca) as vaca_agg, 
+	array_agg(vig) as vig_agg, 
+	array_agg(vca) as vca_agg, 
+	array_agg(vcg) as vcg_agg, 
+	array_agg(taa) as taa_agg, 
+	array_agg(taa_pond) as taa_pond_agg, 
+	array_agg(tag) as tag_agg,
+	array_agg(tag_pond) as tag_pond_agg,
+	sum(taa_pond) as taa_ch,
+	sum(tag_pond) as tag_ch
+	from 
+	(
+	select * from ta_zone_indic_pond_today union
+	select * from ta_zone_indic_pond_prev_month
+	) a
+	group by indic_parent_ch, zone_id, valid_on
+)
+
+select a.*, t.code as territoire_code from ta_ch a
+left join {{ source('db_schema_public', 'territoire') }} t on t.zone_id=a.zone_id
