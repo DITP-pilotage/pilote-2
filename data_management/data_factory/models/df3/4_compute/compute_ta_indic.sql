d9d2edc104a5501b7532d1bd16563884d42d067b
Colin Dugeai <colin.dugeai@gmail.com>
2023-12-04 18:18:42 +0100

    ajout règles calcul TA indic
    cf doc

--- data_management/data_factory/models/df3/4_compute/compute_ta_indic.sql
+++ data_management/data_factory/models/df3/4_compute/compute_ta_indic.sql
@@ -1,23 +1,46 @@
 -- Compute unbounded TA
-with get_unbounded_ta as
+with 
+get_unbounded_ta_hausse as
 (
 	select a.*,
-	-- TAA: Si la VI=VC -> TA = NULL
 	case 
-		when vig=vca then null 
-		when b.tendance='HAUSSE' and vaca>=vca then 100 
-		when b.tendance='BAISSE' and vaca<=vca then 100
+		-- VI>=VC
+		WHEN vig>=vca AND vaca >= vca THEN 100
+		WHEN vig>=vca AND vaca < vca THEN 0
+		-- else
 		else round((100*(vaca-vig)/(vca-vig))::numeric, {{ var('ta_decimales') }}) 
 	end as unbounded_taa,
-	-- TAG: Si la VI=VC-> TA = NULL
 	case 
-		when vig=vcg then null 
-		when b.tendance='HAUSSE' and vacg>=vcg then 100 
-		when b.tendance='BAISSE' and vacg<=vcg then 100
+		-- VI>=VC
+		WHEN vig>=vcg AND vacg >= vcg THEN 100
+		WHEN vig>=vcg AND vacg < vcg THEN 0
 		else round((100*(vacg-vig)/(vcg-vig))::numeric, {{ var('ta_decimales') }}) 
 	end as unbounded_tag
 	from {{ ref('merge_computed_values') }} a
-	left join {{ ref('metadata_parametrage_indicateurs') }} b on a.indic_id=b.indic_id
+	right join (select * from {{ ref('metadata_parametrage_indicateurs') }} where tendance in ('HAUSSE', 'STABLE')) b on a.indic_id=b.indic_id
+),
+get_unbounded_ta_baisse as
+(
+	select a.*,
+	case 
+		-- VI<=VC
+		WHEN vig<=vca AND vaca <= vca THEN 100
+		WHEN vig<=vca AND vaca > vca THEN 0
+		-- else
+		else round((100*(vaca-vig)/(vca-vig))::numeric, {{ var('ta_decimales') }}) 
+	end as unbounded_taa,
+	case 
+		-- VI<=VC
+		WHEN vig<=vcg AND vacg <= vcg THEN 100
+		WHEN vig<=vcg AND vacg > vcg THEN 0
+		-- else
+		else round((100*(vacg-vig)/(vcg-vig))::numeric, {{ var('ta_decimales') }}) 
+	end as unbounded_tag
+	from {{ ref('merge_computed_values') }} a
+	right join (select * from {{ ref('metadata_parametrage_indicateurs') }} where tendance in ('BAISSE')) b on a.indic_id=b.indic_id
+),
+get_unbounded_ta as (
+	select * from get_unbounded_ta_hausse a union (select * from get_unbounded_ta_baisse)
 ),
 -- Compute bounded TA
 get_bounded_ta as (
