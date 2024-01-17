fc031c074a1b38a7e45faef6089996ebcc16d3db
Tristan Conti <86486650+contitri@users.noreply.github.com>
2024-01-17 16:14:43 +0100

    fix: retours de recette (845)

--- src/client/components/PageAccueil/SélecteurVueStatuts/SélecteurVueStatuts.tsx
+++ src/client/components/PageAccueil/SélecteurVueStatuts/SélecteurVueStatuts.tsx
@@ -1,21 +1,10 @@
-import { useCallback } from 'react';
 import ContrôleSegmenté from '@/components/_commons/ContrôleSegmenté/ContrôleSegmenté';
-import { TypeVueStatuts } from '@/client/stores/useStatutsStore/useStatutStore.interface';
-import { actionsStatutsStore, vueStatutsSélectionnéeStore } from '@/client/stores/useStatutsStore/useStatutsStore';
-import optionChoixVueStatuts from './SélecteurVueStatut.interface';
+import { vueStatutsSélectionnéeStore } from '@/client/stores/useStatutsStore/useStatutsStore';
+import useSélecteurVueStatut from './useSélecteurVueStatut.interface';
 
 export default function SélecteurVueStatuts() {
 
-  const { modifierLaVueSélectionnéeEtLesStatutsAssociés } = actionsStatutsStore();
-
-  const options: optionChoixVueStatuts[] = [
-    { valeur: 'BROUILLON_ET_PUBLIE', libellé: 'Chantiers publiés et non publiés' },
-    { valeur: 'PUBLIE', libellé: 'Chantiers publiés' },
-  ];
-  
-  const auChangement = useCallback((vueStatuts: TypeVueStatuts) => {
-    modifierLaVueSélectionnéeEtLesStatutsAssociés(vueStatuts);
-  }, [modifierLaVueSélectionnéeEtLesStatutsAssociés]);
+  const { options, auChangement } = useSélecteurVueStatut();
 
   return (
     <ContrôleSegmenté
