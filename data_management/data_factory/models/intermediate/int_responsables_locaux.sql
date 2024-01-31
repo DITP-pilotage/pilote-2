a2b04fc23063a92f8f4d2fc4ac1f9fca341af52b
contitri <tristan.conti@seenovate.com>
2024-01-31 10:05:09 +0000

    feat: ajoute les responsables locaux à la table chantier (878)

--- data_management/data_factory/models/intermediate/int_responsables_locaux.sql
+++ data_management/data_factory/models/intermediate/int_responsables_locaux.sql
@@ -1,8 +1,8 @@
 -- Responsables locaux avec des habilitations sur les chantiers
 WITH resp_locaux_chantiers AS (
     SELECT 
-        ARRAY_AGG(u.nom || ' ' || u.prenom) AS nom, 
-        ARRAY_AGG(u.email) AS email, 
+        INITCAP(u.prenom) || ' ' || INITCAP(u.nom) AS nom, 
+        u.email AS email, 
         UNNEST(h.territoires) AS territoire_code,
         UNNEST(h.chantiers) AS chantier_id
     FROM 
@@ -11,15 +11,13 @@ WITH resp_locaux_chantiers AS (
     WHERE 
         u.profil_code IN ('RESPONSABLE_REGION', 'RESPONSABLE_DEPARTEMENT') 
         AND h.scope_code = 'lecture'
-    GROUP BY 
-        territoire_code,
-        chantier_id
 ),
+
 -- Directeurs de projets avec des habilitations sur les périmètres
 resp_locaux_perimetres AS (
     SELECT 
-        ARRAY_AGG(u.nom || ' ' || u.prenom) AS nom, 
-        ARRAY_AGG(u.email) AS email, 
+        INITCAP(u.prenom) || ' ' || INITCAP(u.nom) AS nom, 
+        u.email AS email, 
         UNNEST(h.territoires) AS territoire_code,
         UNNEST(h.perimetres) AS perimetre_id
     FROM 
@@ -29,30 +27,38 @@ resp_locaux_perimetres AS (
         u.profil_code IN ('RESPONSABLE_REGION', 'RESPONSABLE_DEPARTEMENT') 
         AND h.scope_code = 'lecture'
         AND h.perimetres <> '{}'
-        AND h.territoires <> '{}'
-    GROUP BY 
-        territoire_code,
-        perimetre_id    	
+        AND h.territoires <> '{}'	
 ),
+
 corresp_perimetres AS (
     SELECT 
         id AS chantier_id, 
         UNNEST(perimetre_ids) AS perimetre_id 
     FROM 
         {{ ref('stg_ppg_metadata__chantiers') }}
+),
+
+resp_locaux_complet AS (
+    SELECT * FROM resp_locaux_chantiers 
+    WHERE chantier_id IS NOT NULL AND territoire_code IS NOT NULL
+    UNION ALL 
+    (
+        SELECT 
+            resp_locaux_perimetres.nom, 
+            resp_locaux_perimetres.email,
+            resp_locaux_perimetres.territoire_code,
+            corresp_perimetres.chantier_id
+        FROM 
+            resp_locaux_perimetres 
+            JOIN corresp_perimetres ON resp_locaux_perimetres.perimetre_id = corresp_perimetres.perimetre_id
+        WHERE chantier_id IS NOT NULL AND territoire_code IS NOT NULL
+    )
 )
 
-SELECT * FROM resp_locaux_chantiers 
-WHERE chantier_id IS NOT NULL AND territoire_code IS NOT NULL
-UNION ALL 
-(
-    SELECT 
-        resp_locaux_perimetres.nom, 
-        resp_locaux_perimetres.email,
-        resp_locaux_perimetres.territoire_code,
-        corresp_perimetres.chantier_id
-    FROM 
-        resp_locaux_perimetres 
-        JOIN corresp_perimetres ON resp_locaux_perimetres.perimetre_id = corresp_perimetres.perimetre_id
-    WHERE chantier_id IS NOT NULL AND territoire_code IS NOT NULL
-)
+SELECT 
+    ARRAY_AGG(nom) AS nom,
+    ARRAY_AGG(email) AS email,
+    chantier_id,
+    territoire_code
+FROM resp_locaux_complet
+GROUP BY chantier_id, territoire_code
---
+++ src/database/prisma/migrations/20240131094439_ajoute_responsables_locaux_chantier/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "public"."chantier" ADD COLUMN     "responsables_locaux" TEXT[],
+ADD COLUMN     "responsables_locaux_mails" TEXT[];
