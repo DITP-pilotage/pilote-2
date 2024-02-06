41c2b7da05c54451ab501bb4ea548959ca15ba42
Colin Dugeai <colin.dugeai@gmail.com>
2024-02-06 10:04:29 +0100

    refacto: simplify delete rows (PIL-44)

--- data_management/data_factory/macros/adhoc/drop_indic_just_hidden.sql
+++ data_management/data_factory/macros/adhoc/drop_indic_just_hidden.sql
@@ -3,25 +3,14 @@
 --  En effet, metadata_indicateur est incremental. Donc si un ligne est supprimée dans metadata_indicateur_hidden,
 --  alors elle ne va pas apparaitre comme ligne modifiée et ne sera donc pas supprimée dans metadata_indicateur.
 
+{% macro drop_indic_just_hidden(table_) %}
 
--- DELETE JOIN impossible avec postgres donc DELETE USING
---  see: https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-delete-join/
-
-{% macro drop_indic_just_hidden(par) %}
-
-DELETE FROM 
-    -- on va supprimer les lignes dans metadata_indicateurs -> old_table
-	{{ par }} old_table USING
-    -- jointure avec la table à jour: metadata_indicateurs_hidden -> latest
-  	(SELECT indic_id, indic_hidden_pilote FROM {{ ref('metadata_indicateurs_hidden') }}) latest
--- jointure sur les indic_id
-WHERE old_table.indic_id = latest.indic_id
-  -- et on ne garde (pour suppression) que les lignes avec indic_hidden_pilote=TRUE
-  AND latest.indic_hidden_pilote
-  -- (optionnel) on retourne les lignes supprimées
-  RETURNING old_table.indic_id, latest.indic_hidden_pilote
-
-
-
+-- on va supprimer les lignes dans metadata_indicateurs
+DELETE FROM {{ table_ }} 
+WHERE indic_id IN (
+	SELECT indic_id FROM {{ ref('metadata_indicateurs_hidden') }}
+  -- si indic_hidden_pilote est à supprimer
+	WHERE indic_hidden_pilote = TRUE
+)
 
 {% endmacro %}
