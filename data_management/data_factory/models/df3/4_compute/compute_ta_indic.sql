03878caabfb730ef7df8dc427e1103964150d1b1
Colin Dugeai <colin.dugeai@gmail.com>
2024-02-12 18:14:42 +0100

    feat: ajout calcul taa_adate (PIL-90)
    + création macro pour calculer les TA

--- data_management/data_factory/models/df3/4_compute/compute_ta_indic.sql
+++ data_management/data_factory/models/df3/4_compute/compute_ta_indic.sql
@@ -3,39 +3,18 @@ with
 get_unbounded_ta_hausse as
 (
 	select a.*,
-	case 
-		-- VI>=VC
-		WHEN vig>=vca AND vaca >= vca THEN 100
-		WHEN vig>=vca AND vaca < vca THEN 0
-		-- else
-		else round((100*(vaca-vig)/(vca-vig))::numeric, {{ var('ta_decimales') }}) 
-	end as unbounded_taa,
-	case 
-		-- VI>=VC
-		WHEN vig>=vcg AND vacg >= vcg THEN 100
-		WHEN vig>=vcg AND vacg < vcg THEN 0
-		else round((100*(vacg-vig)/(vcg-vig))::numeric, {{ var('ta_decimales') }}) 
-	end as unbounded_tag
+	{{ compute_ta_hausse_macro('vig', 'vca', 'vaca') }} as unbounded_taa,
+	{{ compute_ta_hausse_macro('vig', 'vca_a_date', 'vaca') }} as unbounded_taa_adate,
+	{{ compute_ta_hausse_macro('vig', 'vcg', 'vacg') }} as unbounded_tag
 	from {{ ref('merge_computed_values') }} a
 	right join (select * from {{ ref('metadata_parametrage_indicateurs') }} where tendance in ('HAUSSE', 'STABLE')) b on a.indic_id=b.indic_id
 ),
 get_unbounded_ta_baisse as
 (
 	select a.*,
-	case 
-		-- VI<=VC
-		WHEN vig<=vca AND vaca <= vca THEN 100
-		WHEN vig<=vca AND vaca > vca THEN 0
-		-- else
-		else round((100*(vaca-vig)/(vca-vig))::numeric, {{ var('ta_decimales') }}) 
-	end as unbounded_taa,
-	case 
-		-- VI<=VC
-		WHEN vig<=vcg AND vacg <= vcg THEN 100
-		WHEN vig<=vcg AND vacg > vcg THEN 0
-		-- else
-		else round((100*(vacg-vig)/(vcg-vig))::numeric, {{ var('ta_decimales') }}) 
-	end as unbounded_tag
+	{{ compute_ta_baisse_macro('vig', 'vca', 'vaca') }} as unbounded_taa,
+	{{ compute_ta_hausse_macro('vig', 'vca_a_date', 'vaca') }} as unbounded_taa_adate,
+	{{ compute_ta_baisse_macro('vig', 'vcg', 'vacg') }} as unbounded_tag
 	from {{ ref('merge_computed_values') }} a
 	right join (select * from {{ ref('metadata_parametrage_indicateurs') }} where tendance in ('BAISSE')) b on a.indic_id=b.indic_id
 ),
@@ -46,6 +25,7 @@ get_unbounded_ta as (
 get_bounded_ta as (
 	select *,
     case when unbounded_taa is null then null else greatest(least(unbounded_taa, 100), 0)::numeric end as taa,
+    case when unbounded_taa_adate is null then null else greatest(least(unbounded_taa_adate, 100), 0)::numeric end as taa_adate,
     case when unbounded_tag is null then null else greatest(least(unbounded_tag, 100), 0)::numeric end as tag
 	from get_unbounded_ta
 )
