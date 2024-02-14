a8e87daabb79cbc0e7e78c281adff910595a635c
Colin Dugeai <46027833+cdugeai@users.noreply.github.com>
2024-02-14 11:42:16 +0100

    Création TAA_a_date (PIL-90)
    * feat: ajout calcul taa_adate (PIL-90)
    
    + création macro pour calculer les TA
    
    * fix: rename vca -> vca_courant
    
    * fix: harmoniser les noms
    
    * a tester: renommage et simplif + doc
    
    de la remontée du ta chantier + doc dbt des colonnes
    
    * fix: fix doc
    
    * bug :warning: : add col evolution_taa_adate dans indicateur
    
    mais ne fonctionne pas car il y a des null dans "evolution_taa_adate" (lorsque pas de VC lors des années des anciennes VA, ex: 2022, 2021...).
    
    * Revert "bug :warning: : add col evolution_taa_adate dans indicateur"
    
    This reverts commit f39ba86fc8af72881da1d4fcbd86a1d1cbdbc41f.
    
    * feat: ajout calcul taa_adate (PIL-90)
    
    + création macro pour calculer les TA
    
    * fix: rename vca -> vca_courant
    
    * fix: harmoniser les noms
    
    * a tester: renommage et simplif + doc
    
    de la remontée du ta chantier + doc dbt des colonnes
    
    * fix: fix doc
    
    * bug :warning: : add col evolution_taa_adate dans indicateur
    
    mais ne fonctionne pas car il y a des null dans "evolution_taa_adate" (lorsque pas de VC lors des années des anciennes VA, ex: 2022, 2021...).
    
    * Revert "bug :warning: : add col evolution_taa_adate dans indicateur"
    
    This reverts commit f39ba86fc8af72881da1d4fcbd86a1d1cbdbc41f.
    
    * fix: rebase breaking change
    
    adapt code for rebased commit 9d0c508444f9cadcd17d0a41a76b0541b3909c33

--- data_management/data_factory/models/df3/4_compute/compute_ta_indic.sql
+++ data_management/data_factory/models/df3/4_compute/compute_ta_indic.sql
@@ -3,39 +3,18 @@ with
 get_unbounded_ta_hausse as
 (
 	select a.*,
-	case 
-		-- VI>=VC
-		WHEN vig>=vca AND vaca >= vca THEN 100
-		WHEN vig>=vca AND vaca < vca THEN 0
-		-- else
-		else round((100*(vaca-vig)/(vca-vig))::numeric, {{ var('ta_decimales') }}) 
-	end as unbounded_taa,
-	case 
-		-- VI>=VC
-		WHEN vig>=vcg AND vacg >= vcg THEN 100
-		WHEN vig>=vcg AND vacg < vcg THEN 0
-		else round((100*(vacg-vig)/(vcg-vig))::numeric, {{ var('ta_decimales') }}) 
-	end as unbounded_tag
+	{{ compute_ta_hausse_macro('vig', 'vca_courant', 'vaca') }} as unbounded_taa_courant,
+	{{ compute_ta_hausse_macro('vig', 'vca_adate', 'vaca') }} as unbounded_taa_adate,
+	{{ compute_ta_hausse_macro('vig', 'vcg', 'vacg') }} as unbounded_tag
 	from {{ ref('merge_computed_values') }} a
 	right join (select * from {{ ref('metadata_parametrage_indicateurs') }} where tendance in ('HAUSSE', 'STABLE')) b on a.indic_id=b.indic_id
 ),
 get_unbounded_ta_baisse as
 (
 	select a.*,
-	case 
-		-- VI<=VC
-		WHEN vig<=vca AND vaca <= vca THEN 100
-		WHEN vig<=vca AND vaca > vca THEN 0
-		-- else
-		else round((100*(vaca-vig)/(vca-vig))::numeric, {{ var('ta_decimales') }}) 
-	end as unbounded_taa,
-	case 
-		-- VI<=VC
-		WHEN vig<=vcg AND vacg <= vcg THEN 100
-		WHEN vig<=vcg AND vacg > vcg THEN 0
-		-- else
-		else round((100*(vacg-vig)/(vcg-vig))::numeric, {{ var('ta_decimales') }}) 
-	end as unbounded_tag
+	{{ compute_ta_baisse_macro('vig', 'vca_courant', 'vaca') }} as unbounded_taa_courant,
+	{{ compute_ta_hausse_macro('vig', 'vca_adate', 'vaca') }} as unbounded_taa_adate,
+	{{ compute_ta_baisse_macro('vig', 'vcg', 'vacg') }} as unbounded_tag
 	from {{ ref('merge_computed_values') }} a
 	right join (select * from {{ ref('metadata_parametrage_indicateurs') }} where tendance in ('BAISSE')) b on a.indic_id=b.indic_id
 ),
@@ -45,7 +24,8 @@ get_unbounded_ta as (
 -- Compute bounded TA
 get_bounded_ta as (
 	select *,
-    case when unbounded_taa is null then null else greatest(least(unbounded_taa, 100), 0)::numeric end as taa,
+    case when unbounded_taa_courant is null then null else greatest(least(unbounded_taa_courant, 100), 0)::numeric end as taa_courant,
+    case when unbounded_taa_adate is null then null else greatest(least(unbounded_taa_adate, 100), 0)::numeric end as taa_adate,
     case when unbounded_tag is null then null else greatest(least(unbounded_tag, 100), 0)::numeric end as tag
 	from get_unbounded_ta
 )
