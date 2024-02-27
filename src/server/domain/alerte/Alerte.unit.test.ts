7f7553d9a020c825a51923571dc9ae0f4ad05876
contitri <tristan.conti@seenovate.com>
2024-02-27 08:48:13 +0000

    feat: tests alertes ta non calculé (PIL-98)

--- src/server/domain/alerte/Alerte.unit.test.ts
+++ src/server/domain/alerte/Alerte.unit.test.ts
@@ -161,4 +161,28 @@ describe('Alerte', () => {
       expect(estEnAlerteDonnéesNonMàj).toBeFalsy();
     });
   });
+
+  describe('estEnAlerteTauxAvancementNonCalculé', () => {
+    test("le chantier est en alerte si le taux d'avancement est null", () => {
+      // Given
+      const tauxAvancement = null;
+
+      // When
+      const estEnAlerteTauxAvancementNonCalculé = Alerte.estEnAlerteTauxAvancementNonCalculé(tauxAvancement);
+
+      // Then
+      expect(estEnAlerteTauxAvancementNonCalculé).toBeTruthy();
+    });
+
+    test("le chantier n'est pas en alerte si le taux d'avancement est non null", () => {
+      // Given
+      const tauxAvancement = 55;
+
+      // When
+      const estEnAlerteTauxAvancementNonCalculé = Alerte.estEnAlerteTauxAvancementNonCalculé(tauxAvancement);
+
+      // Then
+      expect(estEnAlerteTauxAvancementNonCalculé).toBeFalsy();
+    });
+  });
 });
