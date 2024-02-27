99b1aaa647a82a07f94e2ff50725276a115ce3ea
contitri <tristan.conti@seenovate.com>
2024-02-27 14:18:50 +0000

    feat: ajoute un accordéon pour la saisie des objectifs (PIL-27)

--- src/client/components/_commons/Objectifs/Objectifs.tsx
+++ src/client/components/_commons/Objectifs/Objectifs.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { consignesDÉcritureObjectif, libellésTypesObjectif, TypeObjectif } from '@/client/constants/libellésObjectif';
 import Bloc from '@/components/_commons/Bloc/Bloc';
 import Publication from '@/components/_commons/Publication/Publication';
@@ -6,29 +5,46 @@ import { ObjectifsProps } from './Objectifs.interface';
 
 export default function Objectifs({ objectifs, réformeId, maille, nomTerritoire, typesObjectif, modeÉcriture = false, estInteractif = true }: ObjectifsProps) {
   return (
-    <Bloc titre={nomTerritoire}>
+    <Bloc 
+      contenuClassesSupplémentaires=''
+      titre={nomTerritoire} 
+    >
       {
-          typesObjectif.map((type, i ) => (
-            <Fragment key={type}>
-              {
-                i !== 0 && (
-                  <hr className='fr-hr fr-mx-n2w' />
-                )
-              }
-              <Publication
-                caractéristiques={{
-                  type: type,
-                  entité: 'objectifs',
-                  libelléType: libellésTypesObjectif[type as TypeObjectif],
-                  consigneDÉcriture: consignesDÉcritureObjectif[type as TypeObjectif],
-                }}
-                estInteractif={estInteractif}
-                maille={maille}
-                modeÉcriture={modeÉcriture}
-                publicationInitiale={objectifs?.find(objectif => objectif?.type === type) || null}
-                réformeId={réformeId}
-              />
-            </Fragment>
+          typesObjectif.map((type) => (
+            <section 
+              className='fr-accordion' 
+              key={type}
+            >
+              <h3 className='fr-accordion__title'>
+                <button 
+                  aria-controls={`accordion-${type}`}
+                  aria-expanded='false'
+                  className='fr-accordion__btn'
+                  type='button'
+                >
+                  {libellésTypesObjectif[type as TypeObjectif]}
+                </button>
+              </h3>
+              <div
+                className='fr-collapse' 
+                id={`accordion-${type}`}
+              >
+                <Publication
+                  caractéristiques={{
+                    type: type,
+                    entité: 'objectifs',
+                    libelléType: libellésTypesObjectif[type as TypeObjectif],
+                    consigneDÉcriture: consignesDÉcritureObjectif[type as TypeObjectif],
+                  }}
+                  estInteractif={estInteractif}
+                  maille={maille}
+                  modeÉcriture={modeÉcriture}
+                  publicationInitiale={objectifs?.find(objectif => objectif?.type === type) || null}
+                  réformeId={réformeId}
+                />
+              </div>
+            </section>
+
           ))
         }
     </Bloc>
