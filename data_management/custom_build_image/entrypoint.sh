50a2558b48ecdbc735be7f36d3ececc5ea86de47
Colin Dugeai <colin.dugeai@gmail.com>
2024-02-29 12:07:49 +0100

    update

--- data_management/custom_build_image/entrypoint.sh
+++ data_management/custom_build_image/entrypoint.sh
@@ -1,13 +1,13 @@
 set -o xtrace
 
-# TODO: curl S3 endpoint to get env file
-
+source .env
 echo 'drop database if exists :"v1"' | psql -d $CONTROL_DATABASE_URL -v v1=$PGDATABASE
 echo 'create database :"v1"' | psql -d $CONTROL_DATABASE_URL -v v1=$PGDATABASE
 
-rm -f .env
+# TODO: curl S3 endpoint to get env file
+#echo "" > .env
 
-PIPENV_DONT_LOAD_ENV=1 pipenv run /bin/bash scripts/run_datajobs.sh
+pipenv run /bin/bash scripts/run_datajobs.sh
 
 cd ..
 npm run start
