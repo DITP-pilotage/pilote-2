010acdac5f93c1cee2cce0a3c47d0f63d8520578
Colin Dugeai <colin.dugeai@gmail.com>
2024-02-29 16:30:01 +0100

    update

--- data_management/custom_build_image/entrypoint.sh
+++ data_management/custom_build_image/entrypoint.sh
@@ -7,6 +7,12 @@ echo 'create database :"v1"' | psql -d $CONTROL_DATABASE_URL -v v1=$PGDATABASE
 # TODO: curl S3 endpoint to get env file
 #echo "" > .env
 
+sed -i 's/FORCE_ENVIRONMENT_DATAJOBS=.*/FORCE_ENVIRONMENT_DATAJOBS=LOCAL/g' .env
+pipenv run /bin/bash scripts/run_datajobs.sh
+
+#TODO: descente de prod
+
+sed -i 's/FORCE_ENVIRONMENT_DATAJOBS=.*/FORCE_ENVIRONMENT_DATAJOBS=DEV/g' .env
 pipenv run /bin/bash scripts/run_datajobs.sh
 
 cd ..
