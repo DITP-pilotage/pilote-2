9f15b9332b210ee6fa054b4066b545ff70df79f3
Colin Dugeai <colin.dugeai@gmail.com>
2024-02-29 11:04:59 +0100

    update

--- data_management/custom_build_image/entrypoint.sh
+++ data_management/custom_build_image/entrypoint.sh
@@ -5,7 +5,9 @@ set -o xtrace
 echo 'drop database if exists :"v1"' | psql -d $CONTROL_DATABASE_URL -v v1=$PGDATABASE
 echo 'create database :"v1"' | psql -d $CONTROL_DATABASE_URL -v v1=$PGDATABASE
 
-pipenv run /bin/bash scripts/run_datajobs.sh
+rm -f .env
+
+PIPENV_DONT_LOAD_ENV=1 pipenv run /bin/bash scripts/run_datajobs.sh
 
 cd ..
 npm run start
