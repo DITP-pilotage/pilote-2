bded8cd8f3aef9f8202c465e026d71256c0ae12c
contitri <tristan.conti@seenovate.com>
2024-03-11 14:21:00 +0000

    feat: message d'erreur lorsque le mail ne termine pas par .gouv.fr (PIL-134)

--- src/validation/utilisateur.ts
+++ src/validation/utilisateur.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 import { profilsCodes } from '@/server/domain/utilisateur/Utilisateur.interface';
 
+const customErrorMail = "Vous essayez de créer un compte pour une adresse ne relevant pas du périmètre de l’Etat. Veuillez contacter support.ditp@modernisation.gouv.fr pour plus d’informations"
+
 const customErrorMap: z.ZodErrorMap = (issue, ctx) => {
   if (issue.code === z.ZodIssueCode.invalid_string && issue.validation === 'email') {
     return { message: "L'adresse électronique saisie n'est pas valide" };
@@ -24,7 +26,7 @@ const customErrorMap: z.ZodErrorMap = (issue, ctx) => {
 z.setErrorMap(customErrorMap);
 
 export const validationInfosBaseUtilisateur = z.object( {
-  email: z.string().email().min(1).max(100),
+  email: z.string().email().min(1).max(100).refine((value) => value.endsWith(".gouv.fr"), { message : customErrorMail }),
   nom: z.string().min(1).max(100),
   prénom: z.string().min(1).max(100),
   fonction: z.string().max(100).nullable(),
