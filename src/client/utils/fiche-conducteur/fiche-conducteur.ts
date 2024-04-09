83a34937487818b1f48d451bf4f35d3ba1ed1e52
Jordan Taillefer <jtaillefer02@gmail.com>
2024-04-09 14:50:12 +0200

    fix: modification droit fiche conducteur (PIL-229)

--- src/client/utils/fiche-conducteur/fiche-conducteur.ts
+++ src/client/utils/fiche-conducteur/fiche-conducteur.ts
@@ -1,4 +1,4 @@
-const PROFIL_AUTORISE_A_CONSULTER  = new Set(['DITP_ADMIN', 'DITP_PILOTAGE', 'DIR_PROJET', 'EQUIPE_DIR_PROJET']);
+const PROFIL_AUTORISE_A_CONSULTER  = new Set(['DITP_ADMIN', 'DITP_PILOTAGE', 'PR', 'PM_ET_CABINET', 'CABINET_MTFP', 'DIR_PROJET', 'EQUIPE_DIR_PROJET', 'CABINET_MINISTERIEL']);
 
 export function estAutoriséAConsulterLaFicheConducteur(profil: string): boolean {
   return PROFIL_AUTORISE_A_CONSULTER.has(profil);
--- src/pages/chantier/[id]/fiche-conducteur.tsx
+++ src/pages/chantier/[id]/fiche-conducteur.tsx
@@ -5,16 +5,16 @@ import Head from 'next/head';
 import { authOptions } from '@/server/infrastructure/api/auth/[...nextauth]';
 import { FicheConducteurContrat } from '@/server/fiche-conducteur/app/contrats/FicheConducteurContrat';
 import { ficheConducteurHandler } from '@/server/fiche-conducteur/infrastructure/handlers/FicheConducteurHandler';
-import { estAutoriséAConsulterLaFicheTerritoriale } from '@/client/utils/fiche-territoriale/fiche-territoriale';
 import { PageFicheConducteur } from '@/components/PageFicheConducteur/PageFicheConducteur';
 import { RécupérerVariableContenuUseCase } from '@/server/gestion-contenu/usecases/RécupérerVariableContenuUseCase';
+import { estAutoriséAConsulterLaFicheConducteur } from '@/client/utils/fiche-conducteur/fiche-conducteur';
 
 export const getServerSideProps: GetServerSideProps<FicheConducteurContrat> = async ({ req, res, query }) => {
   const session = await getServerSession(req, res, authOptions);
 
   const estFicheConducteurDisponible = await new RécupérerVariableContenuUseCase().run({ nomVariableContenu: 'NEXT_PUBLIC_FF_FICHE_CONDUCTEUR' });
 
-  if (!estFicheConducteurDisponible || !session || !estAutoriséAConsulterLaFicheTerritoriale(session.profil)) {
+  if (!estFicheConducteurDisponible || !session || !estAutoriséAConsulterLaFicheConducteur(session.profil)) {
     throw new Error('Not connected or not authorized ?');
   }
 
