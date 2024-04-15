e02582f90f888498b994a91a50ed934194876ab3
Colin Dugeai <colin.dugeai@gmail.com>
2024-04-15 12:01:33 +0200

    baro_meta_indicateurs: filter indics baro

--- data_management/data_factory/models/barometre/baro_meta_indicateurs.sql
+++ data_management/data_factory/models/barometre/baro_meta_indicateurs.sql
@@ -3,3 +3,5 @@ indic_id ,
 null as indic_nom, coalesce(indic_nom_baro, indic_nom) as indic_nom_baro, 
 null as indic_descr, coalesce(indic_descr_baro, indic_descr) as indic_descr_baro
 from {{ ref('metadata_indicateurs') }} mi
+where indic_is_baro
+order by indic_id 
