33e9bf852d34ac8534f2ff294bcd93ddb4ccb5c6
Colin Dugeai <colin.dugeai@gmail.com>
2024-04-15 17:31:39 +0200

    baro_meta_indicateurs: add col indic_parent_ch

--- data_management/data_factory/models/barometre/baro_meta_indicateurs.sql
+++ data_management/data_factory/models/barometre/baro_meta_indicateurs.sql
@@ -1,5 +1,6 @@
 select 
-indic_id , 
+indic_id,
+indic_parent_ch,
 null as indic_nom, coalesce(indic_nom_baro, indic_nom) as indic_nom_baro, 
 null as indic_descr, coalesce(indic_descr_baro, indic_descr) as indic_descr_baro
 from {{ ref('metadata_indicateurs') }} mi
