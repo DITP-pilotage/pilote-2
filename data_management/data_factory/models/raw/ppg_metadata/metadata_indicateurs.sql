f20b9eb63a86e9cd2aa10530893d952d0f5db2be
Tristan Conti <86486650+contitri@users.noreply.github.com>
2024-04-23 14:30:55 +0200

    Revert "Revert: decommissionnement ppg metadata indicateurs (PIL-190)"
    This reverts commit 4492965ee845d759a1cf3b1ec3d6c024ab8bd2ab.

--- data_management/data_factory/models/raw/ppg_metadata/metadata_indicateurs.sql
+++ data_management/data_factory/models/raw/ppg_metadata/metadata_indicateurs.sql
@@ -1,3 +1,3 @@
 {{ config(pre_hook="{{drop_indic_just_hidden(this)}}") }}
 
-select * from {{ ref('metadata_indicateurs_hidden') }} where not coalesce(indic_hidden_pilote::text::boolean, false)
+select * from {{ source('import_from_files', 'metadata_indicateurs_hidden') }} where not coalesce(indic_hidden_pilote::text::boolean, false)
--- data_management/data_factory/models/staging/ppg_metadata/stg_ppg_metadata__parametrage_indicateurs.sql
+++ data_management/data_factory/models/staging/ppg_metadata/stg_ppg_metadata__parametrage_indicateurs.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ ref('metadata_parametrage_indicateurs') }}
+    select * from {{ source('import_from_files', 'metadata_parametrage_indicateurs') }}
 
 ),
 
