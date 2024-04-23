9e967a1840eabb3ebf13a1ad4b238ea85352b754
Jordan Taillefer <jtaillefer02@gmail.com>
2024-04-23 17:12:11 +0200

    refactor: activation nouvelle page accueil (PIL-204)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,101 +2,31 @@ import { getServerSession } from 'next-auth/next';
 import Head from 'next/head';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { FunctionComponent } from 'react';
-import { dependencies } from '@/server/infrastructure/Dependencies';
-import Ministère from '@/server/domain/ministère/Ministère.interface';
-import Axe from '@/server/domain/axe/Axe.interface';
 import { authOptions } from '@/server/infrastructure/api/auth/[...nextauth]';
-import PageAccueil from '@/components/PageAccueil/PageAccueil';
-import { ProjetStructurantVueDEnsemble } from '@/server/domain/projetStructurant/ProjetStructurant.interface';
-import RécupérerListeProjetsStructurantsVueDEnsembleUseCase
-  from '@/server/usecase/projetStructurant/RécupérerListeProjetsStructurantsVueDEnsembleUseCase';
-import RécupérerChantiersAccessiblesEnLectureUseCase
-  from '@/server/usecase/chantier/RécupérerChantiersAccessiblesEnLectureUseCase';
-import { RécupérerVariableContenuUseCase } from '@/server/gestion-contenu/usecases/RécupérerVariableContenuUseCase';
-import Chantier from '@/server/domain/chantier/Chantier.interface';
 
-interface NextPageAccueilProps {
-  chantiers: Chantier[]
-  projetsStructurants: ProjetStructurantVueDEnsemble[]
-  ministères: Ministère[]
-  axes: Axe[],
-  estProjetStructurantDisponible: boolean,
-}
-
-export const getServerSideProps: GetServerSideProps<NextPageAccueilProps> = async ({ req, res }) => {
+export const getServerSideProps: GetServerSideProps<{}> = async ({ req, res }) => {
   const session = await getServerSession(req, res, authOptions);
 
-  if (!session || !session.habilitations) {
+  if (session) {
     return {
-      props: {
-        chantiers: [],
-        projetsStructurants: [],
-        ministères: [],
-        axes: [],
-        estProjetStructurantDisponible: false,
+      redirect: {
+        permanent: true,
+        destination: '/accueil/chantier/NAT-FR',
       },
     };
   }
-
-  const chantiers = await new RécupérerChantiersAccessiblesEnLectureUseCase(
-    dependencies.getChantierRepository(),
-    dependencies.getChantierDatesDeMàjRepository(),
-    dependencies.getMinistèreRepository(),
-    dependencies.getTerritoireRepository(),
-  )
-    .run(session.habilitations, session.profil);
-
-  const projetsStructurants: ProjetStructurantVueDEnsemble[] = await new RécupérerListeProjetsStructurantsVueDEnsembleUseCase(
-    dependencies.getProjetStructurantRepository(),
-    dependencies.getTerritoireRepository(),
-    dependencies.getSynthèseDesRésultatsProjetStructurantRepository(),
-  ).run(session.habilitations, session.profil);
-
-  const chantierIds = chantiers.map(chantier => chantier.id);
-
-  const [ministères, axes] = await Promise.all(
-    [
-      dependencies.getMinistèreRepository().getListePourChantiers(chantierIds),
-      dependencies.getAxeRepository().getListePourChantiers(chantierIds),
-    ],
-  );
-
-  const estProjetStructurantDisponible = new RécupérerVariableContenuUseCase().run({ nomVariableContenu: 'NEXT_PUBLIC_FF_PROJETS_STRUCTURANTS' });
-
   return {
-    props: {
-      chantiers,
-      projetsStructurants,
-      ministères,
-      axes,
-      estProjetStructurantDisponible: !!estProjetStructurantDisponible,
-    },
+    props: {},
   };
 };
 
-const NextPageAccueil: FunctionComponent<InferGetServerSidePropsType<typeof getServerSideProps>> = ({
-  chantiers,
-  projetsStructurants,
-  ministères,
-  axes,
-  estProjetStructurantDisponible,
-}) => {
-
+const NextPageAccueil: FunctionComponent<InferGetServerSidePropsType<typeof getServerSideProps>> = () => {
   return (
-    <>
-      <Head>
-        <title>
-          PILOTE - Piloter l’action publique par les résultats
-        </title>
-      </Head>
-      <PageAccueil
-        axes={axes}
-        chantiers={chantiers}
-        estProjetStructurantDisponible={estProjetStructurantDisponible}
-        ministères={ministères}
-        projetsStructurants={projetsStructurants}
-      />
-    </>
+    <Head>
+      <title>
+        PILOTE - Piloter l’action publique par les résultats
+      </title>
+    </Head>
   );
 };
 
