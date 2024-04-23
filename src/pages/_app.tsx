39f4cea96b4cf0b3fca92d064a1de9bcc41211c6
Jordan Taillefer <jtaillefer02@gmail.com>
2024-04-23 11:27:31 +0200

    refactor: restore loader ancienne accueil (PIL-207)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,41 +7,85 @@ import '@/client/styles/app.scss';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import { SessionProvider } from 'next-auth/react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Head from 'next/head';
 import { TRPCClientError } from '@trpc/client';
 import init from '@socialgouv/matomo-next';
+import { Router, useRouter } from 'next/router';
 import MiseEnPage from '@/client/components/_commons/MiseEnPage/MiseEnPage';
 import useDétecterLargeurDÉcran from '@/client/hooks/useDétecterLargeurDÉcran';
 import api from '@/server/infrastructure/api/trpc/api';
 
 const queryClient = new QueryClient({
-  defaultOptions: { 
-    queries: { 
-      refetchOnWindowFocus: false, 
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
       retry: (failureCount, error) => {
         if (error instanceof TRPCClientError && error?.data?.code === 'UNAUTHORIZED') {
           return false;
         }
         return failureCount < 3;
       },
-    }, 
-  }, 
+    },
+  },
 });
 
+const DELAI_AVANT_APPARITION_DU_LOADER_EN_MS = 500;
+
 function MonApplication({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const estNouvellePageAccueil = router.pathname.startsWith('/accueil');
+
   useDétecterLargeurDÉcran();
+  const [afficherLeLoader, setAfficherLeLoader] = useState(false);
+  const [pageEnCoursDeChargement, setPageEnCoursDeChargement] = useState(false);
+
+  const débutChargement = () => {
+    setPageEnCoursDeChargement(true);
+  };
+  const finChargement = () => {
+    setPageEnCoursDeChargement(false);
+  };
+
+  useEffect(() => {
+    if (!estNouvellePageAccueil) {
+      Router.events.on('routeChangeStart', débutChargement);
+      Router.events.on('routeChangeComplete', finChargement);
+      Router.events.on('routeChangeError', finChargement);
+    }
+    return () => {
+      Router.events.off('routeChangeStart', débutChargement);
+      Router.events.off('routeChangeComplete', finChargement);
+      Router.events.off('routeChangeError', finChargement);
+    };
+  }, [estNouvellePageAccueil]);
 
   const matomoUrl = process.env.NEXT_PUBLIC_MATOMO_URL;
   const matomoSiteId = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
   const estRecordAnalyticsActive = process.env.NEXT_PUBLIC_RECORD_ANALYTICS;
 
   useEffect(() => {
-    if (estRecordAnalyticsActive === 'true') {
-      init({ url: matomoUrl as string, siteId: matomoSiteId as string  });
+    if (!estNouvellePageAccueil && estRecordAnalyticsActive === 'true') {
+      init({ url: matomoUrl as string, siteId: matomoSiteId as string });
     }
-  }, [estRecordAnalyticsActive, matomoSiteId, matomoUrl]);
+  }, [estNouvellePageAccueil, estRecordAnalyticsActive, matomoSiteId, matomoUrl]);
+
+  useEffect(() => {
+    if (!estNouvellePageAccueil) {
+      let timer = setTimeout(() => {});
+
+      if (pageEnCoursDeChargement)
+        timer = setTimeout(() => setAfficherLeLoader(true), DELAI_AVANT_APPARITION_DU_LOADER_EN_MS);
+      else {
+        clearTimeout(timer);
+        setAfficherLeLoader(false);
+      }
+
+      return () => clearTimeout(timer);
+    }
+  }, [estNouvellePageAccueil, pageEnCoursDeChargement]);
 
   return (
     <>
@@ -79,7 +123,7 @@ function MonApplication({ Component, pageProps }: AppProps) {
       </Head>
       <QueryClientProvider client={queryClient}>
         <SessionProvider session={pageProps.session}>
-          <MiseEnPage afficherLeLoader={false}>
+          <MiseEnPage afficherLeLoader={afficherLeLoader}>
             <Component {...pageProps} />
           </MiseEnPage>
         </SessionProvider>
