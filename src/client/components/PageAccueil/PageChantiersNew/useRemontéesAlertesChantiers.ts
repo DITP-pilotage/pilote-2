ccf8e49ad793d060cce6a655ac91c7e22fa0b91a
ZakariaSeenovate <zakaria.belghit@seenovate.com>
2024-05-14 14:25:20 +0000

    feat: affichage alerte page chantier (PIL-242)

--- src/client/components/PageAccueil/PageChantiersNew/useRemontéesAlertesChantiers.ts
+++ src/client/components/PageAccueil/PageChantiersNew/useRemontéesAlertesChantiers.ts
@@ -13,39 +13,55 @@ export function useRemontéesAlertesChantiers(territoireCode: string, filtresCom
     estEnAlerteMétéoNonRenseignée: parseAsBoolean.withDefault(false),
   });
 
+  const estEnAlerteTauxAvancementNonCalculé = {
+    nomCritère: 'estEnAlerteTauxAvancementNonCalculé',
+    libellé: 'Taux d’avancement non calculé(s) en raison d’indicateurs non renseignés',
+    nombre: filtresComptesCalculés.estEnAlerteTauxAvancementNonCalculé.nombre,
+    estActivée: filtresAlertes.estEnAlerteTauxAvancementNonCalculé,
+  }; 
+  
+  const estEnAlerteÉcart = {
+    nomCritère: 'estEnAlerteÉcart',
+    libellé: 'Retard supérieur de 10 points par rapport à la moyenne nationale',
+    nombre: filtresComptesCalculés.estEnAlerteÉcart.nombre,
+    estActivée: filtresAlertes.estEnAlerteÉcart,
+  };
+
+  const estEnAlerteBaisseOuStagnation = {
+    nomCritère: 'estEnAlerteBaisseOuStagnation',
+    libellé: 'Tendance(s) en baisse ou en stagnation',
+    nombre: filtresComptesCalculés.estEnAlerteBaisseOuStagnation.nombre,
+    estActivée: filtresAlertes.estEnAlerteBaisseOuStagnation,
+  };
+  
+  const estEnAlerteDonnéesNonMàj = {
+    nomCritère: 'estEnAlerteDonnéesNonMàj',
+    libellé: 'Météo(s) ou commentaire(s) non renseigné(s) ou non mis à jour',
+    nombre: filtresComptesCalculés.estEnAlerteDonnéesNonMàj.nombre,
+    estActivée: filtresAlertes.estEnAlerteDonnéesNonMàj,
+  };
+
+  const estEnAlerteMétéoNonRenseignée = {
+    nomCritère: 'estEnAlerteMétéoNonRenseignée',
+    libellé: 'Météo(s) et synthèse(s) des résultats non renseigné(s)',
+    nombre: filtresComptesCalculés.estEnAlerteMétéoNonRenseignée.nombre,
+    estActivée: filtresAlertes.estEnAlerteMétéoNonRenseignée,
+  };
+
+  const alerteChantierMailleNationale = [
+    estEnAlerteTauxAvancementNonCalculé, 
+    estEnAlerteBaisseOuStagnation,
+    estEnAlerteDonnéesNonMàj,
+  ];
+
+  const alerteChantierMailleDepRep = [
+    estEnAlerteÉcart,
+    estEnAlerteBaisseOuStagnation,
+    estEnAlerteMétéoNonRenseignée,
+  ];
+
   return {
-    remontéesAlertes: [
-      mailleChantier === 'nationale'
-        ? {
-          nomCritère: 'estEnAlerteTauxAvancementNonCalculé',
-          libellé: 'Taux d’avancement non calculé(s) en raison d’indicateurs non renseignés',
-          nombre: filtresComptesCalculés.estEnAlerteTauxAvancementNonCalculé.nombre,
-          estActivée: filtresAlertes.estEnAlerteTauxAvancementNonCalculé,
-        } 
-        : {
-          nomCritère: 'estEnAlerteÉcart',
-          libellé: 'Retard supérieur de 10 points par rapport à la moyenne nationale',
-          nombre: filtresComptesCalculés.estEnAlerteÉcart.nombre,
-          estActivée: filtresAlertes.estEnAlerteÉcart,
-        },
-      {
-        nomCritère: 'estEnAlerteBaisseOuStagnation',
-        libellé: 'Tendance(s) en baisse ou en stagnation',
-        nombre: filtresComptesCalculés.estEnAlerteBaisseOuStagnation.nombre,
-        estActivée: filtresAlertes.estEnAlerteBaisseOuStagnation,
-      },
-      {
-        nomCritère: 'estEnAlerteDonnéesNonMàj',
-        libellé: 'Météo(s) ou commentaire(s) non renseigné(s) ou non mis à jour',
-        nombre: filtresComptesCalculés.estEnAlerteDonnéesNonMàj.nombre,
-        estActivée: filtresAlertes.estEnAlerteDonnéesNonMàj,
-      },
-      {
-        nomCritère: 'estEnAlerteMétéoNonRenseignée',
-        libellé: 'Météo(s) non renseignée(s)',
-        nombre: filtresComptesCalculés.estEnAlerteMétéoNonRenseignée.nombre,
-        estActivée: filtresAlertes.estEnAlerteMétéoNonRenseignée,
-      },
-    ],
+    remontéesAlertes: mailleChantier === 'nationale' ?  alerteChantierMailleNationale  :  alerteChantierMailleDepRep, 
+    
   };
 }
