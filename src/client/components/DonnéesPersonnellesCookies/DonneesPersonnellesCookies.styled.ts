e833d66c68cd20c3729d4a685381cf8c3f38d4e7
Zakaria Seenovate <168171157+ZakariaSeenovate@users.noreply.github.com>
2024-05-15 10:00:33 +0200

    feat: creation cookies page (PIL-245) (#894)

---
+++ src/client/components/DonnéesPersonnellesCookies/DonneesPersonnellesCookies.styled.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const DonneesPersonnellesCookiesStyled = styled.div`
+    main { 
+        background: var(--background-alt-blue-france);
+    };
+    
+    .container {
+        max-width: 920px;
+    }
+
+    h1, h2 {
+        color: var(--text-title-blue-france);
+    };
+`;
+
+export default DonneesPersonnellesCookiesStyled;
