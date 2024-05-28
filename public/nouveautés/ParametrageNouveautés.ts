c11a0a7704cdbc1873406990143d697b003c2406
JBcaut <106659044+JBcaut@users.noreply.github.com>
2024-05-28 16:04:11 +0200

    Update ParametrageNouveautés.ts

--- public/nouveautés/ParametrageNouveautés.ts
+++ public/nouveautés/ParametrageNouveautés.ts
@@ -7,35 +7,36 @@ interface Nouveautés {
 }
 
 export const ParametrageNouveautés: Nouveautés[] = [
-  {
-    version: 'Version 2.4.0',
-    date: '15 novembre 2022',
-    lienCentreAide: 'https://dev-pilote-ditp.osc-secnum-fr1.scalingo.io/centreaide/demarrer-avec-pilote/presentation_de_la_page_daccueil',
-    contenu: 'Colorisation des cartes : toutes les cartes qui sont liées à un taux d’avancement sont dorénavant coloriées différemment afin d’avoir un meilleur aperçu du taux suivant les territoires. La couleur attribuée à un territoire correspond au taux d’avancement du territoire par tranche de 10% comme dans le bloc Distribution des taux d’avancement de la page d’accueil où les mêmes couleurs ont aussi été utilisées. La légende sous les cartes a été adaptée en conséquence.',
-    correctifs: [
-      'Certains projets structurants n’apparaissaient pas pour certains utilisateurs.',
-      'Correction d’un problème d’affichage de la légende des cartes des météos sur mobile.',
-      'Correctif de la dernière version',
-    ],
+  { version: 'Version 2.4.4'
+   date: '27 mai 2024'
+contenu : 'Exports des données : les mailles applicables à chaque chantier sont prises en compte dans les exports .csv. générés à partir du bouton "Exporter les données" sur la page d'accueil',
+correctifs : [ 
+  'Navigation entre les pages de PILOTE : les filtres sélectionnés sur la page d'accueil sont conservés même lorsque l'on navigue entre plusieurs pages.',
+  'Rapport détaillé : il peut désormais être téléchargé à tous les niveaux du territoire, y compris avec le détail des chantiers.',
+  ],
   },
-  {
-    version: 'Version 2.3.0',
-    date: '15 octobre 2022',
-    contenu: 'Colorisation des cartes : toutes les cartes qui sont liées à un taux d’avancement sont dorénavant coloriées différemment afin d’avoir un meilleur aperçu du taux suivant les territoires. La couleur attribuée à un territoire correspond au taux d’avancement du territoire par tranche de 10% comme dans le bloc Distribution des taux d’avancement de la page d’accueil où les mêmes couleurs ont aussi été utilisées. La légende sous les cartes a été adaptée en conséquence.',
-    correctifs: [
-      'Certains projets structurants n’apparaissaient pas pour certains utilisateurs.',
-      'Correction d’un problème d’affichage de la légende des cartes des météos sur mobile.',
-    ],
+
+ { version: 'Version 2.4.3'
+   date: '22 mai 2024'
+contenu : 'Exports des données : pour chaque PPG, les axes sont disponibles dans l'export.', 
+  'Affichage des données : les "référents PILOTE" sont dorénavant renommés en tant que coordinateur PILOTE départemental ou régional.',
   },
-  {
-    version: 'Version 2.2.0',
-    date: '15 mai 2022',
-    lienCentreAide: 'https://dev-pilote-ditp.osc-secnum-fr1.scalingo.io/centreaide/demarrer-avec-pilote/presentation_de_la_page_daccueil',
-    contenu: 'Colorisation des cartes : toutes les cartes qui sont liées à un taux d’avancement sont dorénavant coloriées différemment afin d’avoir un meilleur aperçu du taux suivant les territoires. La couleur attribuée à un territoire correspond au taux d’avancement du territoire par tranche de 10% comme dans le bloc Distribution des taux d’avancement de la page d’accueil où les mêmes couleurs ont aussi été utilisées. La légende sous les cartes a été adaptée en conséquence.',
-    correctifs: [
-      'Certains projets structurants n’apparaissaient pas pour certains utilisateurs.',
-      'Correction d’un problème d’affichage de la légende des cartes des météos sur mobile.',
-      'Un autre correctif',
+
+{ version: 'Version 2.4.2'
+   date: '14 mai 2024'
+contenu : 'Gestion des indicateurs : une interface de gestion des indicateurs des PPG est désormais disponible (pour les profils DITP uniquement)', 
+
+correctifs : [ 
+  'Calcul des taux d'avancement : Si des données sont manquantes pour des indicateurs pris en compte dans le calcul du taux d'avancement de la PPG, alors le taux d'avancement de la PPG n'est plus calculé et apparaît comme non renseigné.',
+  'Groupement des taux d'avancement par ministère sur la page d'accueil : les PPG ne sont plus groupés par ministères par défaut sur la page d'accueil.',
+  'Navigation sur PILOTE : des améliorations techniques ont été apportées afin de diminuer considérablement le temps de chargement des pages.
+  ],
+  },
+
+{ version: 'Version 2.4.2'
+   date: '9 avril 2024'
+contenu : 'Gestion des comptes : la fonctionnalité de gestion des comptes utilisateurs par les coordinateurs PILOTE est disponible. Elle sera déployée progressivement à tous les coordinateurs de chaque territoire.',
+  'Exports des données : les exports .csv réalisés à partir du bouton "exporter les données" peuvent prendre en compte les filtres sélectionnés sur la page d'accueil.',
     ],
   },
 ];
