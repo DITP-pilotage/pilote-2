8bf9beb22a8078f4536bfa8a1ef968801b924b47
Jordan Taillefer <jtaillefer02@gmail.com>
2024-05-28 12:47:47 +0200

    feat: ajout du endpoint healthcheck securise (PIL-217)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,47 @@
 import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+import { TokenAPIJWTService } from '@/server/authentification/infrastructure/adapters/services/TokenAPIJWTService';
 
 const pages = { signIn: '/' };
 
-export default withAuth(
-  function middleware(request) {
-    const response = NextResponse.next();
+export async function middleware(request: NextRequest, event: NextFetchEvent) {
+  if (!request.nextUrl.pathname.startsWith('/api/open-api')) {
+    const authMiddleware = withAuth(
+      function middleware2(requestAuth) {
+        const response = NextResponse.next();
 
-    const cookie = request.cookies.get('csrf');
+        const cookie = requestAuth.cookies.get('csrf');
 
-    if (request.nextauth.token && (cookie === undefined || cookie !== request.nextauth.token.jti)) {
-      const jti = String(request.nextauth.token.jti);
-      response.cookies.set('csrf', jti, { sameSite: 'lax' });
+        if (requestAuth.nextauth.token && (cookie === undefined || cookie !== requestAuth.nextauth.token.jti)) {
+          const jti = String(requestAuth.nextauth.token.jti);
+          response.cookies.set('csrf', jti, { sameSite: 'lax' });
 
-      return response;
+          return response;
+        }
+      },
+      { pages },
+    );
+
+    // @ts-expect-error
+    return authMiddleware(request, event);
+  } else {
+    if (!request.headers.get('Authorization')) {
+      return Response.json(
+        { success: false, message: 'Il vous manque le header Authorization avec le token API' },
+        { status: 401 },
+      );
     }
-  }, 
-  { pages },
-);
+    try {
+      const token = request.headers.get('Authorization')!;
+      await new TokenAPIJWTService({ secret: process.env.TOKEN_API_SECRET! }).decoderTokenAPI(token.split(' ')[1]);
+    } catch {
+      return Response.json(
+        { success: false, message: "Le token n'a pas pu être décodé, assuré vous qu'il soit dans le format 'Bearer <<TokenAPI>>'" },
+        { status: 401 },
+      );
+    }
+  }
+}
 
 export const config = {
   // s'applique à toutes les urls sauf / - ^/js/ - _next/static - _next/image - favicon.ico
