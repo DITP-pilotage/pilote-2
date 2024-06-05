14431a9ae0c8de2d85cf406708010b3deafed01a
Colin Dugeai <colin.dugeai@gmail.com>
2024-06-05 14:41:29 +0200

    fix: lower tendance (PIL-302)
    pour coller à la spec actuelle

--- data_management/data_factory/models/barometre/baro_meta_indicateurs.sql
+++ data_management/data_factory/models/barometre/baro_meta_indicateurs.sql
@@ -5,7 +5,11 @@ SELECT
     COALESCE(mi.indic_nom_baro, mi.indic_nom) AS indic_nom_baro, 
     NULL AS indic_descr, 
     COALESCE(mi.indic_descr_baro, mi.indic_descr) AS indic_descr_baro,
-    mpi.tendance,
+    CASE
+        -- Si la tendance est ('HAUSSE', 'BAISSE') -> ('hausse', 'baisse')
+        WHEN mpi.tendance IN ('HAUSSE', 'BAISSE') THEN lower(mpi.tendance)
+        ELSE 'autre'
+    END as tendance
     CASE 
         WHEN mpi.param_vacg_partition_date LIKE 'from_previous_month%' THEN 'glissant' 
         WHEN mpi.param_vacg_partition_date = 'from_year_start' AND mpi.param_vacg_op = 'sum' THEN 'cumul_annuel'
