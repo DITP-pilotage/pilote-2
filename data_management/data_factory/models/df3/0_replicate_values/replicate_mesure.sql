fa99b400d5f7e1bee3c40aef06c1540903ab05ff
Colin Dugeai <colin.dugeai@gmail.com>
2024-06-10 17:20:58 +0200

    fix: fix model replicate values (PIL-246)

--- data_management/data_factory/models/df3/0_replicate_values/replicate_mesure.sql
+++ data_management/data_factory/models/df3/0_replicate_values/replicate_mesure.sql
@@ -15,34 +15,24 @@ left join df3.zone_parent b on a.replicate_maille_from=b.zone_parent_type and a.
 )
 -- Valeurs de la maille sup (X)
 , valeurs_region_src as (
-select b.chantier_id , a.*  from df3.mesure_last_null_erase_keep_lastvalmonth a
+select b.chantier_id , a.*, c.zone_id as zone_id_child  from df3.mesure_last_null_erase_keep_lastvalmonth a
 left join raw_data.stg_ppg_metadata__indicateurs b on a.indic_id =b.id
 inner join src_chantier_zones_to_replicate c on c.zone_id_parent=a.zone_id and c.chantier_id=b.chantier_id
 --where b.chantier_id ='CH-006' and a.zone_id = 'R84'
 )
 
 
--- Valeurs de la maille sup (X) bien formaté
-, valeurs_region as (
-select 
-date_import, indic_id, metric_date, metric_type, metric_value, 
-null as zone_id, 
-zone_id as zone_id_parent, 
-null as id, 
-rapport_id
- from valeurs_region_src
-)
 
--- Valeurs répliquées (dans Y)
+-- Valeurs répliquées (dans Y) bien formatées
 , replicated_values as (
 select 
 date_import, indic_id, metric_date, metric_type, metric_value, 
-b.zone_id as zone_id,
+zone_id_child as zone_id,
 gen_random_uuid() as id,
 rapport_id
 --, b.*
-from valeurs_region a
-cross join src_chantier_zones_to_replicate b
+from valeurs_region_src a
+order by date_import, indic_id, metric_date
 )
 
 
