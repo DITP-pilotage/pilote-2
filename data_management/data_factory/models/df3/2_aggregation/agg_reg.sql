4071065c2f5956bb37294f569a5bf203e170a4bd
Colin Dugeai <colin.dugeai@gmail.com>
2024-06-27 16:52:04 +0200

    feat: ajout col date_import dans agg (PIL-350)
    Ajout de la colonne date_import dans les aggrégations. Pour le moment, le colonne vaut NULL si la valeur est issue d'une aggrégation.

--- data_management/data_factory/models/df3/2_aggregation/agg_reg.sql
+++ data_management/data_factory/models/df3/2_aggregation/agg_reg.sql
@@ -7,7 +7,7 @@ with
 --	et on y ajoute les paramètres d'aggrégation REG
 mesure_last_params_reg as (
     select 
-        a.id, a.indic_id , metric_date, metric_type, metric_value , zone_id,
+        a.id, a.date_import, a.indic_id , metric_date, metric_type, metric_value , zone_id,
         b.vi_reg_from , b.vi_reg_op , b.va_reg_from, b.va_reg_op, b.vc_reg_from , b.vc_reg_op 
     from {{ ref('mesure_last_null_erase_keep_lastvalmonth') }} a
     left join {{ source('parametrage_indicateurs', 'metadata_parametrage_indicateurs') }} b 
@@ -41,7 +41,7 @@ indic_agg_from_dept as (
 --  et on sélectionne que les valeurs qui sont DEPT avec un parent REG
 -- Ce sont ces données que la DF va aggréger
 mesure_last_params_reg_from_dept as (
-	select a.id, a.indic_id as indic_id1 , metric_date , metric_type , metric_value::float, a.zone_id,
+	select a.id, a.date_import, a.indic_id as indic_id1 , metric_date , metric_type , metric_value::float, a.zone_id,
 	b.zone_type, b.zone_parent , b.zone_parent_type ,
 	c.*
 	from {{ ref('mesure_last_null_erase_keep_lastvalmonth') }} a
@@ -60,6 +60,7 @@ compute_op_sum_avg as (
 	select 
 		-- On met id=NULL lorsque la valeur est générée par aggrégation et non issue d'une mesure saisie
 		null::uuid as id, 
+		null::date as date_import,
 		zone_parent, indic_id as indic_id1, metric_date, metric_type, 
 		sum(metric_value::float) as op_sum,
 		avg(metric_value::float) as op_avg
@@ -88,6 +89,7 @@ compute_op_selected as (
 mesure_last_params_reg_aggregated as (
     select 
 		id,
+		date_import,
         indic_id,
         zone_parent as zone_id,
         metric_date, metric_type,
@@ -100,7 +102,7 @@ mesure_last_params_reg_aggregated as (
 
 
 -- On retourne donc les valeurs REG saisies, et attendues comme tel
-select id, indic_id, zone_id, metric_date, metric_type, metric_value::float from mesure_last_params_reg_user
+select id, date_import, indic_id, zone_id, metric_date, metric_type, metric_value::float from mesure_last_params_reg_user
 union
 -- ET les valeurs agg
 select * from mesure_last_params_reg_aggregated
