66b7434a34b2febd7ab194e9b014d897dae46228
contitri <tristan.conti@seenovate.com>
2024-07-02 13:31:14 +0000

    fix: liste deroulante tri page accueil (PIL-273)

---
+++ src/client/components/_commons/SelecteurCustom/SélecteurAvecRecherche/SélecteurCustom.styled.ts
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+
+const SélecteurCustomStyled = styled.div`
+  position: relative;
+  
+  div[role='menu'] {
+    display: none;
+  }
+
+  div[role='menu'].visible {
+    position: absolute;
+    z-index: 2;
+    display: block;
+    width: 100%;
+    max-height: 20rem;
+    padding: 1rem;
+    overflow: auto;
+    background: white;
+    border: 1px solid grey;
+  }
+
+  button.fr-select {
+    text-align: left;
+  }
+
+  .groupe-label {
+    color: var(--text-mention-grey);
+  }
+
+  .fr-option {
+    font-size: 1rem;
+    line-height: 1.5rem;
+  }
+
+  .fr-option-disabled {
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: var(--text-mention-grey);
+  }
+
+  .fr-option:hover {
+    color: white;
+    background-color: var(--background-active-blue-france);
+  }
+`;
+
+export default SélecteurCustomStyled;
