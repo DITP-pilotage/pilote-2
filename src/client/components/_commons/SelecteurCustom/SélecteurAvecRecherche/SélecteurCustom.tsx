58656a947b4b63175d044256d550451f051f6bd0
Tristan Conti <86486650+contitri@users.noreply.github.com>
2024-07-02 15:42:10 +0200

    fix: liste deroulante tri page accueil (PIL-273)

---
+++ src/client/components/_commons/SelecteurCustom/SélecteurAvecRecherche/SélecteurCustom.tsx
@@ -0,0 +1,75 @@
+import { Fragment, useRef } from 'react';
+import SélecteurCustomStyled from './SélecteurCustom.styled';
+import SélecteurCustomProps from './SélecteurCustom.interface';
+import useSélecteurCustom from './useSélecteurCustom';
+import '@gouvfr/dsfr/dist/component/radio/radio.css';
+import '@gouvfr/dsfr/dist/component/select/select.min.css';
+
+
+export default function SélecteurCustom<T extends string>({
+  htmlName,
+  options,
+  valeurSélectionnée,
+  valeurModifiéeCallback,
+}: SélecteurCustomProps<T>) {
+  const ref = useRef(null);
+  const {
+    estOuvert,
+    setEstOuvert,
+    SélecteurBoutonProps,
+    libelléValeurSélectionnée,
+  } = useSélecteurCustom(options, valeurSélectionnée);
+
+  return (
+    <SélecteurCustomStyled>
+      <button
+        className='fr-select fr-ellipsis'
+        id={htmlName}
+        type='button'
+        {...SélecteurBoutonProps}
+      >
+        {libelléValeurSélectionnée}
+      </button>
+      <div
+        className={estOuvert ? 'visible' : ''}
+        ref={ref}
+        role='menu'
+      >
+        {
+          options?.map(option => (
+            <Fragment key={`${option.valeur}`}>
+              {
+                !option.désactivée ?
+                  <div
+                    className='fr-option'
+                    id={option.valeur}
+                    onClick={(événement) => {
+                      setEstOuvert(false);
+                      return valeurModifiéeCallback && valeurModifiéeCallback(événement.currentTarget.id as T);
+                    }}
+                    onKeyDown={(événement) => {
+                      if ((événement.key === 'Enter' || événement.key === ' ') && !!valeurModifiéeCallback) {
+                        valeurModifiéeCallback(événement.currentTarget.id as T);
+                      }
+                    }}
+                    role='button'
+                    tabIndex={0}
+                  >
+                    {option.libellé}
+                  </div>
+                  :
+                  <div
+                    className='fr-option-disabled'
+                    id={option.valeur}
+                  >
+                    {option.libellé}
+                  </div>
+              }
+
+            </Fragment>
+          ))
+        }
+      </div>
+    </SélecteurCustomStyled>
+  );
+}
