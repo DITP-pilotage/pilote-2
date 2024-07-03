e008e6a50d0e2339bee036b06835e80092e9d094
Tristan Conti <86486650+contitri@users.noreply.github.com>
2024-07-03 11:48:04 +0200

    fix: liste deroulante tri page accueil (PIL-273)

--- src/client/components/PageAccueil/PageChantiersNew/TableauChantiers/ActionsDeTri/TableauChantiersActionsDeTri.tsx
+++ src/client/components/PageAccueil/PageChantiersNew/TableauChantiers/ActionsDeTri/TableauChantiersActionsDeTri.tsx
@@ -1,3 +1,4 @@
+import SélecteurCustom from '@/components/_commons/SelecteurCustom/SélecteurAvecRecherche/SélecteurCustom';
 import BoutonsDeTri from '@/components/_commons/Tableau/EnTête/BoutonsDeTri/BoutonsDeTri';
 import TableauChantiersActionsDeTriProps from '@/components/PageAccueil/PageChantiers/TableauChantiers/ActionsDeTri/TableauChantiersActionsDeTri.interface';
 import TableauChantiersActionsDeTriStyled from '@/components/PageAccueil/PageChantiers/TableauChantiers/ActionsDeTri/TableauChantiersActionsDeTri.styled';
@@ -5,23 +6,23 @@ import TableauChantiersActionsDeTriStyled from '@/components/PageAccueil/PageCha
 const listeColonnesÀtrier = [
   {
     libellé: 'Taux d\'avancement',
-    colonneId: 'avancement',
-    accessible: true,
+    valeur: 'avancement',
+    désactivé: false,
   },
   {
     libellé: 'Météo',
-    colonneId: 'météo',
-    accessible: true,
+    valeur: 'météo',
+    désactivé: false,
   },
   {
     libellé: 'Date de mise à jour des données',
-    colonneId: 'dateDeMàjDonnéesQuantitatives',
-    accessible: process.env.NEXT_PUBLIC_FF_TRI_DATES === 'true',
+    valeur: 'dateDeMàjDonnéesQuantitatives',
+    désactivé: process.env.NEXT_PUBLIC_FF_TRI_DATES !== 'true',
   },
   {
     libellé: 'Date de mise à jour de la météo et de la synthèse des résultats',
-    colonneId: 'dateDeMàjDonnéesQualitatives',
-    accessible: process.env.NEXT_PUBLIC_FF_TRI_DATES === 'true',
+    valeur: 'dateDeMàjDonnéesQualitatives',
+    désactivé: process.env.NEXT_PUBLIC_FF_TRI_DATES !== 'true',
   },
 ];
 
@@ -40,27 +41,12 @@ export default function TableauChantiersActionsDeTri({
         >
           Trier par
         </label>
-        <select
-          className='fr-select'
-          id='tri-tableau-chantiers'
-          name='tri-tableau-chantiers'
-          onChange={(événement) => changementColonneÀTrierCallback(événement.currentTarget.value)}
-          title='Trier par'
-          value={colonneÀTrier}
-        >
-          {
-            listeColonnesÀtrier.map(option => (
-              option.accessible &&
-              <option
-                key={option.colonneId}
-                title={option.libellé}
-                value={option.colonneId}
-              >
-                {option.libellé}
-              </option>
-            ))
-          }
-        </select>
+        <SélecteurCustom
+          htmlName='tri-tableau-chantiers'
+          options={listeColonnesÀtrier}
+          valeurModifiéeCallback={triSélectionné => changementColonneÀTrierCallback(triSélectionné)}
+          valeurSélectionnée={colonneÀTrier}
+        />
       </div>
       <div className='fr-mb-4w fr-ml-1w'>
         <BoutonsDeTri
