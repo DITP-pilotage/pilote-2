00c8c61b1999548579bf28d5ff832d182390270f
Tristan Conti <86486650+contitri@users.noreply.github.com>
2024-07-11 09:16:20 +0200

    fix: bug pondération (PIL-66)

--- src/client/utils/ponderation/ponderation.ts
+++ src/client/utils/ponderation/ponderation.ts
@@ -1,5 +1,5 @@
-export function convertitEnPondération(pondération: number | null) {
-  if (pondération !== null) {
+export function convertitEnPondération(pondération: number | null | undefined) {
+  if (pondération !== null && pondération !== undefined) {
     return Number.isInteger(pondération) ? pondération.toFixed(0) : pondération.toFixed(1);
   }
   
