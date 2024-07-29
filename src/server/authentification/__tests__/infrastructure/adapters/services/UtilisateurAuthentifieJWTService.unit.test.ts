39df03e747c57136efb3b5a62e9d09c1059d71d4
contitri <tristan.conti@seenovate.com>
2024-07-29 09:13:46 +0000

    fix: test token (PIL-313)

--- src/server/authentification/__tests__/infrastructure/adapters/services/UtilisateurAuthentifieJWTService.unit.test.ts
+++ src/server/authentification/__tests__/infrastructure/adapters/services/UtilisateurAuthentifieJWTService.unit.test.ts
@@ -62,6 +62,11 @@ describe('UtilisateurAuthentifieJWTService', () => {
         territoires: [],
         périmètres: [],
       },
+      responsabilite: {
+        chantiers: [],
+        territoires: [],
+        périmètres: [],        
+      },
       'projetsStructurants.lecture': {
         projetsStructurants: [],
       },
