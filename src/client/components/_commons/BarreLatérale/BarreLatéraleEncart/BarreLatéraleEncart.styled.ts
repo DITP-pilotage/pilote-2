0b3ee14e30b062e96014547f83117f983717d837
Tristan Conti <86486650+contitri@users.noreply.github.com>
2024-08-26 09:42:54 +0200

    fix: filtres mobiles retours recette (PIL-325)

--- src/client/components/_commons/BarreLatérale/BarreLatéraleEncart/BarreLatéraleEncart.styled.ts
+++ src/client/components/_commons/BarreLatérale/BarreLatéraleEncart/BarreLatéraleEncart.styled.ts
@@ -1,13 +1,8 @@
 import styled from '@emotion/styled';
-import { breakpointL } from '@/components/_commons/MiseEnPage/MiseEnPage.styled';
 
 const BarreLatéraleEncartStyled = styled.div`
   width: 100%;
   background-color: var(--blue-france-850-200);
-  
-    @media screen and (max-width: ${breakpointL}) {
-      background-color: #ececfe;
-    }
 
   select {
     background-color: var(--background-alt-blue-france);
