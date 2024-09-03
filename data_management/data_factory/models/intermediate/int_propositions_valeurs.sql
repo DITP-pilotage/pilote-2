9abfa8eccd4b4e7fd781fd23831e31acb2121484
contitri <tristan.conti@seenovate.com>
2024-09-03 07:01:40 +0000

    fix: auteur proposition valeur df (PIL-000)

--- data_management/data_factory/models/intermediate/int_propositions_valeurs.sql
+++ data_management/data_factory/models/intermediate/int_propositions_valeurs.sql
@@ -7,10 +7,9 @@ WITH ranked_propositions AS (
         pva.valeur_actuelle_proposee,
         pva.motif_proposition,
         pva.source_donnee_methode_calcul,
-        INITCAP(CONCAT(u.prenom, ' ', u.nom)) AS auteur_proposition,
+        pva.auteur_modification AS auteur_proposition,
         ROW_NUMBER() OVER (PARTITION BY pva.indic_id, pva.territoire_code, pva.date_valeur_actuelle ORDER BY pva.date_proposition DESC) AS rang
     FROM {{ source('db_schema_public', 'proposition_valeur_actuelle') }} pva
-    LEFT JOIN {{ source('db_schema_public', 'utilisateur') }} u ON pva.id_auteur_modification = u.id::text
     WHERE statut = 'EN_COURS'
 )
 SELECT
