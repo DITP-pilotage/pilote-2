755ed6ba19b820d2fb937f93b816f4b209e3fd14
contitri <tristan.conti@seenovate.com>
2024-09-20 13:25:00 +0000

    feat: tri par tendance et ecart mediane (PIL-429)

--- src/client/utils/chantier/météo/météo.ts
+++ src/client/utils/chantier/météo/météo.ts
@@ -3,10 +3,10 @@ import { Météo, météos } from '@/server/domain/météo/Météo.interface';
 
 const ORDRE_DES_MÉTÉOS: typeof météos = ['NON_RENSEIGNEE', 'ORAGE', 'NUAGE', 'COUVERT', 'SOLEIL', 'NON_NECESSAIRE'];
 
-export function comparerMétéo(a: Météo, b: Météo, tri?: SortingState) {
+export function comparerMétéo(a: Météo, b: Météo, tri: SortingState) {
   const indexA = ORDRE_DES_MÉTÉOS.indexOf(a);
   const indexB = ORDRE_DES_MÉTÉOS.indexOf(b);
-  const sensDeTriDesc = tri?.[0].desc;
+  const sensDeTriDesc = tri[0].desc;
   if (indexA ===  ORDRE_DES_MÉTÉOS.indexOf('NON_RENSEIGNEE'))
     return sensDeTriDesc ? -1 : 1;
   if (indexB ===  ORDRE_DES_MÉTÉOS.indexOf('NON_RENSEIGNEE'))
