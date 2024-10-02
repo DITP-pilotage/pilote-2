1829ef87886a199a4e468e6c69105173c4287635
Colin Dugeai <colin.dugeai@gmail.com>
2024-10-02 10:57:57 +0200

    fix: suppr code exemple (PIL-246)
    code temporaire remplacé

--- data_management/data_factory/models/df3/2_aggregation/agg_all_replicate.sql
+++ data_management/data_factory/models/df3/2_aggregation/agg_all_replicate.sql
@@ -1,14 +1,32 @@
 {{ config(materialized = 'table') }}
 
-WITH
+WITH 
+-- Chantiers à répliquer depuis la REG
+to_replicate_from_reg AS (
+    SELECT
+        id,
+        'REG' AS replicate_maille_from,
+        replicate_val_reg_to AS replicate_maille_to
+    FROM {{ ref('stg_ppg_metadata__chantiers') }}
+    WHERE replicate_val_reg_to IS NOT null
+),
+
+-- Chantiers à répliquer depuis la NAT
+to_replicate_from_nat AS (
+    SELECT
+        id,
+        'NAT' AS replicate_maille_from,
+        replicate_val_nat_to AS replicate_maille_to
+    FROM {{ ref('stg_ppg_metadata__chantiers') }}
+    WHERE replicate_val_nat_to IS NOT null
+),
+
 
 -- Tableau des chantier à répliquer de maille X -> Y
 src_chantier_mailles_to_replicate AS (
-    SELECT * FROM (
-        VALUES
-        ('CH-006', 'DEPT', 'REG'), ('CH-015', 'DEPT', 'REG')
-    ) AS t (chantier_id, replicate_maille_to, replicate_maille_from)
-    WHERE chantier_id = 'CH-006' --and zone_id_parent='R84'
+    SELECT * FROM to_replicate_from_reg
+    UNION
+    SELECT * FROM to_replicate_from_nat
 ),
 
 -- Liste des zones où répliquer les données
@@ -59,8 +77,6 @@ replicated_values AS (
     FROM valeurs_region_src
     ORDER BY indic_id, metric_date
 )
-
---select indic_id, zone_id, count(*) as n from replicated_values group by indic_id, zone_id
 
 -- Union des données précédentes avec les données répliquées
 SELECT * FROM {{ ref('agg_all') }}
