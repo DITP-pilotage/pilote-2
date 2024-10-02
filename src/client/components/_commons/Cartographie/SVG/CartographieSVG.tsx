7c4f788fae01527becba319231a83921dd3135b8
Jordan Taillefer <jtaillefer02@gmail.com>
2024-10-02 15:45:49 +0200

    fix: viewbox carto (PIL-152)

--- src/client/components/_commons/Cartographie/SVG/CartographieSVG.tsx
+++ src/client/components/_commons/Cartographie/SVG/CartographieSVG.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, memo, useEffect, useRef, useState } from 'react';
+import { FunctionComponent, memo, useRef } from 'react';
 import hachuresGrisBlanc from '@/client/constants/légendes/hachure/hachuresGrisBlanc';
 import {
   CartographieInfoBulle,
@@ -7,7 +7,6 @@ import {
 } from '@/components/_commons/Cartographie/useCartographie.interface';
 import { CodeInsee } from '@/server/domain/territoire/Territoire.interface';
 import { CartographieSVGContrat } from '@/server/cartographie/app/contrats/CartographieSVGContrat';
-import { Viewbox } from './CartographieSVG.interface';
 import CartographieZoomEtDéplacement from './ZoomEtDéplacement/CartographieZoomEtDéplacement';
 import CartographieSVGStyled from './CartographieSVG.styled';
 import CartographieTerritoireSélectionné from './CartographieTerritoireSélectionné';
@@ -17,6 +16,7 @@ interface CartographieSVGProps {
   options: CartographieOptions,
   territoires: CartographieTerritoires['territoires'],
   frontières: CartographieTerritoires['frontières'],
+  infoBulle: CartographieInfoBulle | null,
   setInfoBulle: (state: CartographieInfoBulle | null) => void,
   auClicTerritoireCallback: (territoireCodeInsee: CodeInsee, territoireSélectionnable: boolean) => void,
 }
@@ -30,6 +30,7 @@ const CartographieSVG: FunctionComponent<CartographieSVGProps> = ({
   options,
   territoires,
   frontières,
+  infoBulle,
   setInfoBulle,
   auClicTerritoireCallback,
 }) => {
@@ -37,18 +38,13 @@ const CartographieSVG: FunctionComponent<CartographieSVGProps> = ({
   const { sourceSvgAsJson } = useCartographieSVG();
 
   const svgRef = useRef<SVGSVGElement | null>(null);
-  const [viewbox, setViewbox] = useState<Viewbox>({
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-  });
 
-  useEffect(() => {
-    if (svgRef?.current?.getBBox) {
-      setViewbox(svgRef.current.getBBox());
-    }
-  }, [svgRef]);
+  const viewbox = {
+    x: 1,
+    y: 0,
+    width: 100,
+    height: 100,
+  };
 
 
   return (
@@ -65,12 +61,7 @@ const CartographieSVG: FunctionComponent<CartographieSVGProps> = ({
         <svg
           ref={svgRef}
           version='1.2'
-          viewBox={`
-          ${viewbox.x}
-          ${viewbox.y}
-          ${viewbox.width}
-          ${viewbox.height}
-        `}
+          viewBox='1 0 100 100'
           xmlns='http://www.w3.org/2000/svg'
         >
           <defs>
@@ -92,7 +83,7 @@ const CartographieSVG: FunctionComponent<CartographieSVGProps> = ({
                     key={`territoire-${territoire.codeInsee}`}
                     onClick={() => options.estInteractif && territoire.estInteractif && auClicTerritoireCallback(territoire.codeInsee, options.territoireSélectionnable)}
                     onMouseEnter={() => {
-                      if (options.estInteractif) {
+                      if (options.estInteractif && territoire.libellé !== infoBulle?.libellé) {
                         setInfoBulle({
                           libellé: territoire.libellé,
                           valeurAffichée: territoire.valeurAffichée,
