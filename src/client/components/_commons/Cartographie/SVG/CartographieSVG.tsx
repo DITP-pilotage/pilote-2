b3363dad3d2fbe553f9f87da00818e6f39a7391d
Jordan Taillefer <jtaillefer02@gmail.com>
2024-10-02 14:46:34 +0200

    feat: new cartographie via SVG (PIL-152)

--- src/client/components/_commons/Cartographie/SVG/CartographieSVG.tsx
+++ src/client/components/_commons/Cartographie/SVG/CartographieSVG.tsx
@@ -1,23 +1,30 @@
 import { FunctionComponent, memo, useEffect, useRef, useState } from 'react';
 import hachuresGrisBlanc from '@/client/constants/légendes/hachure/hachuresGrisBlanc';
 import {
+  CartographieInfoBulle,
   CartographieOptions,
   CartographieTerritoires,
-  CartographieInfoBulle,
 } from '@/components/_commons/Cartographie/useCartographie.interface';
 import { CodeInsee } from '@/server/domain/territoire/Territoire.interface';
+import { CartographieSVGContrat } from '@/server/cartographie/app/contrats/CartographieSVGContrat';
 import { Viewbox } from './CartographieSVG.interface';
 import CartographieZoomEtDéplacement from './ZoomEtDéplacement/CartographieZoomEtDéplacement';
 import CartographieSVGStyled from './CartographieSVG.styled';
 import CartographieTerritoireSélectionné from './CartographieTerritoireSélectionné';
+import { useCartographieSVG } from './useCartographieSVG';
 
 interface CartographieSVGProps {
   options: CartographieOptions,
   territoires: CartographieTerritoires['territoires'],
   frontières: CartographieTerritoires['frontières'],
-  setInfoBulle:  (state: CartographieInfoBulle | null) => void,
+  setInfoBulle: (state: CartographieInfoBulle | null) => void,
   auClicTerritoireCallback: (territoireCodeInsee: CodeInsee, territoireSélectionnable: boolean) => void,
 }
+
+const getTraceSvg = function (svgAsJson: CartographieSVGContrat, territoireCode: string): string {
+  const pathCorrespondantAuTerritoireCode = svgAsJson.svg.g.path.find(path => path['attr-territoire-code'] === territoireCode);
+  return pathCorrespondantAuTerritoireCode?.['attr-d'] || '';
+};
 
 const CartographieSVG: FunctionComponent<CartographieSVGProps> = ({
   options,
@@ -26,6 +33,9 @@ const CartographieSVG: FunctionComponent<CartographieSVGProps> = ({
   setInfoBulle,
   auClicTerritoireCallback,
 }) => {
+
+  const { sourceSvgAsJson } = useCartographieSVG();
+
   const svgRef = useRef<SVGSVGElement | null>(null);
   const [viewbox, setViewbox] = useState<Viewbox>({
     x: 0,
@@ -74,24 +84,25 @@ const CartographieSVG: FunctionComponent<CartographieSVGProps> = ({
           >
             {
               territoires.map(territoire => (
-                <path
-                  className={`territoire-rempli ${(options.estInteractif && territoire.estInteractif) && 'territoire-interactif'}`}
-                  d={territoire.tracéSVG}
-                  fill={territoire.remplissage}
-                  key={`territoire-${territoire.codeInsee}`}
-                  onClick={() => options.estInteractif && territoire.estInteractif && auClicTerritoireCallback(territoire.codeInsee, options.territoireSélectionnable)}
-                  onMouseEnter={() => {
-                    if (options.estInteractif) {
-                      setInfoBulle({
-                        libellé: territoire.libellé,
-                        valeurAffichée: territoire.valeurAffichée,
-                      });
-                    } else {
-                      setInfoBulle(null);
-                    }
-                  }}
-
-                />),
+                sourceSvgAsJson ? (
+                  <path
+                    className={`territoire-rempli ${(options.estInteractif && territoire.estInteractif) && 'territoire-interactif'}`}
+                    d={getTraceSvg(sourceSvgAsJson, territoire.code)}
+                    fill={territoire.remplissage}
+                    key={`territoire-${territoire.codeInsee}`}
+                    onClick={() => options.estInteractif && territoire.estInteractif && auClicTerritoireCallback(territoire.codeInsee, options.territoireSélectionnable)}
+                    onMouseEnter={() => {
+                      if (options.estInteractif) {
+                        setInfoBulle({
+                          libellé: territoire.libellé,
+                          valeurAffichée: territoire.valeurAffichée,
+                        });
+                      } else {
+                        setInfoBulle(null);
+                      }
+                    }}
+                  />
+                ) : null),
               )
             }
             {
