9a9a2aadf29d63402457a03dffd0a3c793d34a21
Colin Dugeai <colin.dugeai@gmail.com>
2024-10-15 15:42:24 +0200

    feat: replace python models with dlt (PIL-480)
    wip, broke other python models

--- data_management/data_factory/models/raw/ppg_metadata/metadata_perimetres_projet_structurant.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-
-def model(dbt, session):
-    ppg_metadata_views = os.getenv('PPG_METADATA_VIEWS')
-    perimetres_ps = pd.read_csv(f'{ppg_metadata_views}/perimetre/mapping_per_pst.csv')
-
-    return perimetres_ps
