3814d8bb1a59c352aba68c39cdcbd9f893d0b498
ZakariaSeenovate <zakaria.belghit@seenovate.com>
2024-10-16 09:25:05 +0000

    fix: ajout sorting et valeur de recherche (PIL-402)

--- src/server/chantiers/app/contrats/FiltreQueryParams.ts
+++ src/server/chantiers/app/contrats/FiltreQueryParams.ts
@@ -1,7 +1,11 @@
+import { ColumnSort } from '@tanstack/react-table';
+
 export type FiltreQueryParams = {
-  perimetres: string[],
-  axes: string[],
+  perimetres: string[]
+  axes: string[]
   statut: string[]
   estTerritorialise: boolean
   estBarometre: boolean
+  sorting: ColumnSort[]
+  valeurDeLaRecherche: string
 };
