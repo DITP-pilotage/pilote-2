97d21597f756b49c25875f491284552cdcc37994
ZakariaSeenovate <zakaria.belghit@seenovate.com>
2024-10-16 12:57:19 +0000

    fix: fix typage filtre (PIL-402)

--- src/server/chantiers/app/contrats/FiltreQueryParams.ts
+++ src/server/chantiers/app/contrats/FiltreQueryParams.ts
@@ -1,11 +1,12 @@
-import { ColumnSort } from '@tanstack/react-table';
-
 export type FiltreQueryParams = {
   perimetres: string[]
   axes: string[]
   statut: string[]
   estTerritorialise: boolean
   estBarometre: boolean
-  sorting: ColumnSort[]
+  sorting: {   
+    desc: boolean;
+    id: string;
+  }
   valeurDeLaRecherche: string
 };
