489c7b87fc364a2034e14c770c4b68af02d4fb9a
Colin Dugeai <colin.dugeai@gmail.com>
2024-10-18 15:56:55 +0200

    fix: script 4 reorganise (PIL-480)
    add 2 folders: public + raw_data.

--- data_management/data_factory/models/seeds_/sql_incremental/raw_data/commentaires.sql
+++ data_management/data_factory/models/seeds_/sql_incremental/raw_data/commentaires.sql
@@ -1,5 +1,3 @@
-{{ config(schema = 'raw_data') }}
-
 select 
     "chantier_id","maille","code_insee","date","type","contenu","auteur","meteo","date_meteo"
-from {{ source('python_load_tmp', 'commentaires_py') }}
+from {{ source('python_load_seeds', 'commentaires_py') }}
