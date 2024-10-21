e634657eb8bbec6852dfcaf83aafbe1042e4f615
Colin Dugeai <colin.dugeai@gmail.com>
2024-10-21 11:11:29 +0200

    wip: todos public.scope (PIL-73)

--- src/database/prisma/migrations/20231109093410_mets_a_jour_les_profils/migration.sql
+++ src/database/prisma/migrations/20231109093410_mets_a_jour_les_profils/migration.sql
@@ -1,7 +1,5 @@
-UPDATE scope SET code = 'saisieCommentaire' WHERE code = 'saisie.commentaire';
-UPDATE scope SET code = 'saisieIndicateur' WHERE code = 'saisie.indicateur';
-UPDATE habilitation SET scope_code = 'saisieCommentaire' WHERE scope_code = 'saisie.commentaire';
-UPDATE habilitation SET scope_code = 'saisieIndicateur' WHERE scope_code = 'saisie.indicateur';
-UPDATE profil 
-SET a_acces_tous_les_territoires_saisie_commentaire = true
-WHERE code IN ('SECRETARIAT_GENERAL', 'EQUIPE_DIR_PROJET', 'DIR_PROJET');
+UPDATE habilitation SET scope_code = 'saisieCommentaire' WHERE scope_code = 'saisie.commentaire'; -- suppr ?
+UPDATE habilitation SET scope_code = 'saisieIndicateur' WHERE scope_code = 'saisie.indicateur'; -- suppr ?
+UPDATE profil  -- suppr ?
+SET a_acces_tous_les_territoires_saisie_commentaire = true -- suppr ?
+WHERE code IN ('SECRETARIAT_GENERAL', 'EQUIPE_DIR_PROJET', 'DIR_PROJET'); -- suppr ?
