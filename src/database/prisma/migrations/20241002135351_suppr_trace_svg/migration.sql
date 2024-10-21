388577021534b553880f92a77aa5fce40fdcadc9
Colin Dugeai <colin.dugeai@gmail.com>
2024-10-21 10:56:50 +0200

    wip: todos public.territoire (PIL-73)

--- src/database/prisma/migrations/20241002135351_suppr_trace_svg/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `trace_svg` on the `territoire` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."territoire" DROP COLUMN "trace_svg";
