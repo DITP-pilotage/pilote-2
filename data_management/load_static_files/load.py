b2a7a18b69361119d3dfa8edefe6c02e14e05f6a
Colin Dugeai <colin.dugeai@gmail.com>
2024-10-25 15:44:11 +0200

    refactor: multiple repos ok (PIL-73)

--- data_management/load_static_files/load.py
+++ data_management/load_static_files/load.py
@@ -33,13 +33,17 @@ engine = create_engine(conn_str)
 
 with engine.connect() as con:
 
-
-    tables = getTablesOfGroup(sys.argv[1], CONFIG)
-    print(len(tables.items()), "tables detected for group", sys.argv[1])
+    cli_param_1 = sys.argv[1]
+    tables = getTablesOfGroup(cli_param_1, CONFIG)
+    repo_name = CONFIG.get('groups').get(cli_param_1, {"github_repo": ""}).get('github_repo')
+    repo_params = CONFIG.get('github_repos').get(repo_name)
+    print(len(tables.items()), "tables detected for group", cli_param_1)
 
     # [config] Repo Github
-    ppg_metadata_branch = os.environ.get("PPG_METADATA_GITHUB_BRANCH", CONFIG.get('github_repo').get(sys.argv[1]).get('default_branch'))
-    ppg_metadata_repo = GithubRepo(os.environ.get('PPG_METADATA_GITHUB_TOKEN'), CONFIG.get('github_repo').get(sys.argv[1]).get('user'), CONFIG.get('github_repo').get(sys.argv[1]).get('repo'))
+    repo = GithubRepo(os.environ.get('PPG_METADATA_GITHUB_TOKEN'), repo_params.get('user'), repo_params.get('repo'))
+    repo_branch = repo_params.get('default_branch')
+    if (repo_params.get('repo')=="PPG_metadata"):
+        repo_branch = os.environ.get("PPG_METADATA_GITHUB_BRANCH", repo_params.get('default_branch'))
 
 
     # Determine all schema used by the tables
@@ -54,7 +58,7 @@ with engine.connect() as con:
         print("Handling "+table_name)
 
         # Download table and add its content to database
-        csv_as_df = ppg_metadata_repo.getFileAsDataframe(table_options.get('path_in_repo'), ppg_metadata_branch)
+        csv_as_df = repo.getFileAsDataframe(table_options.get('path_in_repo'), repo_branch)
         csv_as_df.to_sql(name=table_name, con=con, if_exists='replace', schema=table_options.get('schema'))
 
         # Commit for each table
@@ -62,6 +66,6 @@ with engine.connect() as con:
     # Commit once, if no error
     con.commit()
     con.close()
-    ppg_metadata_repo.close()
+    repo.close()
     print('Data persisted !')
 
