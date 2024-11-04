b9e4574e3e26562c4eda1b9e9e801c8b9c09a58a
Colin Dugeai <colin.dugeai@gmail.com>
2024-11-04 16:19:54 +0100

    feat: add script tristan (PIL-506)

---
+++ scripts/daily/reset_all_tristan.sh
@@ -0,0 +1,17 @@
+# Daily script to:
+#   - reset the db
+#   - run FULL_DJ datajobs
+#   - run a dbt command (for example)
+#   - exec descente de prod partielle
+#   - run PROD datajobs
+# 
+
+echo ">> Reset db"
+/bin/bash scripts/prisma_reset_and_migrate.sh
+cd data_management
+echo ">> Run dj FULL"
+FULL_DJ=true pipenv run /bin/bash scripts/run_datajobs.sh
+echo ">> Run descente de prod"
+pipenv run /bin/bash scripts/descente_de_prod_partielle.sh
+echo ">> Run dj prod"
+FULL_DJ=false pipenv run /bin/bash scripts/run_datajobs.sh
