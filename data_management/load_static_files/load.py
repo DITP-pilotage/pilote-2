fa37013e4d877e89497d36b093a3c3184205b7b1
Colin Dugeai <colin.dugeai@gmail.com>
2024-11-05 18:14:48 +0100

    feat: setup seed prisma (PIL-73)
    from branch PIL-73-refacto-seeds-prisma-v3

--- data_management/load_static_files/load.py
+++ data_management/load_static_files/load.py
@@ -11,16 +11,13 @@ def getTablesOfGroup(group_name, config):
 
 
 # [config] Tables à charger
-CONFIG_PATH='load_ppg_metadata/tables.yml'
+CONFIG_PATH='load_static_files/tables.yml'
 CONFIG=None
 
 with open(CONFIG_PATH, 'r') as file:
     CONFIG = yaml.safe_load(file)
 
 
-# [config] Repo Github
-github_branch = os.environ.get("PPG_METADATA_GITHUB_BRANCH", CONFIG.get('github_repo').get('default_branch'))
-ppg_metadata_repo = GithubRepo(os.environ.get('PPG_METADATA_GITHUB_TOKEN'), CONFIG.get('github_repo').get('user'), CONFIG.get('github_repo').get('repo'))
 
 # [config] Base de données
 conn_str = "".join([
@@ -36,9 +33,17 @@ engine = create_engine(conn_str)
 
 with engine.connect() as con:
 
+    cli_param_1 = sys.argv[1]
+    tables = getTablesOfGroup(cli_param_1, CONFIG)
+    repo_name = CONFIG.get('groups').get(cli_param_1, {"github_repo": ""}).get('github_repo')
+    repo_params = CONFIG.get('github_repos').get(repo_name)
+    print(len(tables.items()), "tables detected for group", cli_param_1)
 
-    tables = getTablesOfGroup(sys.argv[1], CONFIG)
-    print(len(tables.items()), "tables detected for group", sys.argv[1])
+    # [config] Repo Github
+    repo = GithubRepo(os.environ.get('PPG_METADATA_GITHUB_TOKEN'), repo_params.get('user'), repo_params.get('repo'))
+    repo_branch = repo_params.get('default_branch')
+    if (repo_params.get('repo')=="PPG_metadata"):
+        repo_branch = os.environ.get("PPG_METADATA_GITHUB_BRANCH", repo_params.get('default_branch'))
 
 
     # Determine all schema used by the tables
@@ -53,7 +58,7 @@ with engine.connect() as con:
         print("Handling "+table_name)
 
         # Download table and add its content to database
-        csv_as_df = ppg_metadata_repo.getFileAsDataframe(table_options.get('path_in_repo'), github_branch)
+        csv_as_df = repo.getFileAsDataframe(table_options.get('path_in_repo'), repo_branch)
         csv_as_df.to_sql(name=table_name, con=con, if_exists='replace', schema=table_options.get('schema'))
 
         # Commit for each table
@@ -61,6 +66,5 @@ with engine.connect() as con:
     # Commit once, if no error
     con.commit()
     con.close()
-    ppg_metadata_repo.close()
+    repo.close()
     print('Data persisted !')
-
