008fb8a82704368c1ba878c556129c0c384bc734
Colin Dugeai <colin.dugeai@gmail.com>
2024-11-06 12:05:57 +0100

    fix: fix prisma seed (PIL-73)

--- scripts/daily/reset_and_run_dj_colin.sh
+++ scripts/daily/reset_and_run_dj_colin.sh
@@ -8,22 +8,17 @@
 # Warn: only with docker commands
 # You can dupplicate this file and modify it to fit your local env (example: use docker only for data operations)
 
-
 echo ">> Reset db"
 docker compose run --rm pilote_webapp bash scripts/prisma_reset_migrate_seed.sh
 cd data_management
 echo ">> Run dj FULL"
 FULL_DJ=true docker compose run --rm -e FULL_DJ pilote_datajobs
-echo ">> Prisma seed"
-cd ..
-docker compose run --rm pilote_webapp /bin/bash scripts/prisma_seed.sh
 echo ">> Run dbt command"
 docker compose run --rm pilote_dbt dbt run --select barometre
 echo ">> Run descente de prod"
 cd ..
 docker compose run --rm ddp bash docker/entrypoint.ddp.sh
 echo ">> Run dj prod"
-cd data_management
 FULL_DJ=false docker compose run --rm -e FULL_DJ pilote_datajobs
 echo ">> Restart db + webapp"
 cd ..
