b6e8ae39d28e9f7232607ef8e87b0bc9195a9c9b
Jordan Taillefer <jtaillefer02@gmail.com>
2024-11-19 14:05:13 +0100

    fix: taille tag et couleur (PIL-00)

--- src/client/components/_commons/SélecteursMaillesEtTerritoiresChantier/SélecteurMaille/SélecteurMaille.styled.ts
+++ src/client/components/_commons/SélecteursMaillesEtTerritoiresChantier/SélecteurMaille/SélecteurMaille.styled.ts
@@ -2,7 +2,8 @@ import styled from '@emotion/styled';
 
 const SélecteurMailleStyled = styled.div`
  .tag-selectionnee {
-   background-color: var(--hover-tint);
+   background-color: var(--blue-france-sun-113-625);
+   color: white;
  }
 `;
 
