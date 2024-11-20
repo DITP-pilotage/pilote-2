010be49c9e285eb7f6be70e4fa0284cc94d4f5f5
ZakariaSeenovate <zakaria.belghit@seenovate.com>
2024-11-20 15:36:50 +0000

    fix: fix lint error (PIL-502)

--- src/client/components/_commons/Cartographie/BulleDInfo/BulleDInfo.styled.ts
+++ src/client/components/_commons/Cartographie/BulleDInfo/BulleDInfo.styled.ts
@@ -1,15 +1,15 @@
 import styled from '@emotion/styled';
 
-const BulleDInfoStyled = styled.div`
+const BulleDInfoStyled = styled.div` 
   position: fixed;
   z-index: 1;
+  min-width: 8rem;
   font-size: 0.75rem;
   vertical-align: middle;
   pointer-events: none; /* supprime le flickering lorsque le curseur est sur la bulle d'info */
   background-color: var(--background-contrast-grey);
   box-shadow: 0 4px 4px 0 #00000040;
   transform: translate(-50%, -6rem);
-  min-width: 8rem;
 
   div:last-child {
     background-color: var(--background-alt-grey);
