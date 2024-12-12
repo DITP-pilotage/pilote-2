7fa5c9ef1b904839e1ff89a6b846ded4d7d04a3c
Colin Dugeai <colin.dugeai@gmail.com>
2024-12-12 17:28:09 +0100

    feat: exposition ta_prev_year_indic (PIL-519)

---
+++ data_management/models/df3/4_compute/get_ta_indic_prev_year.sql
@@ -0,0 +1,9 @@
+SELECT 
+    -- *,
+	indic_id,
+	zone_id, 
+    taa_courant as taa_prev_year,
+	tag as tag_prev_year ,
+	date_valeur_actuelle::date as date_valeur_actuelle_prev_year,
+	vaca as valeur_actuelle_prev_year
+FROM {{ ref('get_last_vaca_prev_year') }}
