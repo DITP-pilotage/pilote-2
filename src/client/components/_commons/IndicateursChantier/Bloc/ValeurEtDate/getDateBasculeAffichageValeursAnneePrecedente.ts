717bd6991b7a286105c41e016d688f0e65baa1a1
Jordan Taillefer <jtaillefer02@gmail.com>
2025-01-06 12:41:11 +0100

    feat: correction estIntermediaireAnneeEnCours (PIL-520)

--- src/client/components/_commons/IndicateursChantier/Bloc/ValeurEtDate/getDateBasculeAffichageValeursAnneePrecedente.ts
+++ src/client/components/_commons/IndicateursChantier/Bloc/ValeurEtDate/getDateBasculeAffichageValeursAnneePrecedente.ts
@@ -3,16 +3,25 @@ import api from '@/server/infrastructure/api/trpc/api';
 export const getDateBasculeAffichageValeursAnneePrecedente = (): {
   dateBascule: Date, dateBasculeDepassee: boolean
 } => {
-
   const maintenant: Date = new Date();
   const { data: dateBasculeTauxAnnuelAnneeCouranteString } = api.gestionContenu.récupérerVariableContenu.useQuery({ nomVariableContenu: 'NEXT_PUBLIC_DATE_BASCULE_AFFICHAGE_VALEURS_ANNEE_PRECEDENTE' });
   
   const dateBasculeTauxAnnuelAnneeCouranteDate: Date = new Date(dateBasculeTauxAnnuelAnneeCouranteString as string);
-  const dateBasculeTauxAnnuelAnneeCouranteDateAnneeCourante: Date = new Date(dateBasculeTauxAnnuelAnneeCouranteDate.setFullYear(maintenant.getFullYear()));
+
+  dateBasculeTauxAnnuelAnneeCouranteDate.setFullYear(maintenant.getFullYear());
 
   return {
-    dateBascule: dateBasculeTauxAnnuelAnneeCouranteDateAnneeCourante,
-    dateBasculeDepassee: dateBasculeTauxAnnuelAnneeCouranteDateAnneeCourante < maintenant,
+    dateBascule: dateBasculeTauxAnnuelAnneeCouranteDate,
+    dateBasculeDepassee: dateBasculeTauxAnnuelAnneeCouranteDate < maintenant,
   };
+};
 
+export const getAnneeAffichageDateDeBascule = (date: Date, dateBascule: string): number => {
+  const maintenant: Date = new Date();
+
+  const dateBasculeTauxAnnuelAnneeCouranteDate: Date = new Date(dateBascule);
+
+  dateBasculeTauxAnnuelAnneeCouranteDate.setFullYear(maintenant.getFullYear());
+
+  return date < dateBasculeTauxAnnuelAnneeCouranteDate ? dateBasculeTauxAnnuelAnneeCouranteDate.getFullYear() - 1 : dateBasculeTauxAnnuelAnneeCouranteDate.getFullYear();
 };
