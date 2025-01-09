38b30d24051455a7eefc92faee7149d1d19b0af4
Tristan Conti <86486650+contitri@users.noreply.github.com>
2025-01-09 15:13:53 +0100

    refactor: comparaisons territoires graphique evolution (PIL-465)

--- src/client/components/_commons/IndicateursChantier/Bloc/Détails/Évolution/useIndicateurÉvolution.ts
+++ src/client/components/_commons/IndicateursChantier/Bloc/Détails/Évolution/useIndicateurÉvolution.ts
@@ -1,5 +1,5 @@
 import { ChartData, ChartDataset, ChartOptions } from 'chart.js';
-import { formaterDate } from '@/client/utils/date/date';
+import { comparerDates, formaterDate } from '@/client/utils/date/date';
 import { générerCouleursAléatoiresEntreDeuxCouleurs } from '@/client/utils/couleur/couleur';
 import IndicateurÉvolutionProps from './IndicateurÉvolution.interface';
 
@@ -24,15 +24,19 @@ export default function useIndicateurÉvolution(indicateurDétailsParTerritoires
     },
   };
 
-  const libellés = indicateurDétailsPourUnTerritoire.données.historiquesValeurs.map(historique => formaterDate(historique.date, 'MM/YYYY'));
+  const datesDeTousLesIndicateurs = indicateurDétailsParTerritoires.flatMap(detailIndicateur => 
+    detailIndicateur.données.historiquesValeurs.map(h => h.date),
+  );
+  const listeDesDateOrdonnees = [...new Set(datesDeTousLesIndicateurs)].sort((a, b) => comparerDates(a, b));
+  const valeursAxeX = listeDesDateOrdonnees.map(date => formaterDate(date, 'MM/YYYY'));
 
-  if (libellés.length === 1) {
-    libellés.push('');
+  if (valeursAxeX.length === 1) {
+    valeursAxeX.push('');
   }
 
   const évolutions: ChartDataset<'line'>[] = indicateurDétailsParTerritoires.map((détailsParTerritoire, index) => ({
     label: détailsParTerritoire.territoireNom,
-    data: détailsParTerritoire.données.historiquesValeurs.map(historique => historique.valeur),
+    data: listeDesDateOrdonnees.map(date => détailsParTerritoire.données.historiquesValeurs.find(valeurHistorique => valeurHistorique.date === date)?.valeur ?? null),
     pointStyle: 'rect',
     pointRadius: 5,
     borderColor: couleurs[index],
@@ -41,11 +45,11 @@ export default function useIndicateurÉvolution(indicateurDétailsParTerritoires
 
   if (estEnSélectionMultiple()) {
     donnéesParTerritoire = {
-      labels: libellés,
+      labels: valeursAxeX,
       datasets: évolutions,
     };
   } else {
-    const listeValeurCible = Array.from({ length: libellés.length }).map(() => indicateurDétailsPourUnTerritoire.données.valeurCible);
+    const listeValeurCible = Array.from({ length: valeursAxeX.length }).map(() => indicateurDétailsPourUnTerritoire.données.valeurCible);
 
     const valeurCible: ChartDataset<'line'> = {
       label: 'Cible',
@@ -59,8 +63,8 @@ export default function useIndicateurÉvolution(indicateurDétailsParTerritoires
     };
   
     donnéesParTerritoire = {
-      labels: libellés,
-      datasets: indicateurDétailsPourUnTerritoire.données.valeurCible ? [évolutions[0], valeurCible] : [évolutions[0]],
+      labels: valeursAxeX,
+      datasets: indicateurDétailsPourUnTerritoire.données.valeurCible !== null ? [évolutions[0], valeurCible] : [évolutions[0]],
     };
   }
 
