c176db4b9865e73cc4196a85ee9e58c884f0b7d8
Colin Dugeai <colin.dugeai@gmail.com>
2025-01-10 18:39:50 +0100

    fix: fix du taa_prev_year_ch (PIL-536)

--- data_management/models/df3/4_compute/get_ta_ch_prev_year.sql
+++ data_management/models/df3/4_compute/get_ta_ch_prev_year.sql
@@ -4,8 +4,8 @@ SELECT
     zone_id,
     territoire_code,
     indic_ids,
-    date_ta as taa_prev_year_date,
-    taa_adate_ch as taa_prev_year,
+    date_ta as taa_prev_year_date, -- correct ?
+    taa_prev_year_ch as taa_prev_year,
     tag_ch as tag_prev_year
 FROM {{ ref('compute_ta_ch') }}
 WHERE valid_on = 'prev_year'
