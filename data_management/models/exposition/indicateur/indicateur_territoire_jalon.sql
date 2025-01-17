f7bdb96e1ab8b0da460bfa659c7adbcf82123564
Colin Dugeai <colin.dugeai@gmail.com>
2025-01-17 14:40:31 +0100

    wip: fix indicateur_territoire_jalon, perf issue (PIL-530)

--- data_management/models/exposition/indicateur/indicateur_territoire_jalon.sql
+++ data_management/models/exposition/indicateur/indicateur_territoire_jalon.sql
@@ -11,30 +11,34 @@ jalon_annee_precedente as (
 	select 
 	indic_id, zone_id,
     date_part('year', now()) - 1 AS jalon,
-	vca_prev_year_date,
-	taa_prev_year,
-	vca_prev_year,
-	tap_courant
+	vca_prev_year_date::date AS date_valeur_cible,
+	taa_prev_year as taux_avancement,
+	vca_prev_year as valeur_cible,
+	tap_courant as taux_avancement_proposition
 	 from {{ ref('get_ta_indic_prev_year') }}
 ),
 
-jalon_annee_courante_pt1 as (
-	SELECT 
-		indic_id, zone_id,
+jalon_annee_courante as (
+	SELECT
+		meta_indic.id AS indic_id,
+		territoire.zone_id AS zone_id,
         date_part('year', now()) AS jalon,
-	vca_date,
-	vca
-        FROM {{ ref('get_vca') }}
-        WHERE yyear = (date_part('year', now()))
+		pt1.vca_date::date as date_valeur_cible,
+		pt1.vca as valeur_cible,
+		pt2.taa_courant as taux_avancement,
+		pt2.tap_courant as taux_avancement_proposition
+	FROM {{ source('db_schema_public', 'territoire') }} AS territoire
+	CROSS JOIN {{ ref('stg_ppg_metadata__indicateurs') }} AS meta_indic
+	LEFT JOIN {{ ref('get_vca') }} AS pt1 on pt1.indic_id=meta_indic.id and pt1.zone_id=territoire.zone_id
+	LEFT JOIN {{ ref('get_last_vaca') }} AS pt2 on pt2.indic_id=meta_indic.id and pt2.zone_id=territoire.zone_id
+    WHERE pt1.yyear = (date_part('year', now()))
 ),
 
-jalon_annee_courante_pt2 as (
-	SELECT 
-		indic_id, zone_id,
-        date_part('year', now()) AS jalon,
-	taa_courant,
-	tap_courant
-        FROM {{ ref('get_last_vaca') }}
+-- Tous les TA pour tous les jalons
+jalon_annee_precedente_et_courante AS (
+    SELECT * FROM jalon_annee_precedente
+    UNION
+    SELECT * FROM jalon_annee_courante
 ),
 
 -- Les jalons à considerer: ici année en cours et année précédente.
@@ -51,42 +55,14 @@ SELECT
     territoire.maille,
     jalons.jalon AS jalon,
     territoire.zone_id,
-    CASE
-        WHEN
-            -- Nous sommes après la date de bascule, afficher TA de l'année courante
-            date_bascule.date_depassee THEN gvca.vca_date::date
-        -- Nous sommes après la date de bascule, afficher TA de l'année précédente
-        ELSE a_prev_year.vca_prev_year_date::date
-    END AS date_valeur_cible,
-	CASE 
-		WHEN 
-			-- Nous sommes après la date de bascule, afficher TA de l'année courante
-			date_bascule.date_depassee THEN a.taa_courant
-		-- Nous sommes après la date de bascule, afficher TA de l'année précédente
-		ELSE a_prev_year.taa_prev_year
-	END as taux_avancement,
-	CASE 
-		WHEN 
-			-- Nous sommes après la date de bascule, afficher TA de l'année courante
-			date_bascule.date_depassee THEN gvca.vca
-		-- Nous sommes après la date de bascule, afficher TA de l'année précédente
-		ELSE a_prev_year.vca_prev_year
-	END as valeur_cible,
-	CASE 
-		WHEN date_bascule.date_depassee THEN a.tap_courant
-		ELSE a_prev_year.tap_courant
-	END as taux_avancement_proposition
+    tous_jalons.date_valeur_cible,
+    tous_jalons.taux_avancement,
+    tous_jalons.valeur_cible,
+    tous_jalons.taux_avancement_proposition
 FROM {{ source('db_schema_public', 'territoire') }} AS territoire
 CROSS JOIN {{ ref('stg_ppg_metadata__indicateurs') }} AS meta_indic
 CROSS JOIN {{ ref('get_date_bascule_depassee') }} AS date_bascule
 CROSS JOIN jalons_a_considerer as jalons
-LEFT JOIN jalon_annee_courante_pt2 a on a.indic_id=meta_indic.id and a.zone_id=territoire.zone_id
---LEFT JOIN {{ ref('stg_ppg_metadata__zones') }} 
-LEFT JOIN jalon_annee_courante_pt1 AS gvca
-    ON meta_indic.id = gvca.indic_id AND territoire.zone_id = gvca.zone_id
 LEFT JOIN
-    jalon_annee_precedente AS a_prev_year
-    ON
-        meta_indic.id = a_prev_year.indic_id
-        AND territoire.zone_id = a_prev_year.zone_id and a_prev_year.jalon=jalons.jalon
-
+    jalon_annee_precedente_et_courante AS tous_jalons
+    ON territoire.zone_id = tous_jalons.zone_id AND meta_indic.id = tous_jalons.indic_id AND jalons.jalon=tous_jalons.jalon
