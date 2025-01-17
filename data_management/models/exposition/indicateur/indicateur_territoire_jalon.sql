2bffe1677d0cd5f72e968181fc2165bfedf94e38
Colin Dugeai <colin.dugeai@gmail.com>
2025-01-17 15:38:44 +0100

    refactor: sqlfluff indicateur_territoire_jalon (PIL-530)

--- data_management/models/exposition/indicateur/indicateur_territoire_jalon.sql
+++ data_management/models/exposition/indicateur/indicateur_territoire_jalon.sql
@@ -1,4 +1,4 @@
- -- depends_on: {{ ref('indicateur_territoire') }}
+-- depends_on: {{ ref('indicateur_territoire') }}
 
 {{ config(
     materialized = 'incremental', 
@@ -7,30 +7,35 @@
 
 WITH
 
-jalon_annee_precedente as (
-	select 
-	indic_id, zone_id,
-    date_part('year', now()) - 1 AS jalon,
-	vca_prev_year_date::date AS date_valeur_cible,
-	taa_prev_year as taux_avancement,
-	vca_prev_year as valeur_cible,
-	tap_courant as taux_avancement_proposition
-	 from {{ ref('get_ta_indic_prev_year') }}
+jalon_annee_precedente AS (
+    SELECT
+        indic_id,
+        zone_id,
+        date_part('year', now()) - 1 AS jalon,
+        vca_prev_year_date::date AS date_valeur_cible,
+        taa_prev_year AS taux_avancement,
+        vca_prev_year AS valeur_cible,
+        tap_courant AS taux_avancement_proposition
+    FROM {{ ref('get_ta_indic_prev_year') }}
 ),
 
-jalon_annee_courante as (
-	SELECT
-		meta_indic.id AS indic_id,
-		territoire.zone_id AS zone_id,
+jalon_annee_courante AS (
+    SELECT
+        meta_indic.id AS indic_id,
+        territoire.zone_id,
         date_part('year', now()) AS jalon,
-		pt1.vca_date::date as date_valeur_cible,
-		pt1.vca as valeur_cible,
-		pt2.taa_courant as taux_avancement,
-		pt2.tap_courant as taux_avancement_proposition
-	FROM {{ source('db_schema_public', 'territoire') }} AS territoire
-	CROSS JOIN {{ ref('stg_ppg_metadata__indicateurs') }} AS meta_indic
-	LEFT JOIN {{ ref('get_vca') }} AS pt1 on pt1.indic_id=meta_indic.id and pt1.zone_id=territoire.zone_id
-	LEFT JOIN {{ ref('get_last_vaca') }} AS pt2 on pt2.indic_id=meta_indic.id and pt2.zone_id=territoire.zone_id
+        pt1.vca_date::date AS date_valeur_cible,
+        pt1.vca AS valeur_cible,
+        pt2.taa_courant AS taux_avancement,
+        pt2.tap_courant AS taux_avancement_proposition
+    FROM {{ source('db_schema_public', 'territoire') }} AS territoire
+    CROSS JOIN {{ ref('stg_ppg_metadata__indicateurs') }} AS meta_indic
+    LEFT JOIN
+        {{ ref('get_vca') }} AS pt1
+        ON meta_indic.id = pt1.indic_id AND territoire.zone_id = pt1.zone_id
+    LEFT JOIN
+        {{ ref('get_last_vaca') }} AS pt2
+        ON meta_indic.id = pt2.indic_id AND territoire.zone_id = pt2.zone_id
     WHERE pt1.yyear = (date_part('year', now()))
 ),
 
@@ -49,16 +54,17 @@ jalons_a_considerer AS (
 )
 
 SELECT
-    tous_jalons.indic_id as id,
+    tous_jalons.indic_id AS id,
     territoire.code AS territoire_code,
     territoire.code_insee,
     territoire.maille,
-    jalons.jalon AS jalon,
+    jalons.jalon,
     territoire.zone_id,
     tous_jalons.date_valeur_cible,
-	tous_jalons.taux_avancement,
-	tous_jalons.valeur_cible,
-	tous_jalons.taux_avancement_proposition
+    tous_jalons.taux_avancement,
+    tous_jalons.valeur_cible,
+    tous_jalons.taux_avancement_proposition
 FROM jalon_annee_precedente_et_courante AS tous_jalons
-LEFT JOIN {{ source('db_schema_public', 'territoire') }} AS territoire on territoire.zone_id = tous_jalons.zone_id
-RIGHT JOIN jalons_a_considerer as jalons on jalons.jalon = tous_jalons.jalon
+LEFT JOIN {{ source('db_schema_public', 'territoire') }} AS territoire
+    ON tous_jalons.zone_id = territoire.zone_id
+RIGHT JOIN jalons_a_considerer AS jalons ON tous_jalons.jalon = jalons.jalon
