f74defc3ace2759d452f7a4a342ec03229639904
Jordan Taillefer <jtaillefer02@gmail.com>
2025-01-19 17:14:42 +0100

    wip: refactor PrismaSynthèseDesRésultatsRepository PrismaObjectifRepository (PIL-530)

--- src/server/fiche-conducteur/__tests__/infrastructure/adapters/PrismaSynthèseDesRésultatsRepository.integration.test.ts
+++ src/server/fiche-conducteur/__tests__/infrastructure/adapters/PrismaSynthèseDesRésultatsRepository.integration.test.ts
@@ -14,28 +14,46 @@ describe('PrismaSynthèseDesRésultatsRepository', () => {
 
     it('doit récupérer la synthèse la plus récente pour un chantier de maille nationale', async () => {
       // Given
-      await prisma.chantier.create({
-        data: {
-          id: 'CH-168',
-          nom: 'Nom chantier',
-          code_insee: 'FR',
-          maille: 'NAT',
-          territoire_code: 'NAT-FR',
-        },
+      await prisma.chantier_identite.createMany({ data: [{
+        id: 'CH-168',
+        nom: 'Nom chantier OK',
+      }, {
+        id: 'CH-169',
+        nom: 'Nom chantier OK',
+      }],
       });
-      await prisma.chantier.create({
-        data: {
-          id: 'CH-169',
-          nom: 'Nom chantier',
-          code_insee: 'FR',
-          maille: 'NAT',
-          territoire_code: 'NAT-FR',
-        },
+
+      await prisma.chantier_territoire.createMany({ data: [{
+        id: 'CH-168',
+        zone_id: 'FRANCE',
+        maille: 'NAT',
+        code_insee: 'FR',
+        territoire_code: 'NAT-FR',
+      }, {
+        id: 'CH-168',
+        zone_id: 'D01',
+        maille: 'DEPT',
+        code_insee: '34',
+        territoire_code: 'DEPT-34',
+      }, {
+        id: 'CH-169',
+        zone_id: 'FRANCE',
+        maille: 'NAT',
+        code_insee: 'FR',
+        territoire_code: 'NAT-FR',
+      }, {
+        id: 'CH-169',
+        zone_id: 'D01',
+        maille: 'DEPT',
+        code_insee: '34',
+        territoire_code: 'DEPT-34',
+      }],
       });
       await prisma.synthese_des_resultats.create({
         data: {
           id: randomUUID(),
           chantier_id: 'CH-168',
+          territoire_code: 'NAT-FR',
           code_insee: 'FR',
           maille: 'NAT',
           date_commentaire: '2022-01-02T00:00:00.000Z',
@@ -47,6 +65,7 @@ describe('PrismaSynthèseDesRésultatsRepository', () => {
         data: {
           id: randomUUID(),
           chantier_id: 'CH-168',
+          territoire_code: 'NAT-FR',
           code_insee: 'FR',
           maille: 'NAT',
           date_commentaire: '2021-01-02T00:00:00.000Z',
@@ -58,6 +77,7 @@ describe('PrismaSynthèseDesRésultatsRepository', () => {
         data: {
           id: randomUUID(),
           chantier_id: 'CH-168',
+          territoire_code: 'DEPT-34',
           code_insee: '34',
           maille: 'DEPT',
           meteo: 'SOLEIL',
@@ -68,6 +88,7 @@ describe('PrismaSynthèseDesRésultatsRepository', () => {
         data: {
           id: randomUUID(),
           chantier_id: 'CH-169',
+          territoire_code: 'DEPT-34',
           code_insee: '34',
           meteo: 'SOLEIL',
           maille: 'DEPT',
