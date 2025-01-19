530d8d22c5ecbf597f8161d3b9054311a1447cd0
Jordan Taillefer <jtaillefer02@gmail.com>
2025-01-19 17:07:25 +0100

    wip: refactor fiche conducteur chantier repo (PIL-530)

--- src/server/fiche-conducteur/__tests__/infrastructure/adapters/PrismaChantierRepository.integration.test.ts
+++ src/server/fiche-conducteur/__tests__/infrastructure/adapters/PrismaChantierRepository.integration.test.ts
@@ -11,41 +11,41 @@ describe('PrismaChantierRepository', () => {
   describe('#récupérerParIdEtParTerritoireCode', () => {
     it('doit récupérer le chantier associé', async () => {
       // Given
-      await prisma.chantier.create({
-        data: {
+      await prisma.chantier_identite.createMany({
+        data: [{
           id: 'CH-168',
-          nom: 'Nom chantier OK',
-          code_insee: 'FR',
-          maille: 'NAT',
-          territoire_code: 'NAT-FR',
+          nom: 'Chantier 168',
           est_territorialise: true,
           directeurs_administration_centrale: ['DAC 1', 'DAC 2'],
           directeurs_projet: ['DP 1', 'DP 2'],
-        },
-      });
-      await prisma.chantier.create({
-        data: {
-          id: 'CH-168',
-          nom: 'Nom chantier KO maille',
-          code_insee: '01',
-          maille: 'REG',
-          territoire_code: 'REG-01',
-          est_territorialise: false,
-          directeurs_administration_centrale: ['DAC 3', 'DAC 4'],
-          directeurs_projet: ['DP 3', 'DP 4'],
-        },
-      });
-      await prisma.chantier.create({
-        data: {
+        }, {
           id: 'CH-169',
-          nom: 'Nom chantier KO ID',
-          code_insee: 'FR',
-          maille: 'NAT',
-          territoire_code: 'NAT-FR',
+          nom: 'Chantier 169',
           est_territorialise: false,
           directeurs_administration_centrale: ['DAC 5', 'DAC 6'],
           directeurs_projet: ['DP 5', 'DP 6'],
-        },
+        }],
+      });
+      await prisma.chantier_territoire.createMany({
+        data: [{
+          id: 'CH-168',
+          zone_id: 'FRANCE',
+          code_insee: 'FR',
+          maille: 'NAT',
+          territoire_code: 'NAT-FR',
+        }, {
+          id: 'CH-168',
+          zone_id: 'R01',
+          code_insee: '01',
+          maille: 'REG',
+          territoire_code: 'REG-01',
+        }, {
+          id: 'CH-169',
+          zone_id: 'FRANCE',
+          code_insee: 'FR',
+          maille: 'NAT',
+          territoire_code: 'NAT-FR',
+        }],
       });
 
       // When
@@ -53,7 +53,7 @@ describe('PrismaChantierRepository', () => {
       
       // Then
       expect(chantierResult.id).toEqual('CH-168');
-      expect(chantierResult.nom).toEqual('Nom chantier OK');
+      expect(chantierResult.nom).toEqual('Chantier 168');
       expect(chantierResult.estTerritorialise).toEqual(true);
       expect(chantierResult.listeDirecteursAdministrationCentrale).toIncludeSameMembers(['DAC 1', 'DAC 2']);
       expect(chantierResult.listeDirecteursProjet).toIncludeSameMembers(['DP 1', 'DP 2']);
@@ -63,68 +63,92 @@ describe('PrismaChantierRepository', () => {
   describe('#récupérerMailleNatEtDeptParId', () => {
     it('doit récupérer le chantier associé', async () => {
       // Given
-      await prisma.chantier.create({
-        data: {
+      await prisma.chantier_identite.createMany({
+        data: [{
           id: 'CH-168',
-          nom: 'Nom chantier OK',
+          nom: 'Chantier 168',
+          est_territorialise: true,
+          directeurs_administration_centrale: ['DAC 1', 'DAC 2'],
+          directeurs_projet: ['DP 1', 'DP 2'],
+        }, {
+          id: 'CH-169',
+          nom: 'Chantier 169',
+          est_territorialise: false,
+          directeurs_administration_centrale: ['DAC 5', 'DAC 6'],
+          directeurs_projet: ['DP 5', 'DP 6'],
+        }],
+      });
+      await prisma.chantier_territoire.createMany({
+        data: [{
+          id: 'CH-168',
+          zone_id: 'FRANCE',
           code_insee: 'FR',
-          taux_avancement: 10.2,
-          taux_avancement_annuel: 9.2,
           maille: 'NAT',
           territoire_code: 'NAT-FR',
+          taux_avancement_mandat: 10.2,
           meteo: 'SOLEIL',
           est_applicable: true,
-        },
-      });
-      await prisma.chantier.create({
-        data: {
+        }, {
           id: 'CH-168',
-          nom: 'Nom chantier OK maille DEPT',
+          zone_id: 'D01',
           code_insee: '01',
-          taux_avancement: 15.3,
-          taux_avancement_annuel: 13.3,
           maille: 'DEPT',
           territoire_code: 'DEPT-01',
+          taux_avancement_mandat: 15.3,
           meteo: 'COUVERT',
           est_applicable: true,
-        },
-      });
-      await prisma.chantier.create({
-        data: {
+        }, {
           id: 'CH-168',
-          nom: 'Nom chantier OK maille DEPT taux null',
+          zone_id: 'D02',
           code_insee: '02',
-          taux_avancement: null,
-          taux_avancement_annuel: null,
           maille: 'DEPT',
           territoire_code: 'DEPT-02',
+          taux_avancement_mandat: null,
           meteo: 'SOLEIL',
           est_applicable: false,
-        },
-      });
-      await prisma.chantier.create({
-        data: {
+        }, {
           id: 'CH-168',
-          nom: 'Nom chantier KO maille REG',
-          code_insee: '01',
-          taux_avancement: 10,
+          zone_id: 'R01',
+          code_insee: '02',
           maille: 'REG',
           territoire_code: 'REG-01',
+          taux_avancement_mandat: 10,
           meteo: 'SOLEIL',
           est_applicable: false,
-        },
-      });
-      await prisma.chantier.create({
-        data: {
+        }, {
           id: 'CH-169',
-          nom: 'Nom chantier KO ID',
+          zone_id: 'FRANCE',
           code_insee: 'FR',
-          taux_avancement: 10,
           maille: 'NAT',
           territoire_code: 'NAT-FR',
+          taux_avancement_mandat: 10,
           meteo: 'SOLEIL',
           est_applicable: false,
-        },
+        }],
+      });
+      await prisma.chantier_territoire_jalon.createMany({
+        data: [{
+          id: 'CH-168',
+          code_insee: 'FR',
+          maille: 'NAT',
+          territoire_code: 'NAT-FR',
+          jalon: '2025',
+          taux_avancement: 9.2,
+        }, {
+          id: 'CH-168',
+          code_insee: '01',
+          maille: 'DEPT',
+          territoire_code: 'DEPT-01',
+          jalon: '2025',
+          taux_avancement: 13.3,
+        }, {
+          id: 'CH-168',
+          code_insee: '02',
+          maille: 'DEPT',
+          territoire_code: 'DEPT-02',
+          jalon: '2025',
+          taux_avancement: null,
+        }],
       });
 
       // When
