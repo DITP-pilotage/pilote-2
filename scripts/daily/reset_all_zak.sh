7a4876bc8e9c3f8ab2745b1d57c99c1a46dbcaaa
Zakaria Seenovate <168171157+ZakariaSeenovate@users.noreply.github.com>
2025-01-23 15:49:00 +0100

    fix: correction affichage width tooltip (PIL-502) (#1269)
    * fix: correction affichage width tooltip (PIL-502)
    
    * fix: correction lint (PIL-502)

--- scripts/daily/reset_all_zak.sh
+++ scripts/daily/reset_all_zak.sh
@@ -1,20 +1,17 @@
 # Daily script to:
 #   - reset the db
 #   - run FULL_DJ datajobs
-#   - exec descente de prod DUMP
-#   - exec descente de prod RESTORE
+#   - run a dbt command (for example)
+#   - exec descente de prod
 #   - run PROD datajobs
 # 
 
 echo ">> Reset db"
-bash scripts/prisma_reset_and_migrate.sh
-cd data_management
-echo ">> Run dj FULL"
-FULL_DJ=true pipenv run bash scripts/run_datajobs.sh
-echo ">> Run descente de prod"
-cd ..
+npm run database:init
+# npm run database:init-force pour éviter le prompt :p (Colin)
+echo ">> Descente de prod"
 bash scripts/ddp_dump.sh
 bash scripts/ddp_restore.sh
 echo ">> Run dj prod"
 cd data_management
-FULL_DJ=false pipenv run bash scripts/run_datajobs.sh
+FULL_DJ=false pipenv run /bin/bash scripts/run_datajobs.sh
--- src/client/components/_commons/Cartographie/BulleDInfo/BulleDInfo.styled.ts
+++ src/client/components/_commons/Cartographie/BulleDInfo/BulleDInfo.styled.ts
@@ -3,13 +3,14 @@ import styled from '@emotion/styled';
 const BulleDInfoStyled = styled.div`
   position: fixed;
   z-index: 1;
+  min-width: 8rem;
   font-size: 0.75rem;
   vertical-align: middle;
   pointer-events: none; /* supprime le flickering lorsque le curseur est sur la bulle d'info */
   background-color: var(--background-contrast-grey);
   box-shadow: 0 4px 4px 0 #00000040;
   transform: translate(-50%, -3rem);
-
+  
   div:last-child {
     background-color: var(--background-alt-grey);
   }
