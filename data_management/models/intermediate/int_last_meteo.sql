eb0c67917d0a9fedef5bfd22a2a5cfd2b42f76bf
Tristan Conti <86486650+contitri@users.noreply.github.com>
2025-01-27 14:09:37 +0100

    fix: correction datajobs (PIL-530)

---
+++ data_management/models/intermediate/int_last_meteo.sql
@@ -0,0 +1,28 @@
+{{ config(materialized = 'table') }}
+
+WITH
+synthese_triee_par_date AS (
+    SELECT
+        chantier_id,
+        code_insee,
+        maille,
+        meteo,
+        date_meteo,
+        ROW_NUMBER()
+            OVER (
+                PARTITION BY chantier_id, code_insee, maille
+                ORDER BY date_meteo DESC
+            )
+        AS row_id_by_date_meteo_desc
+    FROM {{ source('db_schema_public', 'synthese_des_resultats') }}
+)
+
+SELECT
+    a.chantier_id,
+    t.code AS territoire_code,
+    meteo,
+    date_meteo
+FROM synthese_triee_par_date AS a
+LEFT JOIN {{ source('db_schema_public', 'territoire') }} AS t
+    ON t.maille = LOWER(a.maille)::maille
+WHERE row_id_by_date_meteo_desc = 1
---
+++ data_management/scripts/sql/1_set_flag_true.sql
@@ -0,0 +1,8 @@
+-- SET Flag à TRUE pour ces tables
+
+UPDATE public.axe SET a_supprimer = TRUE;
+UPDATE public.perimetre SET a_supprimer = TRUE;
+UPDATE public.ppg SET a_supprimer = TRUE;
+UPDATE public.chantier_identite SET a_supprimer = TRUE;
+UPDATE public.indicateur_identite SET a_supprimer = TRUE;
+UPDATE public.ministere SET a_supprimer = TRUE;
