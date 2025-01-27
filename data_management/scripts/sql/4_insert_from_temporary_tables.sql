2ea383fb9249ddd173d459ff36ae732178884b56
Tristan Conti <tristan.conti@seenovate.com>
2025-01-27 14:02:40 +0100

    fix: script insert temporary tables (PIL-530)

--- data_management/scripts/sql/4_insert_from_temporary_tables.sql
+++ data_management/scripts/sql/4_insert_from_temporary_tables.sql
@@ -1,6 +1,6 @@
 -- Restaurer les données à partir des tables temporaires
 
-INSERT INTO commentaire	SELECT com.* FROM commentaire_tmp tmp  INNER JOIN chantier_territoire ct ON ct.id = tmp.chantier_id AND ct.territoire_code = tmp.territoire_code;
-INSERT INTO synthese_des_resultats	SELECT * FROM synthese_des_resultats_tmp tmp INNER JOIN chantier_territoire ct ON ct.id = tmp.chantier_id AND ct.maille = LOWER(tmp.maille)::maille AND ct.code_insee = tmp.code_insee;
-INSERT INTO decision_strategique	SELECT * FROM decision_strategique_tmp tmp INNER JOIN chantier_identite ci ON ci.id = tmp.chantier_id;
-INSERT INTO objectif	SELECT * FROM objectif_tmp tmp INNER JOIN chantier_identite ci ON ci.id = tmp.chantier_id;
+INSERT INTO commentaire	SELECT tmp.* FROM commentaire_tmp tmp  INNER JOIN chantier_territoire ct ON ct.id = tmp.chantier_id AND ct.territoire_code = tmp.territoire_code;
+INSERT INTO synthese_des_resultats	SELECT tmp.* FROM synthese_des_resultats_tmp tmp INNER JOIN chantier_territoire ct ON ct.id = tmp.chantier_id AND ct.maille = LOWER(tmp.maille)::maille AND ct.code_insee = tmp.code_insee;
+INSERT INTO decision_strategique	SELECT tmp.* FROM decision_strategique_tmp tmp INNER JOIN chantier_identite ci ON ci.id = tmp.chantier_id;
+INSERT INTO objectif	SELECT tmp.* FROM objectif_tmp tmp INNER JOIN chantier_identite ci ON ci.id = tmp.chantier_id;
