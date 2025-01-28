3fd97e0774c155192d7e236fe777af4186ba286b
ZakariaSeenovate <zakaria.belghit@seenovate.com>
2025-01-28 15:58:14 +0000

    fix: fix linter (PIL-534)

--- src/client/components/_commons/Bloc/Bloc.styled.ts
+++ src/client/components/_commons/Bloc/Bloc.styled.ts
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
 
 const BlocStyled = styled.div`
+  min-width: 100%;
   background: white;
   border: 1px solid var(--border-disabled-grey);
-  border-radius: 8px;
-  min-width: 100%;
+  border-radius: 8px; 
 
   .titre {
     background: var(--background-action-low-blue-france);
