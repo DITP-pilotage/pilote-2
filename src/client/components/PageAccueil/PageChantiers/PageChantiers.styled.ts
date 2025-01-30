8be2850a6529eb9ccd1c4e45e8f79208c72b8dac
Jordan <jordantaillefer@users.noreply.github.com>
2025-01-30 09:43:09 +0100

    fix: css selecteur page ppg (PIL-530) (#1305)

--- src/client/components/PageAccueil/PageChantiers/PageChantiers.styled.ts
+++ src/client/components/PageAccueil/PageChantiers/PageChantiers.styled.ts
@@ -30,18 +30,20 @@ const PageChantiersStyled = styled.main`
     }
   }
 
-  .fr-select-group, .fr-select {
-    width: 3.5rem;
-    height: 1.5rem;
-    padding: 0.07rem 0 0 0.2rem;
-    margin: 0!important;
-    font-size: 0.75rem!important;
-    background-color: transparent;
-    background-position: 100% 70%!important;
-  }
-
-  .fr-select {
-    box-shadow: inset 0 -1px 0 0 var(--border-plain-grey)
+  .select-sm {
+    .fr-select-group, .fr-select {
+      width: 3.5rem;
+      height: 1.5rem;
+      padding: 0.07rem 0 0 0.2rem;
+      margin: 0!important;
+      font-size: 0.75rem!important;
+      background-color: transparent;
+      background-position: 100% 70%!important;
+    }
+  
+    .fr-select {
+      box-shadow: inset 0 -1px 0 0 var(--border-plain-grey)
+    }
   }
 
   .repartition-selecteur-maille .tag-liste {
