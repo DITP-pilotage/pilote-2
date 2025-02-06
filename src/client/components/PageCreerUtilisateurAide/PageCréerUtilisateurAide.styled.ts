8a6c74ae0051f6a6b5dc32b9e06c9de7c36716cf
ZakariaSeenovate <zakaria.belghit@seenovate.com>
2025-02-06 09:56:54 +0000

    feat: bouton créer compte sticky (PIL-403)

--- src/client/components/PageCreerUtilisateurAide/PageCréerUtilisateurAide.styled.ts
+++ src/client/components/PageCreerUtilisateurAide/PageCréerUtilisateurAide.styled.ts
@@ -6,7 +6,16 @@ const PageCréerUtilisateurAideStyled = styled.div`
   .bloc-background-default-grey {
     background: var(--background-default-grey);
   }
-  
+
+  .bouton-sticky {
+    position: sticky;
+    bottom: 0.1px;
+    z-index: 100;
+    background-color: white;
+    width: 100%;
+    padding-top: 1.2rem;
+    padding-bottom: 1.2rem;
+  }
 `;
 
 export default PageCréerUtilisateurAideStyled;
