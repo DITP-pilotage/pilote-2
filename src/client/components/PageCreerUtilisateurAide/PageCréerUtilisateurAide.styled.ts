46548279244e3cb2eb4305c2a47c27f277371acf
Zakaria Seenovate <168171157+ZakariaSeenovate@users.noreply.github.com>
2025-02-10 11:19:45 +0100

    feat: bouton créer compte sticky (PIL-403) (#1324)
    * feat: bouton créer compte sticky (PIL-403)
    
    * feat: correction lint erreur (PIL-403)

--- src/client/components/PageCreerUtilisateurAide/PageCréerUtilisateurAide.styled.ts
+++ src/client/components/PageCreerUtilisateurAide/PageCréerUtilisateurAide.styled.ts
@@ -6,7 +6,16 @@ const PageCréerUtilisateurAideStyled = styled.div`
   .bloc-background-default-grey {
     background: var(--background-default-grey);
   }
-  
+
+  .bouton-sticky {
+    position: sticky;
+    bottom: 0.1px;
+    z-index: 100;
+    width: 100%;
+    padding-top: 1.2rem;
+    padding-bottom: 1.2rem;
+    background-color: white;
+  }
 `;
 
 export default PageCréerUtilisateurAideStyled;
