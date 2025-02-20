3e61afbc6435f23a0310005168cab3e6da11c798
Colin Dugeai <colin.dugeai@gmail.com>
2025-02-20 17:54:17 +0100

    wip: comparaison dept reg nat (PIL-249)

---
+++ data_management/models/df3/2_aggregation/vi_dates_multiples_dept_vers_nat.sql
@@ -0,0 +1,219 @@
+-- Sélection des valeurs et calculs des aggrégation pour le niveau NAT
+--      en fonction des paramètres
+
+
+WITH
+-- On sélectionne certaines colonnes de la table des mesures valides
+--	et on y ajoute les paramètres d'aggrégation NAT
+mesure_last_params_nat AS (
+    SELECT
+        a.id,
+        a.date_import,
+        a.indic_id,
+        metric_date,
+        metric_type,
+        metric_value,
+        zone_id,
+        b.vi_nat_from,
+        b.vi_nat_op,
+        b.va_nat_from,
+        b.va_nat_op,
+        b.vc_nat_from,
+        b.vc_nat_op
+    FROM "qualif_249"."df3"."mesure_last_null_erase_keep_lastvalmonth" AS a
+    LEFT JOIN
+        "qualif_249"."raw_data"."metadata_parametrage_indicateurs" AS b
+        ON a.indic_id = b.indic_id
+),
+
+-- Valeurs NAT saisies directement par l'utilisateur
+mesure_last_params_nat_user AS (
+    SELECT
+        a.*,
+        b.maille AS zone_type
+    FROM mesure_last_params_nat AS a
+    LEFT JOIN "qualif_249"."raw_data"."stg_ppg_metadata__zones" AS b ON a.zone_id = b.id
+    WHERE
+        (
+            (metric_type = 'vi' AND vi_nat_from = 'user_input')
+            OR (metric_type = 'va' AND va_nat_from = 'user_input')
+            OR (metric_type = 'vc' AND vc_nat_from = 'user_input')
+        )
+        AND b.maille = 'NAT'
+),
+
+
+-- Liste des indicateurs qui ont un paramétrage d'aggrégation pour les valeurs NAT
+indic_agg_from_dept AS (
+    SELECT
+        b.indic_id,
+        b.vi_nat_from,
+        b.vi_nat_op,
+        b.va_nat_from,
+        b.va_nat_op,
+        b.vc_nat_from,
+        b.vc_nat_op
+    FROM
+        "qualif_249"."raw_data"."metadata_parametrage_indicateurs" AS b
+    WHERE
+        b.vi_nat_from NOT IN ('_', 'user_input')
+        OR b.va_nat_from NOT IN ('_', 'user_input')
+        OR b.vc_nat_from NOT IN ('_', 'user_input')
+
+),
+
+-- On prend chaque mesure
+--  on lui associe sa zone parente
+--  on lui associe ses paramètres d'aggrégation
+--  et on sélectionne que les valeurs qui sont DEPT avec un parent NAT
+-- Ce sont ces données que la DF va aggréger
+mesure_last_params_nat_from_dept AS (
+    SELECT
+        a.id,
+        a.date_import,
+        a.indic_id AS indic_id1,
+        metric_date,
+        metric_type,
+        metric_value::float,
+        a.zone_id,
+        b.zone_type,
+        b.zone_parent,
+        b.zone_parent_type,
+        c.*
+    FROM "qualif_249"."df3"."mesure_last_null_erase_keep_lastvalmonth" AS a
+    INNER JOIN "qualif_249"."df3"."zone_parent" AS b ON a.zone_id = b.zone_id
+    RIGHT JOIN indic_agg_from_dept AS c ON a.indic_id = c.indic_id
+    WHERE
+        -- uniquement des données NAT avec parent NAT
+        zone_type = 'DEPT'
+        -- et pour lesquels l'agg NAT doit bien se faire depuis les données NAT
+        AND (
+            (metric_type = 'vi' AND vi_nat_from = 'DEPT')
+            OR (metric_type = 'va' AND va_nat_from = 'DEPT')
+            OR (metric_type = 'vc' AND vc_nat_from = 'DEPT')
+        )
+)
+
+
+
+,
+
+-- Ici on calcule une aggrégation en somme ET en moyenne des DEPT vers NAT
+--	La sélection de la valeur correcte se fera ensuite en fonction des paramètres
+compute_op_sum_avg AS (
+    SELECT
+        -- On met id=NULL lorsque la valeur est générée par aggrégation et non issue d'une mesure saisie
+        null::uuid AS id,
+        max(date_import) AS date_import,
+        zone_parent,
+        indic_id AS indic_id1,
+        metric_date,
+        metric_type,
+        sum(metric_value::float) AS op_sum,
+        avg(metric_value::float) AS op_avg,
+        count(zone_id) as n_zone_prises_en_compte,
+        array_agg(zone_id) as zones
+    FROM mesure_last_params_nat_from_dept
+    -- TODO: explo
+    where 
+    --indic_id='IND-895' and
+metric_type='vi'
+and zone_type='DEPT' and zone_parent_type='NAT'
+and vi_nat_from='DEPT'
+    GROUP BY zone_parent, indic_id, metric_date, metric_type
+)
+
+--select * from compute_op_sum_avg 
+
+
+-- On sélectionne le bon résultat de calcul de l'aggrégation 
+, compute_op_selected AS (
+    SELECT
+        b.*,
+        a.*,
+        CASE
+            WHEN metric_type = 'vi' AND vi_nat_op = 'sum' THEN op_sum
+            WHEN metric_type = 'vi' AND vi_nat_op = 'avg' THEN op_avg
+            WHEN metric_type = 'va' AND va_nat_op = 'sum' THEN op_sum
+            WHEN metric_type = 'va' AND va_nat_op = 'avg' THEN op_avg
+            WHEN metric_type = 'vc' AND vc_nat_op = 'sum' THEN op_sum
+            WHEN metric_type = 'vc' AND vc_nat_op = 'avg' THEN op_avg
+            -- Si opération non supportée, ie hors de [sum, avg]
+            ELSE -1.212121
+        END AS op_selected
+    FROM indic_agg_from_dept AS a
+    RIGHT JOIN compute_op_sum_avg AS b ON a.indic_id = b.indic_id1
+)
+
+,
+
+-- On sélectionne qq colonne puis tri
+mesure_last_params_reg_aggregated AS (
+    SELECT
+        id,
+        date_import,
+        indic_id,
+        zone_parent AS zone_id,
+        metric_date,
+        metric_type,
+        op_selected AS metric_value,
+        n_zone_prises_en_compte,
+        zones,
+        rank()
+            OVER (PARTITION BY indic_id, zone_parent ORDER BY date_import DESC, metric_date ASC, random())
+        AS r
+    FROM compute_op_selected
+    order by indic_id, zone_parent, date_import DESC, metric_date ASC
+),
+
+mesure_last_params_reg_aggregated_rank1 as (
+    select 
+        indic_id,
+        zone_id,
+        metric_date,
+        metric_type,
+        metric_value,
+        zones as zones_rank_1
+    from mesure_last_params_reg_aggregated
+    WHERE r=1
+),
+
+mesure_last_params_reg_aggregated_rankn as (
+    select 
+        indic_id,
+        zone_id,
+        metric_date,
+        metric_type,
+        metric_value,
+        r as rrank,
+        zones as zones_rank_n
+    from mesure_last_params_reg_aggregated
+    WHERE r>1
+)
+, compare_rank1_vs_rankn as (
+select 
+    a.indic_id,
+    a.zone_id,
+    a.metric_date,
+    a.metric_type,
+    a.metric_value,
+    a.zones_rank_1,
+    b.metric_date as date_rankn,
+    b.metric_value as value_rankn,
+    b.zones_rank_n,
+    b.rrank as rank_n,
+    a.zones_rank_1 @> b.zones_rank_n as rank1_contains_rankn
+
+from mesure_last_params_reg_aggregated_rank1 a
+right join mesure_last_params_reg_aggregated_rankn b on 
+a.indic_id=b.indic_id and a.zone_id=b.zone_id
+and a.metric_type=b.metric_type
+)
+
+, keep_diffs as (
+    select * from compare_rank1_vs_rankn
+    WHERE not rank1_contains_rankn
+)
+
+select * from keep_diffs
+
