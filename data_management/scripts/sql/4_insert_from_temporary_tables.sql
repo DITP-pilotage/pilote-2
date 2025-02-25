a1a74672755765a9b6448471ab1dad6c25a1ec02
Tristan Conti <86486650+contitri@users.noreply.github.com>
2025-02-25 10:14:46 +0100

    fix: strategie incremental merge tables chantier (PIL-564)

--- data_management/scripts/sql/4_insert_from_temporary_tables.sql
+++
@@ -1,6 +0,0 @@
--- Restaurer les données à partir des tables temporaires
-
-INSERT INTO commentaire	SELECT tmp.* FROM commentaire_tmp tmp  INNER JOIN chantier_territoire ct ON ct.id = tmp.chantier_id AND ct.territoire_code = tmp.territoire_code;
-INSERT INTO synthese_des_resultats	SELECT tmp.* FROM synthese_des_resultats_tmp tmp INNER JOIN chantier_territoire ct ON ct.id = tmp.chantier_id AND ct.maille = LOWER(tmp.maille)::maille AND ct.code_insee = tmp.code_insee;
-INSERT INTO decision_strategique	SELECT tmp.* FROM decision_strategique_tmp tmp INNER JOIN chantier_identite ci ON ci.id = tmp.chantier_id;
-INSERT INTO objectif	SELECT tmp.* FROM objectif_tmp tmp INNER JOIN chantier_identite ci ON ci.id = tmp.chantier_id;
