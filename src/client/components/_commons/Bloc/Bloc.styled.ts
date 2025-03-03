2902701e3925f419f3a4020510a50dc2fba463f4
ZakariaSeenovate <zakaria.belghit@seenovate.com>
2025-03-03 14:28:01 +0000

    feat: correction lint (PIL-430)

--- src/client/components/_commons/Bloc/Bloc.styled.ts
+++ src/client/components/_commons/Bloc/Bloc.styled.ts
@@ -7,10 +7,10 @@ const BlocStyled = styled.div`
   border-radius: 8px; 
 
   .titre {
+    height: 4rem;
     background: var(--background-action-low-blue-france);
     border-bottom: 2px solid var(--border-plain-grey);
     border-radius: 7px 7px 0 0;
-    height: 4rem;
   }
   
   .titre-ellipsis {
--- src/client/components/_commons/Infobulle/Infobulle.styled.ts
+++ src/client/components/_commons/Infobulle/Infobulle.styled.ts
@@ -2,10 +2,10 @@ import styled from '@emotion/styled';
 
 export const InfobulleStyled = styled.div`
     .tooltip-infobulle {
-        width: 100%; 
-        z-index: 10000; 
-        background-color: var(--background-alt-blue-france); 
+        z-index: 10000;
+        width: 100%;
         color: var(--text-title-grey);
+        background-color: var(--background-alt-blue-france);     
         border-radius: 0.5rem;
         box-shadow: 0 4px 2px #0002; 
     }
