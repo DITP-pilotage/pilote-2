d1c1d663c4ca6ebb96e70940816200ca686dc63a
ZakariaSeenovate <zakaria.belghit@seenovate.com>
2025-03-07 14:26:18 +0000

    feat: correction lint erreur (PIL-584)

--- src/client/components/PageAdminIndicateurs/PageAdminIndicateurs.styled.ts
+++ src/client/components/PageAdminIndicateurs/PageAdminIndicateurs.styled.ts
@@ -3,13 +3,14 @@ import styled from '@emotion/styled';
 const PageAdminIndicateursStyled = styled.div`
     .bouton-creation-indicateur {
         display: flex;
+        align-items: flex-end;        
         justify-content: flex-end;
-        align-items: flex-end;
 
         @media(max-width: 576px) {
             justify-content: center;
         }
     }
+        
     .tableau {
         background-color: white;
     }
