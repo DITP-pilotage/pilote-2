ab5c00f6d15fae8075443f6856ec4154129b68ca
Jordan Taillefer <jtaillefer02@gmail.com>
2025-03-27 14:48:37 +0100

    fix: destroy session (PIL-00)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -49,10 +49,6 @@ class MyDocument extends Document<MyDocumentProps> {
         <Head>
           <meta charSet='utf-8' />
           <meta 
-            content='width=device-width, initial-scale=1' 
-            name='viewport' 
-          />
-          <meta 
             content='IE=edge' 
             httpEquiv='X-UA-Compatible' 
           />
