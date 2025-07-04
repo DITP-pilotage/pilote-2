253968a8f8d0143d02664adb441ecf570aa45c29
Tristan Conti <tristan.conti@seenovate.com>
2025-07-04 14:42:39 +0200

    fix: export csv filtres statuts (PIL-713)

--- src/server/chantiers/infrastructure/handlers/ExportCSVIndicateurHandler.ts
+++ src/server/chantiers/infrastructure/handlers/ExportCSVIndicateurHandler.ts
@@ -25,7 +25,7 @@ export const handleExportDesIndicateurs = async (request: NextApiRequest, respon
     perimetreIds: request.query.perimetreIds ? Array.isArray(request.query.perimetreIds) ? request.query.perimetreIds : [request.query.perimetreIds] as string[] : [],
     estBarometre: request.query.estBarometre === 'true',
     territorialisation: request.query.territorialisation ? Array.isArray(request.query.territorialisation) ? request.query.territorialisation.map(maille => maille as Maille) : [request.query.territorialisation as Maille] : [],
-    listeStatuts: request.query.statut ? Array.isArray(request.query.statut) ? request.query.statut : [request.query.statut] as string[] : [],
+    listeStatuts: request.query.statut ? Array.isArray(request.query.statut) ? request.query.statut : [request.query.statut] as string[] : (session.profilAAccèsAuxChantiersBrouillons ? [] : ['PUBLIE', 'BROUILLON']),
     listeChantierId: request.query.listeChantierId ? (request.query.listeChantierId as string).split(',') : [],
     listeMeteos: request.query.meteos ? Array.isArray(request.query.meteos) ? request.query.meteos : [request.query.meteos] as string[] : [],
     listeOptionsExport: request.query.optionsExport ? Array.isArray(request.query.optionsExport) ? request.query.optionsExport : [request.query.optionsExport] as string[] : [],
