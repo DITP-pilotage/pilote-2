698526e548b5e8f01b9bf651375e6482cffb5b43
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-09-01 16:20:19 +0200

    feat: couleurs pour lignes (PIL-822)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         "dsfr-info-main-525": "#0078F3",
+        "dsfr-info-950": "#E8EDFF",
+        "dsfr-moutarde-main-679": "#C3992A",
+        "dsfr-moutarde-main-975": "#FEF5E8",
+        "dsfr-grey-200": "#3A3A3A",
       },
     },
   },
