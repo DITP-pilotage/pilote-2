cba77bd1f3eecc268463bfdf7cd9bce9c894a9a7
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-09-02 12:14:39 +0200

    fix: migration timestamp (PIL-824)

--- src/database/prisma/migrations/20250902101341_ajoute_datajobs_execution/migration.sql
+++ src/database/prisma/migrations/20250902101341_ajoute_datajobs_execution/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "public"."datajobs_execution" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "derniere_date_execution" TIME NOT NULL,
+    "derniere_date_execution" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "datajobs_execution_pkey" PRIMARY KEY ("id")
 );
