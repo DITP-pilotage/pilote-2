77bb201926182a5116d6860bfcf97bec9237a549
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-09-03 15:34:11 +0200

    chore: update commit lint (?) (PIL-00)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,29 +1,9 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   parserPreset: {
     parserOpts: {
-      headerPattern: /^(.+) \((PIL-\d+)\)$/u,
-      breakingHeaderPattern: /^(.+) \((PIL-\d+)\)$/u,
-      headerCorrespondence: [
-        'subject',
-        'ticket'
-      ],
+      headerPattern: /^(.+) \((PIL-\d+)\)$/,
+      breakingHeaderPattern: /^(.+) \((PIL-\d+)\)$/,
     },
   },
-  plugins: [
-    {
-      rules: {
-        "header-match-ticket": (parsed) => {
-          const { ticket } = parsed;
-          if (ticket === null) {
-            return [false, "Il manque le numéro de ticket"];
-          }
-          return [true, ""];
-        },
-      },
-    }
-  ],
-  rules: {
-    "header-match-ticket": [2, "always"],
-  }
-}
+};
