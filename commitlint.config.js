07878cf67782a10950db0264104127d6eef3171d
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-09-03 15:37:16 +0200

    chore: supprime commitlint (PIL-00)

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(.+) \((PIL-\d+)\)$/,
-      breakingHeaderPattern: /^(.+) \((PIL-\d+)\)$/,
-    },
-  },
-};
