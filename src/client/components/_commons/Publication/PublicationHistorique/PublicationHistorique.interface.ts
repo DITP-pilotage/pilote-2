4483f0ad8c145d0ceed914dc41bf257ed4d52a51
Jordan Taillefer <jtaillefer02@gmail.com>
2025-09-12 16:00:10 +0200

    refactor: delete unused components (PIL-645)

--- src/client/components/_commons/Publication/PublicationHistorique/PublicationHistorique.interface.ts
+++
@@ -1,8 +0,0 @@
-import PublicationProps from "@/client/components/_commons/Publication/Publication.interface";
-
-export default interface PublicationHistoriqueProps {
-  type: PublicationProps["caractéristiques"]["type"];
-  entité: PublicationProps["caractéristiques"]["entité"];
-  réformeId: PublicationProps["réformeId"];
-  maille: PublicationProps["maille"];
-}
--- src/client/components/_commons/PublicationChantier/PublicationHistorique/PublicationHistorique.tsx
+++ src/client/components/_commons/PublicationChantier/PublicationHistorique/PublicationHistorique.tsx
@@ -1,7 +1,7 @@
 import { Fragment, FunctionComponent, useMemo } from "react";
 import Modale from "@/components/_commons/Modale/Modale";
-import PublicationAffichage from "@/components/_commons/Publication/PublicationAffichage/PublicationAffichage";
 import BoutonSousLigné from "@/components/_commons/BoutonSousLigné/BoutonSousLigné";
+import PublicationAffichage from "@/components/_commons/PublicationChantier/PublicationAffichage/PublicationAffichage";
 import PublicationHistoriqueProps from "./PublicationHistorique.interface";
 import usePublicationHistorique from "./usePublicationHistorique";
 
