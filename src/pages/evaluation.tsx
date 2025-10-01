ba9069e787e18e17d3bde61de7bf750636b178bc
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-10-01 15:56:50 +0200

    chore: creation de la page

---
+++ src/pages/evaluation.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function EvaluationPage() {
+  return (
+    <main>
+      <Head>
+        <title>PILOTE - Évaluation</title>
+      </Head>
+
+      <div className="min-h-[60vh]">Hello world</div>
+    </main>
+  );
+}
