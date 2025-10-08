4318bb431373e4487c51d9001ebbfb199d8c3ffb
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-10-08 14:42:34 +0200

    refactor: extrait component FormulaireEvaluation

---
+++ src/client/components/PageEvaluation/FormulaireEvaluation.tsx
@@ -0,0 +1,116 @@
+import { FormEvent, useId, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { pageEvaluation } from "@/components/PageEvaluation/PageEvaluationServerSideContext";
+import { useEnregistrerBrouillon } from "@/components/PageEvaluation/useEnregistrerBrouillon";
+import { formSchema, FormValues } from "@/components/PageEvaluation/form";
+import { formaterDate } from "@/client/utils/date/date";
+import { BoutonEnregistrerBrouillon } from "@/components/PageEvaluation/BoutonEnregistrerBrouillon";
+import { Bouton } from "@/components/_commons/Bouton/Bouton";
+import { Icone } from "@/components/_commons/Icone";
+import { ArrowLine1Icon } from "@/components/_commons/Icones/ArrowLine1Icon";
+import { ArrowLine3Icon } from "@/components/_commons/Icones/ArrowLine3Icon";
+import { EtapeCriteres } from "@/components/PageEvaluation/EtapeCriteres";
+import { EtapeObjectifs } from "@/components/PageEvaluation/EtapeObjectifs";
+
+export const FormulaireEvaluation = () => {
+  const { autoEvaluation } = pageEvaluation.useServerSidePropsContext();
+  const [etape, setEtape] = useState<"criteres" | "objectifs">("criteres");
+  const formId = useId();
+  const enregistrerBrouillon = useEnregistrerBrouillon();
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    mode: "onChange",
+    defaultValues: {
+      criteres: autoEvaluation.criteres.map((critere) => ({
+        sousCriteres: critere.sousCriteres.map((sousCritere) => ({
+          id: sousCritere.evaluation.id,
+          note: sousCritere.evaluation.note ?? undefined,
+          commentaire: sousCritere.evaluation.commentaire ?? undefined,
+        })),
+      })),
+      objectifs: autoEvaluation.objectifs.map((objectif) => ({
+        id: objectif.evaluation.id,
+        note: objectif.evaluation.note ?? undefined,
+        commentaire: objectif.evaluation.commentaire ?? undefined,
+      })),
+    },
+  });
+
+  const handleSubmitCriteres = async (e: FormEvent) => {
+    e.preventDefault();
+    const isValid = await form.trigger("criteres", {
+      shouldFocus: true,
+    });
+    if (!isValid) return;
+
+    setEtape("objectifs");
+    window.scrollTo(0, 0);
+  };
+
+  const handleSubmit =
+    etape === "criteres"
+      ? handleSubmitCriteres
+      : form.handleSubmit(enregistrerBrouillon);
+
+  return (
+    <main className="py-6 pt-0">
+      <FormProvider {...form}>
+        <div className="min-h-[60vh] relative">
+          <div className="sticky top-0 bg-white mb-6">
+            <div className="flex items-center justify-between mt-4 mx-auto w-full max-w-4xl px-2 py-4">
+              <span className="italic text-sm">
+                Dernière modification :{" "}
+                {formaterDate(
+                  autoEvaluation.dateDerniereModification,
+                  "DD/MM/YYYY [à] H[h]mm",
+                )}
+              </span>
+              {etape === "criteres" && (
+                <div className="ml-auto flex items-center gap-4">
+                  <BoutonEnregistrerBrouillon formId={formId} />
+                  <Bouton
+                    form={formId}
+                    iconRight={
+                      <Icone className="text-current" icone={ArrowLine1Icon} />
+                    }
+                    label="Objectifs"
+                    type="submit"
+                  />
+                </div>
+              )}
+              {etape === "objectifs" && (
+                <div className="ml-auto flex items-center gap-4">
+                  <Bouton
+                    iconLeft={
+                      <Icone className="text-current" icone={ArrowLine3Icon} />
+                    }
+                    label="Critères"
+                    onClick={() => setEtape("criteres")}
+                    variant="secondary"
+                  />
+
+                  <BoutonEnregistrerBrouillon formId={formId} />
+
+                  <Bouton label="Soumettre" />
+                </div>
+              )}
+            </div>
+          </div>
+          <form
+            className="bg-white mx-auto w-full max-w-4xl"
+            id={formId}
+            onSubmit={handleSubmit}
+          >
+            <header className="p-4 bg-dsfr-blue-france-925 border-b-2 border-black">
+              <span className="font-bold text-sm">Mon auto-évaluation</span>
+            </header>
+
+            {etape === "criteres" && <EtapeCriteres />}
+            {etape === "objectifs" && <EtapeObjectifs />}
+          </form>
+        </div>
+      </FormProvider>
+    </main>
+  );
+};
---
+++ src/client/components/PageEvaluation/useEnregistrerBrouillon.ts
@@ -0,0 +1,35 @@
+import api from "@/server/infrastructure/api/trpc/api";
+import { pageEvaluation } from "@/components/PageEvaluation/PageEvaluationServerSideContext";
+import { FormValues } from "@/components/PageEvaluation/form";
+
+export function useEnregistrerBrouillon() {
+  const enregisterBrouillon = api.evaluation.enregistrerBrouillon.useMutation();
+  const { autoEvaluation } = pageEvaluation.useServerSidePropsContext();
+
+  return (data: FormValues) => {
+    enregisterBrouillon.mutate({
+      ficheEvaluationId: autoEvaluation.ficheEvaluationId,
+      evaluationsObjectifs: autoEvaluation.objectifs.map((objectif, index) => {
+        const evaluation = data.objectifs[index];
+        return {
+          id: evaluation.id,
+          objectifId: objectif.id,
+          note: evaluation.note,
+          commentaire: evaluation.commentaire,
+        };
+      }),
+      evaluationsSousCriteres: autoEvaluation.criteres.flatMap(
+        (critere, index) =>
+          critere.sousCriteres.map((sousCritere, jindex) => {
+            const evaluation = data.criteres[index].sousCriteres[jindex];
+            return {
+              id: evaluation.id,
+              sousCritereId: sousCritere.id,
+              note: evaluation.note,
+              commentaire: evaluation.commentaire,
+            };
+          }),
+      ),
+    });
+  };
+}
