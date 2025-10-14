38a7e81621ea47ea553812432eb34331eb460fa1
Jordan Taillefer <jtaillefer02@gmail.com>
2025-10-14 09:39:58 +0200

    suppression prisma statut + passage eval a critere

--- src/database/prisma/migrations/20251014073926_ajoute_tables_pilote_eval/migration.sql
+++ src/database/prisma/migrations/20251014073926_ajoute_tables_pilote_eval/migration.sql
@@ -1,9 +1,6 @@
 -- CreateEnum
 CREATE TYPE "public"."etape_evaluation_enum" AS ENUM ('AUTO_EVALUATION', 'CONSOLIDATION', 'CONTROLE_QUALITE', 'AJUSTEMENTS', 'CONTRE_PROPOSITION', 'CONTROLE_QUALITE_BIS', 'AJUSTEMENTS_BIS');
 
--- CreateEnum
-CREATE TYPE "public"."statut_evaluation" AS ENUM ('VERIFIE', 'A_VERIFIER');
-
 -- CreateTable
 CREATE TABLE "public"."referentiel_critere" (
     "id" UUID NOT NULL,
@@ -94,7 +91,6 @@ CREATE TABLE "public"."evaluation_objectif" (
     "auteur_id" UUID NOT NULL,
     "note" INTEGER,
     "commentaire" TEXT NOT NULL,
-    "statut_evaluation" "public"."statut_evaluation" NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -102,18 +98,17 @@ CREATE TABLE "public"."evaluation_objectif" (
 );
 
 -- CreateTable
-CREATE TABLE "public"."evaluation_sous_critere" (
+CREATE TABLE "public"."evaluation_critere" (
     "id" UUID NOT NULL,
     "etape_evaluation_id" UUID NOT NULL,
-    "sous_critere_id" UUID NOT NULL,
+    "critere_id" UUID NOT NULL,
     "auteur_id" UUID NOT NULL,
     "note" INTEGER,
     "commentaire" TEXT NOT NULL,
-    "statut_evaluation" "public"."statut_evaluation" NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "evaluation_sous_critere_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "evaluation_critere_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -129,7 +124,7 @@ CREATE UNIQUE INDEX "rattachement_utilisateur_etape_jalon_utilisateur_id_rattach
 CREATE UNIQUE INDEX "evaluation_objectif_etape_evaluation_id_objectif_id_auteur__key" ON "public"."evaluation_objectif"("etape_evaluation_id", "objectif_id", "auteur_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "evaluation_sous_critere_etape_evaluation_id_sous_critere_id_key" ON "public"."evaluation_sous_critere"("etape_evaluation_id", "sous_critere_id", "auteur_id");
+CREATE UNIQUE INDEX "evaluation_critere_etape_evaluation_id_critere_id_auteur_id_key" ON "public"."evaluation_critere"("etape_evaluation_id", "critere_id", "auteur_id");
 
 -- AddForeignKey
 ALTER TABLE "public"."referentiel_sous_critere" ADD CONSTRAINT "referentiel_sous_critere_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "public"."referentiel_critere"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -159,10 +154,10 @@ ALTER TABLE "public"."evaluation_objectif" ADD CONSTRAINT "evaluation_objectif_o
 ALTER TABLE "public"."evaluation_objectif" ADD CONSTRAINT "evaluation_objectif_auteur_id_fkey" FOREIGN KEY ("auteur_id") REFERENCES "public"."utilisateur"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."evaluation_sous_critere" ADD CONSTRAINT "evaluation_sous_critere_etape_evaluation_id_fkey" FOREIGN KEY ("etape_evaluation_id") REFERENCES "public"."etape_evaluation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."evaluation_critere" ADD CONSTRAINT "evaluation_critere_etape_evaluation_id_fkey" FOREIGN KEY ("etape_evaluation_id") REFERENCES "public"."etape_evaluation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."evaluation_sous_critere" ADD CONSTRAINT "evaluation_sous_critere_sous_critere_id_fkey" FOREIGN KEY ("sous_critere_id") REFERENCES "public"."referentiel_sous_critere"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."evaluation_critere" ADD CONSTRAINT "evaluation_critere_critere_id_fkey" FOREIGN KEY ("critere_id") REFERENCES "public"."referentiel_critere"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."evaluation_sous_critere" ADD CONSTRAINT "evaluation_sous_critere_auteur_id_fkey" FOREIGN KEY ("auteur_id") REFERENCES "public"."utilisateur"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."evaluation_critere" ADD CONSTRAINT "evaluation_critere_auteur_id_fkey" FOREIGN KEY ("auteur_id") REFERENCES "public"."utilisateur"("id") ON DELETE CASCADE ON UPDATE CASCADE;
