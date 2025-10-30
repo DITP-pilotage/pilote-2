bf17d50b458153c98e77ecf50c0d0207df15edda
Tristan Conti <tristan.conti@seenovate.com>
2025-10-30 18:33:44 +0100

    affichage front

--- src/client/components/Evaluation/ListeAutoEvaluations.tsx
+++ src/client/components/Evaluation/ListeAutoEvaluations.tsx
@@ -20,6 +20,7 @@ type FicheEvaluation = {
     nombreNotes: number;
     nombreTotal: number;
   };
+  noteCollective: number | null;
 };
 
 const CardEvaluation = ({
@@ -82,7 +83,40 @@ const CardEvaluation = ({
 };
 
 // eslint-disable-next-line react/no-multi-comp
-const CardVide = () => null;
+const CardNoteCollective = ({
+  titre,
+  moyenne,
+  lien,
+}: {
+  titre: string;
+  moyenne: number | null;
+  lien: string;
+}) => {
+  return (
+    <section className="bg-white border border-gray-300 rounded p-4 flex flex-col">
+      <header className="flex mb-2">
+        <h3 className="!text-sm font-semibold grow">{titre}</h3>
+        <div className="items-end flex flex-col">
+          {moyenne !== null ? (
+            <>
+              <span className="text-4xl font-bold ">{moyenne}</span>
+              <span className="text-gray-400 text-sm"> / 100</span>
+            </>
+          ) : (
+            <span className="text-gray-400 !text-sm">N/A</span>
+          )}
+        </div>
+      </header>
+
+      <div className="text-sm text-gray-600 mb-4 mt-auto">
+        Consulter le détail des chantiers collectifs
+      </div>
+      <Link className="fr-btn !w-full !justify-center" href={lien}>
+        Voir le détail
+      </Link>
+    </section>
+  );
+};
 
 // eslint-disable-next-line react/no-multi-comp
 export const ListeAutoEvaluations = ({
@@ -123,7 +157,11 @@ export const ListeAutoEvaluations = ({
                 titre="Manière de servir"
                 variant="secondary"
               />
-              <CardVide />
+              <CardNoteCollective
+                lien={`/evaluation/auto-evaluation/${ficheEvaluation.id}#note-collective`}
+                moyenne={ficheEvaluation.noteCollective}
+                titre="Objectifs collectifs"
+              />
             </div>
           </div>
         </div>
