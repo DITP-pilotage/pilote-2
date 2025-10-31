b5ca72ac6aa718f0302be6457f796776ba2ac33e
Tristan Conti <tristan.conti@seenovate.com>
2025-10-31 14:37:07 +0100

    refactor migrations

--- src/database/prisma/migrations/20251031084319_ajoute_cle_etrangere_chantier_identite_note_collective/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "public"."chantier_evaluation" ADD CONSTRAINT "chantier_evaluation_id_fkey" FOREIGN KEY ("id") REFERENCES "public"."chantier_identite"("id") ON DELETE CASCADE ON UPDATE CASCADE;
