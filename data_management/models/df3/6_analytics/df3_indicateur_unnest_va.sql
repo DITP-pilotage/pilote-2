e09155a5e97d03dfc450c95b93b42726dd2a11ff
Romain Thoyer <51092742+rthoyer@users.noreply.github.com>
2025-11-10 17:45:57 +0100

    Fix casting and format

--- data_management/models/df3/6_analytics/df3_indicateur_unnest_va.sql
+++ data_management/models/df3/6_analytics/df3_indicateur_unnest_va.sql
@@ -4,7 +4,7 @@ WITH unnest_json_array AS (
     SELECT
         id,
         territoire_code,
-        jsonb_array_elements(
+        JSONB_ARRAY_ELEMENTS(
             evolution_valeur_actuelle
         ) AS evolution_valeur_actuelle_unnest
     FROM {{ ref('indicateur_territoire') }}
@@ -15,11 +15,11 @@ SELECT
     territoire_code,
     --evolution_valeur_actuelle_unnest,
     (evolution_valeur_actuelle_unnest ->> 'valeur')
-    ::numeric AS va_unnest_computed,
+    ::NUMERIC AS va_unnest_computed,
     (evolution_valeur_actuelle_unnest ->> 'date')
-    ::date AS va_date_unnest_computed,
+    ::DATE AS va_date_unnest_computed,
     (evolution_valeur_actuelle_unnest ->> 'taa')
-    ::date AS taa_unnest_computed,
+    ::NUMERIC AS taa_unnest_computed,
     (evolution_valeur_actuelle_unnest ->> 'tag')
-    ::date AS tag_unnest_computed
+    ::NUMERIC AS tag_unnest_computed
 FROM unnest_json_array
