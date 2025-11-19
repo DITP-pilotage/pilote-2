dde3148f18ff7a07d1b1a587b79932301a2f8eed
Jordan Taillefer <jtaillefer02@gmail.com>
2025-11-19 16:35:41 +0100

    feat: simplification if

--- src/client/utils/nombre/nombre.ts
+++ src/client/utils/nombre/nombre.ts
@@ -2,7 +2,7 @@ export function formaterNombre(
   nombre: number | null | undefined,
   decimales: number,
 ) {
-  if (nombre === undefined || nombre === null) {
+  if (nombre == null) {
     return "";
   }
 
