4fc5bc5930866b2f4ab66bac5f7260f1347acdf8
Jordan Taillefer <jtaillefer02@gmail.com>
2025-11-19 11:26:38 +0100

    rework: entete accueil auto evaluation

--- src/client/components/Evaluation/ListeAutoEvaluation/ListeAutoEvaluations.tsx
+++ src/client/components/Evaluation/ListeAutoEvaluation/ListeAutoEvaluations.tsx
@@ -1,5 +1,4 @@
 import { $Enums } from "@prisma/client";
-import { BadgeFicheEtape } from "@/components/_commons/BadgeFicheEtape/BadgeFicheEtape";
 import { CardEvaluation } from "./CardEvaluation";
 import { CardNoteCollective } from "./CardNoteCollective";
 
@@ -23,6 +22,23 @@ type FicheEvaluation = {
   noteCollective: number | null;
 };
 
+const formatterTitreEvaluation = ({
+  code,
+  libelle,
+}: {
+  code: string;
+  libelle: string;
+}) => {
+  if (code.startsWith("DEPT")) {
+    return `${code.split("-")[1]} - ${libelle}`;
+  }
+
+  if (code.startsWith("REG")) {
+    return `Région ${libelle}`;
+  }
+  return libelle;
+};
+
 export const ListeAutoEvaluations = ({
   fichesEvaluation,
 }: {
@@ -31,43 +47,45 @@ export const ListeAutoEvaluations = ({
   return (
     <div className="space-y-8">
       {fichesEvaluation.map((ficheEvaluation) => (
-        <div className="bg-white rounded shadow" key={ficheEvaluation.id}>
-          <header className="p-4 border-b-2 border-primary text-primary flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <span className="font-bold text-sm">
-                Auto-évaluation - {ficheEvaluation.rattachement.libelle}
-              </span>
-            </div>
-            <BadgeFicheEtape
-              etape={ficheEvaluation.etapeCourante}
-              taille="grand"
-            />
+        <div className="flex flex-column gap-2" key={ficheEvaluation.id}>
+          <header>
+            <h2 className="!text-2xl !text-primary !mb-0">
+              {formatterTitreEvaluation(ficheEvaluation.rattachement)}
+            </h2>
+            <ul>
+              <li className="!text-pilote-yellow">
+                <b>Objectifs individuels</b> : vous avez auto-évalué 2
+                objectif(s) sur 5
+              </li>
+              <li className="!text-pilote-yellow">
+                <b>Manière de servir</b> : votre auto-évaluation est complète et
+                en attente d'être transmise
+              </li>
+            </ul>
           </header>
 
-          <div className="p-6">
-            <div className="grid md:grid-cols-3 gap-4">
-              <CardEvaluation
-                lien={`/evaluation/auto-evaluation/${ficheEvaluation.id}/objectifs`}
-                moyenne={ficheEvaluation.objectifs.moyenne}
-                nombreNotes={ficheEvaluation.objectifs.nombreNotes}
-                nombreTotal={ficheEvaluation.objectifs.nombreTotal}
-                titre="Objectifs individuels"
-              />
-              <CardEvaluation
-                lien={`/evaluation/auto-evaluation/${ficheEvaluation.id}/manieres-de-servir`}
-                moyenne={ficheEvaluation.criteres.moyenne}
-                nombreNotes={ficheEvaluation.criteres.nombreNotes}
-                nombreTotal={ficheEvaluation.criteres.nombreTotal}
-                titre="Manière de servir"
-                variant="secondary"
-              />
-              <CardNoteCollective
-                lien={`/evaluation/note-collective/${ficheEvaluation.rattachement.code}`}
-                moyenne={ficheEvaluation.noteCollective}
-                rattachement={ficheEvaluation.rattachement.libelle}
-                titre="Objectifs collectifs"
-              />
-            </div>
+          <div className="grid md:grid-cols-3 gap-4">
+            <CardEvaluation
+              lien={`/evaluation/auto-evaluation/${ficheEvaluation.id}/objectifs`}
+              moyenne={ficheEvaluation.objectifs.moyenne}
+              nombreNotes={ficheEvaluation.objectifs.nombreNotes}
+              nombreTotal={ficheEvaluation.objectifs.nombreTotal}
+              titre="Objectifs individuels"
+            />
+            <CardEvaluation
+              lien={`/evaluation/auto-evaluation/${ficheEvaluation.id}/manieres-de-servir`}
+              moyenne={ficheEvaluation.criteres.moyenne}
+              nombreNotes={ficheEvaluation.criteres.nombreNotes}
+              nombreTotal={ficheEvaluation.criteres.nombreTotal}
+              titre="Manière de servir"
+              variant="secondary"
+            />
+            <CardNoteCollective
+              lien={`/evaluation/note-collective/${ficheEvaluation.rattachement.code}`}
+              moyenne={ficheEvaluation.noteCollective}
+              rattachement={ficheEvaluation.rattachement.libelle}
+              titre="Objectifs collectifs"
+            />
           </div>
         </div>
       ))}
