e749ee8d785e39932fb7d79441a1ecad1f35c27b
Tristan Conti <tristan.conti@seenovate.com>
2025-11-20 10:55:42 +0100

    anonymisation email

--- scripts/ddp_restore.sh
+++ scripts/ddp_restore.sh
@@ -47,3 +47,10 @@ echo ">> pg_restore dumped file..."
 time pg_restore -d $DATABASE_URL --verbose \
     --no-owner --no-privileges --exit-on-error \
     $DUMP_DEST
+
+echo ">> Anonymisation des utilisateur..."
+time psql -d $DATABASE_URL -c "
+UPDATE utilisateur
+SET email = REPLACE(CONCAT(id, '@example.com'), ' ', '')
+WHERE email NOT LIKE '%@example%'";
+"
