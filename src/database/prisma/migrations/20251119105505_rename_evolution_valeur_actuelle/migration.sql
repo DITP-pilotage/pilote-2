aaa5b55c70735ccbf16e54e16c8317419e8ddf7b
Romain Thoyer <51092742+rthoyer@users.noreply.github.com>
2025-11-20 10:55:47 +0100

    Fix migr and renaming

--- src/database/prisma/migrations/20251119105505_rename_evolution_valeur_actuelle/migration.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "public"."indicateur_territoire"
-ADD COLUMN "evolution_avancement" JSONB;
-
-UPDATE "public"."indicateur_territoire"
-SET
-    "evolution_avancement" = "evolution_valeur_actuelle";
-
-ALTER TABLE "public"."indicateur_territoire"
-DROP COLUMN "evolution_valeur_actuelle";
