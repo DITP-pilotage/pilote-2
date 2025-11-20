a0dcad5b0a539576a682501a65d57de899b44a1e
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-11-20 10:29:25 +0100

    chore: backend modification objectif

---
+++ src/server/evaluation/handlers/ModifierObjectifHandler.ts
@@ -0,0 +1,60 @@
+import { z } from "zod";
+import { Transaction } from "@/server/db/Transaction";
+import { PrismaPilote } from "@/server/db/PrismaPilote";
+import { getPrisma } from "@/server/db/PrismaTransaction";
+import { NotFoundError } from "@/server/app/error-boundary/not-found-error";
+
+export const modifierObjectifCommandSchema = z.object({
+  ficheEvaluationId: z.string(),
+  objectifId: z.string(),
+  descriptif: z.string(),
+  indicateurCible: z.string(),
+});
+
+export type ModifierObjectifCommand = z.infer<
+  typeof modifierObjectifCommandSchema
+>;
+
+export class ModifierObjectifHandler {
+  constructor(
+    private readonly dependencies: {
+      transaction: Transaction;
+      prisma: PrismaPilote;
+    },
+  ) {}
+
+  async execute(command: ModifierObjectifCommand): Promise<void> {
+    await this.dependencies.transaction.run(async () => {
+      const prisma = getPrisma();
+
+      const objectif = await prisma.referentiel_objectif.findFirst({
+        where: {
+          id: command.objectifId,
+          evaluations: {
+            some: {
+              etape_evaluation: {
+                fiche_evaluation_id: command.ficheEvaluationId,
+              },
+            },
+          },
+        },
+      });
+
+      if (!objectif) {
+        throw new NotFoundError(
+          "Objectif non trouvé pour cette fiche d'évaluation",
+        );
+      }
+
+      await prisma.referentiel_objectif.update({
+        where: {
+          id: command.objectifId,
+        },
+        data: {
+          descriptif: command.descriptif,
+          indicateur_cible: command.indicateurCible,
+        },
+      });
+    });
+  }
+}
