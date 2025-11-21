5f504bd5f614d6811b2f964700f6e98e4a21e1ac
Tristan Conti <tristan.conti@seenovate.com>
2025-11-21 10:08:12 +0100

    fix: space

--- data_management/models/df3/4_compute/merge_computed_values.sql
+++ data_management/models/df3/4_compute/merge_computed_values.sql
@@ -1,10 +1,26 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    pre_hook="SET max_parallel_workers_per_gather = 0;"
+) }}
 
 -- On fait la jointure des:
 --  VACA, VACG, VCA, VCG, VIG
 
-select 
-a.*,
+WITH current_year_vca AS (
+    select indic_id, zone_id, vca, vca_date
+    from {{ ref('get_vca_jalon') }}
+    where jalon = date_part('year', now())
+)
+
+select
+a.id,
+a.date_import,
+a.indic_id,
+a.zone_id,
+a.metric_date,
+a.vi,
+a.va,
+a.vc,
 b.vaca,
 c.vacg,
 vacp.vacp as vacp,
@@ -16,12 +32,12 @@ d.vca as vca_adate, d.vca_date as vca_adate_date,
 e.vig, e.vig_date,
 f.vcg, f.vcg_date
 from {{ ref('pivot_mesures') }} a
-left join {{ ref('compute_vaca') }} b on a.indic_id =b.indic_id and a.zone_id =b.zone_id and a.metric_date = b.metric_date 
-left join {{ ref('compute_vacg') }} c on a.indic_id =c.indic_id and a.zone_id =c.zone_id and a.metric_date = c.metric_date 
+left join {{ ref('compute_vaca') }} b on a.indic_id = b.indic_id and a.zone_id = b.zone_id and a.metric_date = b.metric_date
+left join {{ ref('compute_vacg') }} c on a.indic_id = c.indic_id and a.zone_id = c.zone_id and a.metric_date = c.metric_date
 left join {{ ref('compute_vacp') }} vacp on a.indic_id = vacp.indic_id and a.zone_id = vacp.zone_id
 -- La VCA ici est à l'année de la VA (année de a.metric_date)
-left join {{ ref('get_vca_jalon') }} d on a.indic_id =d.indic_id and a.zone_id =d.zone_id and date_part('year', a.metric_date::date)=d.jalon 
+left join {{ ref('get_vca_jalon') }} d on a.indic_id = d.indic_id and a.zone_id = d.zone_id and date_part('year', a.metric_date::date) = d.jalon
 -- La VCA ici est en date de l'année courante
-left join (select * from {{ ref('get_vca_jalon') }} where jalon=(date_part('year', now()))) d2 on a.indic_id =d2.indic_id and a.zone_id =d2.zone_id
-left join {{ ref('get_vig') }} e on a.indic_id =e.indic_id and a.zone_id =e.zone_id
-left join {{ ref('get_vcg') }} f on a.indic_id =f.indic_id and a.zone_id =f.zone_id
+left join current_year_vca d2 on a.indic_id = d2.indic_id and a.zone_id = d2.zone_id
+left join {{ ref('get_vig') }} e on a.indic_id = e.indic_id and a.zone_id = e.zone_id
+left join {{ ref('get_vcg') }} f on a.indic_id = f.indic_id and a.zone_id = f.zone_id
