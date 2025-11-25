9b14c7f752f7c82639f34fe0d42d277c80a6caaa
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-11-25 12:30:59 +0100

    refactor: extrait component

---
+++ src/client/components/PageAppreciation/LignesEnteteAvancementCompletionAppreciation.tsx
@@ -0,0 +1,55 @@
+import { FicheEvaluation } from "@/server/evaluation/domain/FicheEvaluation";
+
+export const LignesEnteteAvancementCompletionAppreciation = ({
+  autoEvaluation,
+  consolidation,
+  instruction,
+}: {
+  autoEvaluation: FicheEvaluation[];
+  consolidation: FicheEvaluation[];
+  instruction: FicheEvaluation[];
+}) => {
+  const appreciationRequierentAttention = consolidation.filter(
+    (fiche) => !fiche.objectifsValides || !fiche.criteresValides,
+  ).length;
+  const appreciationCompletes = consolidation.filter(
+    (fiche) => fiche.objectifsValides && fiche.criteresValides,
+  ).length;
+  const autoEvaluationEnCours = autoEvaluation.length;
+  const instructionEnCours = instruction.length;
+
+  return (
+    <ul className="list-disc list-inside mt-4 mb-2">
+      {appreciationRequierentAttention > 0 && (
+        <li className="!text-pilote-yellow">
+          <b>Phase d'appréciation</b> : {appreciationRequierentAttention}{" "}
+          formulaire(s){" "}
+          {appreciationRequierentAttention === 1 ? "requiert" : "requièrent"}{" "}
+          votre attention
+        </li>
+      )}
+      {appreciationCompletes > 0 && (
+        <li className="!text-primary">
+          <b>Phase d'appréciation</b> : {appreciationCompletes} formulaire(s){" "}
+          {appreciationCompletes === 1 ? "est complété" : "sont complétés"}{" "}
+          et/ou en train d'être vérifié
+          {appreciationCompletes === 1 ? "" : "s"} par la DITP
+        </li>
+      )}
+      {autoEvaluationEnCours > 0 && (
+        <li className="!text-grey-200">
+          <b>Phase d'auto-évaluation</b> : {autoEvaluationEnCours} formulaire(s){" "}
+          {autoEvaluationEnCours === 1 ? "est" : "sont"} en cours
+          d'auto-évaluation
+        </li>
+      )}
+      {instructionEnCours > 0 && (
+        <li className="!text-grey-200">
+          <b>Phase d'instruction</b> : {instructionEnCours} formulaire(s){" "}
+          {instructionEnCours === 1 ? "est passé" : "sont passés"} en
+          instruction
+        </li>
+      )}
+    </ul>
+  );
+};
