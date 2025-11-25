8c0f49e0d3179e72fb8f849f700be02968956669
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-11-25 15:14:18 +0100

    chore: retour à la ligne

--- src/pages/evaluation/auto-evaluation.tsx
+++ src/pages/evaluation/auto-evaluation.tsx
@@ -56,15 +56,22 @@ const EvaluationPage = (
               Bienvenue sur votre espace d'auto-évaluation 2025
             </h1>
             <h2 className="!text-2xl !text-primary">Calendrier</h2>
-            <p className="text-gray-600">
-              La phase d'auto-évaluation des objectifs individuels et de la
-              manière de servir est ouverte jusqu'à la fin du mois de janvier
-              2026. À l'échéance qui vous a été fixée, les auto-évaluations
-              incomplètes ou non validées seront automatiquement validées. Les
-              objectifs collectifs ne font pas l'objet d'une auto-évaluation et
-              sont mis à disposition à titre informatif. Ils seront évalués à
-              partir des résultats constatés dans PILOTE.
-            </p>
+            <div className="text-gray-600">
+              <p className="!mb-0">
+                La phase d'auto-évaluation des objectifs individuels et de la
+                manière de servir est ouverte jusqu'à la fin du mois de janvier
+                2026.
+              </p>
+              <p className="!mb-0">
+                À l'échéance qui vous a été fixée, les auto-évaluations
+                incomplètes ou non validées seront automatiquement validées.
+              </p>
+              <p className="!mb-0">
+                Les objectifs collectifs ne font pas l'objet d'une
+                auto-évaluation et sont mis à disposition à titre informatif.
+                Ils seront évalués à partir des résultats constatés dans PILOTE.
+              </p>
+            </div>
           </header>
 
           <ListeAutoEvaluations fichesEvaluation={fichesEvaluation} />
