62d239785e5edb103bd628a7b74fe994d0a17ffd
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-11-25 12:27:55 +0100

    feat: prototype wording legende

--- src/client/components/PageAppreciation/ListePhaseEvaluation.tsx
+++ src/client/components/PageAppreciation/ListePhaseEvaluation.tsx
@@ -27,6 +27,60 @@ export const ListePhaseEvaluation = () => {
                 INSTRUCTION.length
               } formulaires`}
             </h2>
+            <ul className="list-disc list-inside mt-4 mb-2">
+              {(() => {
+                const appreciationRequierentAttention = CONSOLIDATION.filter(
+                  (fiche) => !fiche.objectifsValides || !fiche.criteresValides,
+                ).length;
+                const appreciationCompletes = CONSOLIDATION.filter(
+                  (fiche) => fiche.objectifsValides && fiche.criteresValides,
+                ).length;
+                const autoEvaluationEnCours = AUTO_EVALUATION.length;
+                const instructionEnCours = INSTRUCTION.length;
+
+                return (
+                  <>
+                    {appreciationRequierentAttention > 0 && (
+                      <li className="!text-pilote-yellow">
+                        <b>Phase d'appréciation</b> :{" "}
+                        {appreciationRequierentAttention} formulaire(s){" "}
+                        {appreciationRequierentAttention === 1
+                          ? "requiert"
+                          : "requièrent"}{" "}
+                        votre attention
+                      </li>
+                    )}
+                    {appreciationCompletes > 0 && (
+                      <li className="!text-primary">
+                        <b>Phase d'appréciation</b> : {appreciationCompletes}{" "}
+                        formulaire(s){" "}
+                        {appreciationCompletes === 1
+                          ? "est complété"
+                          : "sont complétés"}{" "}
+                        et/ou en train d'être vérifié
+                        {appreciationCompletes === 1 ? "" : "s"} par la DITP
+                      </li>
+                    )}
+                    {autoEvaluationEnCours > 0 && (
+                      <li className="!text-grey-200">
+                        <b>Phase d'auto-évaluation</b> : {autoEvaluationEnCours}{" "}
+                        formulaire(s){" "}
+                        {autoEvaluationEnCours === 1 ? "est" : "sont"} en cours
+                        d'auto-évaluation
+                      </li>
+                    )}
+                    {instructionEnCours > 0 && (
+                      <li className="!text-grey-200">
+                        <b>Phase d'instruction</b> : {instructionEnCours}{" "}
+                        formulaire(s){" "}
+                        {instructionEnCours === 1 ? "est passé" : "sont passés"}{" "}
+                        en instruction
+                      </li>
+                    )}
+                  </>
+                );
+              })()}
+            </ul>
             <Accordion.Root type="multiple">
               <Accordion.Item value="appreciation">
                 <Accordion.Header>
