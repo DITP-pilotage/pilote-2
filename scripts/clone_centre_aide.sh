1e19bc8a7ac8e3fd26435551947f6bdd37511e0e
Jordan Taillefer <jtaillefer02@gmail.com>
2025-12-02 16:27:30 +0100

    commande build scalingo

--- scripts/clone_centre_aide.sh
+++ scripts/clone_centre_aide.sh
@@ -1,16 +1,4 @@
-# Uniquement sur du local
-if [ -z $CENTREAIDE_GITHUB_FOLDER ] || [ -z $CENTREAIDE_GITHUB_TOKEN ];
-then
-  if [ -f .env ];
-  then
-    source .env
-  else
-    echo "ERROR : .env does not exist. Cannot load variables. Exiting"
-    exit 1
-  fi
-fi
-
-rm -rf /tmp/centreaide public/centreaide
-git clone --depth 1 --branch retype-deploy --single-branch https://$CENTREAIDE_GITHUB_TOKEN@github.com/DITP-pilotage/centre-aide-pilote.git /tmp/centreaide
-mkdir public/centreaide
-cp -r /tmp/centreaide/$CENTREAIDE_GITHUB_FOLDER/* public/centreaide
+rm -rf /tmp/centreaide src/pages/centre-aide-pilote-2
+git clone --depth 1 --branch main --single-branch https://github.com/DITP-pilotage/centre-aide-pilote-2.git /tmp/centreaide
+mkdir src/pages/centre-aide-pilote-2
+cp -r /tmp/centreaide/* src/pages/centre-aide-pilote-2
