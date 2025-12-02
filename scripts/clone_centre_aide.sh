5e8882114720d26317ff66e3e41954e71ca87294
Jordan Taillefer <jtaillefer02@gmail.com>
2025-12-02 16:29:42 +0100

    commande build scalingo

--- scripts/clone_centre_aide.sh
+++ scripts/clone_centre_aide.sh
@@ -1,4 +1,4 @@
 rm -rf /tmp/centreaide src/pages/centre-aide-pilote-2
-git clone --depth 1 --branch main --single-branch https://github.com/DITP-pilotage/centre-aide-pilote-2.git /tmp/centreaide
+git clone --depth 1 --branch main --single-branch git@github.com:DITP-pilotage/centre-aide-pilote-2.git /tmp/centreaide
 mkdir src/pages/centre-aide-pilote-2
 cp -r /tmp/centreaide/* src/pages/centre-aide-pilote-2
