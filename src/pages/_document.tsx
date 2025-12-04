41e347a89521e96be27896e061497b9ba87abf2b
Jordan <jordantaillefer@users.noreply.github.com>
2025-12-04 11:46:01 +0100

    fix: image manque centre aide (#1774)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -50,7 +50,7 @@ class MyDocument extends Document<MyDocumentProps> {
     const { nonce, isDevelopment, styles } = this.props;
 
     return (
-      <Html lang="fr">
+      <Html className="light" lang="fr" style={{ colorScheme: "light" }}>
         <Head>
           <meta charSet="utf-8" />
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
