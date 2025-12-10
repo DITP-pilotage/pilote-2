b4095489d655715c0397ba4aca8ba11739c22fda
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-12-10 14:15:31 +0100

    refactor: modale historique publication

--- src/client/components/_commons/PublicationChantier/PublicationHistorique/PublicationHistorique.tsx
+++ src/client/components/_commons/PublicationChantier/PublicationHistorique/PublicationHistorique.tsx
@@ -1,5 +1,5 @@
-import { Fragment, FunctionComponent, useMemo } from "react";
-import Modale__legacy from "@/components/_commons/Modale/Modale__legacy";
+import { Fragment, FunctionComponent } from "react";
+import { Modal } from "@/components/shared/Modal";
 import { BoutonSousLigné } from "@/components/_commons/BoutonSousLigné/BoutonSousLigné";
 import PublicationAffichage from "@/components/_commons/PublicationChantier/PublicationAffichage/PublicationAffichage";
 import PublicationHistoriqueProps from "./PublicationHistorique.interface";
@@ -14,43 +14,42 @@ const PublicationHistorique: FunctionComponent<PublicationHistoriqueProps> = ({
   const { publications, nomTerritoire, récupérerPublications } =
     usePublicationHistorique(type, entité, réformeId, maille);
 
-  const ID_HTML = useMemo(() => `historique-${entité}-${type}`, [entité, type]);
-
   return (
-    <>
-      <BoutonSousLigné
-        aria-controls={ID_HTML}
-        className="fr-mt-1w"
-        dataFrOpened={false}
-        type="button"
-      >
-        Voir l'historique
-      </BoutonSousLigné>
-      <Modale__legacy
-        idHtml={ID_HTML}
-        ouvertureCallback={récupérerPublications}
-        sousTitre={nomTerritoire}
-        titre={`Historique - ${entité}`}
-      >
-        <div>
-          {publications ? (
-            publications.map(
-              (publication, i) =>
-                publication && (
-                  <Fragment key={publication.id}>
-                    {i !== 0 && <hr className="fr-mt-4w" />}
-                    <div className="fr-mx-2w">
-                      <PublicationAffichage publication={publication} />
-                    </div>
-                  </Fragment>
-                ),
-            )
-          ) : (
-            <p>Chargement de l'historique...</p>
-          )}
-        </div>
-      </Modale__legacy>
-    </>
+    <Modal
+      onOpenChange={(open) => {
+        if (open) {
+          récupérerPublications();
+        }
+      }}
+      title={`Historique - ${entité}`}
+      trigger={
+        <BoutonSousLigné className="fr-mt-1w" type="button">
+          Voir l'historique
+        </BoutonSousLigné>
+      }
+    >
+      {nomTerritoire ? (
+        <p className="fr-text--lg bold">{nomTerritoire}</p>
+      ) : null}
+
+      <div>
+        {publications ? (
+          publications.map(
+            (publication, i) =>
+              publication && (
+                <Fragment key={publication.id}>
+                  {i !== 0 && <hr className="fr-mt-4w" />}
+                  <div className="fr-mx-2w">
+                    <PublicationAffichage publication={publication} />
+                  </div>
+                </Fragment>
+              ),
+          )
+        ) : (
+          <p>Chargement de l'historique...</p>
+        )}
+      </div>
+    </Modal>
   );
 };
 
