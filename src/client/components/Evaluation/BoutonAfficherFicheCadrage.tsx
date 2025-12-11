dc8211b8480ebe1126c29b3b315d7be42804b7f7
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-12-11 11:36:10 +0100

    feat: nouveau design pour fiches de cadrage (avec modale)

--- src/client/components/Evaluation/BoutonAfficherFicheCadrage.tsx
+++ src/client/components/Evaluation/BoutonAfficherFicheCadrage.tsx
@@ -11,7 +11,7 @@ export const BoutonAfficherFicheCadrage = ({
 }) => {
   return (
     <Modale
-      title="COucou"
+      title={`Fiche de cadrage - ${critere.libelle}`}
       trigger={
         <Bouton
           className="whitespace-nowrap underline !p-2 !-m-2 self-start gap-1.5 items-center"
@@ -22,7 +22,22 @@ export const BoutonAfficherFicheCadrage = ({
         />
       }
     >
-      <pre>{JSON.stringify(critere, null, 2)}</pre>
+      <div>
+        <p className="whitespace-pre-line !text-sm !m-0">
+          {critere.descriptif}
+        </p>
+
+        <ul className="list-style-none !p-0 !m-0">
+          {critere.sousCriteres.map((sousCritere) => (
+            <li key={sousCritere.id}>
+              <h3 className="!text-base !mb-1 italic">{sousCritere.libelle}</h3>
+              <p className="whitespace-pre-line !text-sm">
+                {sousCritere.descriptif}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </Modale>
   );
 };
