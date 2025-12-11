a02aaad4dd2f18045cc63dc1179e3efcb5ea5417
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-12-11 12:11:13 +0100

    refactor: supprime contexte redondant

--- src/client/components/Evaluation/TableauEvaluation.tsx
+++ src/client/components/Evaluation/TableauEvaluation.tsx
@@ -13,10 +13,7 @@ import {
   COLONNES,
   useTableauEvaluation,
 } from "@/components/Evaluation/useTableauEvaluation";
-import {
-  CriteresProvider,
-  useCriteres,
-} from "@/components/Evaluation/CriteresProvider";
+import { CriteresProvider } from "@/components/Evaluation/CriteresProvider";
 import { AutosaveProvider } from "@/components/Evaluation/AutosaveProvider";
 import { EtapeEvaluationProvider } from "@/components/Evaluation/EtapeEvaluationProvider";
 import { HeaderTableauEvaluation } from "@/components/Evaluation/HeaderTableauEvaluation";
@@ -82,7 +79,6 @@ export const InnerTableauEvaluation = memo(function TableauEvaluation({
     fieldName?: FormCommentaireName | FormNoteName,
   ) => Promise<void>;
 }) {
-  const criteres = useCriteres();
   const form = useForm<FormValues>({
     resolver: zodResolver(baseFormSchema),
     mode: "onChange",
@@ -127,65 +123,63 @@ export const InnerTableauEvaluation = memo(function TableauEvaluation({
   return (
     <EtapeEvaluationProvider value={etape}>
       <AutosaveProvider value={handleAutosave}>
-        <CriteresProvider criteres={criteres}>
-          <FormProvider {...form}>
-            <div className="max-w-[1200px] mx-auto bg-white px-8">
-              <form
-                className="flex flex-col gap-3 py-6 w-full grow border-x border-gray-200"
-                onSubmit={form.handleSubmit(
-                  (values) => onEnregistrer(values, true),
-                  () => table.resetColumnFilters(),
-                )}
-              >
-                <HeaderTableauEvaluation
-                  dateDerniereModification={dateDerniereModification}
-                  estEnLectureSeule={estEnLectureSeule}
-                  etape={etape}
-                  titre={titre}
-                />
-                <FiltresTableauEvaluation table={table} />
+        <FormProvider {...form}>
+          <div className="max-w-[1200px] mx-auto bg-white px-8">
+            <form
+              className="flex flex-col gap-3 py-6 w-full grow border-x border-gray-200"
+              onSubmit={form.handleSubmit(
+                (values) => onEnregistrer(values, true),
+                () => table.resetColumnFilters(),
+              )}
+            >
+              <HeaderTableauEvaluation
+                dateDerniereModification={dateDerniereModification}
+                estEnLectureSeule={estEnLectureSeule}
+                etape={etape}
+                titre={titre}
+              />
+              <FiltresTableauEvaluation table={table} />
 
-                <table className="table-fixed w-full border-collapse">
-                  <colgroup>
-                    {table.getHeaderGroups()[0].headers.map((header) => (
-                      <col
-                        className={clsxm({
-                          "w-[150px]":
-                            header.column.id === COLONNES.RATTACHEMENT_CODE,
-                        })}
-                        key={header.id}
-                      />
-                    ))}
-                  </colgroup>
-                  <tbody>
-                    {rows.map((row) => {
-                      return (
-                        <tr key={row.id}>
-                          {row.getVisibleCells().map((cell) => (
-                            <td
-                              className={clsxm(
-                                "border border-gray-300 px-4",
-                                "first:!border-l-0 last:!border-r-0",
-                                "align-top",
-                                cell.column.id === "id" && "w-auto",
-                              )}
-                              key={cell.id}
-                            >
-                              {flexRender(
-                                cell.column.columnDef.cell,
-                                cell.getContext(),
-                              )}
-                            </td>
-                          ))}
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              </form>
-            </div>
-          </FormProvider>
-        </CriteresProvider>
+              <table className="table-fixed w-full border-collapse">
+                <colgroup>
+                  {table.getHeaderGroups()[0].headers.map((header) => (
+                    <col
+                      className={clsxm({
+                        "w-[150px]":
+                          header.column.id === COLONNES.RATTACHEMENT_CODE,
+                      })}
+                      key={header.id}
+                    />
+                  ))}
+                </colgroup>
+                <tbody>
+                  {rows.map((row) => {
+                    return (
+                      <tr key={row.id}>
+                        {row.getVisibleCells().map((cell) => (
+                          <td
+                            className={clsxm(
+                              "border border-gray-300 px-4",
+                              "first:!border-l-0 last:!border-r-0",
+                              "align-top",
+                              cell.column.id === "id" && "w-auto",
+                            )}
+                            key={cell.id}
+                          >
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext(),
+                            )}
+                          </td>
+                        ))}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </form>
+          </div>
+        </FormProvider>
       </AutosaveProvider>
     </EtapeEvaluationProvider>
   );
