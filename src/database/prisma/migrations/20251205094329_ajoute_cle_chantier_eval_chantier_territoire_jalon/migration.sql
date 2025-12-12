d78d758053914b458168b8876e77d05ba48d86f5
Tristan Conti <tristan.conti@seenovate.com>
2025-12-12 15:40:03 +0100

    refactor migrations

--- src/database/prisma/migrations/20251205094329_ajoute_cle_chantier_eval_chantier_territoire_jalon/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."chantier_evaluation" DROP CONSTRAINT "chantier_evaluation_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "public"."chantier_evaluation" ADD CONSTRAINT "chantier_evaluation_id_territoire_code_jalon_fkey" FOREIGN KEY ("id", "territoire_code", "jalon") REFERENCES "public"."chantier_territoire_jalon"("id", "territoire_code", "jalon") ON DELETE CASCADE ON UPDATE CASCADE;
