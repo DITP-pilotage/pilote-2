81f4c136356595b4198f524980bae83831eb86e8
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-12-19 16:24:45 +0100

    refactor: extrait composant EvaluationsBlock

--- src/client/components/PagePilotage/TableauPilotage.tsx
+++ src/client/components/PagePilotage/TableauPilotage.tsx
@@ -1,72 +1,9 @@
 import { Fragment } from "react";
-import { $Enums } from "@prisma/client";
-import { Row } from "@tanstack/react-table";
-import {
-  ETAPES,
-  FicheEvaluationRow,
-  useTableauPilotage,
-} from "@/components/PagePilotage/useTableauPilotage";
-import { clsxm } from "@/utils/clsxm";
+import { useTableauPilotage } from "@/components/PagePilotage/useTableauPilotage";
 import { useObjectifsCount } from "@/components/PagePilotage/useObjectifsCount";
 import { TableauPilotageHeader } from "@/components/PagePilotage/TableauPilotageHeader";
 import { EnteteGroupeRattachement } from "@/components/PagePilotage/EnteteGroupeRattachement";
-
-function EvaluationsBlock<T>({
-  items,
-  rowGroup,
-  getEvaluation,
-}: {
-  items: T[];
-  rowGroup: Row<FicheEvaluationRow>;
-  getEvaluation(options: {
-    item: T;
-    row: Row<FicheEvaluationRow>;
-    etape: $Enums.etape_evaluation_enum;
-  }): number | null;
-}) {
-  return (
-    <div
-      className="grid gap-0 grid-cols-subgrid border-l border-t !border-black"
-      style={{ gridColumn: `span ${items.length}` }}
-    >
-      {items.map((item) => {
-        return (
-          <>
-            {rowGroup.subRows.map((row) => {
-              const fiche = row.original;
-              return (
-                <div
-                  className="grid grid-cols-3 border-b !border-black border-r"
-                  key={fiche.id}
-                >
-                  {ETAPES.map((etape) => {
-                    const evaluation = getEvaluation({
-                      item,
-                      row,
-                      etape: etape.key,
-                    });
-
-                    return (
-                      <div
-                        className={clsxm(
-                          "flex items-center justify-center text-center whitespace-nowrap",
-                          { "bg-dsfr-grey-925": evaluation == null },
-                        )}
-                        key={etape.key}
-                      >
-                        {evaluation ?? " "}
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
-          </>
-        );
-      })}
-    </div>
-  );
-}
+import { EvaluationsBlock } from "@/components/PagePilotage/EvaluationsBlock";
 
 export const TableauPilotage = () => {
   const { table, fichesSelectionneesIds, criteres } = useTableauPilotage();
