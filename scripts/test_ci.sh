ee07fadb6cb9184cdf3358d98a55286407c1f90f
Antoine Chalifour <antoine.chalifour@gmail.com>
2026-01-14 17:17:25 +0100

    fix: script ci

--- scripts/test_ci.sh
+++ scripts/test_ci.sh
@@ -5,7 +5,7 @@ status=0
 npm run test:client:unit || status=1
 npm run test:client:integration || status=1
 npm run test:server:unit || status=1
-npm run database:init -- --force || status=1
+npm run test:database:init || status=1
 npm run test:server:integration || status=1
 
 exit $status
