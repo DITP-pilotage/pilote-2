f49b7e2f06c52b7f67c7ddb9f18a3e8e0f8f77c6
Antoine Chalifour <antoine.chalifour@gmail.com>
2026-01-14 16:46:21 +0100

    chore: clean ci config

--- jest.env.setup.js
+++ jest.env.setup.js
@@ -1,3 +1,1 @@
-const envFile = process.env.CI ? ".env.ci" : ".env.test";
-require("dotenv").config({ path: envFile });
-require("dotenv").config();
+require("dotenv").config({ path: ".env.test" });
