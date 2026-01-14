d567ea150aac3d9caa7892e716ce7ad0fc183b59
Antoine Chalifour <antoine.chalifour@gmail.com>
2026-01-14 17:12:58 +0100

    fix: script ci

--- scripts/test_ci.sh
+++ scripts/test_ci.sh
@@ -2,10 +2,10 @@
 
 status=0
 
-npx dotenv -e .env.ci -- npm run test:client:unit || status=1
-npx dotenv -e .env.ci -- npm run test:client:integration || status=1
-npx dotenv -e .env.ci -- npm run test:server:unit || status=1
-npx dotenv -e .env.ci -- npm run database:init -- --force || status=1
-npx dotenv -e .env.ci -- npm run test:server:integration || status=1
+npm run test:client:unit || status=1
+npm run test:client:integration || status=1
+npm run test:server:unit || status=1
+npm run database:init -- --force || status=1
+npm run test:server:integration || status=1
 
 exit $status
