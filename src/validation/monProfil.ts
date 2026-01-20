1b0466495a64ef7bb60f7e043146272c9be629c1
Antoine Chalifour <antoine.chalifour@gmail.com>
2026-01-20 21:13:05 +0100

    chore: parse '' to null

--- src/validation/monProfil.ts
+++ src/validation/monProfil.ts
@@ -3,5 +3,9 @@ import { z } from "zod";
 export const validationModifierMonProfil = z.object({
   nom: z.string().min(1, "Ce champ est obligatoire").max(100),
   prenom: z.string().min(1, "Ce champ est obligatoire").max(100),
-  fonction: z.string().max(100).nullable(),
+  fonction: z
+    .string()
+    .max(100)
+    .transform((value) => value || null)
+    .nullable(),
 });
