dee92d4b80af225d07f10f82775b4b32917e784e
Tristan Conti <tristan.conti@seenovate.com>
2026-01-26 09:06:11 +0100

    rebase

--- scripts/ddp_restore.sh
+++ scripts/ddp_restore.sh
@@ -54,5 +54,5 @@ echo ">> Anonymisation des utilisateur..."
 time psql -d $DATABASE_URL -c "
 UPDATE utilisateur
 SET email = REPLACE(CONCAT(id, '@example.com'), ' ', '')
-WHERE email NOT LIKE '%@example%';
+WHERE email NOT LIKE '%@example%' AND email <> 'import.csv@modernisation.gouv.fr';
 "
