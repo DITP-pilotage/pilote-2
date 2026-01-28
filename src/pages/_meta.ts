0eef429655af3551b586945d1a260722ea10c9c9
Jordan Taillefer <jtaillefer02@gmail.com>
2026-01-28 14:58:12 +0100

    chore: update ce2

---
+++ src/pages/_meta.ts
@@ -0,0 +1,11 @@
+import type { DynamicMeta } from "nextra";
+
+export default {
+  "*": {
+    display: "hidden",
+  },
+  "centre-aide-pilote-2": {
+    display: "children",
+    title: "Centre d'aide Pilote 2",
+  },
+} satisfies DynamicMeta;
