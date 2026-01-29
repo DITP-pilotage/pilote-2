36b6537ca45edbe0385a629b3de69458c9642065
Antoine Chalifour <antoine.chalifour@gmail.com>
2026-01-29 09:47:33 +0100

    chore: gestion des labels des profils

--- src/server/rapports-hebdomadaires/infrastructure/adapters/BrevoEnvoieEmailService.ts
+++ src/server/rapports-hebdomadaires/infrastructure/adapters/BrevoEnvoieEmailService.ts
@@ -1,4 +1,5 @@
 import { EmailManager } from "@/server/infrastructure/email-manager";
+import { PrismaPilote } from "@/server/db/PrismaPilote";
 import { EnvoieEmailService } from "@/server/rapports-hebdomadaires/domain/ports/EnvoieEmailService";
 import { RapportHebdomadaire } from "@/server/rapports-hebdomadaires/domain/RapportHebdomadaire";
 
@@ -8,6 +9,7 @@ export class BrevoEnvoieEmailService implements EnvoieEmailService {
   constructor(
     private readonly deps: {
       emailManager: EmailManager;
+      prisma: PrismaPilote;
     },
   ) {}
 
@@ -16,12 +18,41 @@ export class BrevoEnvoieEmailService implements EnvoieEmailService {
   }): Promise<void> {
     const { rapport } = params;
 
+    const profilsMap = await this.getProfilsMap();
+    const mapProfilToLabel = (code: string) => profilsMap.get(code) ?? code;
+
     const afficherComptesCrees =
       rapport.sectionActiviteComptes.comptesCrees.length > 0;
     const afficherComptesDesactives =
       rapport.sectionActiviteComptes.comptesDesactives.length > 0;
     const afficherSectionComptes =
       afficherComptesCrees || afficherComptesDesactives;
+
+    const comptesCrees = rapport.sectionActiviteComptes.comptesCrees.map(
+      (compte) => ({
+        nom: compte.nom,
+        prenom: compte.prenom,
+        email: compte.email,
+        profil: mapProfilToLabel(compte.profil),
+        territoire:
+          compte.territoires.length > 0
+            ? compte.territoires.map((t) => t.nom).join(", ")
+            : undefined,
+      }),
+    );
+
+    const comptesDesactives =
+      rapport.sectionActiviteComptes.comptesDesactives.map((compte) => ({
+        nom: compte.nom,
+        prenom: compte.prenom,
+        email: compte.email,
+        profil: mapProfilToLabel(compte.profil),
+        territoire:
+          compte.territoires.length > 0
+            ? compte.territoires.map((t) => t.nom).join(", ")
+            : undefined,
+      }));
+
     const templateParams = {
       prenom: rapport.coordinateur.prenom,
       nom: rapport.coordinateur.nom,
@@ -30,31 +61,9 @@ export class BrevoEnvoieEmailService implements EnvoieEmailService {
         .join(", "),
       afficherComptesCrees,
       afficherSectionComptes,
-      comptesCrees: rapport.sectionActiviteComptes.comptesCrees.map(
-        (compte) => ({
-          nom: compte.nom,
-          prenom: compte.prenom,
-          email: compte.email,
-          profil: compte.profil,
-          territoire:
-            compte.territoires.length > 0
-              ? compte.territoires.map((t) => t.nom).join(", ")
-              : undefined,
-        }),
-      ),
+      comptesCrees,
       afficherComptesDesactives,
-      comptesDesactives: rapport.sectionActiviteComptes.comptesDesactives.map(
-        (compte) => ({
-          nom: compte.nom,
-          prenom: compte.prenom,
-          email: compte.email,
-          profil: compte.profil,
-          territoire:
-            compte.territoires.length > 0
-              ? compte.territoires.map((t) => t.nom).join(", ")
-              : undefined,
-        }),
-      ),
+      comptesDesactives,
     };
 
     await this.deps.emailManager.sendTransactionalEmail(
@@ -64,5 +73,11 @@ export class BrevoEnvoieEmailService implements EnvoieEmailService {
       TEMPLATE_ID_RAPPORT_COORDINATEURS,
       templateParams,
     );
+  }
+
+  private async getProfilsMap() {
+    const prisma = this.deps.prisma.getInstance();
+    const profils = await prisma.profil.findMany();
+    return new Map(profils.map((p) => [p.code, p.nom]));
   }
 }
