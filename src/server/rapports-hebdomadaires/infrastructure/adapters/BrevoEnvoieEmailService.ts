5810d960e4754e8e1b64efd2584b18766e480b72
Antoine Chalifour <antoine.chalifour@gmail.com>
2026-02-03 20:53:51 +0100

    refactor: extract create params

--- src/server/rapports-hebdomadaires/infrastructure/adapters/BrevoEnvoieEmailService.ts
+++ src/server/rapports-hebdomadaires/infrastructure/adapters/BrevoEnvoieEmailService.ts
@@ -5,6 +5,87 @@ import { RapportHebdomadaire } from "@/server/rapports-hebdomadaires/domain/Rapp
 
 const TEMPLATE_ID_RAPPORT_COORDINATEURS = 60;
 
+export const createBrevoParams = ({
+  profilsMap,
+  rapport,
+}: {
+  profilsMap: Map<string, string>;
+  rapport: RapportHebdomadaire;
+}) => {
+  const mapProfilToLabel = (code: string) => profilsMap.get(code) ?? code;
+
+  const afficherComptesCrees =
+    rapport.sectionActiviteComptes.comptesCrees.length > 0;
+  const afficherComptesDesactives =
+    rapport.sectionActiviteComptes.comptesDesactives.length > 0;
+  const afficherSectionComptes =
+    afficherComptesCrees || afficherComptesDesactives;
+
+  const comptesCrees = rapport.sectionActiviteComptes.comptesCrees.map(
+    (compte) => ({
+      nom: compte.nom,
+      prenom: compte.prenom,
+      email: compte.email,
+      profil: mapProfilToLabel(compte.profil),
+      territoire:
+        compte.territoires.length > 0
+          ? compte.territoires.map((t) => t.nom).join(", ")
+          : undefined,
+    }),
+  );
+
+  const comptesDesactives =
+    rapport.sectionActiviteComptes.comptesDesactives.map((compte) => ({
+      nom: compte.nom,
+      prenom: compte.prenom,
+      email: compte.email,
+      profil: mapProfilToLabel(compte.profil),
+      territoire:
+        compte.territoires.length > 0
+          ? compte.territoires.map((t) => t.nom).join(", ")
+          : undefined,
+    }));
+
+  const afficherSectionChantiersVA = rapport.chantiers.length > 0;
+
+  const chantiers = rapport.chantiers.map((chantier) => ({
+    id: chantier.chantier.id,
+    nom: chantier.chantier.nom,
+    indicateurs: chantier.indicateurs.flatMap((indic) => ({
+      id: indic.indicateur.id,
+      nom: indic.indicateur.nom,
+      territoires: indic.territoires.map((territoire) => ({
+        nom: territoire.territoire.nom,
+        type_valeur: "va",
+        valeur:
+          territoire.valeurApres !== null
+            ? Number.isInteger(territoire.valeurApres)
+              ? territoire.valeurApres.toString()
+              : territoire.valeurApres.toFixed(1)
+            : "-",
+        date_indicateur: new Date(territoire.dateChangement).toLocaleDateString(
+          "fr-FR",
+        ),
+      })),
+    })),
+  }));
+
+  return {
+    prenom: rapport.coordinateur.prenom,
+    nom: rapport.coordinateur.nom,
+    territoire: rapport.coordinateur.territoires
+      .map((territoire) => territoire.nom)
+      .join(", "),
+    afficherComptesCrees,
+    afficherSectionComptes,
+    comptesCrees,
+    afficherComptesDesactives,
+    comptesDesactives,
+    afficherSectionChantiersVA,
+    chantiers,
+  };
+};
+
 export class BrevoEnvoieEmailService implements EnvoieEmailService {
   constructor(
     private readonly deps: {
@@ -19,76 +100,10 @@ export class BrevoEnvoieEmailService implements EnvoieEmailService {
     rapport: RapportHebdomadaire;
   }): Promise<void> {
     const profilsMap = await this.getProfilsMap();
-    const mapProfilToLabel = (code: string) => profilsMap.get(code) ?? code;
-
-    const afficherComptesCrees =
-      rapport.sectionActiviteComptes.comptesCrees.length > 0;
-    const afficherComptesDesactives =
-      rapport.sectionActiviteComptes.comptesDesactives.length > 0;
-    const afficherSectionComptes =
-      afficherComptesCrees || afficherComptesDesactives;
-
-    const comptesCrees = rapport.sectionActiviteComptes.comptesCrees.map(
-      (compte) => ({
-        nom: compte.nom,
-        prenom: compte.prenom,
-        email: compte.email,
-        profil: mapProfilToLabel(compte.profil),
-        territoire:
-          compte.territoires.length > 0
-            ? compte.territoires.map((t) => t.nom).join(", ")
-            : undefined,
-      }),
-    );
-
-    const comptesDesactives =
-      rapport.sectionActiviteComptes.comptesDesactives.map((compte) => ({
-        nom: compte.nom,
-        prenom: compte.prenom,
-        email: compte.email,
-        profil: mapProfilToLabel(compte.profil),
-        territoire:
-          compte.territoires.length > 0
-            ? compte.territoires.map((t) => t.nom).join(", ")
-            : undefined,
-      }));
-
-    const afficherSectionChantiersVA = rapport.chantiers.length > 0;
-
-    const chantiers = rapport.chantiers.map((chantier) => ({
-      id: chantier.chantier.id,
-      nom: chantier.chantier.nom,
-      indicateurs: chantier.indicateurs.flatMap((indic) => ({
-        id: indic.indicateur.id,
-        nom: indic.indicateur.nom,
-        territoires: indic.territoires.map((territoire) => ({
-          nom: territoire.territoire.nom,
-          type_valeur: "va",
-          valeur:
-            territoire.valeurApres !== null
-              ? this.formatValeur(territoire.valeurApres)
-              : "-",
-          date_indicateur: new Date(
-            territoire.dateChangement,
-          ).toLocaleDateString("fr-FR"),
-        })),
-      })),
-    }));
-
-    const templateParams = {
-      prenom: rapport.coordinateur.prenom,
-      nom: rapport.coordinateur.nom,
-      territoire: rapport.coordinateur.territoires
-        .map((territoire) => territoire.nom)
-        .join(", "),
-      afficherComptesCrees,
-      afficherSectionComptes,
-      comptesCrees,
-      afficherComptesDesactives,
-      comptesDesactives,
-      afficherSectionChantiersVA,
-      chantiers,
-    };
+    const templateParams = createBrevoParams({
+      profilsMap: profilsMap,
+      rapport: rapport,
+    });
 
     await this.deps.emailManager.sendTransactionalEmail(
       [{ email: rapport.coordinateur.email }],
@@ -101,9 +116,5 @@ export class BrevoEnvoieEmailService implements EnvoieEmailService {
     const prisma = this.deps.prisma.getInstance();
     const profils = await prisma.profil.findMany();
     return new Map(profils.map((p) => [p.code, p.nom]));
-  }
-
-  private formatValeur(valeur: number): string {
-    return Number.isInteger(valeur) ? valeur.toString() : valeur.toFixed(1);
   }
 }
