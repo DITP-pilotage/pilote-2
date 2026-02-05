bf9b19c1ff588604f52cad1fe452fd104e69d4a9
Jordan Taillefer <jtaillefer02@gmail.com>
2026-02-05 12:10:13 +0100

    fix: getToken

--- src/proxy.ts
+++ src/proxy.ts
@@ -115,6 +115,7 @@ export async function proxy(request: NextRequest) {
     const token = await getToken({
       req: request,
       secret: process.env.NEXTAUTH_SECRET,
+      secureCookie: !isDev,
     });
 
     if (!token) {
