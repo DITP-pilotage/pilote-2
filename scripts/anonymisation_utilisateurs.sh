57b30df448d0bc12a07682c7c44d3155975a877b
Romain Thoyer <51092742+rthoyer@users.noreply.github.com>
2026-02-16 12:43:58 +0100

    Safely insert db url

--- scripts/anonymisation_utilisateurs.sh
+++ scripts/anonymisation_utilisateurs.sh
@@ -1,7 +1,7 @@
 source .env
 
 echo ">> Anonymisation des utilisateurs..."
-time psql -d $DATABASE_URL -c "
+time psql -d "$DATABASE_URL" -c "
 UPDATE utilisateur
 SET email = REPLACE(CONCAT(id, '@example.com'), ' ', '')
 WHERE email NOT LIKE '%@example%' AND email <> 'import.csv@modernisation.gouv.fr';
