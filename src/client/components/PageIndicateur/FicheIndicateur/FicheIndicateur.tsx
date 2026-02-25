2a472ed456a839c38ddf0b23770e79f9ae0e7c6c
Jordan Taillefer <jtaillefer02@gmail.com>
2026-02-25 08:41:34 +0100

    chore: suppression emotion

--- src/client/components/PageIndicateur/FicheIndicateur/FicheIndicateur.tsx
+++ src/client/components/PageIndicateur/FicheIndicateur/FicheIndicateur.tsx
@@ -1,6 +1,5 @@
 import "@gouvfr/dsfr/dist/component/table/table.min.css";
 import { FunctionComponent } from "react";
-import FicheIndicateurStyled from "@/components/PageIndicateur/FicheIndicateur/FicheIndicateur.styled";
 import SectionTableauIndicateur from "@/components/PageIndicateur/FicheIndicateur/SectionTableauIndicateur";
 import SectionDétailsMetadataIndicateur from "@/components/PageIndicateur/FicheIndicateur/SectionDétailsMetadataIndicateur";
 import SectionDétailsMetadataParametreCalculIndicateur from "@/components/PageIndicateur/FicheIndicateur/SectionDétailsMetadataParametreCalculIndicateur";
@@ -21,111 +20,93 @@ const FicheIndicateur: FunctionComponent<FicheIndicateurProps> = ({
   chantiers,
 }) => {
   return (
-    <FicheIndicateurStyled>
-      <div className="fr-mb-2w">
-        <SectionSelectionIndicateur
-          estEnCoursDeModification={estEnCoursDeModification}
-        />
-        <SectionTableauIndicateur
-          indicateur={indicateur}
-          informationHistorisationIndicateur={
-            informationHistorisationIndicateur
-          }
-        />
-        <section className="fr-accordion">
-          <h2 className="fr-accordion__title">
-            <button
-              aria-controls="accordion-identity"
-              aria-expanded="true"
-              className="fr-accordion__btn"
-              type="button"
-            >
-              Identité indicateur
-            </button>
-          </h2>
-          <div className="fr-collapse" id="accordion-identity">
-            <SectionDétailsMetadataIndicateur
-              chantiers={chantiers}
-              estEnCoursDeModification={estEnCoursDeModification}
-              indicateur={indicateur}
-              mapInformationMetadataIndicateur={
-                mapInformationMetadataIndicateur
-              }
-            />
-          </div>
-        </section>
-        <section className="fr-accordion">
-          <h2 className="fr-accordion__title">
-            <button
-              aria-controls="accordion-parametrage"
-              aria-expanded="false"
-              className="fr-accordion__btn"
-              type="button"
-            >
-              Paramétrages
-            </button>
-          </h2>
-          <div className="fr-collapse" id="accordion-parametrage">
-            <SectionDétailsMetadataParametreIndicateurDepartementale
-              estEnCoursDeModification={estEnCoursDeModification}
-              indicateur={indicateur}
-              mapInformationMetadataIndicateur={
-                mapInformationMetadataIndicateur
-              }
-            />
-            <SectionDétailsMetadataParametreIndicateurRegionale
-              estEnCoursDeModification={estEnCoursDeModification}
-              indicateur={indicateur}
-              mapInformationMetadataIndicateur={
-                mapInformationMetadataIndicateur
-              }
-            />
-            <SectionDétailsMetadataParametreIndicateurNationale
-              estEnCoursDeModification={estEnCoursDeModification}
-              indicateur={indicateur}
-              mapInformationMetadataIndicateur={
-                mapInformationMetadataIndicateur
-              }
-            />
-            <SectionDétailsMetadataParametreCalculIndicateur
-              estEnCoursDeModification={estEnCoursDeModification}
-              indicateur={indicateur}
-              mapInformationMetadataIndicateur={
-                mapInformationMetadataIndicateur
-              }
-            />
-            <SectionDétailsMetadataParametrePonderationIndicateur
-              estEnCoursDeModification={estEnCoursDeModification}
-              indicateur={indicateur}
-              mapInformationMetadataIndicateur={
-                mapInformationMetadataIndicateur
-              }
-            />
-          </div>
-        </section>
-        <section className="fr-accordion">
-          <h2 className="fr-accordion__title">
-            <button
-              aria-controls="accordion-autres-informations"
-              aria-expanded="false"
-              className="fr-accordion__btn"
-              type="button"
-            >
-              Autres informations
-            </button>
-          </h2>
-          <div className="fr-collapse" id="accordion-autres-informations">
-            <SectionDétailsMetadataAutresIndicateur
-              estEnCoursDeModification={estEnCoursDeModification}
-              indicateur={indicateur}
-              mapInformationMetadataIndicateur={
-                mapInformationMetadataIndicateur
-              }
-            />
-          </div>
-        </section>
-      </div>
-    </FicheIndicateurStyled>
+    <div className="mb-4">
+      <SectionSelectionIndicateur
+        estEnCoursDeModification={estEnCoursDeModification}
+      />
+      <SectionTableauIndicateur
+        indicateur={indicateur}
+        informationHistorisationIndicateur={informationHistorisationIndicateur}
+      />
+      <section className="fr-accordion">
+        <h2 className="fr-accordion__title">
+          <button
+            aria-controls="accordion-identity"
+            aria-expanded="true"
+            className="fr-accordion__btn"
+            type="button"
+          >
+            Identité indicateur
+          </button>
+        </h2>
+        <div className="fr-collapse" id="accordion-identity">
+          <SectionDétailsMetadataIndicateur
+            chantiers={chantiers}
+            estEnCoursDeModification={estEnCoursDeModification}
+            indicateur={indicateur}
+            mapInformationMetadataIndicateur={mapInformationMetadataIndicateur}
+          />
+        </div>
+      </section>
+      <section className="fr-accordion">
+        <h2 className="fr-accordion__title">
+          <button
+            aria-controls="accordion-parametrage"
+            aria-expanded="false"
+            className="fr-accordion__btn"
+            type="button"
+          >
+            Paramétrages
+          </button>
+        </h2>
+        <div className="fr-collapse" id="accordion-parametrage">
+          <SectionDétailsMetadataParametreIndicateurDepartementale
+            estEnCoursDeModification={estEnCoursDeModification}
+            indicateur={indicateur}
+            mapInformationMetadataIndicateur={mapInformationMetadataIndicateur}
+          />
+          <SectionDétailsMetadataParametreIndicateurRegionale
+            estEnCoursDeModification={estEnCoursDeModification}
+            indicateur={indicateur}
+            mapInformationMetadataIndicateur={mapInformationMetadataIndicateur}
+          />
+          <SectionDétailsMetadataParametreIndicateurNationale
+            estEnCoursDeModification={estEnCoursDeModification}
+            indicateur={indicateur}
+            mapInformationMetadataIndicateur={mapInformationMetadataIndicateur}
+          />
+          <SectionDétailsMetadataParametreCalculIndicateur
+            estEnCoursDeModification={estEnCoursDeModification}
+            indicateur={indicateur}
+            mapInformationMetadataIndicateur={mapInformationMetadataIndicateur}
+          />
+          <SectionDétailsMetadataParametrePonderationIndicateur
+            estEnCoursDeModification={estEnCoursDeModification}
+            indicateur={indicateur}
+            mapInformationMetadataIndicateur={mapInformationMetadataIndicateur}
+          />
+        </div>
+      </section>
+      <section className="fr-accordion">
+        <h2 className="fr-accordion__title">
+          <button
+            aria-controls="accordion-autres-informations"
+            aria-expanded="false"
+            className="fr-accordion__btn"
+            type="button"
+          >
+            Autres informations
+          </button>
+        </h2>
+        <div className="fr-collapse" id="accordion-autres-informations">
+          <SectionDétailsMetadataAutresIndicateur
+            estEnCoursDeModification={estEnCoursDeModification}
+            indicateur={indicateur}
+            mapInformationMetadataIndicateur={mapInformationMetadataIndicateur}
+          />
+        </div>
+      </section>
+    </div>
   );
 };
 
