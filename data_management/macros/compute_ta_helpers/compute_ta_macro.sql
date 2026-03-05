d16d9a8c11fb2a2a7e60f67284dd3c24336cf41d
Romain Thoyer <51092742+rthoyer@users.noreply.github.com>
2026-03-05 23:49:53 +0100

    Fix macro

--- data_management/macros/compute_ta_helpers/compute_ta_macro.sql
+++ data_management/macros/compute_ta_helpers/compute_ta_macro.sql
@@ -35,11 +35,11 @@ CASE
         THEN 
             CASE
                 -- VI<=VC
-                WHEN {{ vi_col }} <= {{ vc_col }} AND {{ vac_col }} <= {{ vc_col }}
+                WHEN {{ vi_col }} >= {{ vc_col }} AND {{ vac_col }} >= {{ vc_col }}
                     THEN 100
-                WHEN {{ vi_col }} <= {{ vc_col }} AND {{ vac_col }} > {{ vc_col }}
+                WHEN {{ vi_col }} >= {{ vc_col }} AND {{ vac_col }} < {{ vc_col }}
                     THEN 0
-                ELSE ROUND((100*({{ vac_col }} - {{ vi_col }}) / ({{ vc_col }} - {{ vi_col }}))::NUMERIC, {{ var('ta_decimales') }}) 
+                ELSE ROUND((100 * ({{ vac_col }} - {{ vi_col }}) / ({{ vc_col }} - {{ vi_col }}))::NUMERIC, {{ var('ta_decimales') }}) 
             END
     WHEN {{ tendance_col }} IN ('BAISSE')
         THEN 
@@ -49,7 +49,7 @@ CASE
                     THEN 100
                 WHEN {{ vi_col }} <= {{ vc_col }} AND {{ vac_col }} > {{ vc_col }}
                     THEN 0
-                ELSE ROUND((100*({{ vac_col }} - {{ vi_col }}) / ({{ vc_col }} - {{ vi_col }}))::NUMERIC, {{ var('ta_decimales') }}) 
+                ELSE ROUND((100 * ({{ vac_col }} - {{ vi_col }}) / ({{ vc_col }} - {{ vi_col }}))::NUMERIC, {{ var('ta_decimales') }}) 
             END
     ELSE NULL
 END
