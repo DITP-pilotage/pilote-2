a0a5d528a3c60088179b7ad26ede9f31bdcfc50f
Tristan Conti <tristan.conti@seenovate.com>
2026-03-06 13:50:52 +0100

    usecases

---
+++ src/server/commentaires/usecases/ModifierBrouillonCommentaireUseCase.ts
@@ -0,0 +1,38 @@
+import Habilitation from "@/server/domain/utilisateur/habilitation/Habilitation";
+import { Habilitations } from "@/server/domain/utilisateur/habilitation/Habilitation.interface";
+import { CommentaireV2 } from "@/server/domain/chantier/commentaire/Commentaire.interface";
+import CommentaireRepository from "@/server/domain/chantier/commentaire/CommentaireRepository.interface";
+import { modifierCommentaireBrouillon } from "@/server/domain/chantier/commentaire/Commentaire";
+
+export class ModifierBrouillonCommentaireUseCase {
+  constructor(
+    private readonly dependencies: {
+      commentaireRepository: CommentaireRepository;
+    },
+  ) {}
+
+  async execute({
+    brouillon,
+    contenu,
+    auteurModificationId,
+    dateModification,
+    habilitations,
+  }: {
+    brouillon: CommentaireV2;
+    contenu: string;
+    auteurModificationId: string;
+    dateModification: string;
+    habilitations: Habilitations;
+  }): Promise<void> {
+    new Habilitation(
+      habilitations,
+    ).vérifierLesHabilitationsEnSaisieDesPublications(
+      brouillon.chantierId,
+      brouillon.territoireCode,
+    );
+
+    const commentaire = modifierCommentaireBrouillon(brouillon, { contenu, auteurModificationId, dateModification });
+
+    await this.dependencies.commentaireRepository.save(commentaire);
+  }
+}
