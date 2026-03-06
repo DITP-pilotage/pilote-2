ddf825d0056c5082b9c6a540125f59314c27b914
Tristan Conti <tristan.conti@seenovate.com>
2026-03-06 13:50:52 +0100

    usecases

---
+++ src/server/commentaires/usecases/EnregistrerBrouillonCommentaireUseCase.ts
@@ -0,0 +1,39 @@
+import Habilitation from "@/server/domain/utilisateur/habilitation/Habilitation";
+import { Habilitations } from "@/server/domain/utilisateur/habilitation/Habilitation.interface";
+import { TypeCommentaireChantier } from "@/server/domain/chantier/commentaire/Commentaire.interface";
+import CommentaireRepository from "@/server/domain/chantier/commentaire/CommentaireRepository.interface";
+import { creerCommentaireBrouillon } from "@/server/domain/chantier/commentaire/Commentaire";
+
+export class EnregistrerBrouillonCommentaireUseCase {
+  constructor(
+    private readonly dependencies: {
+      commentaireRepository: CommentaireRepository;
+    },
+  ) {}
+
+  async execute({
+    chantierId,
+    territoireCode,
+    type,
+    contenu,
+    auteurId,
+    date,
+    habilitations,
+  }: {
+    chantierId: string;
+    territoireCode: string;
+    type: TypeCommentaireChantier;
+    contenu: string;
+    auteurId: string;
+    date: string;
+    habilitations: Habilitations;
+  }): Promise<void> {
+    new Habilitation(
+      habilitations,
+    ).vérifierLesHabilitationsEnSaisieDesPublications(chantierId, territoireCode);
+
+    const commentaire = creerCommentaireBrouillon({ chantierId, territoireCode, type, contenu, auteurId, date });
+
+    await this.dependencies.commentaireRepository.save(commentaire);
+  }
+}
---
+++ src/server/commentaires/usecases/ModifierCommentairePublieUseCase.ts
@@ -0,0 +1,38 @@
+import Habilitation from "@/server/domain/utilisateur/habilitation/Habilitation";
+import { Habilitations } from "@/server/domain/utilisateur/habilitation/Habilitation.interface";
+import { CommentaireV2 } from "@/server/domain/chantier/commentaire/Commentaire.interface";
+import CommentaireRepository from "@/server/domain/chantier/commentaire/CommentaireRepository.interface";
+import { modifierCommentaire } from "@/server/domain/chantier/commentaire/Commentaire";
+
+export class ModifierCommentairePublieUseCase {
+  constructor(
+    private readonly dependencies: {
+      commentaireRepository: CommentaireRepository;
+    },
+  ) {}
+
+  async execute({
+    commentaireAModifier,
+    contenu,
+    auteurModificationId,
+    dateModification,
+    habilitations,
+  }: {
+    commentaireAModifier: CommentaireV2;
+    contenu: string;
+    auteurModificationId: string;
+    dateModification: string;
+    habilitations: Habilitations;
+  }): Promise<void> {
+    new Habilitation(
+      habilitations,
+    ).vérifierLesHabilitationsEnSaisieDesPublications(
+      commentaireAModifier.chantierId,
+      commentaireAModifier.territoireCode,
+    );
+
+    const commentaire = modifierCommentaire(commentaireAModifier, { contenu, auteurModificationId, dateModification });
+
+    await this.dependencies.commentaireRepository.save(commentaire);
+  }
+}
