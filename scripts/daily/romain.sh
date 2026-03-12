5e6866edbe9d4cfb594a1f65bd25319efcb775ce
Romain Thoyer <51092742+rthoyer@users.noreply.github.com>
2026-03-12 17:19:02 +0100

    [PIL-1267] Add historique ta indic (#1975)
    * add historique ta indic
    
    * Update romain.sh daily script
    
    * Fix macro
    
    * add from and to boundaries
    
    * finish
    
    * Fix and lint
    
    * Add historique TA chantier

--- scripts/daily/romain.sh
+++ scripts/daily/romain.sh
@@ -2,8 +2,7 @@
 #   - reset the db
 #   - exec descente de dev
 #   - run datajobs with FULL_DJ=false
-# 
-# Warn: only with docker commands
+
 
 
 echo ">> Reset db"
@@ -13,4 +12,4 @@ bash scripts/ddp_dump.sh
 bash scripts/ddp_restore.sh
 echo ">> Run dj prod"
 cd data_management
-FULL_DJ=false docker compose run --rm -e FULL_DJ pilote_datajobs
+FULL_DJ=false pipenv run python3 scripts/__main__.py
