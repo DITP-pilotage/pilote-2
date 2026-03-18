0336a6df75d55ac8c601a58bc4a653f083aa3268
Jordan Taillefer <jtaillefer02@gmail.com>
2026-03-18 11:37:14 +0100

    fix: tests

--- src/server/profil-utilisateur/__tests__/queries/GetProfilUtilisateurQuery.integration.test.ts
+++ src/server/profil-utilisateur/__tests__/queries/GetProfilUtilisateurQuery.integration.test.ts
@@ -39,6 +39,7 @@ describe("GetProfilUtilisateurQuery", () => {
           perimetreMinisteriel: null,
           service: "prefecture-de-region",
           serviceAutre: null,
+          dateModification: expect.any(String),
         });
       }),
     );
@@ -73,6 +74,7 @@ describe("GetProfilUtilisateurQuery", () => {
           perimetreMinisteriel: null,
           service: null,
           serviceAutre: null,
+          dateModification: expect.any(String),
         });
       }),
     );
