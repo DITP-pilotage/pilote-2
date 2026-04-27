73fae58e8d1cbc53ffcd10166d2d20436c18a6fe
Jordan Taillefer <jtaillefer02@gmail.com>
2026-04-27 14:47:19 +0200

    fix: simplifier scripts daily jordan et corriger chemin data-management

--- apps/pilote-ppg/scripts/daily/jordan.sh
+++ apps/pilote-ppg/scripts/daily/jordan.sh
@@ -4,20 +4,10 @@
 #   - run a dbt command (for example)
 #   - exec descente de prod
 #   - run PROD datajobs
-# 
-# Warn: only with docker commands
-# You can dupplicate this file and modify it to fit your local env (example: use docker only for data operations)
 
 
 echo ">> Reset db"
 pnpm database:init
 
 bash scripts/ddp_dump.sh
-bash scripts/ddp_restore.sh
-bash scripts/anonymisation_utilisateurs.sh
-# ou ddp via docker avec: "docker compose run --rm ddp bash docker/entrypoint.ddp.sh"
-#   pré-requis: mettre des clés ssh dans pilote-2/docker (voir pilote-2/docker/.gitignore) 
-#       pour que le tunnel vers la db soit fait dans le container (Colin)
-echo ">> Run dj prod"
-cd data_management
-FULL_DJ=false docker compose run --rm -e FULL_DJ pilote_datajobs
+bash scripts/daily/jordan_only_restore.sh
