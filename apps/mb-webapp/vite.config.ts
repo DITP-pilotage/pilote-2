c4ea8aae017e30cfb489de1606e939f608bb3b77
Jordan Taillefer <jtaillefer02@gmail.com>
2026-04-30 11:50:46 +0200

    chore(mb-webapp): install TanStack Router + configure Vite plugin

--- apps/mb-webapp/vite.config.ts
+++ apps/mb-webapp/vite.config.ts
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    // tanstackRouter DOIT être avant react()
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
