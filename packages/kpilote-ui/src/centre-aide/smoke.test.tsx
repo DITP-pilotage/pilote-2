3a83959f16cbe05524055d804183e2d9eb6aeb30
Jordan Taillefer <jtaillefer02@gmail.com>
2026-07-23 09:48:10 +0200

    feat(kpilote-ui): export public @pilote/kpilote-ui/centre-aide

--- packages/kpilote-ui/src/centre-aide/smoke.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-it('rend un noeud React sous jsdom', () => {
-  render(<p>centre daide</p>)
-  expect(screen.getByText('centre daide')).toBeInTheDocument()
-})
